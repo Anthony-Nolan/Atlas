b3dd6cb6bc9600910778b9280b89426befd3998f
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2018-01-23 15:37:12 +0000

    NOVA-748: Client class now based on Nova.Utils.WebApi.Client.ClientBase.

--- Nova.SearchAlgorithm.Client/SearchAlgorithmClient.cs
+++ Nova.SearchAlgorithm.Client/SearchAlgorithmClient.cs
@@ -1,71 +1,27 @@
-﻿using Nova.SearchAlgorithm.Client.Models;
-using Nova.Utils.Http.Exceptions;
-using Nova.Utils.WebApi.Filters;
-using RestSharp;
-using System.Configuration;
-using System.Net;
+﻿using Newtonsoft.Json;
+using Nova.SearchAlgorithm.Client.Models;
+using Nova.Utils.WebApi.Client;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace Nova.SearchAlgorithm.Client
 {
     public interface ISearchAlgorithmClient
     {
-        IRestResponse CreateSearchRequest(SearchRequest searchRequest);
+        Task<int> CreateSearchRequest(SearchRequest searchRequest);
     }
 
-    public class SearchAlgorithmClient: ISearchAlgorithmClient
+    public class SearchAlgorithmClient : ClientBase, ISearchAlgorithmClient
     {
-        private readonly string baseUrl;
-        private readonly string apiKey;
-
-        public SearchAlgorithmClient(string baseUrl, string apiKey = null)
+        public SearchAlgorithmClient(string baseUrl, string apiKey, JsonSerializerSettings settings)
+            : base(baseUrl, apiKey, "search_algorithm_client", settings)
         {
-            this.baseUrl = baseUrl;
-            this.apiKey = apiKey;
         }
 
-        public IRestResponse CreateSearchRequest(SearchRequest searchRequest)
+        public async Task<int> CreateSearchRequest(SearchRequest searchRequest)
         {
-            var client = GetSearchAlgorithmClient();
-            var request = new RestRequest("create-search-request", Method.POST) { RequestFormat = DataFormat.Json };
-            request.AddBody(searchRequest);
-            var response = client.Execute(request);
-
-            AssertResponseOk(response);
-            return response;
-        }
-
-        private RestClient GetSearchAlgorithmClient()
-        {
-            var client = new RestClient(baseUrl);
-            client.AddDefaultHeader(ApiKeyRequiredAttribute.HEADER_KEY, GetApiKey());
-            return client;
-        }
-
-        private string GetApiKey()
-        {
-            if (apiKey != null)
-                return apiKey;
-
-            // TODO NOVA-367 - This is really hacky.
-            var configApiKey = ConfigurationManager.AppSettings["SearchAlgorithmApiKey"];
-            if (configApiKey != null)
-                return configApiKey;
-
-            throw new ConfigurationErrorsException("No API key found.");
-        }
-
-        private static void AssertResponseOk(IRestResponse response)
-        {
-            // ReSharper disable once SwitchStatementMissingSomeCases
-            switch (response.StatusCode)
-            {
-                case HttpStatusCode.OK:
-                    return;
-                case HttpStatusCode.NotFound:
-                    throw new NovaNotFoundException(response.ErrorMessage);
-                default:
-                    throw new NovaHttpException(response.StatusCode, response.ErrorMessage, response.ErrorException);
-            }
+            var request = GetRequest(HttpMethod.Post, "search/create-search-request", body: searchRequest);
+            return await MakeRequestAsync<int>(request);
         }
     }
 }
