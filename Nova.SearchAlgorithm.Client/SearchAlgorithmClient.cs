e86176454d4f2b0b02e3b7d7a3330516d4bb979e
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2018-01-23 16:06:05 +0000

    NOVA-748: changed search request endpoint

--- Nova.SearchAlgorithm.Client/SearchAlgorithmClient.cs
+++ Nova.SearchAlgorithm.Client/SearchAlgorithmClient.cs
@@ -20,7 +20,7 @@ namespace Nova.SearchAlgorithm.Client
 
         public async Task<int> CreateSearchRequest(SearchRequestCreationModel searchRequestCreationModel)
         {
-            var request = GetRequest(HttpMethod.Post, "search/create-search-request", body: searchRequestCreationModel);
+            var request = GetRequest(HttpMethod.Post, "search-algorithm/create-search-request", body: searchRequestCreationModel);
             return await MakeRequestAsync<int>(request);
         }
     }
