caf4c0351d2808dbbed61995f4aee2dd0c001c7f
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2018-01-23 18:32:57 +0000

    NOVA-748: Added requirement validation to search request model

--- Nova.SearchAlgorithm.Client/Models/SearchRequestCreationModel.cs
+++ Nova.SearchAlgorithm.Client/Models/SearchRequestCreationModel.cs
@@ -1,18 +1,32 @@
-﻿using System.Collections.Generic;
+﻿using FluentValidation;
+using System.Collections.Generic;
 
 namespace Nova.SearchAlgorithm.Client.Models
 {
     public enum SearchType
     {
-        Adult,
-        Cord,
-        Nima
+        Adult = 1,
+        Cord = 2,
+        Nima = 3
     }
 
+    [FluentValidation.Attributes.Validator(typeof(SearchRequestCreationModelValidator))]
     public class SearchRequestCreationModel
     {
         public SearchType SearchType { get; set; }
         public MismatchCriteria MismatchCriteria { get; set; }
         public IEnumerable<string> RegistriesToSearch { get; set; }
     }
+
+    public class SearchRequestCreationModelValidator : AbstractValidator<SearchRequestCreationModel>
+    {
+        public SearchRequestCreationModelValidator()
+        {
+            RuleFor(x => x.SearchType).NotEmpty();
+            RuleFor(x => x.MismatchCriteria).NotEmpty();
+            RuleFor(x => x.RegistriesToSearch).NotEmpty();
+        }
+    }
+
+
 }
