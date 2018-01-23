4f35b071f3d18e7cc05378b01c8d9064a27ededa
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2018-01-23 13:25:40 +0000

    NOVA-748: Client model classes added for SearchRequest and MismatchCriteria types.

---
+++ Nova.SearchAlgorithm.Client/Models/MismatchCriteria.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nova.SearchAlgorithm.Client.Models
+{
+    public class MismatchCriteria
+    {
+    }
+}
---
+++ Nova.SearchAlgorithm.Client/Models/SearchRequest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Nova.SearchAlgorithm.Client.Models
+{
+    public enum SearchType
+    {
+        Adult,
+        Cord,
+        Nima
+    }
+
+    public class SearchRequest
+    {
+        public SearchType SearchType { get; set; }
+        public MismatchCriteria MismatchCriteria { get; set; }
+        public IEnumerable<string> Registries { get; set; }
+    }
+}
