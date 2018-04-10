9e121a913c261f3c0e401dd3946bc904a7d00ef9
Simon Wilson <simon.wilson@softwire.com>
2018-04-10 13:52:11 +0000

    NOVA-929 trigger endpoint for donor hla update process

---
+++ Nova.SearchAlgorithm/Controllers/HlaUpdateController.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Nova.SearchAlgorithm.Client.Models;
+using Nova.SearchAlgorithm.Services;
+
+namespace Nova.SearchAlgorithm.Controllers
+{
+    public class HlaUpdateController : ApiController
+    {
+        [HttpPost]
+        [Route("trigger-hla-update")]
+        public IHttpActionResult TriggerSingleImport()
+        {
+            // TODO: NOVA-929 implement a process to update the HLA of every donor.
+            return Ok();
+        }
+    }
+}
