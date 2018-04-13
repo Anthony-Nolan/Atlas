1269b348c890de63f373fc9301f07f6b6573e137
Simon Wilson <simon.wilson@softwire.com>
2018-04-13 10:09:45 +0000

    NOVA-931 proof of concept for integration test framework

---
+++ Nova.SearchAlgorithm.Test/Integration/SearchTests.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ApplicationInsights;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Nova.SearchAlgorithm.Config;
+using Nova.SearchAlgorithm.Services;
+using Nova.Utils.ApplicationInsights;
+using Nova.Utils.Solar;
+using Nova.Utils.TestUtils.Assertions;
+using NUnit.Framework;
+using Autofac;
+using Autofac.Builder;
+using FluentAssertions;
+using Nova.SearchAlgorithm.Client.Models;
+
+namespace Nova.SearchAlgorithm.Test.Integration
+{
+    [TestFixture]
+    public class SearchTests
+    {
+        // Destined for base class
+        private StorageEmulator emulator = new StorageEmulator();
+        protected IContainer container;
+
+        // Test only
+        private IDonorImportService donorImportService;
+        private ISearchService searchService;
+
+        private IContainer CreateContainer()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterInstance(AutomapperConfig.CreateMapper())
+                .SingleInstance()
+                .AsImplementedInterfaces();
+
+            builder.RegisterType<Repositories.SearchRequests.SearchRequestRepository>().AsImplementedInterfaces().InstancePerLifetimeScope();
+            builder.RegisterType<Repositories.Donors.DonorRepository>().AsImplementedInterfaces().InstancePerLifetimeScope();
+            builder.RegisterType<Repositories.Hla.HlaRepository>().AsImplementedInterfaces().InstancePerLifetimeScope();
+            builder.RegisterType<Repositories.SolarDonorRepository>().AsImplementedInterfaces().InstancePerLifetimeScope();
+
+            builder.RegisterType<Services.SearchRequestService>().AsImplementedInterfaces().InstancePerLifetimeScope();
+            builder.RegisterType<Services.SearchService>().AsImplementedInterfaces().InstancePerLifetimeScope();
+            builder.RegisterType<Services.DonorImportService>().AsImplementedInterfaces().InstancePerLifetimeScope();
+            
+            builder.RegisterType<Repositories.CloudTableFactory>().AsImplementedInterfaces().SingleInstance();
+            builder.RegisterType<SolarConnectionFactory>().AsImplementedInterfaces().SingleInstance();
+
+            var solarSettings = new SolarConnectionSettings
+            {
+                //ConnectionString = ConfigurationManager.ConnectionStrings["SolarConnectionString"].ConnectionString
+            };
+            builder.RegisterInstance(solarSettings).AsSelf().SingleInstance();
+
+            //var logger = new RequestAwareLogger(new TelemetryClient(),
+            //    ConfigurationManager.AppSettings["insights.logLevel"].ToLogLevel());
+            //builder.RegisterInstance(logger).AsImplementedInterfaces().SingleInstance();
+
+            return builder.Build();
+        }
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            container = CreateContainer();
+            emulator.Start();
+        }
+
+        [OneTimeTearDown]
+        public void ShutdownStorage()
+        {
+            emulator.Stop();
+        }
+
+        [SetUp]
+        public void CleanStorage()
+        {
+            emulator.ClearBlobItems();
+            emulator.ClearTableItems();
+        }
+
+        [OneTimeSetUp]
+        public void ImportTestDonors()
+        {
+            // TODO: realistically use the repo directly to import
+            // bespoke test data relevant to this classes tests
+            // Using ImportSingleTestDonor is a POC shortcut
+            donorImportService = container.Resolve<IDonorImportService>();
+            donorImportService.ImportSingleTestDonor();
+        }
+
+        [SetUp]
+        public void ResolveSearchService()
+        {
+            searchService = container.Resolve<ISearchService>();
+        }
+
+        [Test]
+        public void TestSomething()
+        {
+            IEnumerable<DonorMatch> results = searchService.Search(new SearchRequest
+            {
+            });
+
+            results.Should().Contain(d => d.DonorId == "1");
+        }
+    }
+}
---
+++ Nova.SearchAlgorithm.Test/Integration/StorageEmulator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nova.SearchAlgorithm.Test.Integration
+{
+    public class StorageEmulator
+    {
+        // TODO:configure this in case it changes from machine to machine
+        private const string StorageEmulatorLocation = @"C:\Program Files (x86)\Microsoft SDKs\Azure\Storage Emulator\AzureStorageEmulator.exe";
+
+        public void Start()
+        {
+            ExecuteCommandOnEmulator("start");
+        }
+
+        public void Stop()
+        {
+            ExecuteCommandOnEmulator("stop");
+        }
+
+        public void ClearBlobItems()
+        {
+            ExecuteCommandOnEmulator("clear blob");
+        }
+
+        public void ClearTableItems()
+        {
+            ExecuteCommandOnEmulator("clear table");
+        }
+
+        private void ExecuteCommandOnEmulator(string arguments)
+        {
+            ProcessStartInfo start = new ProcessStartInfo
+            {
+                Arguments = arguments,
+                FileName = StorageEmulatorLocation
+            };
+            Process proc = new Process
+            {
+                StartInfo = start
+            };
+
+            proc.Start();
+            proc.WaitForExit();
+        }
+    }
+}
