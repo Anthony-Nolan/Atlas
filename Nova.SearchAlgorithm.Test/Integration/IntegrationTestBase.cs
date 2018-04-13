265f5a1c139766e759850246e6a3a47fdd95e687
Simon Wilson <simon.wilson@softwire.com>
2018-04-13 14:07:01 +0000

    NOVA-931 an azure storage emulator integration test base

---
+++ Nova.SearchAlgorithm.Test/Integration/IntegrationTestBase.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using Microsoft.ApplicationInsights;
+using Nova.Utils.ApplicationInsights;
+using Nova.Utils.Auth;
+using Nova.Utils.Solar;
+using Nova.Utils.WebApi.ApplicationInsights;
+using Nova.SearchAlgorithm.Config;
+using NUnit.Framework;
+using Autofac;
+
+namespace Nova.SearchAlgorithm.Test.Integration
+{
+    public class IntegrationTestBase
+    {
+        private StorageEmulator emulator = new StorageEmulator();
+        protected IContainer container;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            container = CreateContainer();
+            emulator.Start();
+            CleanStorage();
+        }
+
+        public void CleanStorage()
+        {
+            emulator.ClearBlobItems();
+            emulator.ClearTableItems();
+        }
+
+        [OneTimeTearDown]
+        public void ShutdownStorage()
+        {
+            emulator.Stop();
+        }
+
+        private IContainer CreateContainer()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterInstance(AutomapperConfig.CreateMapper())
+                .SingleInstance()
+                .AsImplementedInterfaces();
+
+            builder.RegisterType<Repositories.SearchRequests.SearchRequestRepository>().AsImplementedInterfaces().InstancePerLifetimeScope();
+            builder.RegisterType<Repositories.Donors.DonorRepository>().AsImplementedInterfaces().InstancePerLifetimeScope();
+            builder.RegisterType<Repositories.Hla.HlaRepository>().AsImplementedInterfaces().InstancePerLifetimeScope();
+            builder.RegisterType<Repositories.SolarDonorRepository>().AsImplementedInterfaces().InstancePerLifetimeScope();
+
+            builder.RegisterType<Services.SearchRequestService>().AsImplementedInterfaces().InstancePerLifetimeScope();
+            builder.RegisterType<Services.SearchService>().AsImplementedInterfaces().InstancePerLifetimeScope();
+            builder.RegisterType<Services.DonorImportService>().AsImplementedInterfaces().InstancePerLifetimeScope();
+
+            builder.RegisterType<Repositories.CloudTableFactory>().AsImplementedInterfaces().SingleInstance();
+            builder.RegisterType<SolarConnectionFactory>().AsImplementedInterfaces().SingleInstance();
+
+            // Tests should not use Solar, so don't provide an actual connection string.
+            var solarSettings = new SolarConnectionSettings();
+            builder.RegisterInstance(solarSettings).AsSelf().SingleInstance();
+
+            var logger = new RequestAwareLogger(new TelemetryClient(),
+                ConfigurationManager.AppSettings["insights.logLevel"].ToLogLevel());
+            builder.RegisterInstance(logger).AsImplementedInterfaces().SingleInstance();
+
+            return builder.Build();
+        }
+    }
+}
--- Nova.SearchAlgorithm.Test/Integration/SearchTests.cs
+++ Nova.SearchAlgorithm.Test/Integration/SearchTests.cs
@@ -1,89 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Configuration;
-using System.Diagnostics;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using Microsoft.ApplicationInsights;
-using Microsoft.WindowsAzure.Storage;
-using Microsoft.WindowsAzure.Storage.Blob;
-using Nova.SearchAlgorithm.Config;
 using Nova.SearchAlgorithm.Services;
-using Nova.Utils.ApplicationInsights;
-using Nova.Utils.Solar;
 using Nova.Utils.TestUtils.Assertions;
 using NUnit.Framework;
 using Autofac;
-using Autofac.Builder;
 using FluentAssertions;
 using Nova.SearchAlgorithm.Client.Models;
 
 namespace Nova.SearchAlgorithm.Test.Integration
 {
     [TestFixture]
-    public class SearchTests
+    public class SearchTests : IntegrationTestBase
     {
-        // Destined for base class
-        private StorageEmulator emulator = new StorageEmulator();
-        protected IContainer container;
-
-        // Test only
         private IDonorImportService donorImportService;
         private ISearchService searchService;
-
-        private IContainer CreateContainer()
-        {
-            var builder = new ContainerBuilder();
-
-            builder.RegisterInstance(AutomapperConfig.CreateMapper())
-                .SingleInstance()
-                .AsImplementedInterfaces();
-
-            builder.RegisterType<Repositories.SearchRequests.SearchRequestRepository>().AsImplementedInterfaces().InstancePerLifetimeScope();
-            builder.RegisterType<Repositories.Donors.DonorRepository>().AsImplementedInterfaces().InstancePerLifetimeScope();
-            builder.RegisterType<Repositories.Hla.HlaRepository>().AsImplementedInterfaces().InstancePerLifetimeScope();
-            builder.RegisterType<Repositories.SolarDonorRepository>().AsImplementedInterfaces().InstancePerLifetimeScope();
-
-            builder.RegisterType<Services.SearchRequestService>().AsImplementedInterfaces().InstancePerLifetimeScope();
-            builder.RegisterType<Services.SearchService>().AsImplementedInterfaces().InstancePerLifetimeScope();
-            builder.RegisterType<Services.DonorImportService>().AsImplementedInterfaces().InstancePerLifetimeScope();
-            
-            builder.RegisterType<Repositories.CloudTableFactory>().AsImplementedInterfaces().SingleInstance();
-            builder.RegisterType<SolarConnectionFactory>().AsImplementedInterfaces().SingleInstance();
-
-            var solarSettings = new SolarConnectionSettings
-            {
-                //ConnectionString = ConfigurationManager.ConnectionStrings["SolarConnectionString"].ConnectionString
-            };
-            builder.RegisterInstance(solarSettings).AsSelf().SingleInstance();
-
-            //var logger = new RequestAwareLogger(new TelemetryClient(),
-            //    ConfigurationManager.AppSettings["insights.logLevel"].ToLogLevel());
-            //builder.RegisterInstance(logger).AsImplementedInterfaces().SingleInstance();
-
-            return builder.Build();
-        }
-
-        [OneTimeSetUp]
-        public void Setup()
-        {
-            container = CreateContainer();
-            emulator.Start();
-        }
-
-        [OneTimeTearDown]
-        public void ShutdownStorage()
-        {
-            emulator.Stop();
-        }
-
-        [SetUp]
-        public void CleanStorage()
-        {
-            emulator.ClearBlobItems();
-            emulator.ClearTableItems();
-        }
 
         [OneTimeSetUp]
         public void ImportTestDonors()
