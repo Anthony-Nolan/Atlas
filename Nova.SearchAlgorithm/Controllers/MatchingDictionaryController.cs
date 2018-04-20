11b593a0f82bdae2b1874526f44919f8035181c6
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2018-04-21 00:11:56 +0100

    NOVA-928: Added empty controller for matching dictionary.

---
+++ Nova.SearchAlgorithm/Controllers/MatchingDictionaryController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Http;
+
+namespace Nova.SearchAlgorithm.Controllers
+{
+    public class MatchingDictionaryController : ApiController
+    {
+        public MatchingDictionaryController()
+        {
+        }
+
+        [HttpPost]
+        [Route("manage")]
+        public IHttpActionResult RecreateMatchingDictionary()
+        {
+            return Ok();
+        }
+    }
+}
