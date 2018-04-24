a63bcb1252017dbcff80005ac768bacdb3cc82cd
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2018-04-24 16:17:24 +0100

    NOVA-1099: Dictionary now stores complete, truncated, & first field allele names using new models and table entity.

---
+++ Nova.SearchAlgorithm.MatchingDictionary/Models/Dictionary/MatchingDictionaryEntry.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Nova.SearchAlgorithm.MatchingDictionary.Models.HLATypes;
+using System.Collections.Generic;
+
+namespace Nova.SearchAlgorithm.MatchingDictionary.Models.Dictionary
+{
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum TypingMethod
+    {
+        Molecular,
+        Serology
+    }
+
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum MolecularSubtype
+    {
+        NotMolecularType = 0,
+        CompleteAllele = 1,
+        TruncatedAllele = 2,
+        FirstFieldAllele = 3
+    }
+
+    public class MatchingDictionaryEntry
+    {
+        public string MatchLocus { get; }
+        public string LookupName { get; }
+        public TypingMethod TypingMethod { get; }
+        public MolecularSubtype MolecularSubtype { get; }
+        public SerologySubtype SerologySubtype { get; }
+        public IEnumerable<string> MatchingPGroups { get; }
+        public IEnumerable<SerologyEntry> MatchingSerology { get; }
+
+        public MatchingDictionaryEntry(
+            string matchLocus,
+            string lookupName,
+            TypingMethod typingMethod,
+            MolecularSubtype molecularSubtype,
+            SerologySubtype serologySubtype,
+            IEnumerable<string> matchingPGroups,
+            IEnumerable<SerologyEntry> matchingSerology
+            )
+        {
+            MatchLocus = matchLocus;
+            LookupName = lookupName;
+            TypingMethod = typingMethod;
+            MolecularSubtype = molecularSubtype;
+            SerologySubtype = serologySubtype;
+            MatchingPGroups = matchingPGroups;
+            MatchingSerology = matchingSerology;
+        }
+    }
+}
