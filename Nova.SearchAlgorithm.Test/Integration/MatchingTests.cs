ee5420eb845a04f8becbf728b532d7dd350f0824
Simon Wilson <simon.wilson@softwire.com>
2018-04-26 09:16:43 +0000

    NOVA-931 adding test cases for sql integration matching test

--- Nova.SearchAlgorithm.Test/Integration/MatchingTests.cs
+++ Nova.SearchAlgorithm.Test/Integration/MatchingTests.cs
@@ -15,31 +15,7 @@ namespace Nova.SearchAlgorithm.Test.Integration
     [TestFixture]
     public class MatchingTests : IntegrationTestBase
     {
-        private readonly DonorMatchCriteria searchCriteria = new DonorMatchCriteria
-        {
-            SearchType = SearchType.Adult,
-            RegistriesToSearch = new List<RegistryCode> { RegistryCode.AN },
-            DonorMismatchCountTier1 = 0,
-            DonorMismatchCountTier2 = 0,
-            LocusMismatchA = new DonorLocusMatchCriteria
-            {
-                MismatchCount = 0,
-                HlaNamesToMatchInPositionOne = new List<string> { "01:01P", "01:02" },
-                HlaNamesToMatchInPositionTwo = new List<string> { "01:01P", "02:01" }
-            },
-            LocusMismatchB = new DonorLocusMatchCriteria
-            {
-                MismatchCount = 0,
-                HlaNamesToMatchInPositionOne = new List<string> { "07:02P" },
-                HlaNamesToMatchInPositionTwo = new List<string> { "08:01P" }
-            },
-            LocusMismatchDRB1 = new DonorLocusMatchCriteria
-            {
-                MismatchCount = 0,
-                HlaNamesToMatchInPositionOne = new List<string> { "01:11P" },
-                HlaNamesToMatchInPositionTwo = new List<string> { "03:41P" }
-            }
-        };
+        private DonorMatchCriteria searchCriteria;
 
         private IDonorMatchRepository searchRepo;
 
@@ -81,6 +57,104 @@ namespace Nova.SearchAlgorithm.Test.Integration
                     DRB1_2 = new ExpandedHla { PGroups = new List<string> { "03:41P" } }
                 }
             });
+
+            // potential 1/2 match at locus A - 1/2 in HvG direction, 2/2 in GvH direction
+            searchRepo.InsertDonor(new SearchableDonor
+            {
+                RegistryCode = RegistryCode.AN,
+                DonorType = "Adult",
+                DonorId = 3,
+                MatchingHla = new PhenotypeInfo<ExpandedHla>
+                {
+                    A_1 = new ExpandedHla { PGroups = new List<string> { "01:01P", "30:02P" } },
+                    A_2 = new ExpandedHla { PGroups = new List<string> { "26:04", "30:02P" } },
+                    B_1 = new ExpandedHla { PGroups = new List<string> { "07:02P" } },
+                    B_2 = new ExpandedHla { PGroups = new List<string> { "08:01P" } },
+                    DRB1_1 = new ExpandedHla { PGroups = new List<string> { "01:11P" } },
+                    DRB1_2 = new ExpandedHla { PGroups = new List<string> { "03:41P" } }
+                }
+            });
+
+            // potential 1/2 match at locus A - 1/2 in both directions
+            searchRepo.InsertDonor(new SearchableDonor
+            {
+                RegistryCode = RegistryCode.AN,
+                DonorType = "Adult",
+                DonorId = 4,
+                MatchingHla = new PhenotypeInfo<ExpandedHla>
+                {
+                    A_1 = new ExpandedHla { PGroups = new List<string> { "01:02", "02:01" } },
+                    A_2 = new ExpandedHla { PGroups = new List<string> { "26:04", "30:02P" } },
+                    B_1 = new ExpandedHla { PGroups = new List<string> { "07:02P" } },
+                    B_2 = new ExpandedHla { PGroups = new List<string> { "08:01P" } },
+                    DRB1_1 = new ExpandedHla { PGroups = new List<string> { "01:11P" } },
+                    DRB1_2 = new ExpandedHla { PGroups = new List<string> { "03:41P" } }
+                }
+            });
+
+            // 0/2 at locus A
+            searchRepo.InsertDonor(new SearchableDonor
+            {
+                RegistryCode = RegistryCode.AN,
+                DonorType = "Adult",
+                DonorId = 5,
+                MatchingHla = new PhenotypeInfo<ExpandedHla>
+                {
+                    A_1 = new ExpandedHla { PGroups = new List<string> { "11:59", "30:02P" } },
+                    A_2 = new ExpandedHla { PGroups = new List<string> { "26:04", "30:02P" } },
+                    B_1 = new ExpandedHla { PGroups = new List<string> { "07:02P" } },
+                    B_2 = new ExpandedHla { PGroups = new List<string> { "08:01P" } },
+                    DRB1_1 = new ExpandedHla { PGroups = new List<string> { "01:11P" } },
+                    DRB1_2 = new ExpandedHla { PGroups = new List<string> { "03:41P" } }
+                }
+            });
+
+            // 0/2 at locus A, 1/2 at locus B
+            searchRepo.InsertDonor(new SearchableDonor
+            {
+                RegistryCode = RegistryCode.AN,
+                DonorType = "Adult",
+                DonorId = 6,
+                MatchingHla = new PhenotypeInfo<ExpandedHla>
+                {
+                    A_1 = new ExpandedHla { PGroups = new List<string> { "11:59", "30:02P" } },
+                    A_2 = new ExpandedHla { PGroups = new List<string> { "11:59", "30:02P" } },
+                    B_1 = new ExpandedHla { PGroups = new List<string> { "07:02P" } },
+                    B_2 = new ExpandedHla { PGroups = new List<string> { "07:02P" } },
+                    DRB1_1 = new ExpandedHla { PGroups = new List<string> { "01:11P" } },
+                    DRB1_2 = new ExpandedHla { PGroups = new List<string> { "03:41P" } }
+                }
+            });
+        }
+
+        [SetUp]
+        public void ResetSearchCriteria()
+        {
+            searchCriteria = new DonorMatchCriteria
+            {
+                SearchType = SearchType.Adult,
+                RegistriesToSearch = new List<RegistryCode> { RegistryCode.AN },
+                DonorMismatchCountTier1 = 0,
+                DonorMismatchCountTier2 = 0,
+                LocusMismatchA = new DonorLocusMatchCriteria
+                {
+                    MismatchCount = 0,
+                    HlaNamesToMatchInPositionOne = new List<string> { "01:01P", "01:02" },
+                    HlaNamesToMatchInPositionTwo = new List<string> { "01:01P", "02:01" }
+                },
+                LocusMismatchB = new DonorLocusMatchCriteria
+                {
+                    MismatchCount = 0,
+                    HlaNamesToMatchInPositionOne = new List<string> { "07:02P" },
+                    HlaNamesToMatchInPositionTwo = new List<string> { "08:01P" }
+                },
+                LocusMismatchDRB1 = new DonorLocusMatchCriteria
+                {
+                    MismatchCount = 0,
+                    HlaNamesToMatchInPositionOne = new List<string> { "01:11P" },
+                    HlaNamesToMatchInPositionTwo = new List<string> { "03:41P" }
+                }
+            };
         }
 
         [Test]
@@ -95,6 +169,16 @@ namespace Nova.SearchAlgorithm.Test.Integration
         {
             IEnumerable<PotentialMatch> results = searchRepo.Search(searchCriteria);
             results.Should().Contain(d => d.DonorId == 2);
+        }
+
+        [Test]
+        public void OneHalfMatchesNotPresentInExactSearchResults()
+        {
+            IEnumerable<PotentialMatch> results = searchRepo.Search(searchCriteria);
+            results.Should().NotContain(d => d.DonorId == 3);
+            results.Should().NotContain(d => d.DonorId == 4);
+            results.Should().NotContain(d => d.DonorId == 5);
+            results.Should().NotContain(d => d.DonorId == 6);
         }
     }
 }
