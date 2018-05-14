a5f9c275e5211137df18eb83fe3468aa6f859a68
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2018-05-14 23:27:52 +0100

    NOVA-1196: Store/export the MatchLocus string value.

--- Nova.SearchAlgorithm.MatchingDictionary/Models/HLATypes/MatchLocus.cs
+++ Nova.SearchAlgorithm.MatchingDictionary/Models/HLATypes/MatchLocus.cs
@@ -1,5 +1,9 @@
-﻿namespace Nova.SearchAlgorithm.MatchingDictionary.Models.HLATypes
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Nova.SearchAlgorithm.MatchingDictionary.Models.HLATypes
 {
+    [JsonConverter(typeof(StringEnumConverter))]
     public enum MatchLocus
     {
         A = 1,
