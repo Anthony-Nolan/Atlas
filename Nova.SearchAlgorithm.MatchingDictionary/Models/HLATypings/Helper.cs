bf19602216bd69c2e14fc24ff32fc0410a9a7ab9
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2018-05-31 14:24:43 +0100

    NOVA-1035: Moved DRB345 serology class to extensions class where it is actually used.

--- Nova.SearchAlgorithm.MatchingDictionary/Models/HLATypings/Helper.cs
+++ Nova.SearchAlgorithm.MatchingDictionary/Models/HLATypings/Helper.cs
@@ -1,11 +1,5 @@
 ﻿namespace Nova.SearchAlgorithm.MatchingDictionary.Models.HLATypings
 {
-    internal static class Drb345Typings
-    {
-        public const string SerologyLocus = "DR";
-        public static readonly string[] SerologyTypings = { "51", "52", "53" };
-    }
-
     internal static class AlleleExpression
     {
         public static string[] NullExpressionSuffixes = { "N" };
--- Nova.SearchAlgorithm.MatchingDictionary/Models/Wmda/WmdaHlaTypingExtensions.cs
+++ Nova.SearchAlgorithm.MatchingDictionary/Models/Wmda/WmdaHlaTypingExtensions.cs
@@ -5,9 +5,15 @@ namespace Nova.SearchAlgorithm.MatchingDictionary.Models.Wmda
 {
     internal static class WmdaHlaTypingExtensions
     {
+        private static class Drb345Serologies
+        {
+            public const string Locus = "DR";
+            public static readonly string[] Typings = { "51", "52", "53" };
+        }
+
         public static bool IsDrb345SerologyTyping(this IWmdaHlaTyping typing)
         {
-            return typing.WmdaLocus.Equals(Drb345Typings.SerologyLocus) && Drb345Typings.SerologyTypings.Contains(typing.Name);
+            return typing.WmdaLocus.Equals(Drb345Serologies.Locus) && Drb345Serologies.Typings.Contains(typing.Name);
         }
     }
 }
