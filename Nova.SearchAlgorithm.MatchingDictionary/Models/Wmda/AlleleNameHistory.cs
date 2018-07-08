9d14da9897f7334df83f22311430ba42ae44121e
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2018-07-08 16:36:59 +0100

    NOVA-1385: Moved logic to property getters.

--- Nova.SearchAlgorithm.MatchingDictionary/Models/Wmda/AlleleNameHistory.cs
+++ Nova.SearchAlgorithm.MatchingDictionary/Models/Wmda/AlleleNameHistory.cs
@@ -13,43 +13,30 @@ namespace Nova.SearchAlgorithm.MatchingDictionary.Models.Wmda
         /// HLA Accession ID
         /// </summary>
         public string Name { get; set; }
-        public IEnumerable<VersionedAlleleName> VersionedAlleleNames { get; private set; }
-        public string CurrentAlleleName { get; private set; }
-        public IEnumerable<string> DistinctAlleleNames { get; private set; }
+        public IEnumerable<VersionedAlleleName> VersionedAlleleNames { get; }
+
+        public string CurrentAlleleName => VersionedAlleleNames
+            .FirstOrDefault()
+            ?.AlleleName;
+
+        public IEnumerable<string> DistinctAlleleNames => VersionedAlleleNames
+            .Select(allele => allele.AlleleName)
+            .Where(name => !string.IsNullOrEmpty(name))
+            .Distinct();
 
         /// <summary>
         /// This property is useful for deleted alleles that have been shown to be identical to another allele.
         /// They will have no current name, but will have had a name assigned to them in the past.
         /// For valid or renamed alleles, values for "current" and "most recent" names will be identical.
         /// </summary>
-        public string MostRecentAlleleName { get; private set; }
+        public string MostRecentAlleleName => DistinctAlleleNames
+            .FirstOrDefault();
 
         public AlleleNameHistory(string locus, string hlaId, IEnumerable<VersionedAlleleName> versionedAlleleNames)
         {
             Locus = locus;
             Name = hlaId;
-            SetAlleleNameProperties(versionedAlleleNames);
-        }
-
-        private void SetAlleleNameProperties(IEnumerable<VersionedAlleleName> versionedAlleleNames)
-        {
-            var alleleNamesQuery = versionedAlleleNames
-                .OrderByDescending(x => x.HlaDatabaseVersion)
-                .ToList();
-
-            VersionedAlleleNames = alleleNamesQuery;
-
-            CurrentAlleleName = alleleNamesQuery
-                .FirstOrDefault()
-                ?.AlleleName;
-
-            DistinctAlleleNames = alleleNamesQuery
-                .Select(allele => allele.AlleleName)
-                .Where(name => !string.IsNullOrEmpty(name))
-                .Distinct();
-
-            MostRecentAlleleName = DistinctAlleleNames
-                .FirstOrDefault();
+            VersionedAlleleNames = versionedAlleleNames;
         }
 
         public bool DistinctAlleleNamesContain(IWmdaHlaTyping allele)
