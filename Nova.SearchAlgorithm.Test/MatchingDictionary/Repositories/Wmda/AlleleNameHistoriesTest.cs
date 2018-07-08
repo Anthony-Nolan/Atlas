56fcf073b06f2b74fa9eeecbd3915e28d6ae04d6
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2018-07-08 16:08:12 +0100

    NOVA-1385: Explicitly declared expected values for AlleleNameHistories tests.

--- Nova.SearchAlgorithm.Test/MatchingDictionary/Repositories/Wmda/AlleleNameHistoriesTest.cs
+++ Nova.SearchAlgorithm.Test/MatchingDictionary/Repositories/Wmda/AlleleNameHistoriesTest.cs
@@ -1,23 +1,15 @@
-﻿using Nova.SearchAlgorithm.MatchingDictionary.Models.Wmda;
+﻿using FluentAssertions;
+using Nova.SearchAlgorithm.MatchingDictionary.Models.HLATypings;
+using Nova.SearchAlgorithm.MatchingDictionary.Models.Wmda;
 using NUnit.Framework;
 using System.Collections.Generic;
 using System.Linq;
-using FluentAssertions;
-using Nova.SearchAlgorithm.MatchingDictionary.Models.HLATypings;
 
 namespace Nova.SearchAlgorithm.Test.MatchingDictionary.Repositories.Wmda
 {
     [TestFixtureSource(typeof(WmdaRepositoryTestFixtureArgs), nameof(WmdaRepositoryTestFixtureArgs.AllelesNameHistoriesTestArgs))]
     public class AlleleNameHistoriesTest : WmdaRepositoryTestBase<AlleleNameHistory>
     {
-        private static readonly string[] AllExpectedHlaDatabaseVersions =
-        {
-            "3310","3300","3290","3280","3270","3260","3250","3240","3230","3220",
-            "3210","3200","3190","3180","3170","3160","3150","3140","3131","3120",
-            "3110","3100","3090","3080","3070","3060","3050","3040","3030","3020",
-            "3010","3000"
-        };
-
         public AlleleNameHistoriesTest(IEnumerable<AlleleNameHistory> alleleNameHistories, IEnumerable<string> matchLoci)
             : base(alleleNameHistories, matchLoci)
         {
@@ -35,48 +27,64 @@ namespace Nova.SearchAlgorithm.Test.MatchingDictionary.Repositories.Wmda
                 ));
         }
 
-        [TestCase("A*", "HLA00001", new[] { "3310" }, new[] { "01:01:01:01" }, Description = "Allele with same name from latest version to v3.0.0")]
-        [TestCase("B*", "HLA13015", new[] { "3310", "3190" }, new[] { "07:242", null }, Description = "Allele discovered after v3.0.0")]
-        [TestCase("A*", "HLA00003", new[] { "3310", "3270" }, new[] { "01:03:01:01", "01:03" }, Description = "Allele that has been renamed")]
-        [TestCase("DQB1*", "HLA16167", new[] { "3310", "3290", "3260" }, new[] { null, "04:02:01:02", null }, Description = "Allele that was shown to be identical to another allele")]
-        public void WmdaDataRepository_WhenAssignedAlleleName_AlleleNameHistoriesSuccessfullyCaptured(
-            string locus, string hlaId, string[] versionsWhenAlleleNameChanged, string[] alleleNames)
+        [TestCaseSource(typeof(AlleleNameHistoriesTestCaseSources), nameof(AlleleNameHistoriesTestCaseSources.SequencesToTest))]
+        public void WmdaDataRepository_WhenAssignedAlleleName_TypingMethodIsMolecular(
+            string[] sequenceToTest
+        )
         {
-            var actualAlleleNameHistory = GetSingleWmdaHlaTyping(locus, hlaId);
-            var expectedVersionedAlleleNames = GetExpectedVersionedAlleleNames(versionsWhenAlleleNameChanged, alleleNames);
-            var expectedCurrentAlleleName = alleleNames[0];
-            var expectedDistinctAlleleNames = alleleNames.Where(name => !string.IsNullOrEmpty(name));
-            var expectedMostRecentAlleleName = expectedDistinctAlleleNames.First();
+            var actualAlleleNameHistory = GetSingleWmdaHlaTyping(sequenceToTest[0], sequenceToTest[1]);
 
             actualAlleleNameHistory.TypingMethod.Should().Be(TypingMethod.Molecular);
-            actualAlleleNameHistory.Locus.Should().Be(locus);
-            actualAlleleNameHistory.Name.Should().Be(hlaId);
-            actualAlleleNameHistory.VersionedAlleleNames.ShouldBeEquivalentTo(expectedVersionedAlleleNames);
-            actualAlleleNameHistory.CurrentAlleleName.Should().Be(expectedCurrentAlleleName);
-            actualAlleleNameHistory.DistinctAlleleNames.ShouldBeEquivalentTo(expectedDistinctAlleleNames);
-            actualAlleleNameHistory.MostRecentAlleleName.Should().Be(expectedMostRecentAlleleName);
         }
 
-        private static IEnumerable<VersionedAlleleName> GetExpectedVersionedAlleleNames(
-            IReadOnlyList<string> versionsWhenAlleleNameChanged, IReadOnlyList<string> alleleNames)
+        [TestCaseSource(typeof(AlleleNameHistoriesTestCaseSources), nameof(AlleleNameHistoriesTestCaseSources.ExpectedVersionedAlleleNames))]
+        public void WmdaDataRepository_WhenAssignedAlleleName_VersionedAllelesNamesSuccessfullyCaptured(
+            string[] sequenceToTest,
+            string[][] expectedVersionedAlleleNames
+        )
         {
-            var currentIndex = 0;
-            var alleleName = "";
-            var expectedVersionedAlleleNames = new List<VersionedAlleleName>();
+            var actualAlleleNameHistory = GetSingleWmdaHlaTyping(sequenceToTest[0], sequenceToTest[1]);
 
-            foreach (var version in AllExpectedHlaDatabaseVersions)
-            {
-                if (versionsWhenAlleleNameChanged.Count > currentIndex &&
-                    version.Equals(versionsWhenAlleleNameChanged[currentIndex]))
-                {
-                    alleleName = alleleNames[currentIndex];
-                    currentIndex++;
-                }
+            var actualVersionedAlleleNames = actualAlleleNameHistory
+                .VersionedAlleleNames
+                .Select(versionedAlleleName =>
+                    new[] { versionedAlleleName.HlaDatabaseVersion, versionedAlleleName.AlleleName })
+                .ToArray();
 
-                expectedVersionedAlleleNames.Add(new VersionedAlleleName(version, alleleName));
-            }
+            actualVersionedAlleleNames.ShouldBeEquivalentTo(expectedVersionedAlleleNames);
+        }
 
-            return expectedVersionedAlleleNames;
+        [TestCaseSource(typeof(AlleleNameHistoriesTestCaseSources), nameof(AlleleNameHistoriesTestCaseSources.ExpectedCurrentAlleleNames))]
+        public void WmdaDataRepository_WhenAssignedAlleleName_CurrentAlleleNameIsCorrect(
+            string[] sequenceToTest,
+            string expectedCurrentAlleleName
+        )
+        {
+            var actualAlleleNameHistory = GetSingleWmdaHlaTyping(sequenceToTest[0], sequenceToTest[1]);
+
+            actualAlleleNameHistory.CurrentAlleleName.Should().Be(expectedCurrentAlleleName);
+        }
+
+        [TestCaseSource(typeof(AlleleNameHistoriesTestCaseSources), nameof(AlleleNameHistoriesTestCaseSources.ExpectedDistinctAlleleNames))]
+        public void WmdaDataRepository_WhenAssignedAlleleName_DistinctAlleleNamesIsCorrect(
+            string[] sequenceToTest,
+            string[] expectedDistinctAlleleNames
+        )
+        {
+            var actualAlleleNameHistory = GetSingleWmdaHlaTyping(sequenceToTest[0], sequenceToTest[1]);
+
+            actualAlleleNameHistory.DistinctAlleleNames.ShouldBeEquivalentTo(expectedDistinctAlleleNames);
+        }
+
+        [TestCaseSource(typeof(AlleleNameHistoriesTestCaseSources), nameof(AlleleNameHistoriesTestCaseSources.ExpectedMostRecentAlleleNames))]
+        public void WmdaDataRepository_WhenAssignedAlleleName_MostRecentAlleleNameIsCorrect(
+            string[] sequenceToTest,
+            string expectedMostRecentAlleleName
+        )
+        {
+            var actualAlleleNameHistory = GetSingleWmdaHlaTyping(sequenceToTest[0], sequenceToTest[1]);
+
+            actualAlleleNameHistory.MostRecentAlleleName.Should().Be(expectedMostRecentAlleleName);
         }
     }
 }
---
+++ Nova.SearchAlgorithm.Test/MatchingDictionary/Repositories/Wmda/AlleleNameHistoriesTestCaseSources.cs
@@ -0,0 +1,154 @@
+﻿namespace Nova.SearchAlgorithm.Test.MatchingDictionary.Repositories.Wmda
+{
+    public class AlleleNameHistoriesTestCaseSources
+    {
+        private static readonly string[] AlleleWithSameNameFromCurrentVersionToVersion3000 = { "A*", "HLA00001" };
+        private static readonly string[] AlleleDiscoveredAfterVersion3000 = { "B*", "HLA13015" };
+        private static readonly string[] AlleleRenamed = { "A*", "HLA00003" };
+        private static readonly string[] AlleleShownToBeIdenticalToAnotherAllele = { "DQB1*", "HLA16167" };
+
+        public static readonly object[] SequencesToTest =
+        {
+            AlleleWithSameNameFromCurrentVersionToVersion3000,
+            AlleleDiscoveredAfterVersion3000,
+            AlleleRenamed,
+            AlleleShownToBeIdenticalToAnotherAllele
+        };
+
+        public static readonly object[] ExpectedVersionedAlleleNames =
+        {
+            new object[]
+            {
+                AlleleWithSameNameFromCurrentVersionToVersion3000,
+                new[]
+                {
+                    new[] {"3310", "01:01:01:01"}, new[] {"3300", "01:01:01:01"}, new[] {"3290", "01:01:01:01"}, new[] {"3280", "01:01:01:01"},
+                    new[] {"3270", "01:01:01:01"}, new[] {"3260", "01:01:01:01"}, new[] {"3250", "01:01:01:01"}, new[] {"3240", "01:01:01:01"},
+                    new[] {"3230", "01:01:01:01"}, new[] {"3220", "01:01:01:01"}, new[] {"3210", "01:01:01:01"}, new[] {"3200", "01:01:01:01"},
+                    new[] {"3190", "01:01:01:01"}, new[] {"3180", "01:01:01:01"}, new[] {"3170", "01:01:01:01"}, new[] {"3160", "01:01:01:01"},
+                    new[] {"3150", "01:01:01:01"}, new[] {"3140", "01:01:01:01"}, new[] {"3131", "01:01:01:01"}, new[] {"3120", "01:01:01:01"},
+                    new[] {"3110", "01:01:01:01"}, new[] {"3100", "01:01:01:01"}, new[] {"3090", "01:01:01:01"}, new[] {"3080", "01:01:01:01"},
+                    new[] {"3070", "01:01:01:01"}, new[] {"3060", "01:01:01:01"}, new[] {"3050", "01:01:01:01"}, new[] {"3040", "01:01:01:01"},
+                    new[] {"3030", "01:01:01:01"}, new[] {"3020", "01:01:01:01"}, new[] {"3010", "01:01:01:01"}, new[] {"3000", "01:01:01:01"}
+                }
+            },
+            new object[]
+            {
+                AlleleDiscoveredAfterVersion3000,
+                new[]
+                {
+                    new[] {"3310", "07:242"}, new[] {"3300", "07:242"}, new[] {"3290", "07:242"}, new[] {"3280", "07:242"},
+                    new[] {"3270", "07:242"}, new[] {"3260", "07:242"}, new[] {"3250", "07:242"}, new[] {"3240", "07:242"},
+                    new[] {"3230", "07:242"}, new[] {"3220", "07:242"}, new[] {"3210", "07:242"}, new[] {"3200", "07:242"},
+                    new[] {"3190", null}, new[] {"3180", null}, new[] {"3170", null}, new[] {"3160", null},
+                    new[] {"3150", null}, new[] {"3140", null}, new[] {"3131", null}, new[] {"3120", null},
+                    new[] {"3110", null}, new[] {"3100", null}, new[] {"3090", null}, new[] {"3080", null},
+                    new[] {"3070", null}, new[] {"3060", null}, new[] {"3050", null}, new[] {"3040", null},
+                    new[] {"3030", null}, new[] {"3020", null}, new[] {"3010", null}, new[] {"3000", null}
+                }
+            },
+            new object[]
+            {
+                AlleleRenamed,
+                new[]
+                {
+                    new[] {"3310", "01:03:01:01"}, new[] {"3300", "01:03:01:01"}, new[] {"3290", "01:03:01:01"}, new[] {"3280", "01:03:01:01"},
+                    new[] {"3270", "01:03"}, new[] {"3260", "01:03"}, new[] {"3250", "01:03"}, new[] {"3240", "01:03"},
+                    new[] {"3230", "01:03"}, new[] {"3220", "01:03"}, new[] {"3210", "01:03"}, new[] {"3200", "01:03"},
+                    new[] {"3190", "01:03"}, new[] {"3180", "01:03"}, new[] {"3170", "01:03"}, new[] {"3160", "01:03"},
+                    new[] {"3150", "01:03"}, new[] {"3140", "01:03"}, new[] {"3131", "01:03"}, new[] {"3120", "01:03"},
+                    new[] {"3110", "01:03"}, new[] {"3100", "01:03"}, new[] {"3090", "01:03"}, new[] {"3080", "01:03"},
+                    new[] {"3070", "01:03"}, new[] {"3060", "01:03"}, new[] {"3050", "01:03"}, new[] {"3040", "01:03"},
+                    new[] {"3030", "01:03"}, new[] {"3020", "01:03"}, new[] {"3010", "01:03"}, new[] {"3000", "01:03"}
+                }
+            },
+            new object[]
+            {
+                AlleleShownToBeIdenticalToAnotherAllele,
+                new[]
+                {
+                    new[] {"3310", null}, new[] {"3300", null}, new[] {"3290", "04:02:01:02"}, new[] {"3280", "04:02:01:02"},
+                    new[] {"3270", "04:02:01:02"}, new[] {"3260", null}, new[] {"3250", null}, new[] {"3240", null},
+                    new[] {"3230", null}, new[] {"3220", null}, new[] {"3210", null}, new[] {"3200", null},
+                    new[] {"3190", null}, new[] {"3180", null}, new[] {"3170", null}, new[] {"3160", null},
+                    new[] {"3150", null}, new[] {"3140", null}, new[] {"3131", null}, new[] {"3120", null},
+                    new[] {"3110", null}, new[] {"3100", null}, new[] {"3090", null}, new[] {"3080", null},
+                    new[] {"3070", null}, new[] {"3060", null}, new[] {"3050", null}, new[] {"3040", null},
+                    new[] {"3030", null}, new[] {"3020", null}, new[] {"3010", null}, new[] {"3000", null}
+                }
+            }
+        };
+
+        public static readonly object[] ExpectedCurrentAlleleNames =
+        {
+            new object[]
+            {
+                AlleleWithSameNameFromCurrentVersionToVersion3000,
+                "01:01:01:01"
+            },
+            new object[]
+            {
+                AlleleDiscoveredAfterVersion3000,
+                "07:242"
+            },
+            new object[]
+            {
+                AlleleRenamed,
+                "01:03:01:01"
+            },
+            new object[]
+            {
+                AlleleShownToBeIdenticalToAnotherAllele,
+                null
+            }
+        };
+
+        public static readonly object[] ExpectedDistinctAlleleNames =
+        {
+            new object[]
+            {
+                AlleleWithSameNameFromCurrentVersionToVersion3000,
+                new[] { "01:01:01:01"}
+            },
+            new object[]
+            {
+                AlleleDiscoveredAfterVersion3000,
+                new[] { "07:242" }
+            },
+            new object[]
+            {
+                AlleleRenamed,
+                new[] { "01:03:01:01", "01:03" }
+            },
+            new object[]
+            {
+                AlleleShownToBeIdenticalToAnotherAllele,
+                new[] { "04:02:01:02" }
+            }
+        };
+
+        public static readonly object[] ExpectedMostRecentAlleleNames =
+        {
+            new object[]
+            {
+                AlleleWithSameNameFromCurrentVersionToVersion3000,
+                "01:01:01:01"
+            },
+            new object[]
+            {
+                AlleleDiscoveredAfterVersion3000,
+                "07:242"
+            },
+            new object[]
+            {
+                AlleleRenamed,
+                "01:03:01:01"
+            },
+            new object[]
+            {
+                AlleleShownToBeIdenticalToAnotherAllele,
+                "04:02:01:02"
+            }
+        };
+    }
+}
