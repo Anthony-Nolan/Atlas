438241eaff9dfcf34f67c24a2fbe181a136c428f
Ben Below <benjamin.below@softwire.com>
2018-07-12 10:38:58 +0100

    NOVA-NONE: Move matching dictionary extensions to extensions folder

---
+++ Nova.SearchAlgorithm/Extensions/MatchingDictionaryConversionExtensions/LocusExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Nova.SearchAlgorithm.Common.Models;
+using Nova.SearchAlgorithm.Exceptions;
+using Nova.SearchAlgorithm.MatchingDictionary.Models.HLATypings;
+
+namespace Nova.SearchAlgorithm.MatchingDictionaryConversions
+{
+    public static class LocusExtensions
+    {
+        public static MatchLocus ToMatchLocus(this Locus locus)
+        {
+            switch (locus)
+            {
+                case Locus.A:
+                    return MatchLocus.A;
+                case Locus.B:
+                    return MatchLocus.B;
+                case Locus.C:
+                    return MatchLocus.C;
+                case Locus.Dqb1:
+                    return MatchLocus.Dqb1;
+                case Locus.Drb1:
+                    return MatchLocus.Drb1;
+                default:
+                    throw new SearchHttpException($"Unable to convert unknown loci {locus} for matching dictionary lookup");
+            }
+        }
+    }
+}
