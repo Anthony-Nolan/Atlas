54f0656dbba9ad0c782f998cca093a17b60a6ca2
Ben Below <benjamin.below@softwire.com>
2018-07-12 10:16:36 +0100

    NOVA-1442: Remove repositories folder
    No longer any repositories in the Search Algorithm project - all storage integration is now in either the Data and MatchingDictionary projects

---
+++ Nova.SearchAlgorithm/Extensions/ExpandedHlaExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Nova.SearchAlgorithm.Common.Models;
+
+namespace Nova.SearchAlgorithm.Extensions
+{
+    static class ExpandedHlaExtensions
+    {
+        public static IEnumerable<string> AllMatchingHlaNames(this ExpandedHla hla)
+        {
+            return hla.PGroups ?? Enumerable.Empty<string>();
+        }
+    }
+}
