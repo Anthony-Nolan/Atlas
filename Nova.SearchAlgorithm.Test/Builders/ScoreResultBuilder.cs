ece3bd1ab0f5ca9b8d58f199d08165a972bd4d14
Ben Below <benjamin.below@softwire.com>
2018-07-18 10:20:06 +0100

    NOVA-1448: Rank results by total match count

---
+++ Nova.SearchAlgorithm.Test/Builders/ScoreResultBuilder.cs
@@ -0,0 +1,19 @@
+﻿using Nova.SearchAlgorithm.Common.Models.SearchResults;
+
+namespace Nova.SearchAlgorithm.Test.Builders
+{
+    public class ScoreResultBuilder
+    {
+        private readonly ScoreResult scoreResult;
+
+        public ScoreResultBuilder()
+        {
+            scoreResult = new ScoreResult();
+        }
+
+        public ScoreResult Build()
+        {
+            return scoreResult;
+        }
+    }
+}
---
+++ Nova.SearchAlgorithm.Test/Services/Scoring/RankingServiceTests.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Nova.SearchAlgorithm.Common.Models;
+using Nova.SearchAlgorithm.Common.Models.SearchResults;
+using Nova.SearchAlgorithm.Services.Scoring;
+using Nova.SearchAlgorithm.Test.Builders;
+using NUnit.Framework;
+
+namespace Nova.SearchAlgorithm.Test.Services.Scoring
+{
+    [TestFixture]
+    public class RankingServiceTests
+    {
+        private IRankingService rankingService;
+        
+        [SetUp]
+        public void SetUp()
+        {
+            rankingService = new RankingService();
+        }
+
+        [Test]
+        public void RankSearchResults_OrdersResultsByMismatchCount()
+        {
+            var resultWithMoreMismatches = new MatchAndScoreResult
+            {
+                MatchResult = new MatchResultBuilder()
+                    .WithMatchCountAtLocus(Locus.A, 2)
+                    .WithMatchCountAtLocus(Locus.B, 2)
+                    .WithMatchCountAtLocus(Locus.Drb1, 1)
+                    .Build(),
+            };
+            var resultWithFewerMismatches = new MatchAndScoreResult
+            {
+                MatchResult = new MatchResultBuilder()
+                    .WithMatchCountAtLocus(Locus.A, 2)
+                    .WithMatchCountAtLocus(Locus.B, 2)
+                    .WithMatchCountAtLocus(Locus.Drb1, 2)
+                    .Build(),
+            };
+
+            var unorderedSearchResults = new List<MatchAndScoreResult>
+            {
+                resultWithMoreMismatches,
+                resultWithFewerMismatches
+            };
+
+            var orderedSearchResults = rankingService.RankSearchResults(unorderedSearchResults);
+
+            orderedSearchResults.Should().ContainInOrder(new List<MatchAndScoreResult>
+            {
+                resultWithFewerMismatches,
+                resultWithMoreMismatches
+            });
+        }
+    }
+}
