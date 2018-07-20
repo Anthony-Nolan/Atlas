76a1f2fedf710e1bc9d03450877609c7a5906c2f
Ben Below <benjamin.below@softwire.com>
2018-07-20 15:17:28 +0100

    NOVA-NONE: Update to version 5.0.0

--- Nova.SearchAlgorithm/Properties/AssemblyInfo.cs
+++ Nova.SearchAlgorithm/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using Nova.Utils.WebApi.Attributes;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("3.1.0.0")]
-[assembly: AssemblyFileVersion("3.1.0.0")]
+[assembly: AssemblyVersion("5.0.0.0")]
+[assembly: AssemblyFileVersion("5.0.0.0")]
