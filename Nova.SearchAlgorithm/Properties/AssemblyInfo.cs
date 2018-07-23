4d5020a171709d8f9a8daddcb426f1ab5f692c7f
Ben Below <benjamin.below@softwire.com>
2018-07-23 16:59:43 +0100

    NOVA-NONE: Update client version to 6.0.0

--- Nova.SearchAlgorithm/Properties/AssemblyInfo.cs
+++ Nova.SearchAlgorithm/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using Nova.Utils.WebApi.Attributes;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("5.0.0.0")]
-[assembly: AssemblyFileVersion("5.0.0.0")]
+[assembly: AssemblyVersion("6.0.0.0")]
+[assembly: AssemblyFileVersion("6.0.0.0")]
