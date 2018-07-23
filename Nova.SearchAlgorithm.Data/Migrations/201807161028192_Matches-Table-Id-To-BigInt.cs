2ee4476f049da1b01d551d5e2dcf86d6afb40017
Ben Below <benjamin.below@softwire.com>
2018-07-23 11:06:54 +0100

    NOVA-NONE: TEMPORARY - disable database migration
    This has been done manually to the dev db, so we want EF to think it's run a migration without actually running it.
    This should be immendiately reverted so it can be run on UAT and LIVE properly

--- Nova.SearchAlgorithm.Data/Migrations/201807161028192_Matches-Table-Id-To-BigInt.cs
+++ Nova.SearchAlgorithm.Data/Migrations/201807161028192_Matches-Table-Id-To-BigInt.cs
@@ -7,21 +7,21 @@ namespace Nova.SearchAlgorithm.Data.Migrations
     {
         public override void Up()
         {
-            DropPrimaryKey("dbo.MatchingHlaAtA");
-            DropPrimaryKey("dbo.MatchingHlaAtB");
-            DropPrimaryKey("dbo.MatchingHlaAtC");
-            DropPrimaryKey("dbo.MatchingHlaAtDQB1");
-            DropPrimaryKey("dbo.MatchingHlaAtDRB1");
-            AlterColumn("dbo.MatchingHlaAtA", "Id", c => c.Long(nullable: false, identity: true));
-            AlterColumn("dbo.MatchingHlaAtB", "Id", c => c.Long(nullable: false, identity: true));
-            AlterColumn("dbo.MatchingHlaAtC", "Id", c => c.Long(nullable: false, identity: true));
-            AlterColumn("dbo.MatchingHlaAtDQB1", "Id", c => c.Long(nullable: false, identity: true));
-            AlterColumn("dbo.MatchingHlaAtDRB1", "Id", c => c.Long(nullable: false, identity: true));
-            AddPrimaryKey("dbo.MatchingHlaAtA", "Id");
-            AddPrimaryKey("dbo.MatchingHlaAtB", "Id");
-            AddPrimaryKey("dbo.MatchingHlaAtC", "Id");
-            AddPrimaryKey("dbo.MatchingHlaAtDQB1", "Id");
-            AddPrimaryKey("dbo.MatchingHlaAtDRB1", "Id");
+//            DropPrimaryKey("dbo.MatchingHlaAtA");
+//            DropPrimaryKey("dbo.MatchingHlaAtB");
+//            DropPrimaryKey("dbo.MatchingHlaAtC");
+//            DropPrimaryKey("dbo.MatchingHlaAtDQB1");
+//            DropPrimaryKey("dbo.MatchingHlaAtDRB1");
+//            AlterColumn("dbo.MatchingHlaAtA", "Id", c => c.Long(nullable: false, identity: true));
+//            AlterColumn("dbo.MatchingHlaAtB", "Id", c => c.Long(nullable: false, identity: true));
+//            AlterColumn("dbo.MatchingHlaAtC", "Id", c => c.Long(nullable: false, identity: true));
+//            AlterColumn("dbo.MatchingHlaAtDQB1", "Id", c => c.Long(nullable: false, identity: true));
+//            AlterColumn("dbo.MatchingHlaAtDRB1", "Id", c => c.Long(nullable: false, identity: true));
+//            AddPrimaryKey("dbo.MatchingHlaAtA", "Id");
+//            AddPrimaryKey("dbo.MatchingHlaAtB", "Id");
+//            AddPrimaryKey("dbo.MatchingHlaAtC", "Id");
+//            AddPrimaryKey("dbo.MatchingHlaAtDQB1", "Id");
+//            AddPrimaryKey("dbo.MatchingHlaAtDRB1", "Id");
         }
         
         public override void Down()
