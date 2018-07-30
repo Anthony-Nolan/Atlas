03f0b5d945e0b4118d5f5ace639f88e2bd124c0b
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2018-07-30 12:53:59 +0100

    NOVA-1446: Added list of empty unit test definitons for grading service.

--- Nova.SearchAlgorithm.Common/Models/Scoring/MatchGradeResult.cs
+++ Nova.SearchAlgorithm.Common/Models/Scoring/MatchGradeResult.cs
@@ -5,10 +5,25 @@ namespace Nova.SearchAlgorithm.Common.Models.Scoring
 {
     public class MatchGradeResult
     {
-        // The grade given to this match - it is the best grade calculated across all typing combinations
-        public MatchGrade GradeResult;
-        // The orientation(s) of the best grade calculated for this match.
-        // It is an IEnumerable to account for the case of both orientations having a joint best match grade
-        public IEnumerable<MatchOrientation> Orientations;
+        /// <summary>
+        /// The grade given to this match - it is the best grade calculated across all typing combinations
+        /// </summary>
+        public MatchGrade GradeResult { get; set; }
+
+        /// <summary>
+        /// The orientation(s) of the best grade calculated for this match.
+        /// It is an IEnumerable to account for the case of both orientations having a joint best match grade
+        /// </summary>
+        public IEnumerable<MatchOrientation> Orientations { get; set; }
+
+        public MatchGradeResult()
+        {
+        }
+
+        public MatchGradeResult(MatchGrade gradeResult, IEnumerable<MatchOrientation> orientations)
+        {
+            GradeResult = gradeResult;
+            Orientations = orientations;
+        }
     }
 }
