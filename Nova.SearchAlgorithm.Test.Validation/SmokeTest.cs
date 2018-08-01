4d02ea78484f91137d7119a0ce5b67abcd2d67e3
Ben Below <benjamin.below@softwire.com>
2018-08-01 15:41:31 +0100

    NOVA-1538: Add validation test project
    Copied from tests project, so may have redundant dependencies.
    OWIN self hosting will not work with the background jobs used in startup to warm the caches - for now we can comment them out, in practice we should switch to a better solution for warming the cache e.g. Hangfire

---
+++ Nova.SearchAlgorithm.Test.Validation/SmokeTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.Owin.Testing;
+using NUnit.Framework;
+
+namespace Nova.SearchAlgorithm.Test.Validation
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public async Task Test()
+        {
+            using (var server = TestServer.Create<Startup>())
+            {
+                var result = await server.HttpClient.GetAsync("/service-status");
+                result.StatusCode.Should().Be(HttpStatusCode.OK);
+            }
+        }
+    }
+}
