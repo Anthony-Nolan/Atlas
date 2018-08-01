6fa56e41896f7cd60122772a9e07ef05e8c1f2fc
Ben Below <benjamin.below@softwire.com>
2018-08-01 19:44:13 +0100

    NOVA-1538: Split feature files and step definitions into separate folders

--- Nova.SearchAlgorithm.Test.Validation/ValidationTests/StepDefinitions/SearchSteps.cs
+++ Nova.SearchAlgorithm.Test.Validation/ValidationTests/StepDefinitions/SearchSteps.cs
@@ -3,41 +3,25 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using FluentAssertions;
-using Microsoft.Owin.Testing;
 using Newtonsoft.Json;
-using Nova.SearchAlgorithm.Client.Models.SearchRequests;
 using Nova.SearchAlgorithm.Client.Models.SearchResults;
 using Nova.SearchAlgorithm.Common.Models;
 using Nova.SearchAlgorithm.Test.Integration.TestHelpers.Builders;
-using NUnit.Framework;
+using TechTalk.SpecFlow;
 
 namespace Nova.SearchAlgorithm.Test.Validation
 {
-    [TestFixture]
-    public class SearchTests
+    [Binding]
+    public class SearchSteps : BaseStepDefinitions
     {
-        private TestServer server;
+        private SearchRequestBuilder searchRequestBuilder = new SearchRequestBuilder();
+        private HttpResponseMessage result;
 
-        [OneTimeSetUp]
-        public void SetUp()
-        {
-            server = TestServer.Create<Startup>();
-        }
 
-        [OneTimeTearDown]
-        public void TearDown()
+        [Given(@"I search for recognised hla")]
+        public void GivenISearchForRecognisedHla()
         {
-            server.Dispose();
-        }
-
-        [Test]
-        public async Task Search_ReturnsDonors()
-        {
-            var searchRequest = new SearchRequestBuilder()
-                .WithTotalMismatchCount(0)
-                .WithLocusMismatchCount(Locus.A, 0)
-                .WithLocusMismatchCount(Locus.B, 0)
-                .WithLocusMismatchCount(Locus.Drb1, 0)
+            searchRequestBuilder = searchRequestBuilder
                 .WithLocusMatchHla(Locus.A, TypePositions.One, "2")
                 .WithLocusMatchHla(Locus.A, TypePositions.Two, "68")
                 .WithLocusMatchHla(Locus.B, TypePositions.One, "7")
@@ -47,18 +31,43 @@ namespace Nova.SearchAlgorithm.Test.Validation
                 .WithLocusMatchHla(Locus.C, TypePositions.One, "04:01")
                 .WithLocusMatchHla(Locus.C, TypePositions.Two, "15:02")
                 .WithLocusMatchHla(Locus.Dqb1, TypePositions.One, "05:01")
-                .WithLocusMatchHla(Locus.Dqb1, TypePositions.Two, "06:01")
+                .WithLocusMatchHla(Locus.Dqb1, TypePositions.Two, "06:01");
+        }
+
+        [Given(@"The search type is adult")]
+        public void GivenTheSearchTypeIsAdult()
+        {
+            // TODO
+        }
+
+        [Given(@"The search is run for Anthony Nolan's registry only")]
+        public void GivenTheSearchIsRunForAnthonyNolanSRegistryOnly()
+        {
+            // TODO
+        }
+
+        [When(@"I run a 6/6 search")]
+        public async Task WhenIRunASearch()
+        {
+            var searchRequest = searchRequestBuilder
+                .WithTotalMismatchCount(0)
+                .WithLocusMismatchCount(Locus.A, 0)
+                .WithLocusMismatchCount(Locus.B, 0)
+                .WithLocusMismatchCount(Locus.Drb1, 0)
                 .Build();
             
-            var result = await server.CreateRequest("/search")
+            result = await Server.CreateRequest("/search")
                 .AddHeader("X-Samples-ApiKey", "test-key")
                 .And(request => request.Content = new StringContent(JsonConvert.SerializeObject(searchRequest), Encoding.UTF8, "application/json"))
                 .PostAsync();
+        }
+
+        [Then(@"The result should contain at least one donor")]
+        public async Task ThenTheResultShouldContainAtLeastOneDonor()
+        {
             var content = await result.Content.ReadAsStringAsync();
             var deserialisedContent = JsonConvert.DeserializeObject<SearchResultSet>(content);
             deserialisedContent.SearchResults.Count().Should().BeGreaterThan(0);
         }
-        
-        
     }
 }
