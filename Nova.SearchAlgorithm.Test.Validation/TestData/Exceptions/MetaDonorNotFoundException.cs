f07092414e872e1ff55b671634d34ea7bd0c0538
Ben Below <benjamin.below@softwire.com>
2018-08-16 14:20:55 +0100

    NOVA-1649: Add meta donor selector failure unit tests

---
+++ Nova.SearchAlgorithm.Test.Validation/TestData/Exceptions/MetaDonorNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Nova.SearchAlgorithm.Test.Validation.TestData.Exceptions
+{
+    public class MetaDonorNotFoundException : Exception
+    {
+        public MetaDonorNotFoundException(string message) : base(message)
+        {
+        }
+    }
+}
--- Nova.SearchAlgorithm.Test.Validation/TestData/Services/PatientDataSelection/MetaDonorSelector.cs
+++ Nova.SearchAlgorithm.Test.Validation/TestData/Services/PatientDataSelection/MetaDonorSelector.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using Nova.SearchAlgorithm.Test.Validation.TestData.Exceptions;
 using Nova.SearchAlgorithm.Test.Validation.TestData.Models;
 using Nova.SearchAlgorithm.Test.Validation.TestData.Models.PatientDataSelection;
 using Nova.SearchAlgorithm.Test.Validation.TestData.Repositories;
@@ -22,12 +23,39 @@ namespace Nova.SearchAlgorithm.Test.Validation.TestData.Services.PatientDataSele
         public MetaDonor GetMetaDonor(MetaDonorSelectionCriteria criteria)
         {
             var matchingMetaDonors = metaDonorRepository.AllMetaDonors()
-                .Where(md => criteria.MatchingDonorType == md.DonorType)
-                .Where(md => criteria.MatchingRegistry == md.Registry)
-                .Where(md => criteria.MatchingTgsTypingCategories.Equals(md.GenotypeCriteria.TgsHlaCategories))
-                .Where(md => criteria.MatchLevels.ToEnumerable().All(ml => ml != MatchLevel.PGroup)
-                             || md.GenotypeCriteria.HasNonUniquePGroups.ToEnumerable().Any(x => x));
+                .Where(md => FulfilsDonorTypeCriteria(criteria, md)
+                             && FulfilsRegistryCriteria(criteria, md)
+                             && FulfilsTgsTypingCategoryCriteria(criteria, md)
+                             && FulfilsMatchLevelCriteria(criteria, md))
+                .ToList();
+
+            if (!matchingMetaDonors.Any())
+            {
+                throw new MetaDonorNotFoundException("No meta-donors found matching specified criteria.");
+            }
+
             return matchingMetaDonors.First();
+        }
+
+        private static bool FulfilsMatchLevelCriteria(MetaDonorSelectionCriteria criteria, MetaDonor md)
+        {
+            return criteria.MatchLevels.ToEnumerable().All(ml => ml != MatchLevel.PGroup)
+                   || md.GenotypeCriteria.HasNonUniquePGroups.ToEnumerable().Any(x => x);
+        }
+
+        private static bool FulfilsTgsTypingCategoryCriteria(MetaDonorSelectionCriteria criteria, MetaDonor md)
+        {
+            return criteria.MatchingTgsTypingCategories.Equals(md.GenotypeCriteria.TgsHlaCategories);
+        }
+
+        private static bool FulfilsRegistryCriteria(MetaDonorSelectionCriteria criteria, MetaDonor md)
+        {
+            return criteria.MatchingRegistry == md.Registry;
+        }
+
+        private static bool FulfilsDonorTypeCriteria(MetaDonorSelectionCriteria criteria, MetaDonor metaDonor)
+        {
+            return criteria.MatchingDonorType == metaDonor.DonorType;
         }
     }
 }
