db8e4a0ca84d77c47d0cf1c598edcd2d4932723b
Ben Below <benjamin.below@softwire.com>
2018-08-16 12:30:48 +0100

    NOVA-1649: Restructure / clean up patient data selector

--- Nova.SearchAlgorithm.Test.Validation/TestData/Services/PatientDataSelection/PatientDataSelector.cs
+++ Nova.SearchAlgorithm.Test.Validation/TestData/Services/PatientDataSelection/PatientDataSelector.cs
@@ -13,16 +13,19 @@ namespace Nova.SearchAlgorithm.Test.Validation.TestData.Services.PatientDataSele
 {
     public interface IPatientDataSelector
     {
-        void SetPatientUntypedAt(Locus locus);
-        void SetAsTenOutOfTenMatch();
-        void SetAsEightOutOfEightMatch();
         void SetAsSixOutOfSixMatch();
-        void SetMatchingDonorUntypedAtLocus(Locus locus);
+        void SetAsEightOutOfEightMatch();
+        void SetAsTenOutOfTenMatch();
+
+        void SetMatchingDonorType(DonorType donorType);
+        void SetMatchingRegistry(RegistryCode registry);
+
+        void SetAsMatchLevelAtAllLoci(MatchLevel matchLevel);
         void SetFullMatchingTypingResolution(HlaTypingResolution resolution);
         void SetFullMatchingTgsCategory(TgsHlaTypingCategory tgsCategory);
-        void SetAsMatchLevelAtAllLoci(MatchLevel matchLevel);
-        void SetMatchingRegistry(RegistryCode registry);
-        void SetMatchingDonorType(DonorType donorType);
+        void SetMatchingDonorUntypedAtLocus(Locus locus);
+
+        void SetPatientUntypedAt(Locus locus);
 
         PhenotypeInfo<string> GetPatientHla();
         int GetExpectedMatchingDonorId();
@@ -34,29 +37,13 @@ namespace Nova.SearchAlgorithm.Test.Validation.TestData.Services.PatientDataSele
     /// </summary>
     public class PatientDataSelector : IPatientDataSelector
     {
-        public bool HasMatch { get; set; }
-
         private readonly IMetaDonorSelector metaDonorSelector;
         private readonly IDatabaseDonorSelector databaseDonorSelector;
         private readonly IPatientHlaSelector patientHlaSelector;
 
         private MetaDonor selectedMetaDonor;
 
-        private static readonly PhenotypeInfo<MatchLevel> DefaultMatchLevels = new PhenotypeInfo<MatchLevel>
-        {
-            A_1 = MatchLevel.Allele,
-            A_2 = MatchLevel.Allele,
-            B_1 = MatchLevel.Allele,
-            B_2 = MatchLevel.Allele,
-            C_1 = MatchLevel.Allele,
-            C_2 = MatchLevel.Allele,
-            DPB1_1 = MatchLevel.Allele,
-            DPB1_2 = MatchLevel.Allele,
-            DQB1_1 = MatchLevel.Allele,
-            DQB1_2 = MatchLevel.Allele,
-            DRB1_1 = MatchLevel.Allele,
-            DRB1_2 = MatchLevel.Allele,
-        };
+        private static readonly PhenotypeInfo<MatchLevel> DefaultMatchLevels = new PhenotypeInfo<MatchLevel>().Map((l, p, noop) => MatchLevel.Allele);
 
         private readonly MetaDonorSelectionCriteria metaDonorSelectionCriteria = new MetaDonorSelectionCriteria {MatchLevels = DefaultMatchLevels};
         private readonly DatabaseDonorSelectionCriteria databaseDonorSelectionCriteria = new DatabaseDonorSelectionCriteria();
@@ -73,78 +60,27 @@ namespace Nova.SearchAlgorithm.Test.Validation.TestData.Services.PatientDataSele
             this.patientHlaSelector = patientHlaSelector;
         }
 
-        public void SetPatientUntypedAt(Locus locus)
+        public void SetAsSixOutOfSixMatch()
         {
-            patientHlaSelectionCriteria.PatientTypingResolutions.SetAtLocus(locus, TypePositions.Both, HlaTypingResolution.Untyped);
-        }
-
-        public void SetAsTenOutOfTenMatch()
-        {
-            patientHlaSelectionCriteria.HlaMatches = patientHlaSelectionCriteria.HlaMatches.Map((locus, p, hasMatch) => 
-                locus != Locus.Dpb1);
+            var matches = new PhenotypeInfo<bool>().Map((locus, p, noop) => locus != Locus.C || locus != Locus.Dqb1 || locus != Locus.Dpb1);
+            patientHlaSelectionCriteria.HlaMatches = matches;
         }
 
         public void SetAsEightOutOfEightMatch()
         {
-            patientHlaSelectionCriteria.HlaMatches = patientHlaSelectionCriteria.HlaMatches.Map((locus, p, hasMatch) => 
-                locus != Locus.Dqb1 || locus != Locus.Dpb1);
+            var matches = new PhenotypeInfo<bool>().Map((locus, p, noop) => locus != Locus.Dqb1 || locus != Locus.Dpb1);
+            patientHlaSelectionCriteria.HlaMatches = matches;
         }
 
-        public void SetAsSixOutOfSixMatch()
+        public void SetAsTenOutOfTenMatch()
         {
-            patientHlaSelectionCriteria.HlaMatches = patientHlaSelectionCriteria.HlaMatches.Map((locus, p, hasMatch) => 
-                locus != Locus.C || locus != Locus.Dqb1 || locus != Locus.Dpb1);
+            var matches = new PhenotypeInfo<bool>().Map((locus, p, noop) => locus != Locus.Dpb1);
+            patientHlaSelectionCriteria.HlaMatches = matches;
         }
 
-        /// <summary>
-        /// Will set the desired typing resolution at all positions
-        /// </summary>
-        public void SetFullMatchingTypingResolution(HlaTypingResolution resolution)
+        public void SetMatchingDonorType(DonorType donorType)
         {
-            databaseDonorSelectionCriteria.MatchingTypingResolutions.A_1 = resolution;
-            databaseDonorSelectionCriteria.MatchingTypingResolutions.A_2 = resolution;
-            databaseDonorSelectionCriteria.MatchingTypingResolutions.B_1 = resolution;
-            databaseDonorSelectionCriteria.MatchingTypingResolutions.B_2 = resolution;
-            databaseDonorSelectionCriteria.MatchingTypingResolutions.C_1 = resolution;
-            databaseDonorSelectionCriteria.MatchingTypingResolutions.C_2 = resolution;
-            databaseDonorSelectionCriteria.MatchingTypingResolutions.DPB1_1 = resolution;
-            databaseDonorSelectionCriteria.MatchingTypingResolutions.DPB1_2 = resolution;
-            databaseDonorSelectionCriteria.MatchingTypingResolutions.DQB1_1 = resolution;
-            databaseDonorSelectionCriteria.MatchingTypingResolutions.DQB1_2 = resolution;
-            databaseDonorSelectionCriteria.MatchingTypingResolutions.DRB1_1 = resolution;
-            databaseDonorSelectionCriteria.MatchingTypingResolutions.DRB1_2 = resolution;
-        }
-
-        /// <summary>
-        /// Will set the desired tgs typing category at all positions
-        /// </summary>
-        public void SetFullMatchingTgsCategory(TgsHlaTypingCategory tgsCategory)
-        {
-            metaDonorSelectionCriteria.MatchingTgsTypingCategories.A_1 = tgsCategory;
-            metaDonorSelectionCriteria.MatchingTgsTypingCategories.A_2 = tgsCategory;
-            metaDonorSelectionCriteria.MatchingTgsTypingCategories.B_1 = tgsCategory;
-            metaDonorSelectionCriteria.MatchingTgsTypingCategories.B_2 = tgsCategory;
-            metaDonorSelectionCriteria.MatchingTgsTypingCategories.C_1 = tgsCategory;
-            metaDonorSelectionCriteria.MatchingTgsTypingCategories.C_2 = tgsCategory;
-            metaDonorSelectionCriteria.MatchingTgsTypingCategories.DQB1_1 = tgsCategory;
-            metaDonorSelectionCriteria.MatchingTgsTypingCategories.DQB1_2 = tgsCategory;
-            metaDonorSelectionCriteria.MatchingTgsTypingCategories.DRB1_1 = tgsCategory;
-            metaDonorSelectionCriteria.MatchingTgsTypingCategories.DRB1_2 = tgsCategory;
-
-            //There is no DPB1 test data with fewer than 4 fields
-            metaDonorSelectionCriteria.MatchingTgsTypingCategories.DPB1_1 = TgsHlaTypingCategory.FourFieldAllele;
-            metaDonorSelectionCriteria.MatchingTgsTypingCategories.DPB1_2 = TgsHlaTypingCategory.FourFieldAllele;
-        }
-
-        public void SetMatchingDonorUntypedAtLocus(Locus locus)
-        {
-            databaseDonorSelectionCriteria.MatchingTypingResolutions.SetAtLocus(locus, TypePositions.Both, HlaTypingResolution.Untyped);
-        }
-
-        public void SetAsMatchLevelAtAllLoci(MatchLevel matchLevel)
-        {
-            var matchLevels = new PhenotypeInfo<int>().Map((l, p, level) => matchLevel);
-            SetMatchLevels(matchLevels);
+            metaDonorSelectionCriteria.MatchingDonorType = donorType;
         }
 
         public void SetMatchingRegistry(RegistryCode registry)
@@ -152,9 +88,43 @@ namespace Nova.SearchAlgorithm.Test.Validation.TestData.Services.PatientDataSele
             metaDonorSelectionCriteria.MatchingRegistry = registry;
         }
 
-        public void SetMatchingDonorType(DonorType donorType)
+        public void SetAsMatchLevelAtAllLoci(MatchLevel matchLevel)
         {
-            metaDonorSelectionCriteria.MatchingDonorType = donorType;
+            var matchLevels = new PhenotypeInfo<int>().Map((l, p, noop) => matchLevel);
+            SetMatchLevels(matchLevels);
+        }
+
+        /// <summary>
+        /// Will set the desired typing resolution at all positions
+        /// </summary>
+        public void SetFullMatchingTypingResolution(HlaTypingResolution resolution)
+        {
+            var resolutions = new PhenotypeInfo<bool>().Map((l, p, noop) => resolution);
+            databaseDonorSelectionCriteria.MatchingTypingResolutions = resolutions;
+        }
+
+        /// <summary>
+        /// Will set the desired tgs typing category at all positions
+        /// </summary>
+        public void SetFullMatchingTgsCategory(TgsHlaTypingCategory tgsCategory)
+        {
+            var categories = new PhenotypeInfo<bool>().Map((locus, p, noop) =>
+                locus == Locus.Dpb1
+                    //There is no DPB1 test data with fewer than 4 fields
+                    ? TgsHlaTypingCategory.FourFieldAllele
+                    : tgsCategory
+            );
+            metaDonorSelectionCriteria.MatchingTgsTypingCategories = categories;
+        }
+
+        public void SetMatchingDonorUntypedAtLocus(Locus locus)
+        {
+            databaseDonorSelectionCriteria.MatchingTypingResolutions.SetAtLocus(locus, TypePositions.Both, HlaTypingResolution.Untyped);
+        }
+
+        public void SetPatientUntypedAt(Locus locus)
+        {
+            patientHlaSelectionCriteria.PatientTypingResolutions.SetAtLocus(locus, TypePositions.Both, HlaTypingResolution.Untyped);
         }
 
         public PhenotypeInfo<string> GetPatientHla()
