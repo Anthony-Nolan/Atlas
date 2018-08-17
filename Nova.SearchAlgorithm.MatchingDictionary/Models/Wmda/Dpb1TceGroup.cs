171dbf4267a80b4f950438807256dd2e0ac8e69e
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2018-08-17 16:38:07 +0100

    NOVA-1298: Added TCE group extractor with WMDA repo tests.

---
+++ Nova.SearchAlgorithm.MatchingDictionary/Models/Wmda/Dpb1TceGroup.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Nova.SearchAlgorithm.MatchingDictionary.Models.HLATypings;
+
+namespace Nova.SearchAlgorithm.MatchingDictionary.Models.Wmda
+{
+    public class Dpb1TceGroup : IWmdaHlaTyping, IEquatable<Dpb1TceGroup>
+    {
+        public TypingMethod TypingMethod => TypingMethod.Molecular;
+
+        /// <summary>
+        /// Locus will always be DPB1*
+        /// </summary>
+        public string Locus
+        {
+            get => "DPB1*";
+            set {  }
+        }
+
+        /// <summary>
+        /// DPB1 Allele Name
+        /// </summary>
+        public string Name { get; set; }
+
+        public string ProteinName { get; }
+        public string VersionOneAssignment { get; }
+        public string VersionTwoAssignment { get; }
+
+        public Dpb1TceGroup(
+            string alleleName,
+            string proteinName, 
+            string versionOneAssignment, 
+            string versionTwoAssignment)
+        {
+            Name = alleleName;
+            ProteinName = proteinName;
+            VersionOneAssignment = versionOneAssignment;
+            VersionTwoAssignment = versionTwoAssignment;
+        }
+
+        public bool Equals(Dpb1TceGroup other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return 
+                string.Equals(Locus, other.Locus) && 
+                string.Equals(Name, other.Name) && 
+                string.Equals(ProteinName, other.ProteinName) && 
+                string.Equals(VersionOneAssignment, other.VersionOneAssignment) && 
+                string.Equals(VersionTwoAssignment, other.VersionTwoAssignment);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Dpb1TceGroup) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = Locus.GetHashCode();
+                hashCode = (hashCode * 397) ^ Name.GetHashCode();
+                hashCode = (hashCode * 397) ^ ProteinName.GetHashCode();
+                hashCode = (hashCode * 397) ^ VersionOneAssignment.GetHashCode();
+                hashCode = (hashCode * 397) ^ VersionTwoAssignment.GetHashCode();
+                return hashCode;
+            }
+        }
+    }
+}
---
+++ Nova.SearchAlgorithm.MatchingDictionary/Repositories/Wmda/Dpb1TceGroupExtractor.cs
@@ -0,0 +1,34 @@
+﻿using Nova.SearchAlgorithm.MatchingDictionary.Models.Wmda;
+using System.Text.RegularExpressions;
+
+namespace Nova.SearchAlgorithm.MatchingDictionary.Repositories.Wmda
+{
+    internal class Dpb1TceGroupExtractor : WmdaDataExtractor<Dpb1TceGroup>
+    {
+        private const string FileName = "tce/dpb_tce.csv";
+        private const string RegexPattern = @"^DPB1\*([\w:]+),DPB1\*([\w:]+),(\w?).*,(\w?).*,";
+
+        public Dpb1TceGroupExtractor() : base(FileName)
+        {
+        }
+
+        protected override Dpb1TceGroup MapLineOfFileContentsToWmdaHlaTypingElseNull(string line)
+        {
+            var regex = new Regex(RegexPattern);
+
+            if (!regex.IsMatch(line))
+            {
+                return null;
+            }
+
+            var extractedData = regex.Match(line).Groups;
+
+            return new Dpb1TceGroup(
+                    extractedData[1].Value,
+                    extractedData[2].Value,
+                    extractedData[3].Value,
+                    extractedData[4].Value
+                );
+        }
+    }
+}
