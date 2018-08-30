6783a6cfbddf6062d0f42630c2d49272a205b797
Ben Below <benjamin.below@softwire.com>
2018-08-30 13:56:10 +0100

    NOVA-1684: Check for genotype mismatch criteria in meta donor selector

--- Nova.SearchAlgorithm.Test.Validation/TestData/Models/PatientDataSelection/DatabaseDonorSpecification.cs
+++ Nova.SearchAlgorithm.Test.Validation/TestData/Models/PatientDataSelection/DatabaseDonorSpecification.cs
@@ -9,6 +9,7 @@ namespace Nova.SearchAlgorithm.Test.Validation.TestData.Models.PatientDataSelect
     /// </summary>
     public class DatabaseDonorSpecification
     {
+
         /// <summary>
         /// Determines to what resolution the expected matched donor is typed
         /// </summary>
@@ -19,5 +20,40 @@ namespace Nova.SearchAlgorithm.Test.Validation.TestData.Models.PatientDataSelect
         /// If false, the hla will be instead selected from a 'non-matching' hla allele source
         /// </summary>
         public PhenotypeInfo<bool> ShouldMatchGenotype { get; set; } = new PhenotypeInfo<bool>(true);
+
+        #region Equality
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((DatabaseDonorSpecification) obj);
+        }
+
+        private bool Equals(DatabaseDonorSpecification other)
+        {
+            return Equals(MatchingTypingResolutions, other.MatchingTypingResolutions) && Equals(ShouldMatchGenotype, other.ShouldMatchGenotype);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return ((MatchingTypingResolutions != null ? MatchingTypingResolutions.GetHashCode() : 0) * 397) ^ (ShouldMatchGenotype != null ? ShouldMatchGenotype.GetHashCode() : 0);
+            }
+        }
+
+        public static bool operator ==(DatabaseDonorSpecification left, DatabaseDonorSpecification right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(DatabaseDonorSpecification left, DatabaseDonorSpecification right)
+        {
+            return !Equals(left, right);
+        }
+
+        #endregion
     }
 }
--- Nova.SearchAlgorithm.Test.Validation/TestData/Services/PatientDataSelection/DataSelectors/MetaDonorSelector.cs
+++ Nova.SearchAlgorithm.Test.Validation/TestData/Services/PatientDataSelection/DataSelectors/MetaDonorSelector.cs
@@ -60,7 +60,7 @@ namespace Nova.SearchAlgorithm.Test.Validation.TestData.Services.PatientDataSele
                    && FulfilsExpressionSuffixCriteria(criteria, metaDonor)
                    && FulfilsAlleleStringCriteria(criteria, metaDonor)
                    && FulfilsDatasetCriteria(criteria, metaDonor)
-                   && FulfilsTypingResolutionCriteria(criteria, metaDonor);
+                   && FulfilsDatabaseDonorCriteria(criteria, metaDonor);
         }
 
         private static bool FulfilsExpressionSuffixCriteria(MetaDonorSelectionCriteria criteria, MetaDonor metaDonor)
@@ -105,11 +105,11 @@ namespace Nova.SearchAlgorithm.Test.Validation.TestData.Services.PatientDataSele
             return perLocusFulfilment.ToEnumerable().All(x => x);
         }
 
-        private static bool FulfilsTypingResolutionCriteria(MetaDonorSelectionCriteria criteria, MetaDonor metaDonor)
+        private static bool FulfilsDatabaseDonorCriteria(MetaDonorSelectionCriteria criteria, MetaDonor metaDonor)
         {
-            return criteria.DatabaseDonorDetailsSets
-                .All(donorDetails => metaDonor.DatabaseDonorSpecifications
-                    .Any(d => donorDetails.MatchingTypingResolutions == d.MatchingTypingResolutions));
+            return criteria.DatabaseDonorDetailsSets.All(donorDetails =>
+                metaDonor.DatabaseDonorSpecifications.Any(d => donorDetails == d)
+            );
         }
 
         private static bool FulfilsDatasetCriteria(MetaDonorSelectionCriteria criteria, MetaDonor metaDonor)
