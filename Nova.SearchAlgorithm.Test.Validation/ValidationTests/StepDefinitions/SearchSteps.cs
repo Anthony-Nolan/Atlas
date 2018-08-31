31a325f13192f2d87fea6e337cbe508a806b3d6e
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2018-08-31 14:11:09 +0100

    NOVA-1683: Removed unused refs.

--- Nova.SearchAlgorithm.Test.Validation/ValidationTests/StepDefinitions/SearchSteps.cs
+++ Nova.SearchAlgorithm.Test.Validation/ValidationTests/StepDefinitions/SearchSteps.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using FluentAssertions;
+﻿using FluentAssertions;
 using Nova.SearchAlgorithm.Client.Models;
 using Nova.SearchAlgorithm.Common.Models;
 using Nova.SearchAlgorithm.Test.Integration.TestHelpers.Builders;
