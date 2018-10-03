610a40481b6015415d9282da6159034521ba6c8c
Ben Below <benjamin.below@softwire.com>
2018-10-03 16:32:08 +0100

    NOVA-NONE: Split out some services / models

---
+++ Nova.SearchAlgorithm.Test.Performance/PerformanceHarness.cs
@@ -0,0 +1,49 @@
+﻿using System.Threading.Tasks;
+using Nova.SearchAlgorithm.Client.Models;
+using Nova.SearchAlgorithm.Common.Models;
+using Nova.SearchAlgorithm.Test.Integration.TestHelpers.Builders;
+using Nova.SearchAlgorithm.Test.Performance.Models;
+
+namespace Nova.SearchAlgorithm.Test.Performance
+{
+    internal class Program
+    {
+        /// <summary>
+        /// This should be manually updated before running performance tests.
+        /// Only local environment info should be checked in.
+        /// </summary>
+        private static readonly AlgorithmInstanceInfo AlgorithmInstanceInfo = new AlgorithmInstanceInfo
+        {
+            BaseUrl = "http://localhost:30508",
+            Apikey = "test-key",
+            Environment = Environment.Local,
+            AzureStorageEnvironment = Environment.Local,
+        };
+        
+        public static async Task Main(string[] args)
+        {
+            var searchRequest = new SearchRequestBuilder()
+                .WithTotalMismatchCount(0)
+                .ForRegistries(new []{RegistryCode.AN, RegistryCode.DKMS, RegistryCode.NHSBT, RegistryCode.WBS, RegistryCode.FRANCE, RegistryCode.NMDP})
+                .WithLocusMismatchCount(Locus.A, 0)
+                .WithLocusMismatchCount(Locus.B, 0)
+                .WithLocusMismatchCount(Locus.Drb1, 0)
+                .WithSearchHla(new PhenotypeInfo<string>
+                {
+                    A_1 = "24:02",
+                    A_2 = "29:02",
+                    B_1 = "45:01",
+                    B_2 = "15:01",
+                    C_1 = "03:03",
+                    C_2 = "06:02",
+                    Drb1_1 = "04:01",
+                    Drb1_2 = "11:01",
+                    Dqb1_1 = "03:01",
+                    Dqb1_2 = "03:02"
+                })
+                .Build();
+
+            var metrics = await SearchTimingService.TimeSearchRequest(searchRequest, AlgorithmInstanceInfo);
+        }
+    }
+}
---
+++ Nova.SearchAlgorithm.Test.Performance/SearchTimingService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Nova.SearchAlgorithm.Client.Models.SearchRequests;
+using Nova.SearchAlgorithm.Client.Models.SearchResults;
+using Nova.SearchAlgorithm.Test.Performance.Models;
+
+namespace Nova.SearchAlgorithm.Test.Performance
+{
+    public static class SearchTimingService
+    {
+        public static async Task<SearchMetrics> TimeSearchRequest(SearchRequest request, AlgorithmInstanceInfo algorithmInstanceInfo)
+        {
+            var client = GetClient(algorithmInstanceInfo);
+            var stopwatch = new Stopwatch();
+            
+            var httpRequest = GetSearchHttpRequest(request);
+
+            stopwatch.Start();
+            var result = await client.SendAsync(httpRequest);
+            stopwatch.Stop();
+            
+            var content = await result.Content.ReadAsStringAsync();
+            if (!result.IsSuccessStatusCode)
+            {
+                throw new Exception($"Search request failed: {content}");
+            }
+            
+            var deserialisedContent = JsonConvert.DeserializeObject<SearchResultSet>(content);
+
+            return new SearchMetrics
+            {
+                ElapsedMilliseconds = stopwatch.ElapsedMilliseconds,
+                DonorsReturned = deserialisedContent.TotalResults,
+            };
+        }
+        
+        private static HttpClient GetClient(AlgorithmInstanceInfo algorithmInstanceInfo)
+        {
+            var httpClient = new HttpClient(new HttpClientHandler())
+            {
+                BaseAddress = new Uri(algorithmInstanceInfo.BaseUrl),
+            };
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            httpClient.DefaultRequestHeaders.Add("X-Samples-ApiKey", algorithmInstanceInfo.Apikey);
+            return httpClient;
+        }
+
+        private static HttpRequestMessage GetSearchHttpRequest(object body)
+        {
+            var serialisedBody = JsonConvert.SerializeObject(body);
+            var bodyContent = new StringContent(serialisedBody, Encoding.UTF8, "application/json");
+            return new HttpRequestMessage(HttpMethod.Post, "search") {Content = bodyContent};
+        }
+    }
+}
