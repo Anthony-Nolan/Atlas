5189c49351b29fa155b22cfe01a9b507896f250b
Ben Below <benjamin.below@softwire.com>
2018-10-09 12:25:21 +0100

    NOVA-1954: Add comment explaining solar timing source

--- Nova.SearchAlgorithm.Test.Performance/Models/TestInput.cs
+++ Nova.SearchAlgorithm.Test.Performance/Models/TestInput.cs
@@ -15,7 +15,13 @@ namespace Nova.SearchAlgorithm.Test.Performance.Models
 
         public PhenotypeInfo<string> Hla { get; set; }
         
+        /// <summary>
+        /// Solar timings were recorded by Sharon on the search team, running against SOLAR via the SOLAR application
+        /// </summary>
         public int? SolarSearchElapsedMilliseconds { get; set; }
+        /// <summary>
+        /// Solar results were recorded by Sharon on the search team, running against SOLAR via the SOLAR application
+        /// </summary>
         public int? SolarSearchMatchedDonors { get; set; }
     }
 }
