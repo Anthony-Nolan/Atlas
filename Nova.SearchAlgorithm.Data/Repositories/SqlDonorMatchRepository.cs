c76448bbed346cafd6f8b6357c36cedd28f735db
Ben Below <benjamin.below@softwire.com>
2018-10-11 17:34:10 +0100

    NOVA-1997: Re-write donor fetching query to not use Where/In statement

--- Nova.SearchAlgorithm.Data/Repositories/SqlDonorMatchRepository.cs
+++ Nova.SearchAlgorithm.Data/Repositories/SqlDonorMatchRepository.cs
@@ -102,7 +102,7 @@ GROUP BY DonorId, TypePosition";
                 // TODO NOVA-1427: Do not fetch PGroups for loci that have already been matched at the DB level
                 foreach (var locus in LocusHelpers.AllLoci().Except(new[] {Locus.Dpb1}))
                 {
-                    var pGroups = await conn.QueryAsync<DonorMatchWithName>($@"
+                    var sql = $@"
 SELECT m.DonorId, m.TypePosition, p.Name as PGroupName FROM {MatchingTableName(locus)} m
 JOIN PGroupNames p 
 ON m.PGroup_Id = p.Id
@@ -112,8 +112,8 @@ INNER JOIN (
 )
 AS DonorIds 
 ON m.DonorId = DonorIds.Id
-WHERE DonorId IN ({string.Join(",", donorIds.Select(id => id.ToString()))})
-");
+";
+                    var pGroups = await conn.QueryAsync<DonorMatchWithName>(sql, commandTimeout: 300);
                     foreach (var donorGroups in pGroups.GroupBy(p => p.DonorId))
                     {
                         foreach (var pGroupGroup in donorGroups.GroupBy(p => (TypePositions) p.TypePosition))
@@ -137,10 +137,16 @@ WHERE DonorId IN ({string.Join(",", donorIds.Select(id => id.ToString()))})
             
             using (var conn = new SqlConnection(connectionString))
             {
-                var donors = await conn.QueryAsync<Donor>($@"
+                var sql = $@"
 SELECT * FROM Donors 
-WHERE DonorId IN ({string.Join(",", donorIds.Select(id => id.ToString()))})
-");
+INNER JOIN (
+    SELECT '{donorIds.FirstOrDefault()}' AS Id
+    UNION ALL SELECT '{string.Join("' UNION ALL SELECT '", donorIds.Skip(1))}'
+)
+AS DonorIds 
+ON DonorId = DonorIds.Id
+";
+                var donors = await conn.QueryAsync<Donor>(sql, commandTimeout: 300);
                 return donors.Select(d => d.ToDonorResult());
             }
         }
