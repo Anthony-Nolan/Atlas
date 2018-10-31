6a3c236090d8a21142dcb2497d4aa1fbe84a6155
Ben Below <benjamin.below@softwire.com>
2018-10-31 16:15:20 +0000

    NOVA-2122: Combine two donor import jobs into a single controller

--- Nova.SearchAlgorithm/Controllers/DonorImportController.cs
+++ Nova.SearchAlgorithm/Controllers/DonorImportController.cs
@@ -7,10 +7,12 @@ namespace Nova.SearchAlgorithm.Controllers
     public class DonorImportController : ApiController
     {
         private readonly IDonorImportService donorImportService;
+        private readonly IHlaUpdateService hlaUpdateService;
 
-        public DonorImportController(IDonorImportService donorImportService)
+        public DonorImportController(IDonorImportService donorImportService, IHlaUpdateService hlaUpdateService)
         {
             this.donorImportService = donorImportService;
+            this.hlaUpdateService = hlaUpdateService;
         }
 
         [HttpPost]
@@ -18,6 +20,13 @@ namespace Nova.SearchAlgorithm.Controllers
         public async Task TriggerImport()
         {
             await donorImportService.StartDonorImport();
+        }
+        
+        [HttpPost]
+        [Route("trigger-donor-hla-update")]
+        public async Task TriggerSingleImport()
+        {
+            await hlaUpdateService.UpdateDonorHla();
         }
     }
 }
