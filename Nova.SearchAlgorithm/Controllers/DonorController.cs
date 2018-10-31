db6bdd5284bfface85bd3480603c0576ce2e8424
Ben Below <benjamin.below@softwire.com>
2018-10-31 15:53:52 +0000

    NOVA-2122: Remove existing donor endpoints
    I believe they're unused, and the data they retrieve is easily accessible directly from sql if we need it for debugging

--- Nova.SearchAlgorithm/Controllers/DonorController.cs
+++ Nova.SearchAlgorithm/Controllers/DonorController.cs
@@ -7,36 +7,11 @@ using Nova.Utils.Http.Exceptions;
 
 namespace Nova.SearchAlgorithm.Controllers
 {
-    /// <summary>
-    /// Endpoints to expose the donor data we are storing, for debugging and verification.
-    /// </summary>
     [RoutePrefix("donor")]
     public class DonorController : ApiController
     {
-        private readonly IDonorInspectionRepository donorRepository;
-
-        public DonorController(IDonorInspectionRepository donorRepository)
+        public DonorController()
         {
-            this.donorRepository = donorRepository;
-        }
-
-        [HttpGet]
-        [Route("{id}")]
-        public Task<DonorResult> GetDonor(int id)
-        {
-            var donor = donorRepository.GetDonor(id);
-            if (donor == null)
-            {
-                throw new NovaNotFoundException($"No donor available with ID {id}");
-            }
-            return donor;
-        }
-
-        [HttpGet]
-        [Route("highest-donor-id")]
-        public Task<int> GetHighestDonorId()
-        {
-            return donorRepository.HighestDonorId();
         }
     }
 }
