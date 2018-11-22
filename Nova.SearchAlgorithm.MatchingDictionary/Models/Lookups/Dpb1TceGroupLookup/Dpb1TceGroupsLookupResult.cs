a6d816dba23d99e2523309e05590089d783f58c7
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2018-11-22 15:57:30 +0000

    NOVA-1301: Removed redundant IEquatable<T>.

--- Nova.SearchAlgorithm.MatchingDictionary/Models/Lookups/Dpb1TceGroupLookup/Dpb1TceGroupsLookupResult.cs
+++ Nova.SearchAlgorithm.MatchingDictionary/Models/Lookups/Dpb1TceGroupLookup/Dpb1TceGroupsLookupResult.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Nova.SearchAlgorithm.MatchingDictionary.Models.HLATypings;
+﻿using Nova.SearchAlgorithm.MatchingDictionary.Models.HLATypings;
 using Nova.SearchAlgorithm.MatchingDictionary.Repositories.AzureStorage;
 
 namespace Nova.SearchAlgorithm.MatchingDictionary.Models.Lookups.Dpb1TceGroupLookup
@@ -9,7 +8,7 @@ namespace Nova.SearchAlgorithm.MatchingDictionary.Models.Lookups.Dpb1TceGroupLoo
         string TceGroup { get; }
     }
 
-    public class Dpb1TceGroupsLookupResult : IEquatable<IDpb1TceGroupsLookupResult>, IDpb1TceGroupsLookupResult
+    public class Dpb1TceGroupsLookupResult : IDpb1TceGroupsLookupResult
     {
         public MatchLocus MatchLocus => MatchLocus.Dpb1;
         public string LookupName { get; }
@@ -28,27 +27,6 @@ namespace Nova.SearchAlgorithm.MatchingDictionary.Models.Lookups.Dpb1TceGroupLoo
         public HlaLookupTableEntity ConvertToTableEntity()
         {
             return new HlaLookupTableEntity(this);
-        }
-
-        public bool Equals(IDpb1TceGroupsLookupResult other)
-        {
-            return string.Equals(LookupName, other.LookupName) && string.Equals(TceGroup, other.TceGroup);
-        }
-
-        public override bool Equals(object obj)
-        {
-            if (ReferenceEquals(null, obj)) return false;
-            if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != this.GetType()) return false;
-            return Equals((IDpb1TceGroupsLookupResult) obj);
-        }
-
-        public override int GetHashCode()
-        {
-            unchecked
-            {
-                return (LookupName.GetHashCode() * 397) ^ TceGroup.GetHashCode();
-            }
         }
     }
 }
