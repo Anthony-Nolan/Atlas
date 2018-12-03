2cfcc24607a4c2996cc7dff8724bb6a9d7ec22f5
Ben Below <benjamin.below@softwire.com>
2018-12-03 13:32:45 +0000

    NOVA-1648: Apply validation to serach hla

--- Nova.SearchAlgorithm/Config/ValidatorConfig.cs
+++ Nova.SearchAlgorithm/Config/ValidatorConfig.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Linq;
 using System.Web.Http;
-using Autofac;
 using FluentValidation;
 using FluentValidation.WebApi;
 using Nova.Utils.Common;
@@ -10,7 +9,7 @@ namespace Nova.SearchAlgorithm.Config
 {
     public static class ValidatorConfig
     {
-        public static void ConfigureValidation(this HttpConfiguration config, IContainer container)
+        public static void ConfigureValidation(this HttpConfiguration config)
         {
             FluentValidationModelValidatorProvider.Configure(config);
 
@@ -18,8 +17,7 @@ namespace Nova.SearchAlgorithm.Config
             ValidatorOptions.PropertyNameResolver = (type, memberInfo, expression) =>
             {
                 var sep = ValidatorOptions.PropertyChainSeparator;
-                var nameParts = baseResolver(type, memberInfo, expression)
-                    .Split(new[] { sep }, StringSplitOptions.RemoveEmptyEntries);
+                var nameParts = baseResolver(type, memberInfo, expression).Split(new[] { sep }, StringSplitOptions.RemoveEmptyEntries);
                 // Convert the default name (which uses TitleCase property names) to camelCase.
                 // If the name has multiple parts, each one needs to be camelCased.
                 return string.Join(sep, nameParts.Select(n => n.ToCamelCase()));
