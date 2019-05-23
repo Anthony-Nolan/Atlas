8d6b54cf5a36f1343935cf4dafea29f0ae0324e3
Hereward Mills <hereward.mills@softwire.com>
2019-05-23 19:53:23 +0100

    NOVA-NONE: Resolve a todo and add the autogenerated versions.tf

--- terraform/app_service.tf
+++ terraform/app_service.tf
@@ -47,15 +47,7 @@ resource "azurerm_app_service" "search_algorithm" {
     min_tls_version = local.min_tls_version
 
     cors {
-      # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
-      # force an interpolation expression to be interpreted as a list by wrapping it
-      # in an extra set of list brackets. That form was supported for compatibilty in
-      # v0.11, but is no longer supported in Terraform v0.12.
-      #
-      # If the expression in the following list itself returns a list, remove the
-      # brackets to avoid interpretation as a list of lists. If the expression
-      # returns a single list item then leave it as-is and remove this TODO comment.
-      allowed_origins = [local.cors_urls]
+      allowed_origins = local.cors_urls
     }
   }
 
