23487e74fc7d35c3f9fdd28ebc6229b259c675f6
Ben Below <benjamin.below@softwire.com>
2019-06-13 18:00:27 +0100

    NOVA-NONE: Use executing assembly file location, not test location
    in devops, the latter assumes prefixes of /{Configuration}/{framework} which have been removed.

--- Nova.SearchAlgorithm.Test/MatchingDictionary/Data/WmdaTestFileImporter.cs
+++ Nova.SearchAlgorithm.Test/MatchingDictionary/Data/WmdaTestFileImporter.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using Nova.SearchAlgorithm.MatchingDictionary.Data;
 using NUnit.Framework;
 
@@ -8,7 +9,7 @@ namespace Nova.SearchAlgorithm.Test.MatchingDictionary.Data
 {
     public class WmdaTestFileImporter : IWmdaFileReader
     {
-        private static readonly string TestDir = TestContext.CurrentContext.TestDirectory;
+        private static readonly string TestDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
         private const string FilePath = "\\MatchingDictionary\\Data\\wmda-v";
 
         public IEnumerable<string> GetFileContentsWithoutHeader(string hlaDatabaseVersion, string fileName)
