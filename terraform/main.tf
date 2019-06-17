ae319253c54b9f1a82a69ea8a7acb422b00c1b59
Ben Below <benjamin.below@softwire.com>
2019-06-17 14:01:32 +0100

    NOVA-3671: Split out remote states terraform file

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,42 +7,6 @@ terraform {
   }
 }
 
-data "terraform_remote_state" "nova_core" {
-  backend   = "azurerm"
-  workspace = terraform.workspace
-
-  config = {
-    storage_account_name = "novaterraform"
-    container_name       = "terraform-state"
-    key                  = "core.terraform.tfstate"
-    resource_group_name  = "AN-RESOURCE-GROUP"
-  }
-}
-
-data "terraform_remote_state" "hla" {
-  backend   = "azurerm"
-  workspace = terraform.workspace
-
-  config = {
-    storage_account_name = "novaterraform"
-    container_name       = "terraform-state"
-    key                  = "hla.terraform.tfstate"
-    resource_group_name  = "AN-RESOURCE-GROUP"
-  }
-}
-
-data "terraform_remote_state" "donor" {
-  backend   = "azurerm"
-  workspace = terraform.workspace
-
-  config = {
-    storage_account_name = "novaterraform"
-    container_name       = "terraform-state"
-    key                  = "donor.terraform.tfstate"
-    resource_group_name  = "AN-RESOURCE-GROUP"
-  }
-}
-
 locals {
   repository_name                    = "Nova.SearchAlgorithm"
   environment                        = data.terraform_remote_state.nova_core.outputs.general.environment
