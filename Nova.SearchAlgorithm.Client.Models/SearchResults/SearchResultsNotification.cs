0dec3b9cb4f59d80da2be57ea0ac40c268607d24
Ben Below <benjamin.below@softwire.com>
2019-07-17 16:53:48 +0100

    NOVA-3566: Return wmda version with results notification

--- Nova.SearchAlgorithm.Client.Models/SearchResults/SearchResultsNotification.cs
+++ Nova.SearchAlgorithm.Client.Models/SearchResults/SearchResultsNotification.cs
@@ -9,6 +9,10 @@ namespace Nova.SearchAlgorithm.Client.Models.SearchResults
         /// The version of the deployed search algorithm that ran the search request
         /// </summary>
         public string SearchAlgorithmServiceVersion { get; set; }
+        /// <summary>
+        /// The version of the hla database used to run the search request - used for analysing both donor and patient hla.
+        /// </summary>
+        public string WmdaHlaDatabaseVersion { get; set; }
         public string BlobStorageContainerName { get; set; }
         public long SearchTimeInMilliseconds { get; set; }
     }
