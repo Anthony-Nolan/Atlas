e8c68abb7aea1f6947f53fb550a1755c408cf4e3
Ben Below <benjamin.below@softwire.com>
2019-07-31 11:25:17 +0100

    NOVA-NONE: Add all azure database tiers available from azure portal
    Means any database scaling tweaks can only be app setting changes and not require code changes.

--- Nova.SearchAlgorithm/Clients/AzureManagement/Extensions/AzureDatabaseSizeExtensions.cs
+++ Nova.SearchAlgorithm/Clients/AzureManagement/Extensions/AzureDatabaseSizeExtensions.cs
@@ -6,6 +6,10 @@ namespace Nova.SearchAlgorithm.Clients.AzureManagement.Extensions
 {
     public static class AzureDatabaseSizeExtensions
     {
+        private const string BasicTier = "Basic";
+        private const string StandardTier = "Standard";
+        private const string PremiumTier = "Premium";
+
         public static string ToAzureApiUpdateBody(this AzureDatabaseSize databaseSize)
         {
             string tier;
@@ -13,24 +17,68 @@ namespace Nova.SearchAlgorithm.Clients.AzureManagement.Extensions
 
             switch (databaseSize)
             {
+                case AzureDatabaseSize.Basic:
+                    tier = BasicTier;
+                    break;
                 case AzureDatabaseSize.S0:
-                    tier = "Standard";
+                    tier = StandardTier;
                     capacity = 10;
                     break;
+                case AzureDatabaseSize.S1:
+                    tier = StandardTier;
+                    capacity = 20;
+                    break;
+                case AzureDatabaseSize.S2:
+                    tier = StandardTier;
+                    capacity = 50;
+                    break;
                 case AzureDatabaseSize.S3:
-                    tier = "Standard";
+                    tier = StandardTier;
                     capacity = 100;
                     break;
                 case AzureDatabaseSize.S4:
-                    tier = "Standard";
+                    tier = StandardTier;
                     capacity = 200;
                     break;
-                case AzureDatabaseSize.P15:
-                    tier = "Premium";
-                    capacity = 4000;
+                case AzureDatabaseSize.S6:
+                    tier = StandardTier;
+                    capacity = 400;
                     break;
-                case AzureDatabaseSize.Basic:
-                    tier = "Basic";
+                case AzureDatabaseSize.S7:
+                    tier = StandardTier;
+                    capacity = 800;
+                    break;
+                case AzureDatabaseSize.S9:
+                    tier = StandardTier;
+                    capacity = 1600;
+                    break;
+                case AzureDatabaseSize.S12:
+                    tier = StandardTier;
+                    capacity = 3000;
+                    break;
+                case AzureDatabaseSize.P1:
+                    tier = PremiumTier;
+                    capacity = 125;
+                    break;
+                case AzureDatabaseSize.P2:
+                    tier = PremiumTier;
+                    capacity = 250;
+                    break;
+                case AzureDatabaseSize.P4:
+                    tier = PremiumTier;
+                    capacity = 500;
+                    break;
+                case AzureDatabaseSize.P6:
+                    tier = PremiumTier;
+                    capacity = 1000;
+                    break;
+                case AzureDatabaseSize.P11:
+                    tier = PremiumTier;
+                    capacity = 1750;
+                    break;
+                case AzureDatabaseSize.P15:
+                    tier = PremiumTier;
+                    capacity = 4000;
                     break;
                 default:
                     throw new ArgumentOutOfRangeException(nameof(databaseSize), databaseSize, null);
