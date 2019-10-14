0f81803452817489b5a8d1add405813ee1fdeb6d
Ben Below <benjamin.below@softwire.com>
2019-10-14 17:58:56 +0100

    NOVA-4563: Move Permissive Mismatch check within switch statement

--- Nova.SearchAlgorithm/Services/Search/Scoring/Categorisation/MatchCategoriser.cs
+++ Nova.SearchAlgorithm/Services/Search/Scoring/Categorisation/MatchCategoriser.cs
@@ -10,22 +10,11 @@ namespace Nova.SearchAlgorithm.Services.Search.Scoring.Categorisation
         public static MatchCategory CategoriseMatch(ScoreResult scoreResult)
         {
             var overallMatchConfidence = scoreResult.OverallMatchConfidence;
-
-            // The only way to have an overall confidence of mismatch with no per-position mismatch is for all mismatches to be permissive.
-            if (overallMatchConfidence == MatchConfidence.Mismatch && scoreResult.AllGrades.All(g => g != MatchGrade.Mismatch))
-            {
-                return MatchCategory.PermissiveMismatch;
-            }
-
-            return MapConfidenceToCategory(overallMatchConfidence);
-        }
-
-        private static MatchCategory MapConfidenceToCategory(MatchConfidence matchConfidence)
-        {
-            switch (matchConfidence)
+            switch (overallMatchConfidence)
             {
                 case MatchConfidence.Mismatch:
-                    return MatchCategory.Mismatch;
+                    // The only way to have an overall confidence of mismatch with no per-position mismatch is for all mismatches to be permissive.
+                    return scoreResult.AllGrades.All(g => g != MatchGrade.Mismatch) ? MatchCategory.PermissiveMismatch : MatchCategory.Mismatch;
                 case MatchConfidence.Potential:
                     return MatchCategory.Potential;
                 case MatchConfidence.Exact:
@@ -33,7 +22,7 @@ namespace Nova.SearchAlgorithm.Services.Search.Scoring.Categorisation
                 case MatchConfidence.Definite:
                     return MatchCategory.Definite;
                 default:
-                    throw new ArgumentOutOfRangeException(nameof(matchConfidence), matchConfidence, null);
+                    throw new ArgumentOutOfRangeException(nameof(overallMatchConfidence), overallMatchConfidence, null);
             }
         }
     }
