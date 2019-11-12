4c07558bc33a8737ae6d3207453eae55eed4f27a
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2019-11-12 14:54:34 +0000

    NOVA-4859: Validation applied to mismatch criteria & all non-HLA rules covered by integration tests.

--- Nova.SearchAlgorithm/Validators/MismatchCriteriaValidator.cs
+++ Nova.SearchAlgorithm/Validators/MismatchCriteriaValidator.cs
@@ -7,9 +7,11 @@ namespace Nova.SearchAlgorithm.Validators
     {
         public MismatchCriteriaValidator()
         {
-            RuleFor(x => x.LocusMismatchA).NotNull();
-            RuleFor(x => x.LocusMismatchB).NotNull();
-            RuleFor(x => x.LocusMismatchDrb1).NotNull();
+            RuleFor(x => x.LocusMismatchA).NotEmpty().SetValidator(new LocusMismatchCriteriaValidator());
+            RuleFor(x => x.LocusMismatchB).NotEmpty().SetValidator(new LocusMismatchCriteriaValidator());
+            RuleFor(x => x.LocusMismatchDrb1).NotEmpty().SetValidator(new LocusMismatchCriteriaValidator());
+            RuleFor(x => x.LocusMismatchC).SetValidator(new LocusMismatchCriteriaValidator());
+            RuleFor(x => x.LocusMismatchDqb1).SetValidator(new LocusMismatchCriteriaValidator());
             RuleFor(x => x.DonorMismatchCount).NotNull().GreaterThanOrEqualTo(0).LessThanOrEqualTo(4);
         }
     }
--- Nova.SearchAlgorithm/Validators/SearchRequestValidator.cs
+++ Nova.SearchAlgorithm/Validators/SearchRequestValidator.cs
@@ -10,16 +10,16 @@ namespace Nova.SearchAlgorithm.Validators
         public SearchRequestValidator()
         {
             RuleFor(x => x.SearchType).NotEmpty().IsInEnum();
-            RuleFor(x => x.MatchCriteria).NotEmpty();
+            RuleFor(x => x.MatchCriteria).NotEmpty().SetValidator(new MismatchCriteriaValidator());
             RuleFor(x => x.SearchHlaData).NotEmpty().SetValidator(new SearchHlaDataValidator());
             RuleFor(x => x.SearchHlaData.LocusSearchHlaC).NotEmpty().When(x => x.MatchCriteria?.LocusMismatchC != null);
             RuleFor(x => x.SearchHlaData.LocusSearchHlaDqb1).NotEmpty().When(x => x.MatchCriteria?.LocusMismatchDqb1 != null);
             RuleFor(x => x.RegistriesToSearch).NotEmpty();
+            RuleForEach(x => x.RegistriesToSearch).IsInEnum();
             RuleFor(x => x.LociToExcludeFromAggregateScore)
                 .NotNull()
                 .Must(loci => loci?.All(l => l.ToAlgorithmLocus() != null) ?? false)
-                .WithMessage("Only valid search loci can be excluded from aggregation. They are: A, B, C, Dpb1, Dqb1. Drb1");
-            RuleForEach(x => x.RegistriesToSearch).IsInEnum();
+                .WithMessage("Only valid search loci can be excluded from aggregation. They are: A, B, C, Dpb1, Dqb1, Drb1");
         }
     }
 }
