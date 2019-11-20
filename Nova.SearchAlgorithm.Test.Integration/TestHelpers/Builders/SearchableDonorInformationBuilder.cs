cd264d29741091ccc6e30e465a7d9724dadd9a79
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2019-11-20 11:09:18 +0000

    NOVA-4802: Refactor builder.

--- Nova.SearchAlgorithm.Test.Integration/TestHelpers/Builders/SearchableDonorInformationBuilder.cs
+++ Nova.SearchAlgorithm.Test.Integration/TestHelpers/Builders/SearchableDonorInformationBuilder.cs
@@ -10,21 +10,16 @@ namespace Nova.SearchAlgorithm.Test.Integration.TestHelpers.Builders
         private const string DefaultDonorType = "A";
         private const string DefaultRegistryCode = "DKMS";
 
-        public static Builder<SearchableDonorInformation> New
-        {
-            get
-            {
-                return Builder<SearchableDonorInformation>.New
-                    .With(x => x.DonorId, DonorIdGenerator.NextId())
-                    .With(x => x.DonorType, DefaultDonorType)
-                    .With(x => x.RegistryCode, DefaultRegistryCode)
-                    .With(x => x.A_1, DefaultHlaName)
-                    .With(x => x.A_2, DefaultHlaName)
-                    .With(x => x.B_1, DefaultHlaName)
-                    .With(x => x.B_2, DefaultHlaName)
-                    .With(x => x.DRB1_1, DefaultHlaName)
-                    .With(x => x.DRB1_2, DefaultHlaName);
-            }
-        }
+        public static Builder<SearchableDonorInformation> New =>
+            Builder<SearchableDonorInformation>.New
+                .With(x => x.DonorId, DonorIdGenerator.NextId())
+                .With(x => x.DonorType, DefaultDonorType)
+                .With(x => x.RegistryCode, DefaultRegistryCode)
+                .With(x => x.A_1, DefaultHlaName)
+                .With(x => x.A_2, DefaultHlaName)
+                .With(x => x.B_1, DefaultHlaName)
+                .With(x => x.B_2, DefaultHlaName)
+                .With(x => x.DRB1_1, DefaultHlaName)
+                .With(x => x.DRB1_2, DefaultHlaName);
     }
 }
