7f0f262eaa3373f6a66ff1bbd81715f8d54a9cd8
Mike.McLean <Mike.McLean@softwire.com>
2020-04-21 15:50:36 +0100

    review: ATLAS-128: Review Markups

--- MiscTestingAndDebuggingResources/MatchingAlgorithm/InitialRefreshData.sql
+++ MiscTestingAndDebuggingResources/MatchingAlgorithm/InitialRefreshData.sql
@@ -9,7 +9,7 @@ INSERT INTO DataRefreshHistory (
      VALUES (
             GETDATE()-1, GETDATE(),
             'DatabaseA',
-            '3390',
+            '3390', -- Note that the Hardcoded test data is v3.3.0; but we don't expect this to cause problems in practice.
             1)
 GO
 
