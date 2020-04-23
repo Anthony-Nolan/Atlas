b63523a40da25007276dd9c0c95f6027c0d9cbe9
Ben Below <benjamin.below@softwire.com>
2020-04-23 13:13:59 +0100

    fix: ATLAS-135: In validation tests, do not attempt to delete existing data when database does not yet exist

--- Atlas.MatchingAlgorithm.Test.Validation/TestData/Repositories/TestDataRepository.cs
+++ Atlas.MatchingAlgorithm.Test.Validation/TestData/Repositories/TestDataRepository.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System;
+using Microsoft.EntityFrameworkCore;
 using Atlas.MatchingAlgorithm.Data.Context;
 using Atlas.MatchingAlgorithm.Data.Models.Entities;
 using Atlas.MatchingAlgorithm.Data.Persistent;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
 
 namespace Atlas.MatchingAlgorithm.Test.Validation.TestData.Repositories
 {
@@ -66,7 +69,7 @@ namespace Atlas.MatchingAlgorithm.Test.Validation.TestData.Repositories
 
         private void RemoveTestData()
         {
-            if (DoesDonorTableExist())
+            if (TransientDatabaseExists() && DonorTableExists())
             {
                 context.Database.ExecuteSqlCommand("TRUNCATE TABLE [Donors]");
                 context.Database.ExecuteSqlCommand("TRUNCATE TABLE [MatchingHlaAtA]");
@@ -78,7 +81,7 @@ namespace Atlas.MatchingAlgorithm.Test.Validation.TestData.Repositories
             }
         }
 
-        private bool DoesDonorTableExist()
+        private bool DonorTableExists()
         {
             var conn = context.Database.GetDbConnection();
             if (conn.State.Equals(ConnectionState.Closed))
@@ -94,6 +97,11 @@ namespace Atlas.MatchingAlgorithm.Test.Validation.TestData.Repositories
     WHERE T.Name = 'Donors'";
                 return command.ExecuteScalar() != null;
             }
+        }
+
+        private bool TransientDatabaseExists()
+        {
+            return (context.Database.GetService<IDatabaseCreator>() as RelationalDatabaseCreator)?.Exists() ?? false;
         }
     }
 }
