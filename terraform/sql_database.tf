558108560c5e6f393b04b2a5757ef22daeb7716a
Josh Powell <josh.powell@softwire.com>
2020-04-24 15:48:11 +0100

    ATLAS-126: add auto connection strings

--- terraform/sql_database.tf
+++ terraform/sql_database.tf
@@ -1,23 +1,23 @@
 resource "azurerm_sql_database" "atlas-data-refresh-a" {
-  name = "${local.environment}-ATLAS-DATA_REFRESH-DB-A"
+  name = lower("${local.environment}-ATLAS-DATA_REFRESH-DB-A")
   resource_group_name = azurerm_resource_group.atlas_resource_group.name
   location = local.location
   server_name = azurerm_sql_server.atlas_sql_server.name
-  tags = local.common.tags
+  tags = local.common_tags
 }
 
 resource "azurerm_sql_database" "atlas-data-refresh-b" {
-  name = "${local.environment}-ATLAS-DATA_REFRESH-DB-B"
+  name = lower("${local.environment}-ATLAS-DATA_REFRESH-DB-B")
   resource_group_name = azurerm_resource_group.atlas_resource_group.name
   location = local.location
   server_name = azurerm_sql_server.atlas_sql_server.name
-  tags = local.common.tags
+  tags = local.common_tags
 }
 
 resource "azurerm_sql_database" "atlas-persistent" {
-  name = "${local.environment}-ATLAS-PERSISTENT-DB"
+  name = lower("${local.environment}-ATLAS-PERSISTENT-DB")
   resource_group_name = azurerm_resource_group.atlas_resource_group.name
   location = local.location
   server_name = azurerm_sql_server.atlas_sql_server.name
-  tags = local.common.tags
+  tags = local.common_tags
 }
