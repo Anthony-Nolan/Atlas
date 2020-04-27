1d930ea93c3e8a8a966ff61794aa936e6cd444bf
Tom Johnson <tom.johnson@softwire.com>
2020-04-27 10:16:14 +0100

    chore: ATLAS-51: Porting over all utils core dependencies
    chore: ATLAS-51: Moved files to correct namespace
    
    
    
    
    chore: ATLAS-51: Added Utils tests
    
    
    fix: ATLAS-51: Fixed failing tests from port
    
    
    chore: ATLAS-118: Ported over all utils storage

---
+++ Atlas.Utils/Storage/ApplicationInsights/AzureStorageEventModel.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using Atlas.Utils.Core.ApplicationInsights;
+using Atlas.Utils.Core.ApplicationInsights.EventModels;
+
+namespace Atlas.Utils.Core.Storage.ApplicationInsights
+{
+    class AzureStorageEventModel : EventModel
+    {
+        private readonly Stopwatch azureStorageUploadTimer;
+
+        public AzureStorageEventModel(string filename, string container)
+            : base("Azure Storage")
+        {
+            azureStorageUploadTimer = new Stopwatch();
+            Properties.Add("Filename", filename);
+            Properties.Add("Container", container);
+            Level = LogLevel.Verbose;
+        }
+
+        public AzureStorageEventModel(string filename)
+            : base("Azure Storage")
+        {
+            azureStorageUploadTimer = new Stopwatch();
+            Properties.Add("Filename", filename);
+            Level = LogLevel.Verbose;
+        }
+
+        public void StartAzureStorageCommunication()
+        {
+            azureStorageUploadTimer.Start();
+        }
+
+        public void EndAzureStorageCommunication(string action)
+        {
+            azureStorageUploadTimer.Stop();
+            Metrics.Add($"Azure Storage - {action} - duration /ms", azureStorageUploadTimer.ElapsedMilliseconds);
+        }
+    }
+}
---
+++ Atlas.Utils/Storage/ClaimCheck/ClaimCheckConstants.cs
@@ -0,0 +1,7 @@
+﻿namespace Atlas.Utils.Core.Storage.ClaimCheck
+{
+    public static class ClaimCheckConstants
+    {
+        public const string IsCheckedPropertyKey = "is-claim-checked";
+    }
+}
