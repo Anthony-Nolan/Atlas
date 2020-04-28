a9a8c70ff8ac502b3ad1b667ab2e7baea2a82d12
Ben Below <benjamin.below@softwire.com>
2020-04-28 15:36:41 +0100

    chore: order variables alphabetically
    Variables will be shown alphabetically in both Azure Devops and Azure, so this makes the list easier to cross-reference

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,20 +1,36 @@
-variable LOCATION {
-  type    = string
-  default = "uksouth"
+variable "APIKEY" {
+  type = string
 }
 
-variable ENVIRONMENT {
+variable "APPLICATION_INSIGHTS_LOG_LEVEL" {
   type    = string
+  default = "Info"
 }
 
-variable "FUNCTION_STORAGE_TIER" {
+variable "ATLAS_DATABASE_SERVER_ADMIN_LOGIN" {
   type    = string
-  default = "Standard"
+  default = "atlas-admin"
 }
 
-variable "FUNCTION_STORAGE_REPLICATION_TYPE" {
-  type    = string
-  default = "LRS"
+variable "ATLAS_DATABASE_SERVER_LOGIN_PASSWORD" {
+  type = string
+}
+
+variable "ATLAS_MATCHING_DATABASE_USERNAME" {
+  type    = "string"
+  default = "atlas-matching"
+}
+
+variable "ATLAS_MATCHING_DATABASE_PASSWORD" {
+  type = "string"
+}
+
+variable "AZURE_CLIENT_ID" {
+  type = string
+}
+
+variable "AZURE_CLIENT_SECRET" {
+  type = string
 }
 
 variable "AZURE_STORAGE_TIER" {
@@ -25,34 +41,6 @@ variable "AZURE_STORAGE_TIER" {
 variable "AZURE_STORAGE_REPLICATION_TYPE" {
   type    = string
   default = "LRS"
-}
-
-variable "SERVICE_PLAN_SKU" {
-  type    = object({
-    tier = string,
-    size = string
-  })
-  default = {
-    tier = "Standard"
-    size = "S1"
-  }
-}
-
-variable "APIKEY" {
-  type = string
-}
-
-variable "APPLICATION_INSIGHTS_LOG_LEVEL" {
-  type    = string
-  default = "Info"
-}
-
-variable "AZURE_CLIENT_ID" {
-  type = string
-}
-
-variable "AZURE_CLIENT_SECRET" {
-  type = string
 }
 
 variable "DATA_REFRESH_DB_SIZE_ACTIVE" {
@@ -73,15 +61,6 @@ variable "DATA_REFRESH_DB_SIZE_REFRESH" {
 variable "DATA_REFRESH_CRONTAB" {
   type    = string
   default = "0 0 0 * * Monday"
-}
-
-variable "ATLAS_DATABASE_SERVER_ADMIN_LOGIN" {
-  type = string
-  default = "atlas-admin"
-}
-
-variable "ATLAS_DATABASE_SERVER_LOGIN_PASSWORD" {
-  type = string
 }
 
 variable "DATA_REFRESH_DONOR_IMPORT_FUNCTION_NAME" {
@@ -113,6 +92,24 @@ variable "DONOR_SERVICE_BASEURL" {
   type = string
 }
 
+variable ENVIRONMENT {
+  type = string
+}
+
+variable "FUNCTION_APP_SUBSCRIPTION_ID" {
+  type = string
+}
+
+variable "FUNCTION_STORAGE_TIER" {
+  type    = string
+  default = "Standard"
+}
+
+variable "FUNCTION_STORAGE_REPLICATION_TYPE" {
+  type    = string
+  default = "LRS"
+}
+
 variable "HLA_SERVICE_APIKEY" {
   type = string
 }
@@ -121,8 +118,9 @@ variable "HLA_SERVICE_BASEURL" {
   type = string
 }
 
-variable "FUNCTION_APP_SUBSCRIPTION_ID" {
-  type = string
+variable LOCATION {
+  type    = string
+  default = "uksouth"
 }
 
 variable "MESSAGING_BUS_CONNECTION_STRING" {
@@ -130,12 +128,12 @@ variable "MESSAGING_BUS_CONNECTION_STRING" {
 }
 
 variable "MESSAGING_BUS_DONOR_BATCH_SIZE" {
-  type = number
+  type    = number
   default = 350
 }
 
 variable "MESSAGING_BUS_DONOR_CRON_SCHEDULE" {
-  type = string
+  type    = string
   default = "0 */1 * * * *"
 }
 
@@ -173,6 +171,17 @@ variable "NOTIFICATIONS_BUS_NOTIFICATIONS_TOPIC" {
   default = "support-notifications"
 }
 
+variable "SERVICE_PLAN_SKU" {
+  type    = object({
+    tier = string,
+    size = string
+  })
+  default = {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
 variable "WMDA_FILE_URL" {
   type    = string
   default = "https://raw.githubusercontent.com/ANHIG/IMGTHLA/"
@@ -181,13 +190,4 @@ variable "WMDA_FILE_URL" {
 variable "WEBSITE_RUN_FROM_PACKAGE" {
   type    = string
   default = "1"
-}
-
-variable "ATLAS_MATCHING_DATABASE_USERNAME" {
-  type    = "string"
-  default = "atlas-matching"
-}
-
-variable "ATLAS_MATCHING_DATABASE_PASSWORD" {
-  type    = "string"
 }
