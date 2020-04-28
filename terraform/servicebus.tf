4ef1c2ca0586ee9b7618e73a806b0c93d5babf45
Mike.McLean <Mike.McLean@softwire.com>
2020-04-28 17:14:29 +0100

    feature: ATLAS-124: Second attempt; general markups, aligning, local variables, environment dependency.

--- terraform/servicebus.tf
+++ terraform/servicebus.tf
@@ -1,17 +1,27 @@
-// Three SB "Namespaces". All identical except name.
+
+locals {
+  default-sku                    = "Standard"
+  long-expiry                    = "P9999D" // 2.75 years
+  audit-subscription-idle-delete = "P8D"
+  default-read-lock              = "PT5M"
+  default-bus-size               = 5120 // 5GB
+  default-message-retries        = 10
+}
+
+// Three ServiceBus "Namespaces". All identical except name.
 // "Standard Tier" is required for Topics.
 resource "azurerm_servicebus_namespace" "general" {
-  name                = "dev-atlas-general"
+  name                = "${local.environment}-atlas-general"
   location            = azurerm_resource_group.atlas_resource_group.location
   resource_group_name = azurerm_resource_group.atlas_resource_group.name
-  sku                 = "Standard"
+  sku                 = locals.default-sku
 }
 
 resource "azurerm_servicebus_namespace" "notifications" {
-  name                = "dev-atlas-notifications"
+  name                = "${local.environment}-atlas-notifications"
   location            = azurerm_resource_group.atlas_resource_group.location
   resource_group_name = azurerm_resource_group.atlas_resource_group.name
-  sku                 = "Standard"
+  sku                 = locals.default-sku
 }
 
 // Topics for Notifications, Results, and Donor Updates; Queue for Searches.
@@ -20,111 +30,111 @@ resource "azurerm_servicebus_namespace" "notifications" {
 //   support_ordering for donor updates.
 
 resource "azurerm_servicebus_topic" "notifications" {
-  name                = "notifications"
-  resource_group_name = azurerm_resource_group.atlas_resource_group.name
-  namespace_name      = azurerm_servicebus_namespace.notifications.name
-  auto_delete_on_idle = "P9999D" //2.75 years
-  default_message_ttl = "P9999D" //2.75 years
-  max_size_in_megabytes = 5120   //5 GB
+  name                  = "notifications"
+  resource_group_name   = azurerm_resource_group.atlas_resource_group.name
+  namespace_name        = azurerm_servicebus_namespace.notifications.name
+  auto_delete_on_idle   = locals.long-expiry
+  default_message_ttl   = locals.long-expiry
+  max_size_in_megabytes = locals.default-bus-size
 }
 
 resource "azurerm_servicebus_topic" "alerts" {
-  name                = "alerts"
-  resource_group_name = azurerm_resource_group.atlas_resource_group.name
-  namespace_name      = azurerm_servicebus_namespace.notifications.name
-  auto_delete_on_idle = "P9999D" //2.75 years
-  default_message_ttl = "P9999D" //2.75 years
-  max_size_in_megabytes = 5120   //5 GB
+  name                  = "alerts"
+  resource_group_name   = azurerm_resource_group.atlas_resource_group.name
+  namespace_name        = azurerm_servicebus_namespace.notifications.name
+  auto_delete_on_idle   = locals.long-expiry
+  default_message_ttl   = locals.long-expiry
+  max_size_in_megabytes = locals.default-bus-size
 }
 
 resource "azurerm_servicebus_topic" "matching-results-ready" {
-  name                = "matching-results-ready"
-  resource_group_name = azurerm_resource_group.atlas_resource_group.name
-  namespace_name      = azurerm_servicebus_namespace.general.name
-  auto_delete_on_idle = "P9999D" //2.75 years
-  default_message_ttl = "P9999D" //2.75 years
-  max_size_in_megabytes = 5120   //5 GB
-  enable_express      = true
+  name                  = "matching-results-ready"
+  resource_group_name   = azurerm_resource_group.atlas_resource_group.name
+  namespace_name        = azurerm_servicebus_namespace.general.name
+  auto_delete_on_idle   = locals.long-expiry
+  default_message_ttl   = locals.long-expiry
+  max_size_in_megabytes = locals.default-bus-size
+  support_ordering      = true
 }
 
 resource "azurerm_servicebus_topic" "updated-searchable-donors" {
-  name                = "updated-searchable-donors"
-  resource_group_name = azurerm_resource_group.atlas_resource_group.name
-  namespace_name      = azurerm_servicebus_namespace.general.name
-  auto_delete_on_idle = "P9999D" //2.75 years
-  default_message_ttl = "P9999D" //2.75 years
-  max_size_in_megabytes = 5120   //5 GB
-  support_ordering    = true
+  name                  = "updated-searchable-donors"
+  resource_group_name   = azurerm_resource_group.atlas_resource_group.name
+  namespace_name        = azurerm_servicebus_namespace.general.name
+  auto_delete_on_idle   = locals.long-expiry
+  default_message_ttl   = locals.long-expiry
+  max_size_in_megabytes = locals.default-bus-size
+  support_ordering      = true
 }
 
 resource "azurerm_servicebus_subscription" "donor-update" {
-  name                = "donor-update"
-  resource_group_name = azurerm_resource_group.atlas_resource_group.name
-  namespace_name      = azurerm_servicebus_namespace.general.name
-  topic_name          = azurerm_servicebus_topic.updated-searchable-donors.name
-  auto_delete_on_idle = "P9999D" //2.75 years
-  default_message_ttl = "P9999D" //2.75 years
-  lock_duration       = "PT5M"
-  max_delivery_count  = 10
+  name                                 = "donor-update"
+  resource_group_name                  = azurerm_resource_group.atlas_resource_group.name
+  namespace_name                       = azurerm_servicebus_namespace.general.name
+  topic_name                           = azurerm_servicebus_topic.updated-searchable-donors.name
+  auto_delete_on_idle                  = locals.long-expiry
+  default_message_ttl                  = locals.long-expiry
+  lock_duration                        = locals.default-read-lock
+  max_delivery_count                   = locals.default-message-retries
   dead_lettering_on_message_expiration = true
 }
 
 resource "azurerm_servicebus_queue" "matching-requests" {
-  name                = "matching-requests"
-  resource_group_name = azurerm_resource_group.atlas_resource_group.name
-  namespace_name      = azurerm_servicebus_namespace.general.name
-  lock_duration       = "PT5M"
-  max_delivery_count  = 10
+  name                                 = "matching-requests"
+  resource_group_name                  = azurerm_resource_group.atlas_resource_group.name
+  namespace_name                       = azurerm_servicebus_namespace.general.name
+  lock_duration                        = locals.default-read-lock
+  max_delivery_count                   = locals.default-message-retries
   dead_lettering_on_message_expiration = false
 }
 
 // Add audit subscriptions to all the Topics.
 // Identical settings
 resource "azurerm_servicebus_subscription" "audit-notifications" {
-  name                = "audit"
-  resource_group_name = azurerm_resource_group.atlas_resource_group.name
-  namespace_name      = azurerm_servicebus_namespace.notifications.name
-  topic_name          = azurerm_servicebus_topic.notifications.name
-  auto_delete_on_idle = "P8D"   
-  default_message_ttl = "P9999D" //2.75 years
-  lock_duration       = "PT5M"
-  max_delivery_count  = 10
-  dead_lettering_on_message_expiration = true
+  name                                 = "audit"
+  resource_group_name                  = azurerm_resource_group.atlas_resource_group.name
+  namespace_name                       = azurerm_servicebus_namespace.notifications.name
+  topic_name                           = azurerm_servicebus_topic.notifications.name
+  auto_delete_on_idle                  = locals.audit-subscription-idle-delete
+  default_message_ttl                  = locals.long-expiry
+  lock_duration                        = locals.default-read-lock
+  max_delivery_count                   = locals.default-message-retries
+  dead_lettering_on_message_expiration = false
 }
 
 resource "azurerm_servicebus_subscription" "audit-alerts" {
-  name                = "audit"
-  resource_group_name = azurerm_resource_group.atlas_resource_group.name
-  namespace_name      = azurerm_servicebus_namespace.notifications.name
-  topic_name          = azurerm_servicebus_topic.alerts.name
-  auto_delete_on_idle = "P8D"   
-  default_message_ttl = "P9999D" //2.75 years
-  lock_duration       = "PT5M"
-  max_delivery_count  = 10
-  dead_lettering_on_message_expiration = true
+  name                                 = "audit"
+  resource_group_name                  = azurerm_resource_group.atlas_resource_group.name
+  namespace_name                       = azurerm_servicebus_namespace.notifications.name
+  topic_name                           = azurerm_servicebus_topic.alerts.name
+  auto_delete_on_idle                  = locals.audit-subscription-idle-delete
+  default_message_ttl                  = locals.long-expiry
+  lock_duration                        = locals.default-read-lock
+  max_delivery_count                   = locals.default-message-retries
+  dead_lettering_on_message_expiration = false
 }
 
 resource "azurerm_servicebus_subscription" "audit-matching-results-ready" {
-  name                = "audit"
-  resource_group_name = azurerm_resource_group.atlas_resource_group.name
-  namespace_name      = azurerm_servicebus_namespace.general.name
-  topic_name          = azurerm_servicebus_topic.matching-results-ready.name
-  auto_delete_on_idle = "P8D"   
-  default_message_ttl = "P9999D" //2.75 years
-  lock_duration       = "PT5M"
-  max_delivery_count  = 10
-  dead_lettering_on_message_expiration = true
+  name                                 = "audit"
+  resource_group_name                  = azurerm_resource_group.atlas_resource_group.name
+  namespace_name                       = azurerm_servicebus_namespace.general.name
+  topic_name                           = azurerm_servicebus_topic.matching-results-ready.name
+  auto_delete_on_idle                  = locals.audit-subscription-idle-delete
+  default_message_ttl                  = locals.long-expiry
+  lock_duration                        = locals.default-read-lock
+  max_delivery_count                   = locals.default-message-retries
+  dead_lettering_on_message_expiration = false
 }
 
 resource "azurerm_servicebus_subscription" "audit-updated-searchable-donors" {
-  name                = "audit"
-  resource_group_name = azurerm_resource_group.atlas_resource_group.name
-  namespace_name      = azurerm_servicebus_namespace.general.name
-  topic_name          = azurerm_servicebus_topic.updated-searchable-donors.name
-  auto_delete_on_idle = "P8D"   
-  default_message_ttl = "P9999D" //2.75 years
-  lock_duration       = "PT5M"
-  max_delivery_count  = 10
-  dead_lettering_on_message_expiration = true
+  name                                 = "audit"
+  resource_group_name                  = azurerm_resource_group.atlas_resource_group.name
+  namespace_name                       = azurerm_servicebus_namespace.general.name
+  topic_name                           = azurerm_servicebus_topic.updated-searchable-donors.name
+  auto_delete_on_idle                  = locals.audit-subscription-idle-delete
+  default_message_ttl                  = locals.long-expiry
+  lock_duration                        = locals.default-read-lock
+  max_delivery_count                   = locals.default-message-retries
+  dead_lettering_on_message_expiration = false
 }
 
