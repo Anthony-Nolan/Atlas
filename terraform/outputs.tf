80518498b25317ba6c550b2946f88c8f83db84b7
Ben Below <benjamin.below@softwire.com>
2020-04-29 13:55:32 +0100

    chore: ATLAS-75: add outputs for database names
    these will be used by Azure Devops to know how to run migrations

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,5 +2,12 @@ output "matching_algorithm" {
   value = {
     base_url = "https://${azurerm_function_app.atlas_matching_algorithm_function.default_hostname}"
     api_key  = var.APIKEY
+    persistent_database_name = azurerm_sql_database.atlas-persistent.name
+    transient_a_database_name = azurerm_sql_database.atlas-matching-transient-a.name
+    transient_b_database_name = azurerm_sql_database.atlas-matching-transient-b.name
   }
+}
+
+output "sql_server" {
+  value = azurerm_sql_server.atlas_sql_server.name
 }
