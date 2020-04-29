24da69b965c3b78dc5df260d967c999e77a43230
Ben Below <benjamin.below@softwire.com>
2020-04-29 11:09:30 +0100

    chore: ATLAS-75: add firewall rule to allow other azure services access

--- terraform/sql_server.tf
+++ terraform/sql_server.tf
@@ -7,3 +7,11 @@ resource "azurerm_sql_server" "atlas_sql_server" {
   administrator_login = var.ATLAS_DATABASE_SERVER_ADMIN_LOGIN
   administrator_login_password = var.ATLAS_DATABASE_SERVER_LOGIN_PASSWORD
 }
+
+resource "azurerm_sql_firewall_rule" "firewall_rule_allow_azure" {
+  name                = "AlllowAzureServices"
+  resource_group_name = azurerm_resource_group.atlas_resource_group.name
+  server_name         = azurerm_sql_server.atlas_sql_server.name
+  start_ip_address    = "0.0.0.0"
+  end_ip_address      = "0.0.0.0"
+}
