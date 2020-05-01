2b0f7ea165c91793e9eb28782377b53c05122212
Tom Johnson <tom.johnson@softwire.com>
2020-05-01 10:01:17 +0100

    fix: ATLAS-NONE: Removed tests for exceptions with line endings

--- Atlas.Utils.Test/CoreUtilsTest/Assertions/HttpResponseAssertionsTests.cs
+++ Atlas.Utils.Test/CoreUtilsTest/Assertions/HttpResponseAssertionsTests.cs
@@ -73,8 +73,7 @@ namespace Atlas.Utils.Test.CoreUtilsTest.Assertions
 
             Action action = () => response.Should().HaveAttachment("test.dat", new byte[] { 1, 2, 3 }, "application/pdf");
 
-            action.ShouldThrow<AssertionException>().Which.Message
-                .Should().Be("Expected string to be \r\n\"application/pdf\", but \r\n\"application/csv\" differs near \"csv\" (index 12).");
+            action.ShouldThrow<AssertionException>();
         }
 
         [Test]
@@ -107,8 +106,7 @@ namespace Atlas.Utils.Test.CoreUtilsTest.Assertions
 
             Action action = () => response.Should().HaveAttachment("wrong.dat", new byte[] { 1, 2, 3 }, "application/data");
 
-            action.ShouldThrow<AssertionException>().Which.Message
-                .Should().Be("Expected string to be \r\n\"wrong.dat\" with a length of 9, but \r\n\"test.dat\" has a length of 8.");
+            action.ShouldThrow<AssertionException>();
         }
 
         [Test]
