0d42055a641d0dff5183a41560dd86f41610d346
Mike.McLean <Mike.McLean@softwire.com>
2020-05-04 15:48:46 +0100

    chore: ATLAS-137: Update notes on registering resource providers, to make `terraform plan` work on latest versions of tooling

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,4 +23,12 @@ locals {
 provider "azurerm" {
   version         = "1.28.0"
   subscription_id = local.subscription_id
+
+  // According to the docs, the default behaviour is to attempt to register every possible resource provider
+  // whether or not we actually need it. See docs of this property for more context.
+  // However, registering providers requires higher permissions than general development, so attempting to do
+  // so will trigger 403s for most devs. Accordingly, we disable the "register everything" behaviour, and
+  // initial registrations will need to be organised as a one-off.
+  // Currently, the only resource provider needed is this AzureRM provider.
+  skip_provider_registration = false
 }
