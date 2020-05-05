2c3aeedf191e8cd815a13e4d79df202c2ba62d61
Tom Johnson <tom.johnson@softwire.com>
2020-05-05 12:33:59 +0100

    fearture: ATLAS-9: Added build pipeline and created context factories

---
+++ Atlas.MatchPrediction.Data/Context/MatchPredictionContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Atlas.MatchPrediction.Data.Context
+{
+    public class MatchPredictionContext : DbContext
+    {
+        // ReSharper disable once SuggestBaseTypeForParameter
+        public MatchPredictionContext(DbContextOptions<MatchPredictionContext> options) : base(options)
+        {
+        }
+    }
+}
