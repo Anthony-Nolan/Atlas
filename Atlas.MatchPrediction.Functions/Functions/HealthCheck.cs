a49fdfca1023ed0ed5ec91f1e341a23ba5c1b8a5
Tom Johnson <tom.johnson@softwire.com>
2020-05-06 10:49:36 +0100

    feature: ATLAS-9: Created swagger for new function

--- Atlas.MatchPrediction.Functions/Functions/HealthCheck.cs
+++ Atlas.MatchPrediction.Functions/Functions/HealthCheck.cs
@@ -1,13 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs.Extensions.Http;
 
 namespace Atlas.MatchPrediction.Functions.Functions
 {
     public class HealthCheck
     {
         [FunctionName("HealthCheck")]
-        public static OkObjectResult Run([HttpTrigger] HttpRequest req)
+        public static OkObjectResult Run([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequest req)
         {
             string responseMessage = "This HTTP triggered function executed successfully.";
             return new OkObjectResult(responseMessage);
