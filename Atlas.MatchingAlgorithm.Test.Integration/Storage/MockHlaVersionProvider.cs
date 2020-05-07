544c2e68047c10013d1d2bc6347e26e994364f80
Mike.McLean <Mike.McLean@softwire.com>
2020-05-07 15:16:22 +0100

    refactor: ATLAS-46: Update test references to HlaVersionProviders.

--- Atlas.MatchingAlgorithm.Test.Integration/Storage/MockHlaVersionProvider.cs
+++ Atlas.MatchingAlgorithm.Test.Integration/Storage/MockHlaVersionProvider.cs
@@ -4,7 +4,7 @@ namespace Atlas.MatchingAlgorithm.Test.Integration.Storage.FileBackedHlaLookupRe
 {
     // We don't care about this version info for any of the existing tests, and we don't want to faff around with setting up the data or allowing null responses in the live code.
     // So provide a trivial stubbed version of this class for tests.
-    public class MockWmdaHlaVersionProvider : IWmdaHlaVersionProvider
+    public class MockHlaVersionProvider : IWmdaHlaVersionProvider, IActiveHlaVersionAccessor
     {
         public string GetActiveHlaDatabaseVersion() => null;
         public string GetLatestStableHlaDatabaseVersion() => null;
