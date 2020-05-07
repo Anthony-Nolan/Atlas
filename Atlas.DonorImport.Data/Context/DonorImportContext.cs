1c9d042a5a6668f172a9cc3a8fb5394f1d451c0e
Josh Powell <josh.powell@softwire.com>
2020-05-07 15:46:18 +0100

    fix ATLAS-166: Add donor import context to data proj

---
+++ Atlas.DonorImport.Data/Context/DonorImportContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace Atlas.MatchingAlgorithm.Data.Context
+{
+    public class DonorImportContext : DbContext
+    {
+        // ReSharper disable once SuggestBaseTypeForParameter
+        public DonorImportContext(DbContextOptions<DonorImportContext> options) : base(options)
+        {
+        }
+    }
+}
