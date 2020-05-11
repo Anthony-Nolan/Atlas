24de5d7b254eda45b5a06dd9948ade185ab08b0a
Mike.McLean <Mike.McLean@softwire.com>
2020-05-11 10:58:18 +0100

    Update DataRefresh tests around HlaMetadata recreation.

--- Atlas.MatchingAlgorithm/Services/DataRefresh/DataRefreshOrchestrator.cs
+++ Atlas.MatchingAlgorithm/Services/DataRefresh/DataRefreshOrchestrator.cs
@@ -6,7 +6,6 @@ using Atlas.MatchingAlgorithm.Data.Persistent.Models;
 using Atlas.MatchingAlgorithm.Data.Persistent.Repositories;
 using Atlas.MatchingAlgorithm.Extensions;
 using Atlas.MatchingAlgorithm.Services.AzureManagement;
-using Atlas.MatchingAlgorithm.Services.ConfigurationProviders;
 using Atlas.MatchingAlgorithm.Services.ConfigurationProviders.TransientSqlDatabase;
 using Atlas.MatchingAlgorithm.ConfigSettings;
 using Atlas.MatchingAlgorithm.Services.MatchingDictionary;
