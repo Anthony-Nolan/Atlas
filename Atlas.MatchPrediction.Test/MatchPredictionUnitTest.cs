8d8471df88c027a5e3a730098321edac51086a04
Tom Johnson <tom.johnson@softwire.com>
2020-05-11 17:30:09 +0100

    chore: ATLAS-9: Added token test

---
+++ Atlas.MatchPrediction.Test/MatchPredictionUnitTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace Atlas.MatchPrediction.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TokenTest()
+        {
+            Assert.Pass();
+        }
+    }
+}
