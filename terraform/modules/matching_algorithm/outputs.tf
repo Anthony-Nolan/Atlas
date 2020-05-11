5b123c478db6916a025fb59965b1b96bd34b51f9
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2020-05-11 10:12:26 +0100

    review: ATLAS-141: Review markups related to module release vars.

--- terraform/modules/matching_algorithm/outputs.tf
+++ terraform/modules/matching_algorithm/outputs.tf
@@ -1,7 +1,7 @@
 output "general" {
   value = {
     base_url                  = "https://${azurerm_function_app.atlas_matching_algorithm_function.default_hostname}"
-    api_key                   = var.MATCHING_FUNCTION_HOST_KEY
+    api_key                   = var.FUNCTION_HOST_KEY
     persistent_database_name  = azurerm_sql_database.atlas-persistent.name
     transient_a_database_name = azurerm_sql_database.atlas-matching-transient-a.name
     transient_b_database_name = azurerm_sql_database.atlas-matching-transient-b.name
