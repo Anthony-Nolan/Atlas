f7731f5b232efe33728f57496512196e6398923b
Mike.McLean <Mike.McLean@softwire.com>
2020-05-11 15:01:04 +0100

    refactor: ATLAS-46: Remove direct dependencies on bits of MetadataDictionary which we want to be internal

---
+++ Atlas.MatchingAlgorithm.Test/TestHelpers/Builders/HlaMetadataDictionaryBuilder.cs
@@ -0,0 +1,71 @@
+﻿using Atlas.HlaMetadataDictionary.Services;
+using Atlas.MatchingAlgorithm.Services.ConfigurationProviders;
+using Atlas.MatchingAlgorithm.Services.MatchingDictionary;
+using NSubstitute;
+
+namespace Atlas.MatchingAlgorithm.Test.TestHelpers.Builders
+{
+    internal class HlaMetadataDictionaryBuilder
+    {
+        IRecreateHlaMetadataService recreate;
+        IAlleleNamesLookupService name;
+        IHlaMatchingLookupService matching;
+        ILocusHlaMatchingLookupService locus;
+        IHlaScoringLookupService scoring;
+        IHlaLookupResultsService all;
+        IDpb1TceGroupLookupService dpb1;
+        IActiveHlaVersionAccessor activeVersion;
+        IWmdaHlaVersionProvider wmdaVersion;
+
+        public HlaMetadataDictionaryBuilder()
+        {
+            ResetAllDependencies();
+        }
+
+        public void ResetAllDependencies()
+        {
+            recreate = Substitute.For<IRecreateHlaMetadataService>();
+            name = Substitute.For<IAlleleNamesLookupService>();
+            matching = Substitute.For<IHlaMatchingLookupService>();
+            locus = Substitute.For<ILocusHlaMatchingLookupService>();
+            scoring = Substitute.For<IHlaScoringLookupService>();
+            all = Substitute.For<IHlaLookupResultsService>();
+            dpb1 = Substitute.For<IDpb1TceGroupLookupService>();
+            activeVersion = Substitute.For<IActiveHlaVersionAccessor>();
+            wmdaVersion = Substitute.For<IWmdaHlaVersionProvider>();
+        }
+
+        public HlaMetadataDictionaryBuilder Using<T>(T dependency)
+        {
+            switch (dependency)
+            {
+                case IRecreateHlaMetadataService typedDependency: recreate = typedDependency; break;
+                case IAlleleNamesLookupService typedDependency: name = typedDependency; break;
+                case IHlaMatchingLookupService typedDependency: matching = typedDependency; break;
+                case ILocusHlaMatchingLookupService typedDependency: locus = typedDependency; break;
+                case IHlaScoringLookupService typedDependency: scoring = typedDependency; break;
+                case IHlaLookupResultsService typedDependency: all = typedDependency; break;
+                case IDpb1TceGroupLookupService typedDependency: dpb1 = typedDependency; break;
+                case IActiveHlaVersionAccessor typedDependency: activeVersion = typedDependency; break;
+                case IWmdaHlaVersionProvider typedDependency: wmdaVersion = typedDependency; break;
+            }
+
+            return this;
+        }
+
+        public IHlaMetadataDictionary Build()
+        {
+            return new MatchingAlgorithm.Services.MatchingDictionary.HlaMetadataDictionary(
+                recreate,
+                name,
+                matching,
+                locus,
+                scoring,
+                all,
+                dpb1,
+                activeVersion,
+                wmdaVersion
+                    );
+        }
+    }
+}
