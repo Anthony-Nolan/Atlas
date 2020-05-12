0dfda59a1657b366986a371e7268ee6055dea376
Josh Powell <josh.powell@softwire.com>
2020-05-12 09:49:16 +0100

    fix: atlas-213: use ef code first schema for donor tab;e setup

---
+++ Atlas.DonorImport.Data/Context/DonorContext.cs
@@ -0,0 +1,19 @@
+﻿using Atlas.DonorImport.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Atlas.DonorImport.Data.Context
+{
+    public class DonorContext : DbContext
+    {
+        // ReSharper disable once SuggestBaseTypeForParameter
+        public DonorContext(DbContextOptions<DonorContext> options) : base(options)
+        {       
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Donor>().Property(p => p.Id).ValueGeneratedOnAdd();
+        }
+
+        public DbSet<Donor> Donors { get; set; }
+    }
+}
---
+++ Atlas.DonorImport.Data/Models/Donor.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Atlas.DonorImport.Data.Models
+{
+    [Table("Donors")]
+    public class Donor
+    {
+        public int Id { get; set; }
+        public int DonorId { get; set; }
+        public int DonorType { get; set; }
+        public string Ethnicity { get; set; }
+        public int RegistryCode { get; set; }
+        public string A_1 { get; set; }
+        public string A_2 { get; set; }
+        public string B_1 { get; set; }
+        public string B_2 { get; set; }
+        public string C_1 { get; set; }
+        public string C_2 { get; set; }
+        public string DPB_1 { get; set; }
+        public string DPB_2 { get; set; }
+        public string DQB1_1 { get; set; }
+        public string DQB1_2 { get; set; }
+        public string DRB1_1 { get; set; }
+        public string DRB1_2 { get; set; }
+        public string Hash { get; set; }
+    }
+}
