84008358538e45838623474b07dc1991f35b2b6d
Josh Powell <josh.powell@softwire.com>
2020-05-12 14:34:28 +0100

    feat ATLAS-42: Add empty MAC matching project

---
+++ Atlas.MultipleAlleleCodeLookup.Test/MultileAlleleCodeLookupUnitTest.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Atlas.MultipleAlleleCodeLookup.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TokenTest()
+        {
+            Assert.Pass();
+        }
+    }
+}
