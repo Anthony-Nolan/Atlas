c5f2f8a7a6c70a63f627455419a9135eb35e3380
Josh Powell <josh.powell@softwire.com>
2020-05-12 16:59:36 +0100

    FIX: Atlas-42: add missing terraform variable

--- terraform/mulltiple_allele_code_storage.tf
+++ terraform/mulltiple_allele_code_storage.tf
@@ -1,4 +1,5 @@
 resource "azurerm_storage_table" "atlas_multiple_allele_code_storage" {
   name                 = lower("${local.environment}-ATLAS-MULTIPLE-ALLELE-CODE-TABLE")
   storage_account_name = azurerm_storage_account.azure_storage.name
+  resource_group_name  = azurerm_resource_group.atlas_resource_group.name
 }
