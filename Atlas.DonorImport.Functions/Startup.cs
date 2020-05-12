f90cd15051292129322830400ce2bcd99e6ca3bc
Josh Powell <josh.powell@softwire.com>
2020-05-12 15:27:52 +0100

    FIX: Atlas-166: Add startup and host files

---
+++ Atlas.DonorImport.Functions/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(Atlas.DonorImport.Functions.Startup))]
+
+namespace Atlas.DonorImport.Functions
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+
+        }
+    }
+}
