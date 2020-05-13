5ef564839e95aba8cd6a3ab4e0cdc6eacc7765ec
Tom Johnson <tom.johnson@softwire.com>
2020-05-13 18:15:40 +0100

    feature: ATLAS-15: Importing set data

---
+++ Atlas.MatchPrediction.Functions/Functions/ImportHaplotypeFrequencySet.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using Microsoft.Azure.WebJobs;
+
+namespace Atlas.MatchPrediction.Functions.Functions
+{
+    public class ImportHaplotypeFrequencySet
+    {
+        [FunctionName("ImportSetWithRegistryEthnicityFilename")]
+        public static void Run([BlobTrigger("{registry}/{ethnicity}/{filename}", Connection = "")]
+            Stream myBlob, string filename, string ethnicity, string registry)
+        {
+
+        }
+
+        [FunctionName("ImportSetWithRegistryFilename")]
+        public static void Run([BlobTrigger("{registry}/{filename}", Connection = "")]
+            Stream myBlob, string filename, string ethnicity)
+        {
+
+        }
+
+        [FunctionName("ImportSetWithFilename")]
+        public static void Run([BlobTrigger("{filename}", Connection = "")]
+            Stream myBlob, string filename)
+        {
+
+        }
+    }
+}
