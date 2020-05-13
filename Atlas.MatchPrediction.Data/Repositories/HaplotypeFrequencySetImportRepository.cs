49c5284c32b2448dbb86f75305e55a6984e6d398
Tom Johnson <tom.johnson@softwire.com>
2020-05-13 18:15:40 +0100

    feature: ATLAS-15: Importing set data

---
+++ Atlas.MatchPrediction.Data/Repositories/HaplotypeFrequencySetImportRepository.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using Atlas.MatchPrediction.Data.Context;
+using Atlas.MatchPrediction.Data.Models;
+
+namespace Atlas.MatchPrediction.Data.Repositories
+{
+    public interface IHaplotypeFrequencySetImportRepository
+    {
+        Task InsertHaplotypeFrequencySet(HaplotypeFrequencySet haplotypeFrequencySet);
+    }
+
+    public class HaplotypeFrequencySetImportRepository : IHaplotypeFrequencySetImportRepository
+    {
+        private readonly MatchPredictionContext context;
+
+        public HaplotypeFrequencySetImportRepository(MatchPredictionContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task InsertHaplotypeFrequencySet(HaplotypeFrequencySet haplotypeFrequencySet)
+        {
+            context.HaplotypeFrequencySets.Add(haplotypeFrequencySet);
+            await context.SaveChangesAsync();
+        }
+    }
+}
