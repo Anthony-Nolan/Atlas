78f7fa2678acd72c7c0c7f9b01b56b6318d2b165
Josh Powell <josh.powell@softwire.com>
2020-05-13 15:24:00 +0100

    FIX: Atlas-42: Rename MAC lookup to AMC dictionary and fix terraform table naming error

--- terraform/mulltiple_allele_code_storage.tf
+++ terraform/mulltiple_allele_code_storage.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_table" "atlas_multiple_allele_code_storage" {
-  name                 = lower("${local.environment}-ATLAS-MULTIPLE-ALLELE-CODE-TABLE")
+  name                 = "${title(lower(local.environment))}AtlasMultipleAlleleCodeTable"
   storage_account_name = azurerm_storage_account.azure_storage.name
   resource_group_name  = azurerm_resource_group.atlas_resource_group.name
 }
