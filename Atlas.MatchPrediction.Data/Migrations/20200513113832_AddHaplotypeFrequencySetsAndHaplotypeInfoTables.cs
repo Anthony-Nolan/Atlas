2be7e6a8dffae8fe429291b9a057fbda60f55e50
Tom Johnson <tom.johnson@softwire.com>
2020-05-13 12:39:54 +0100

    feature: ATLAS-14: Created code first EF migration for MatchPrediction
    e

---
+++ Atlas.MatchPrediction.Data/Migrations/20200513113832_AddHaplotypeFrequencySetsAndHaplotypeInfoTables.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Atlas.MatchPrediction.Data.Migrations
+{
+    public partial class AddHaplotypeFrequencySetsAndHaplotypeInfoTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "HaplotypeFrequencySets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Registry = table.Column<string>(nullable: true),
+                    Ethnicity = table.Column<string>(nullable: true),
+                    Active = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HaplotypeFrequencySets", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "HaplotypeInfo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Set_Id = table.Column<int>(nullable: true),
+                    Frequency = table.Column<decimal>(nullable: false),
+                    A = table.Column<string>(nullable: false),
+                    B = table.Column<string>(nullable: false),
+                    C = table.Column<string>(nullable: false),
+                    DQB1 = table.Column<string>(nullable: false),
+                    DRB1 = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HaplotypeInfo", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_HaplotypeInfo_HaplotypeFrequencySets_Set_Id",
+                        column: x => x.Set_Id,
+                        principalTable: "HaplotypeFrequencySets",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RegistryAndEthnicity",
+                table: "HaplotypeFrequencySets",
+                columns: new[] { "Ethnicity", "Registry" },
+                unique: true,
+                filter: "[Active] = 'True'");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HaplotypeInfo_Set_Id",
+                table: "HaplotypeInfo",
+                column: "Set_Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HaplotypeInfo");
+
+            migrationBuilder.DropTable(
+                name: "HaplotypeFrequencySets");
+        }
+    }
+}
---
+++ Atlas.MatchPrediction.Data/Models/HaplotypeInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Atlas.MatchPrediction.Data.Models
+{
+    public class HaplotypeInfo
+    {
+        public int Id { get; set; }
+        [ForeignKey("Set_Id")]
+        public HaplotypeFrequencySets SetId { get; set; }
+        public decimal Frequency { get; set; }
+        [Required]
+        public string A { get; set; }
+        [Required]
+        public string B { get; set; }
+        [Required]
+        public string C { get; set; }
+        [Required]
+        public string DQB1 { get; set; }
+        [Required]
+        public string DRB1 { get; set; }
+    }
+}
