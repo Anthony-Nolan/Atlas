04a689fdfbb404209cf4019d18b72b9862b5f46c
Josh Powell <josh.powell@softwire.com>
2020-05-14 17:15:38 +0100

    review: Atlas-166: update donor blob storage name and remove vs config

--- terraform/donor_blob_storage.tf
+++ terraform/donor_blob_storage.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_container" "donor_blob_storage" {
-  name                  = "donorBlobStorage"
+  name                  = "donors"
   storage_account_name  = azurerm_storage_account.azure_storage.name
   container_access_type = "private"
 }
