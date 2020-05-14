09f3d53d5961227a15d74b7f4e31deb03711ab4e
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2020-05-14 15:34:34 +0100

    refactor: ATLAS-141: Rename MAC lookup storage table.

--- terraform/modules/multiple_allele_code_lookup/storage.tf
+++ terraform/modules/multiple_allele_code_lookup/storage.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_table" "atlas_multiple_allele_code_storage" {
-  name                 = "${title(lower(var.general.environment))}AtlasMultipleAlleleCodeTable"
+  name                 = "AtlasMultipleAlleleCodes"
   storage_account_name = var.azure_storage.name
   resource_group_name  = var.app_service_plan.resource_group_name
 }
