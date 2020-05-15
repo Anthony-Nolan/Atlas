7506f890ec9b1347ba0e27b8cf48b4476aa174b7
Josh Powell <josh.powell@softwire.com>
2020-05-15 10:41:41 +0100

    Fix: ATLAS-166: add resource group name to blob donor storage

--- terraform/donor_blob_storage.tf
+++ terraform/donor_blob_storage.tf
@@ -2,4 +2,5 @@ resource "azurerm_storage_container" "donor_blob_storage" {
   name                  = "donors"
   storage_account_name  = azurerm_storage_account.azure_storage.name
   container_access_type = "private"
+  resource_group_name   = azurerm_resource_group.atlas_resource_group.name
 }
