c5bdd4d7308f67da8bde95aa4a71034b46df12da
Mike.McLean <Mike.McLean@softwire.com>
2020-05-18 16:45:33 +0100

    refactor: ATLAS-47: Move required common code into Atlas.Common

---
+++ Atlas.Common/Utils/Extensions/DependencyInjectionUtils.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Atlas.Common.Utils.Extensions
+{
+    public static class DependencyInjectionUtils
+    {
+        public static void RegisterOptions<T>(this IServiceCollection services, string sectionName) where T : class
+        {
+            services.AddOptions<T>().Configure<IConfiguration>((settings, config) => { config.GetSection(sectionName).Bind(settings); });
+        }
+    }
+}
