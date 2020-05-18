3ba9e5e915b055f48677340be32c1362da5e57d1
Mike.McLean <Mike.McLean@softwire.com>
2020-05-18 19:23:55 +0100

    chore: ATLAS-46: Correct `MultipleAlleleCodeLookup` to `MultipleAlleleCodeDictionary`

---
+++ Atlas.MultipleAlleleCodeDictionary.Test/MultileAlleleCodeDictionaryUnitTest.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Atlas.MultipleAlleleCodeDictionary.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TokenTest()
+        {
+            Assert.Pass();
+        }
+    }
+}
