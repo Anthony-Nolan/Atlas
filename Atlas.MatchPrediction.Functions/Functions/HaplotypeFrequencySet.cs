9d12d37b57d52fe61962ff5f776eaaeb20d055ab
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2020-05-19 16:44:32 +0100

    feature: ATLAS-15: Complete azure storage configuration for blob storage function.

---
+++ Atlas.MatchPrediction.Functions/Functions/HaplotypeFrequencySet.cs
@@ -0,0 +1,25 @@
+﻿using Atlas.MatchPrediction.Services;
+using Microsoft.Azure.WebJobs;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Atlas.MatchPrediction.Functions.Functions
+{
+    public class HaplotypeFrequencySet
+    {
+        private readonly IHaplotypeFrequencySetService haplotypeFrequencySetService;
+        
+        public HaplotypeFrequencySet(IHaplotypeFrequencySetService haplotypeFrequencySetService)
+        {
+            this.haplotypeFrequencySetService = haplotypeFrequencySetService;
+        }
+
+        [FunctionName(nameof(ImportHaplotypeFrequencySet))]
+        public async Task ImportHaplotypeFrequencySet(
+            [BlobTrigger("%AzureStorage:HaplotypeFrequencySetImportContainer%/{fileName}", Connection = "AzureStorage:ConnectionString")] Stream blob,
+            string fileName)
+        {
+            await haplotypeFrequencySetService.ImportHaplotypeFrequencySet(fileName, blob);
+        }
+    }
+}
