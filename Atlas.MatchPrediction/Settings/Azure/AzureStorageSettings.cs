85e103d0919300d806f43a33a80bcd251a959bef
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2020-05-19 16:44:32 +0100

    feature: ATLAS-15: Complete azure storage configuration for blob storage function.

--- Atlas.MatchPrediction/Settings/Azure/AzureStorageSettings.cs
+++ Atlas.MatchPrediction/Settings/Azure/AzureStorageSettings.cs
@@ -3,5 +3,6 @@ namespace Atlas.MatchPrediction.Settings.Azure
     public class AzureStorageSettings
     {
         public string ConnectionString { get; set; }
+        public string HaplotypeFrequencySetImportContainer { get; set; }
     }
 }
