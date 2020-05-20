4bf882916a26f102e1b39668b4bb1b3ac0089ac6
Ben Below <benjamin.below@softwire.com>
2020-05-20 11:54:38 +0100

    fix: ATLAS-256: Store donor registry information as string, not int

---
+++ Atlas.DonorImport.Data/Migrations/20200520105351_UseStringForDonorRegistry.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Atlas.DonorImport.Data.Migrations
+{
+    public partial class UseStringForDonorRegistry : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "RegistryCode",
+                table: "Donors",
+                nullable: true,
+                oldClrType: typeof(int));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "RegistryCode",
+                table: "Donors",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
