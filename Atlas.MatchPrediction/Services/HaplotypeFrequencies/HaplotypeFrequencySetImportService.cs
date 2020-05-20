b1d93445c913149f1268d1331e766c5db3566bb8
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2020-05-20 14:13:19 +0100

    feature: ATLAS-15: Add meta data service & new meta data table columns (also refactor import service and repo interfaces - not yet implemented).

---
+++ Atlas.MatchPrediction/Services/HaplotypeFrequencies/HaplotypeFrequencySetImportService.cs
@@ -0,0 +1,70 @@
+﻿using Atlas.MatchPrediction.Data.Models;
+using Atlas.MatchPrediction.Data.Repositories;
+using Atlas.MatchPrediction.Models;
+using CsvHelper;
+using CsvHelper.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Atlas.MatchPrediction.Services.HaplotypeFrequencies
+{
+    public interface IHaplotypeFrequencySetImportService
+    {
+        Task Import(Stream blob, HaplotypeFrequencySetMetaData metaData);
+    }
+
+    public class HaplotypeFrequencySetImportService : IHaplotypeFrequencySetImportService
+    {
+        private readonly IHaplotypeFrequencySetImportRepository importRepository;
+
+        public HaplotypeFrequencySetImportService(IHaplotypeFrequencySetImportRepository importRepository)
+        {
+            this.importRepository = importRepository;
+        }
+
+        public async Task Import(Stream blob, HaplotypeFrequencySetMetaData metaData)
+        {
+            var setId = await importRepository.AddHaplotypeFrequencySet(
+                new HaplotypeFrequencySet
+                {
+                    Registry = metaData.Registry,
+                    Ethnicity = metaData.Ethnicity,
+                    Active = true,
+                    Name = metaData.Name,
+                    DateTimeAdded = DateTimeOffset.Now
+                });
+
+            var frequencies = ReadAllHaplotypeFrequencies(blob);
+
+            await importRepository.AddHaplotypeFrequencies(setId, frequencies);
+        }
+
+        private static IEnumerable<HaplotypeFrequency> ReadAllHaplotypeFrequencies(Stream blob)
+        {
+            using var reader = new StreamReader(blob);
+            using var csv = new CsvReader(reader);
+            csv.Configuration.Delimiter = ";";
+            csv.Configuration.PrepareHeaderForMatch = (string header, int index) => header.ToUpper();
+            csv.Configuration.RegisterClassMap<HaplotypeFrequencyMap>();
+            return csv.GetRecords<HaplotypeFrequency>().ToList();
+        }
+
+        public sealed class HaplotypeFrequencyMap : ClassMap<HaplotypeFrequency>
+        {
+            public HaplotypeFrequencyMap()
+            {
+                Map(m => m.A);
+                Map(m => m.B);
+                Map(m => m.C);
+                Map(m => m.DQB1);
+                Map(m => m.DRB1);
+                Map(m => m.Frequency).Name("freq");
+                Map(m => m.Id).Ignore();
+                Map(m => m.Set).Ignore();
+            }
+        }
+    }
+}
