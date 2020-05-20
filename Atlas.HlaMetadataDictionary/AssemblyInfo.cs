a4dd207e5470cf140053cb8e1a3c0b491ff883c7
Mike.McLean <Mike.McLean@softwire.com>
2020-05-20 14:37:29 +0100

    Better solution to publicly-accessed type in Library.

---
+++ Atlas.HlaMetadataDictionary/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Atlas.HlaMetadataDictionary.Test")]
+[assembly: InternalsVisibleTo("Atlas.MatchingAlgorithm.Test")] //Currently only so that it can created Exceptions to simulate errors.
