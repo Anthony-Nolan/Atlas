bf0dac62fc2d846f6cc9b6aeb6ddb9bd31e80864
Mike.McLean <Mike.McLean@softwire.com>
2020-05-21 17:38:30 +0100

    refactor: ATLAS-46: HlaTypingCategory: Refactor since ScoringLookupResult doesn't need a Category in the first place

--- Atlas.HlaMetadataDictionary/Models/LookupEntities/HlaLookupTableEntity.cs
+++ Atlas.HlaMetadataDictionary/Models/LookupEntities/HlaLookupTableEntity.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using Atlas.Common.GeneticData;
 using Atlas.Common.GeneticData.Hla.Models;
 using Atlas.HlaMetadataDictionary.Models.Lookups;
@@ -16,76 +14,63 @@ namespace Atlas.HlaMetadataDictionary.Models.LookupEntities
     public class HlaLookupTableEntity : TableEntity
     {
         public string LocusAsString { get; set; }
-        public string TypingMethodAsString { get; set; }
-        public string LookupName { get; set; }
-        public string SerialisedHlaInfo { get; set; }
         public Locus Locus => LocusAsString.ParseToEnum<Locus>();
+        public string TypingMethodAsString { get; set; }
         public TypingMethod TypingMethod => TypingMethodAsString.ParseToEnum<TypingMethod>();
+        public string LookupName { get; set; }
+        public string SerialisedHlaInfoType { get; set; }
+        public string SerialisedHlaInfo { get; set; }
 
         public HlaLookupTableEntity() { }
 
-        private HlaLookupTableEntity(IHlaLookupResult lookupResult, bool allowComplexSerialisation)
+        public HlaLookupTableEntity(IHlaLookupResult lookupResult)
             : base(HlaLookupTableKeyManager.GetEntityPartitionKey(lookupResult.Locus),
                 HlaLookupTableKeyManager.GetEntityRowKey(lookupResult.LookupName, lookupResult.TypingMethod))
         {
             LocusAsString = lookupResult.Locus.ToString();
             TypingMethodAsString = lookupResult.TypingMethod.ToString();
             LookupName = lookupResult.LookupName;
-            SerialisedHlaInfo = SerialiseHlaInfo(lookupResult.HlaInfoToSerialise, allowComplexSerialisation);
+            SerialisedHlaInfoType = lookupResult.HlaInfoToSerialise.GetType().Name; 
+            SerialisedHlaInfo = SerialiseHlaInfo(lookupResult.HlaInfoToSerialise);
         }
-
-        public HlaLookupTableEntity(IHlaLookupResult lookupResult)
-            : this(lookupResult, false)
-        { }
 
         #region HlaScoringLookupTableEntity
         // This region should really be an entire separate sub-class, but AzureStorage doesn't really support that nicely :(
         // It's all related specifically to the Serialised data associated with Scoring LookupResults which is sub-typed depending on the
         // nature of the allele descriptor in question.
 
-        [Obsolete("Should only EVER be used INSIDE the Entity class. Wants to be private really, but needs to be public to be written to the AzureStorage")]
-        public string HlaInfoTypeSerialised_AsString { get; set; }
-        [Obsolete("Deprecated in favour of " + nameof(HlaInfoTypeSerialised_AsString))]//TODO: Delete this property once all the extant Storages have been re-generated.
+        [Obsolete("Deprecated in favour of " + nameof(SerialisedHlaInfoType))]//TODO: Delete this property once all the extant Storages have been re-generated.
         public string LookupNameCategoryAsString { get; set; }
-        public HlaTypingCategory HlaInfoTypeSerialised
+        public string BackwardsCompatible_SerialisedHlaInfoType
         {
             get
             {
-                var stringToRead = string.IsNullOrWhiteSpace(HlaInfoTypeSerialised_AsString)
-                    ? LookupNameCategoryAsString
-                    : HlaInfoTypeSerialised_AsString;
-
-                return stringToRead.ParseToEnum<HlaTypingCategory>();
-            }
-        }
-        
-        public HlaLookupTableEntity(IHlaLookupResult lookupResult, HlaTypingCategory hlaInfoTypeToBeSerialised)
-            : this(lookupResult, true)
-        {
-            if (!new[]{Serology, Allele, NmdpCode, XxCode}.Contains(hlaInfoTypeToBeSerialised))
-            {
-                throw new InvalidOperationException($"This deserialisation process currently only support certain values of {nameof(HlaTypingCategory)} currently, of which '{hlaInfoTypeToBeSerialised}' is not one.");
-            }
-            HlaInfoTypeSerialised_AsString = hlaInfoTypeToBeSerialised.GetStringValue();
-        }
-
-        private static string SerialiseHlaInfo(object hlaInfo, bool allowComplexSerialisation)
-        {
-            if (hlaInfo is string || hlaInfo is IEnumerable<string>)
-            {
-                return JsonConvert.SerializeObject(hlaInfo);
-            }
-
-            if (hlaInfo is IHlaScoringInfo)
-            {
-                if (allowComplexSerialisation)
+                if (!string.IsNullOrWhiteSpace(SerialisedHlaInfoType))
                 {
-                    return JsonConvert.SerializeObject(hlaInfo);
+                    return SerialisedHlaInfoType;
                 }
-                throw new InvalidOperationException("If you wish to create a TableEntity that serialises more complex Scoring data, then you must use the constructor that declares the nature of that Scoring data.");
-            }
 
-            throw new InvalidOperationException("This is an entirely new and currently unsupported usage of a TableEntity.");
+                var oldCategoryEnum = LookupNameCategoryAsString.ParseToEnum<HlaTypingCategory>();
+
+                switch (oldCategoryEnum)
+                {
+                    case Serology:
+                        return nameof(SerologyScoringInfo);
+                    case Allele:
+                        return nameof(SingleAlleleScoringInfo);
+                    case NmdpCode:
+                        return nameof(MultipleAlleleScoringInfo);
+                    case XxCode:
+                        return nameof(ConsolidatedMolecularScoringInfo);
+                    default:
+                        return LookupNameCategoryAsString;
+                }
+            }
+        }
+
+        private static string SerialiseHlaInfo(object hlaInfo)
+        {
+            return JsonConvert.SerializeObject(hlaInfo);
         }
 
         public T GetHlaInfo<T>()
@@ -95,15 +80,15 @@ namespace Atlas.HlaMetadataDictionary.Models.LookupEntities
 
         public IHlaScoringInfo DeserialiseTypedScoringInfo()
         {
-            switch (HlaInfoTypeSerialised)
+            switch (BackwardsCompatible_SerialisedHlaInfoType)
             {
-                case Serology:
+                case nameof(SerologyScoringInfo):
                     return GetHlaInfo<SerologyScoringInfo>();
-                case Allele:
+                case nameof(SingleAlleleScoringInfo):
                     return GetHlaInfo<SingleAlleleScoringInfo>();
-                case NmdpCode:
+                case nameof(MultipleAlleleScoringInfo):
                     return GetHlaInfo<MultipleAlleleScoringInfo>();
-                case XxCode:
+                case nameof(ConsolidatedMolecularScoringInfo):
                     return GetHlaInfo<ConsolidatedMolecularScoringInfo>();
                 default:
                     throw new ArgumentOutOfRangeException();
