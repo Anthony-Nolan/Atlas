d7434679b8919023ebd6e0ff17d5f567f6b1bdcf
Ben Below <benjamin.below@softwire.com>
2020-05-21 18:21:11 +0100

    feature: ATLAS-167: Add integration tests for donor import - initial, additions only
    # Conflicts:
    #	Atlas.sln

---
+++ Atlas.DonorImport.Test.Integration/TestHelpers/DonorInspectionRepository.cs
@@ -0,0 +1,38 @@
+using System.Threading.Tasks;
+using Atlas.DonorImport.Data.Models;
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace Atlas.DonorImport.Test.Integration.TestHelpers
+{
+    /// <summary>
+    /// Contains methods for inspecting donor data that are only necessary in the context of integration tests.
+    /// </summary>
+    public interface IDonorInspectionRepository
+    {
+        Task<Donor> GetDonor(string externalDonorId);
+        Task<int> DonorCount();
+    }
+    
+    public class DonorInspectionRepository: IDonorInspectionRepository
+    {
+        private readonly string connectionString;
+
+        public DonorInspectionRepository(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+    
+        public async Task<Donor> GetDonor(string externalDonorId)
+        {
+            await using var conn = new SqlConnection(connectionString);
+            return await conn.QuerySingleOrDefaultAsync<Donor>($"SELECT * FROM Donors WHERE DonorId = {externalDonorId}", commandTimeout: 300);
+        }
+
+        public async Task<int> DonorCount()
+        {
+            await using var conn = new SqlConnection(connectionString);
+            return await conn.QuerySingleOrDefaultAsync<int>($"SELECT COUNT(*) FROM Donors", commandTimeout: 300);
+        }
+    }
+}
