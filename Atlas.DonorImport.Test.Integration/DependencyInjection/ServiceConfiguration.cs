1b32f9ca7c4d726e93508d5c62ff049f8660f238
Ben Below <benjamin.below@softwire.com>
2020-05-21 18:21:11 +0100

    feature: ATLAS-167: Add integration tests for donor import - initial, additions only

---
+++ Atlas.DonorImport.Test.Integration/DependencyInjection/ServiceConfiguration.cs
@@ -0,0 +1,67 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Atlas.Common.Notifications;
+using Atlas.DonorImport.Clients;
+using Atlas.DonorImport.Data.Context;
+using Atlas.DonorImport.DependencyInjection;
+using Atlas.DonorImport.Test.Integration.TestHelpers;
+using Atlas.MatchingAlgorithm.Client.Models.Donors;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NSubstitute;
+
+namespace Atlas.DonorImport.Test.Integration.DependencyInjection
+{
+    public class ServiceConfiguration
+    {
+        public static IServiceProvider CreateProvider()
+        {
+            var services = new ServiceCollection();
+            SetUpConfiguration(services);
+            services.RegisterDonorImportTypes();
+            RegisterIntegrationTestServices(services);
+            SetUpMockServices(services);
+            return services.BuildServiceProvider();
+        }
+
+        private static void RegisterIntegrationTestServices(IServiceCollection services)
+        {
+            services.AddScoped(sp =>
+            {
+                var connectionString = GetSqlConnectionString(sp);
+                return new ContextFactory().Create(connectionString);
+            });
+            
+            services.AddSingleton<IDonorInspectionRepository>(sp =>
+                new DonorInspectionRepository(GetSqlConnectionString(sp))
+            );
+        }
+
+        private static void SetUpConfiguration(IServiceCollection services)
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .Build();
+
+            services.AddSingleton<IConfiguration>(sp => configuration);
+        }
+
+        private static void SetUpMockServices(IServiceCollection services)
+        {
+            var mockSearchServiceBusClient = Substitute.For<IMessagingServiceBusClient>();
+            mockSearchServiceBusClient
+                .PublishDonorUpdateMessage(Arg.Any<SearchableDonorUpdate>())
+                .Returns(Task.CompletedTask);
+
+            services.AddScoped(sp => mockSearchServiceBusClient);
+            services.AddScoped(sp => Substitute.For<INotificationsClient>());
+        }
+
+        private static string GetSqlConnectionString(IServiceProvider serviceProvider)
+        {
+            return serviceProvider.GetService<IConfiguration>().GetSection("ConnectionStrings")["Sql"];
+        }
+    }
+}
---
+++ Atlas.DonorImport.Test.Integration/TestHelpers/DatabaseManager.cs
@@ -0,0 +1,34 @@
+using System;
+using Atlas.DonorImport.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Atlas.DonorImport.Test.Integration.TestHelpers
+{
+    public static class DatabaseManager
+    {
+        /// <summary>
+        /// Creates if necessary, and runs migrations on both a transient and persistent database
+        /// </summary>
+        public static void SetupDatabase()
+        {
+            var context = DependencyInjection.DependencyInjection.Provider.GetService<DonorContext>();
+
+            if (context == null)
+            {
+                throw new Exception("Database context could resolved - DI has not been correctly configured.");
+            }
+
+            context.Database.Migrate();
+        }
+        
+        /// <summary>
+        /// Clears the test database of data. Can be accessed by fixtures to run after each fixture, but not after each test.
+        /// </summary>
+        public static void ClearDatabases()
+        {
+            var context = DependencyInjection.DependencyInjection.Provider.GetService<DonorContext>();
+            context?.Database.ExecuteSqlRaw("TRUNCATE TABLE [Donors]");
+        }
+    }
+}
