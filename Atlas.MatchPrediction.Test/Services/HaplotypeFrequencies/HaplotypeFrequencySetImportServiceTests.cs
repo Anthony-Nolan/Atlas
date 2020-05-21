cac1788794531735b99d396f4837aefa5126c2c7
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2020-05-21 18:11:47 +0100

    feature: ATLAS-15: Stream and store frequencies from CSV file in batches.

--- Atlas.MatchPrediction.Test/Services/HaplotypeFrequencies/HaplotypeFrequencySetImportServiceTests.cs
+++ Atlas.MatchPrediction.Test/Services/HaplotypeFrequencies/HaplotypeFrequencySetImportServiceTests.cs
@@ -7,6 +7,7 @@ using NSubstitute;
 using NSubstitute.ReturnsExtensions;
 using NUnit.Framework;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,7 @@ namespace Atlas.MatchPrediction.Test.Services.HaplotypeFrequencies
 {
     public class HaplotypeFrequencySetImportServiceTests
     {
+        private IHaplotypeFrequenciesStreamReader frequenciesStreamReader;
         private IHaplotypeFrequencySetRepository setRepository;
         private IHaplotypeFrequenciesRepository frequenciesRepository;
 
@@ -23,13 +25,21 @@ namespace Atlas.MatchPrediction.Test.Services.HaplotypeFrequencies
         [SetUp]
         public void Setup()
         {
+            frequenciesStreamReader = Substitute.For<IHaplotypeFrequenciesStreamReader>();
             setRepository = Substitute.For<IHaplotypeFrequencySetRepository>();
             frequenciesRepository = Substitute.For<IHaplotypeFrequenciesRepository>();
+
+            // have to supply a final empty list to prevent infinite loop
+            frequenciesStreamReader.GetFrequencies(Arg.Any<Stream>(), Arg.Any<int>(), Arg.Any<int>())
+                .Returns(new List<HaplotypeFrequency> {new HaplotypeFrequency()}, new List<HaplotypeFrequency>());
 
             setRepository.AddSet(Arg.Any<HaplotypeFrequencySet>())
                 .Returns(new HaplotypeFrequencySet { Id = 1 });
 
-            importService = new HaplotypeFrequencySetImportService(setRepository, frequenciesRepository);
+            importService = new HaplotypeFrequencySetImportService(
+                frequenciesStreamReader,
+                setRepository,
+                frequenciesRepository);
         }
 
         [Test]
@@ -86,7 +96,7 @@ namespace Atlas.MatchPrediction.Test.Services.HaplotypeFrequencies
                 Registry = registry,
                 Ethnicity = ethnicity
             };
-            var existingSet = new HaplotypeFrequencySet {Id = setId};
+            var existingSet = new HaplotypeFrequencySet { Id = setId };
 
             setRepository.GetActiveSet(registry, ethnicity)
                 .Returns(existingSet);
@@ -150,6 +160,70 @@ namespace Atlas.MatchPrediction.Test.Services.HaplotypeFrequencies
                 x.Ethnicity == ethnicity &&
                 x.Name == name &&
                 x.DateTimeAdded.ToString(dateTimeFormat).Equals(today)));
+        }
+
+        [Test]
+        public async Task Import_GetsFrequenciesFromStartOfStream()
+        {
+            await using (var stream = new MemoryStream(Encoding.UTF8.GetBytes("test")))
+            {
+                await importService.Import(new HaplotypeFrequencySetMetaData(), stream);
+            }
+
+            frequenciesStreamReader.Received(1).GetFrequencies(Arg.Any<Stream>(), batchSize: Arg.Any<int>(), startFrom: Arg.Is(0));
+        }
+
+        [Test]
+        public async Task Import_StoresFrequenciesInRepository()
+        {
+            // have to supply empty list to prevent infinite loop
+            var nonEmptyList = new List<HaplotypeFrequency> { new HaplotypeFrequency() };
+            var emptyList = new List<HaplotypeFrequency>();
+            frequenciesStreamReader.GetFrequencies(Arg.Any<Stream>(), Arg.Any<int>(), Arg.Any<int>())
+                .Returns(nonEmptyList, emptyList);
+
+            await using (var stream = new MemoryStream(Encoding.UTF8.GetBytes("test")))
+            {
+                await importService.Import(new HaplotypeFrequencySetMetaData(), stream);
+            }
+
+            await frequenciesRepository.Received(1)
+                .AddHaplotypeFrequencies(Arg.Any<int>(), Arg.Any<IEnumerable<HaplotypeFrequency>>());
+        }
+
+        [Test]
+        public async Task Import_GetFrequenciesWithIncrementedStartFromValue()
+        {
+            var nonEmptyList = new List<HaplotypeFrequency>
+            {
+                new HaplotypeFrequency(),
+                new HaplotypeFrequency()
+            };
+            var emptyList = new List<HaplotypeFrequency>();
+
+            // have to supply final empty list to prevent infinite loop
+            frequenciesStreamReader.GetFrequencies(Arg.Any<Stream>(), Arg.Any<int>(), Arg.Any<int>())
+                .Returns(nonEmptyList, emptyList);
+
+            await using (var stream = new MemoryStream(Encoding.UTF8.GetBytes("test")))
+            {
+                await importService.Import(new HaplotypeFrequencySetMetaData(), stream);
+            }
+
+            frequenciesStreamReader.Received(1).GetFrequencies(Arg.Any<Stream>(), Arg.Any<int>(), Arg.Is(nonEmptyList.Count));
+        }
+
+        [Test]
+        public void Import_NoFrequencies_ThrowsException()
+        {
+            frequenciesStreamReader.GetFrequencies(Arg.Any<Stream>(), Arg.Any<int>(), Arg.Any<int>())
+                .Returns(new List<HaplotypeFrequency>());
+
+            using (var stream = new MemoryStream(Encoding.UTF8.GetBytes("test")))
+            {
+                importService.Invoking(service => service.Import(new HaplotypeFrequencySetMetaData(), stream))
+                    .Should().Throw<Exception>();
+            }
         }
     }
 }
