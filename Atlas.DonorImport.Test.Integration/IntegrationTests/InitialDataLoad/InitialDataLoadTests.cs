03495a80cb3ed867cc611528dece5318c366f58b
Ben Below <benjamin.below@softwire.com>
2020-05-21 18:21:11 +0100

    feature: ATLAS-167: Add integration tests for donor import - initial, additions only

---
+++ Atlas.DonorImport.Test.Integration/IntegrationTests/InitialDataLoad/InitialDataLoadTests.cs
@@ -0,0 +1,58 @@
+using System.Reflection;
+using System.Threading.Tasks;
+using Atlas.DonorImport.Services;
+using Atlas.DonorImport.Test.Integration.TestHelpers;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+
+namespace Atlas.DonorImport.Test.Integration.IntegrationTests.InitialDataLoad
+{
+    [TestFixture]
+    public class InitialDataLoadTests
+    {
+        private IDonorInspectionRepository donorRepository;
+        
+        private IDonorFileImporter donorFileImporter;
+        
+        [OneTimeSetUp]
+        public async Task OneTimeSetUp()
+        {
+            donorRepository = DependencyInjection.DependencyInjection.Provider.GetService<IDonorInspectionRepository>();
+            donorFileImporter = DependencyInjection.DependencyInjection.Provider.GetService<IDonorFileImporter>();
+            // Run operation under test once for this fixture, to (a) improve performance (b) remove the need to clean up duplicate ids between runs
+            await ImportFile();
+        }
+
+        [Test]
+        public async Task ImportDonors_ForAllAdditions_AddsAllDonorsToDatabase()
+        {
+            var importedDonors = await donorRepository.DonorCount();
+
+            importedDonors.Should().Be(1000);
+        }
+
+        /// <summary>
+        /// Snapshot test of an arbitrary donor, to test mapping and plumbing working as expected
+        /// </summary>
+        /// <returns></returns>
+        [Test]
+        public async Task ImportDonors_NewDonor_IsAddedCorrectly()
+        {
+            const string selectedDonorId = "1";
+            const string expectedDonorHash = "eZOqelWRzPRjIlO/E8b6OQ=="; 
+
+            var actualDonor = await donorRepository.GetDonor(selectedDonorId);
+            
+            actualDonor.Hash.Should().Be(expectedDonorHash);
+            actualDonor.CalculateHash().Should().Be(expectedDonorHash);
+        }
+
+        private async Task ImportFile()
+        {
+            const string donorTestFile = "Atlas.DonorImport.Test.Integration.IntegrationTests.InitialDataLoad.1000-initial-donors.json";
+            await using var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(donorTestFile);
+            await donorFileImporter.ImportDonorFile(stream, donorTestFile);
+        }
+    }
+}
