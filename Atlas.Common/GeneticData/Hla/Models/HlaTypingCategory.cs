6e0a556dbca0a945f49fa72bd119502b2a64e136
Mike.McLean <Mike.McLean@softwire.com>
2020-05-21 17:14:05 +0100

    refactor: ATLAS-46: HlaTypingCategory: Allow for flexible decoding from the previous values and object structures

--- Atlas.Common/GeneticData/Hla/Models/HlaTypingCategory.cs
+++ Atlas.Common/GeneticData/Hla/Models/HlaTypingCategory.cs
@@ -1,17 +1,81 @@
-﻿using Newtonsoft.Json;
+﻿using System;
+using EnumStringValues;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 
 namespace Atlas.Common.GeneticData.Hla.Models
 {
+    internal class TargetedFlexibleEnumConverter<TEnum> : StringEnumConverter where TEnum : Enum
+    {
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            try
+            {
+                return base.ReadJson(reader, objectType, existingValue, serializer);
+            }
+            catch (JsonSerializationException e)
+            {
+                if (e.Message.StartsWith("Error converting value"))
+                {
+                    try
+                    {
+                        return reader.Value.ToString().ParseToEnum<TEnum>();
+                    }
+                    catch (Exception)
+                    {
+                        // Do nothing and allow the previous Exception to be throw.
+                    }
+                }
+
+                throw;
+            }
+        }
+    }
+
+    [JsonConverter(typeof(TargetedFlexibleEnumConverter<HlaTypingCategory>))]
     public enum HlaTypingCategory
     {
+        /// <summary>
+        /// Typed as the fully-qualified Allele
+        /// </summary>
+        [StringValue(nameof(Allele), true), StringValue("OriginalAllele")]
         Allele,
+
+        /// <summary>
+        /// Typed as a P-Group
+        /// </summary>
         GGroup,
+
+        /// <summary>
+        /// Typed as a G-Group
+        /// </summary>
         PGroup,
+
+        /// <summary>
+        /// Typed as a string of arbitrary alleles e.g. 01:02/01:03/02:04
+        /// </summary>
+        [StringValue(nameof(AlleleStringOfNames), true), StringValue("MultipleAlleles")]
         AlleleStringOfNames,
+
+        /// <summary>
+        /// Typed as a string of subtypes with common first field. e.g. 01:02/03/04
+        /// </summary>
         AlleleStringOfSubtypes,
+
+        /// <summary>
+        /// Typed as the compressed allele name, as used in NMDP codes.
+        /// </summary>
+        [StringValue(nameof(NmdpCode), true), StringValue("NmdpCodeAllele")]
         NmdpCode,
+
+        /// <summary>
+        /// Typed as the XX code first field value.
+        /// </summary>
         XxCode,
+
+        /// <summary>
+        /// Typed as the original serology name
+        /// </summary>
         Serology
     }
 
