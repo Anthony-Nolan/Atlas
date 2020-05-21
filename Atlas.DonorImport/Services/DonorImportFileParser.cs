ca2b4bd02668462e2e6bf6393301b48fb740095a
Ben Below <benjamin.below@softwire.com>
2020-05-21 09:06:16 +0100

    feature: ATLAS-167: Write donors from file to sql database
    - currently writes one donor at a time, but repository is set up to handle bulk inserts

--- Atlas.DonorImport/Services/DonorImportFileParser.cs
+++ Atlas.DonorImport/Services/DonorImportFileParser.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Threading.Tasks;
 using Atlas.DonorImport.Models.FileSchema;
 using Newtonsoft.Json;
 
@@ -8,41 +9,41 @@ namespace Atlas.DonorImport.Services
 {
     internal interface IDonorImportFileParser
     {
-        public IEnumerable<DonorUpdate> LazilyParseDonorUpdates(Stream stream);
+        public IAsyncEnumerable<DonorUpdate> LazilyParseDonorUpdates(Stream stream);
     }
 
     internal class DonorImportFileParser : IDonorImportFileParser
     {
-        public IEnumerable<DonorUpdate> LazilyParseDonorUpdates(Stream stream)
+        public async IAsyncEnumerable<DonorUpdate> LazilyParseDonorUpdates(Stream stream)
         {
             using var streamReader = new StreamReader(stream);
             using var reader = new JsonTextReader(streamReader);
             var serializer = new JsonSerializer();
 
             // Loops through top level JSON
-            while (reader.Read())
+            while (await reader.ReadAsync())
             {
                 if (reader.TokenType == JsonToken.PropertyName)
                 {
-                    var propertyName = reader.Value.ToString();
+                    var propertyName = reader.Value?.ToString();
                     switch (propertyName)
                     {
                         case "updateMode":
                             // Read into property
-                            reader.Read();
+                            await reader.ReadAsync();
                             var updateMode = serializer.Deserialize<string>(reader);
                             // We do not yet care about the update mode
                             break;
                         case "donors":
-                            reader.Read(); // Read into property
-                            reader.Read(); // Read into array. Do not deserialize to collection, as the collection can be very large and requires streaming.
+                            await reader.ReadAsync(); // Read into property
+                            await reader.ReadAsync(); // Read into array. Do not deserialize to collection, as the collection can be very large and requires streaming.
 
                             // Loops through all donors in array
                             do
                             {
                                 var donorOperation = serializer.Deserialize<DonorUpdate>(reader);
                                 yield return donorOperation;
-                            } while (reader.Read() && reader.TokenType != JsonToken.EndArray);
+                            } while (await reader.ReadAsync() && reader.TokenType != JsonToken.EndArray);
 
                             break;
                         default:
