73080b06ec3b929b34a36fe12da6959eac0813af
Ben Below <benjamin.below@softwire.com>
2020-05-22 13:54:15 +0100

    feature: ATLAS-167: Publish updates to matching algorithm topic
    - disable for initial load of donors, as performance of a full donor set import via incremental updates is not going to be fast enough - in this case a full data refresh will need to be run.
    
    # Conflicts:
    #	Atlas.DonorImport/Services/DonorFileImporter.cs
    #	Atlas.DonorImport/Services/DonorImportFileParser.cs
    #	Atlas.DonorImport/Services/DonorRecordChangeApplier.cs

---
+++ Atlas.DonorImport.Test/TestHelpers/Builders/DonorImportStreamBuilder.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Atlas.DonorImport.Models.FileSchema;
+using Newtonsoft.Json;
+
+namespace Atlas.DonorImport.Test.TestHelpers.Builders
+{
+    internal static class DonorImportStreamBuilder
+    {
+        public static Stream BuildFileStream(IEnumerable<DonorUpdate> donorUpdates)
+        {
+            var file = new DonorFile {donors = donorUpdates};
+            var fileJson = JsonConvert.SerializeObject(file);
+            return new MemoryStream(Encoding.Default.GetBytes(fileJson));
+        }
+
+        public static Stream BuildFileStream(int numberOfDonors)
+        {
+            var donorUpdates = Enumerable.Range(0, numberOfDonors).Select(i => DonorUpdateBuilder.New.Build());
+            return BuildFileStream(donorUpdates);
+        }
+
+        private class DonorFile
+        {
+            // ReSharper disable once InconsistentNaming
+            public IEnumerable<DonorUpdate> donors;
+        }
+    }
+}
