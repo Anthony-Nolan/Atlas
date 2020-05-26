1a0aa07202ae203e6718bbaee45e92ab2fa7cdbc
Ben Below <benjamin.below@softwire.com>
2020-05-26 14:26:31 +0100

    feature: ATLAS-267: Add max length to donor ethnicity and registry codes

---
+++ Atlas.DonorImport.Data/Migrations/20200526132537_AddMaxLengthForDonorEthnicityAndRegistryCodes.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Atlas.DonorImport.Data.Migrations
+{
+    public partial class AddMaxLengthForDonorEthnicityAndRegistryCodes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "RegistryCode",
+                table: "Donors",
+                maxLength: 256,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EthnicityCode",
+                table: "Donors",
+                maxLength: 256,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "RegistryCode",
+                table: "Donors",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 256,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EthnicityCode",
+                table: "Donors",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 256,
+                oldNullable: true);
+        }
+    }
+}
