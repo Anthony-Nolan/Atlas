6ca8f365af08b5907c5fc2155711879158e51259
Ben Below <benjamin.below@softwire.com>
2020-05-26 15:09:27 +0100

    fix: ATLAS-15: Test fixes
    - Ensure new set is actually marked as active
    - Remove redundant positioning of stream

--- Atlas.MatchPrediction.Data/Repositories/HaplotypeFrequencySetRepository.cs
+++ Atlas.MatchPrediction.Data/Repositories/HaplotypeFrequencySetRepository.cs
@@ -1,15 +1,14 @@
-﻿using Atlas.MatchPrediction.Data.Context;
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Atlas.MatchPrediction.Data.Context;
 using Atlas.MatchPrediction.Data.Models;
 using Microsoft.EntityFrameworkCore;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Atlas.MatchPrediction.Data.Repositories
 {
     public interface IHaplotypeFrequencySetRepository
     {
         Task<HaplotypeFrequencySet> GetActiveSet(string registry, string ethnicity);
-        Task DeactivateSet(HaplotypeFrequencySet set);
         Task<HaplotypeFrequencySet> AddSet(HaplotypeFrequencySet set);
         Task ActivateSet(int setId);
     }
@@ -30,13 +29,6 @@ namespace Atlas.MatchPrediction.Data.Repositories
                 .SingleOrDefaultAsync();
         }
 
-        public async Task DeactivateSet(HaplotypeFrequencySet set)
-        {
-            set.Active = false;
-            context.HaplotypeFrequencySets.Update(set);
-            await context.SaveChangesAsync();
-        }
-
         public async Task<HaplotypeFrequencySet> AddSet(HaplotypeFrequencySet set)
         {
             await context.HaplotypeFrequencySets.AddAsync(set);
@@ -44,12 +36,16 @@ namespace Atlas.MatchPrediction.Data.Repositories
             return set;
         }
 
-        // TODO: Integration tests for this
+        // TODO: ATLAS-15: Integration tests for this
         public async Task ActivateSet(int setId)
         {
             var set = await context.HaplotypeFrequencySets.SingleAsync(s => s.Id == setId);
             set.Active = true;
-            var otherMatchingSets = context.HaplotypeFrequencySets.Where(s => s.Ethnicity == set.Ethnicity && s.Registry == set.Registry);
+            var otherMatchingSets = context.HaplotypeFrequencySets.Where(s =>
+                s.Id != setId
+                && s.Ethnicity == set.Ethnicity
+                && s.Registry == set.Registry
+            );
             foreach (var otherMatchingSet in otherMatchingSets)
             {
                 otherMatchingSet.Active = false;
