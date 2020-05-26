32aeda199b2059ae98782e0b9ef6d847b4102b7b
Mike.McLean <Mike.McLean@softwire.com>
2020-05-26 07:57:19 +0100

    fix: ATLAS-46: Extension method to render types nicely.

---
+++ Atlas.Common/Utils/Extensions/TypeExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace Atlas.Common.Utils.Extensions
+{
+    public static class TypeExtensions
+    {
+        public static string GetNeatCSharpName<T>()
+        {
+            return typeof(T).GetNeatCSharpName();
+        }
+
+        public static string GetNeatCSharpName(this Type t)
+        {
+            if (!t.IsGenericType)
+            {
+                return t.Name;
+            }
+
+            var genericArgs = t
+                .GetGenericArguments()
+                .Select(GetNeatCSharpName)
+                .StringJoin(", ");
+            string trimmedGenericBaseName = TrimGenericArgMarkerFromTypeName(t.Name);
+
+            return $"{trimmedGenericBaseName}<{genericArgs}>";
+        }
+
+        private static string TrimGenericArgMarkerFromTypeName(string typeName)
+        {
+            var genericMarkerLocation = typeName.IndexOf("`");
+            if (genericMarkerLocation > -1)
+            {
+                return typeName.Substring(0, genericMarkerLocation);
+            }
+
+            return typeName;
+        }
+
+    }
+}
