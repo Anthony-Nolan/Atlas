7c44f8fc13ecd80cb1e57ffa3540eb3d390901f7
Ben Below <benjamin.below@softwire.com>
2020-05-26 09:00:41 +0100

    fix: ATLAS-15: Correct app setting key name for match prediction import container

--- terraform/modules/match_prediction/function.tf
+++ terraform/modules/match_prediction/function.tf
@@ -12,15 +12,15 @@ resource "azurerm_function_app" "atlas_match_prediction_function" {
   app_settings = {
     // APPINSIGHTS_INSTRUMENTATIONKEY
     //      The azure functions dashboard requires the instrumentation key with this name to integrate with application insights.
-    "ApplicationInsights:InstrumentationKey"              = var.application_insights.instrumentation_key
-    "APPINSIGHTS_INSTRUMENTATIONKEY"                      = var.application_insights.instrumentation_key
-    "ApplicationInsights:LogLevel"                        = var.APPLICATION_INSIGHTS_LOG_LEVEL
-    "AzureStorage:ConnectionString"                       = var.azure_storage.primary_connection_string
-    "AzureStorage:HaplotypeFrequencySetBlobBlobContainer" = azurerm_storage_container.haplotype_frequency_set_blob_container.name
-    "NotificationsServiceBus:ConnectionString"            = var.servicebus_namespace_authorization_rules.write-only.primary_connection_string
-    "NotificationsServiceBus:AlertsTopic"                 = var.servicebus_topics.alerts.name
-    "NotificationsServiceBus:NotificationsTopic"          = var.servicebus_topics.notifications.name
-    "WEBSITE_MAX_DYNAMIC_APPLICATION_SCALE_OUT"           = "1"
-    "WEBSITE_RUN_FROM_PACKAGE"                            = var.WEBSITE_RUN_FROM_PACKAGE
+    "ApplicationInsights:InstrumentationKey"            = var.application_insights.instrumentation_key
+    "APPINSIGHTS_INSTRUMENTATIONKEY"                    = var.application_insights.instrumentation_key
+    "ApplicationInsights:LogLevel"                      = var.APPLICATION_INSIGHTS_LOG_LEVEL
+    "AzureStorage:ConnectionString"                     = var.azure_storage.primary_connection_string
+    "AzureStorage:HaplotypeFrequencySetImportContainer" = azurerm_storage_container.haplotype_frequency_set_blob_container.name
+    "NotificationsServiceBus:ConnectionString"          = var.servicebus_namespace_authorization_rules.write-only.primary_connection_string
+    "NotificationsServiceBus:AlertsTopic"               = var.servicebus_topics.alerts.name
+    "NotificationsServiceBus:NotificationsTopic"        = var.servicebus_topics.notifications.name
+    "WEBSITE_MAX_DYNAMIC_APPLICATION_SCALE_OUT"         = "1"
+    "WEBSITE_RUN_FROM_PACKAGE"                          = var.WEBSITE_RUN_FROM_PACKAGE
   }
 }
