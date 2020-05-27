43852af12b14fd96d13bc6ad65db09a7637c5154
Mike.McLean <Mike.McLean@softwire.com>
2020-05-27 19:03:55 +0100

    fix: ATLAS-282: First portion of removing Backwards Compatibility. Still need to move file.

--- Atlas.Common/GeneticData/Hla/Models/HlaTypingCategory.cs
+++ Atlas.Common/GeneticData/Hla/Models/HlaTypingCategory.cs
@@ -1,48 +1,14 @@
-﻿using System;
-using EnumStringValues;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 
 namespace Atlas.Common.GeneticData.Hla.Models
 {
-    /// <summary>
-    /// EnumConverter that recognizes the StringValue attributes and thus can parse out-dated representations of the Enum.
-    /// </summary>
-    /// <typeparam name="TEnum"></typeparam>
-    internal class TargetedFlexibleEnumConverter<TEnum> : StringEnumConverter where TEnum : Enum
-    {
-        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
-        {
-            try
-            {
-                return base.ReadJson(reader, objectType, existingValue, serializer);
-            }
-            catch (JsonSerializationException e)
-            {
-                if (e.Message.StartsWith("Error converting value"))
-                {
-                    try
-                    {
-                        return reader.Value.ToString().ParseToEnum<TEnum>();
-                    }
-                    catch (Exception)
-                    {
-                        // Do nothing and allow the previous Exception to be throw.
-                    }
-                }
-
-                throw;
-            }
-        }
-    }
-
-    [JsonConverter(typeof(TargetedFlexibleEnumConverter<HlaTypingCategory>))]//TODO: ATLAS-282 All of this stuff about parsing old enum values should be removed.
+    [JsonConverter(typeof(StringEnumConverter))]
     public enum HlaTypingCategory
     {
         /// <summary>
         /// Typed as the fully-qualified Allele
         /// </summary>
-        [StringValue(nameof(Allele), true), StringValue("OriginalAllele")]
         Allele,
 
         /// <summary>
@@ -58,7 +24,6 @@ namespace Atlas.Common.GeneticData.Hla.Models
         /// <summary>
         /// Typed as a string of arbitrary alleles e.g. 01:02/01:03/02:04
         /// </summary>
-        [StringValue(nameof(AlleleStringOfNames), true), StringValue("MultipleAlleles")]
         AlleleStringOfNames,
 
         /// <summary>
@@ -69,7 +34,6 @@ namespace Atlas.Common.GeneticData.Hla.Models
         /// <summary>
         /// Typed as the compressed allele name, as used in NMDP codes.
         /// </summary>
-        [StringValue(nameof(NmdpCode), true), StringValue("NmdpCodeAllele")]
         NmdpCode,
 
         /// <summary>
@@ -102,5 +66,4 @@ namespace Atlas.Common.GeneticData.Hla.Models
             return TypingMethod.Molecular;
         }
     }
-
 }
