2e32fdc89bddc8bc219d92adb456a31682405569
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2020-05-27 15:24:52 +0100

    fix: ATLAS-295: Store haplotype frequency to 20 d.p.

---
+++ Atlas.MatchPrediction.Data/Migrations/20200527141834_StoreFrequencyTo20DecimalPlaces.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Atlas.MatchPrediction.Data.Migrations
+{
+    public partial class StoreFrequencyTo20DecimalPlaces : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Frequency",
+                table: "HaplotypeFrequencies",
+                type: "decimal(21,20)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Frequency",
+                table: "HaplotypeFrequencies",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(21,20)");
+        }
+    }
+}
