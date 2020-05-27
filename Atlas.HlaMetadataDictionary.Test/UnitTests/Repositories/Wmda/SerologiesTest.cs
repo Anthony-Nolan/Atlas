c24d17f5448e401fe6ce23600e443456dcefe1fe
Mike.McLean <Mike.McLean@softwire.com>
2020-05-27 14:25:51 +0100

    refactor: ATLAS-46: Migrate Test Code to new test project

---
+++ Atlas.HlaMetadataDictionary.Test/UnitTests/Repositories/Wmda/SerologiesTest.cs
@@ -0,0 +1,56 @@
+﻿using System.Linq;
+using ApprovalTests;
+using ApprovalTests.Reporters;
+using Atlas.Common.GeneticData.Hla.Models;
+using Atlas.HlaMetadataDictionary.Models.Wmda;
+using NUnit.Framework;
+
+namespace Atlas.HlaMetadataDictionary.Test.UnitTests.Repositories.Wmda
+{
+    [UseReporter(typeof(DiffReporter))]
+    internal class SerologiesTest : WmdaRepositoryTestBase<HlaNom>
+    {
+        protected override void SetupTestData()
+        {
+            SetTestData(WmdaDataRepository.GetWmdaDataset(HlaDatabaseVersionToTest).Serologies, SerologyLoci);
+        }
+
+        [TestCase("DQ", "1")]
+        [TestCase("A", "29")]
+        [TestCase("B", "703")]
+        [TestCase("DR", "1404")]
+        public void WmdaDataRepository_WhenValidSerology_SuccessfullyCaptured(string locus, string serologyName)
+        {
+            var expectedSerology = new HlaNom(TypingMethod.Serology, locus, serologyName);
+
+            var actualSerology = GetSingleWmdaHlaTyping(locus, serologyName);
+            
+            Assert.AreEqual(expectedSerology, actualSerology);
+        }
+
+        [Test]
+        public void WmdaDataRepository_WhenDeletedSerology_SuccessfullyCaptured()
+        {
+            const string locus = "Cw";
+            const string serologyName = "11";
+            const string identicalHla = "1";
+
+            var expectedSerology = new HlaNom(TypingMethod.Serology, locus, serologyName, true, identicalHla);
+
+            var actualSerology = GetSingleWmdaHlaTyping(locus, serologyName);
+            
+            Assert.AreEqual(expectedSerology, actualSerology);
+        }
+
+        [Test]
+        public void WmdaDataRepository_SerologiesCollection_ContainsAllExpectedSerologies()
+        {
+            var str = string.Join("\r\n", WmdaHlaTypings
+                .OrderBy(s => s.TypingLocus)
+                .ThenBy(s => int.Parse(s.Name))
+                .Select(s => $"{s.TypingLocus}\t{s.Name}")
+                .ToList());
+            Approvals.Verify(str);
+        }
+    }
+}
