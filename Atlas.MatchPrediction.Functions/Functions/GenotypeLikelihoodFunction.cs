97e31a1fc51b0e9dfab5a0647f7b4b943ea31c2e
Tom Johnson <tom.johnson@softwire.com>
2020-05-27 14:40:04 +0100

    feature: ATLAS-39: Added service and response failure

--- Atlas.MatchPrediction.Functions/Functions/GenotypeLikelihoodFunction.cs
+++ Atlas.MatchPrediction.Functions/Functions/GenotypeLikelihoodFunction.cs
@@ -1,7 +1,10 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Threading.Tasks;
+using FluentValidation;
 using Atlas.Common.GeneticData.PhenotypeInfo;
-using Atlas.MatchPrediction.Functions.Models;
+using Atlas.MatchPrediction.Client.Models.GenotypeLikelihood;
+using Atlas.MatchPrediction.Services.GenotypeLikelihood;
 using AzureFunctions.Extensions.Swashbuckle.Attribute;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -13,13 +16,29 @@ namespace Atlas.MatchPrediction.Functions.Functions
 {
     public class GenotypeLikelihoodFunction
     {
+        private readonly IGenotypeLikelihoodService genotypeLikelihoodService;
+
+        public GenotypeLikelihoodFunction(IGenotypeLikelihoodService genotypeLikelihoodService)
+        {
+            this.genotypeLikelihoodService = genotypeLikelihoodService;
+        }
+
         [FunctionName(nameof(CalculateGenotypeLikelihood))]
         public async Task<IActionResult> CalculateGenotypeLikelihood([HttpTrigger(AuthorizationLevel.Function, "post")]
             [RequestBodyType(typeof(PhenotypeInfo<string>), "genotype info")] HttpRequest request)
         {
             var genotype = JsonConvert.DeserializeObject<PhenotypeInfo<string>>(await new StreamReader(request.Body).ReadToEndAsync());
 
-            return new JsonResult(new GenotypeLikelihoodResponse() { Likelihood = 1 });
+            try
+            {
+                var likelihood = genotypeLikelihoodService.CalculateLikelihood(genotype);
+                return new JsonResult(new GenotypeLikelihoodResponse() { Likelihood = likelihood });
+            }
+            catch (Exception e)
+            {
+                return new BadRequestObjectResult(e);
+            }
+
         }
     }
 }
---
+++ Atlas.MatchPrediction/Services/GenotypeLikelihood/GenotypeLikelihoodService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Atlas.Common.GeneticData.PhenotypeInfo;
+
+namespace Atlas.MatchPrediction.Services.GenotypeLikelihood
+{
+    public interface IGenotypeLikelihoodService
+    {
+        public decimal CalculateLikelihood(PhenotypeInfo<string> genotype);
+    }
+
+    public class GenotypeLikelihoodService : IGenotypeLikelihoodService
+    {
+        public decimal CalculateLikelihood(PhenotypeInfo<string> genotype)
+        {
+            return 1;
+        }
+    }
+}
