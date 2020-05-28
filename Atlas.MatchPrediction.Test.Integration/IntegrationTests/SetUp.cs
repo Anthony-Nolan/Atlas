507dab5cf0aab5745ab38556f15f775697d5dc5e
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2020-05-28 13:41:31 +0100

    test: ATLAS-15: Setup integration test project with a single Token test.

---
+++ Atlas.MatchPrediction.Test.Integration/IntegrationTests/SetUp.cs
@@ -0,0 +1,23 @@
+﻿using Atlas.MatchPrediction.Test.Integration.DependencyInjection;
+using Atlas.MatchPrediction.Test.Integration.TestHelpers;
+using NUnit.Framework;
+
+namespace Atlas.MatchPrediction.Test.Integration.IntegrationTests
+{
+    [SetUpFixture]
+    public class IntegrationTestSetUp
+    {
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            DependencyInjection.DependencyInjection.Provider = ServiceConfiguration.CreateProvider();
+            ResetDatabase();
+        }
+
+        private static void ResetDatabase()
+        {
+            DatabaseManager.SetupDatabase();
+            DatabaseManager.ClearDatabases();
+        }
+    }
+}
---
+++ Atlas.MatchPrediction.Test.Integration/IntegrationTests/Tests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace Atlas.MatchPrediction.Test.Integration.IntegrationTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TokenTest()
+        {
+            Assert.Pass();
+        }
+    }
+}
