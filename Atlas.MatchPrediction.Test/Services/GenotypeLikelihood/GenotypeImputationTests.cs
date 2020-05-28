222b33249a8969ae1c1a8b28943f98b951d8238d
Tom Johnson <tom.johnson@softwire.com>
2020-05-28 16:35:44 +0100

    Test

--- Atlas.MatchPrediction.Test/Services/GenotypeLikelihood/GenotypeImputationTests.cs
+++ Atlas.MatchPrediction.Test/Services/GenotypeLikelihood/GenotypeImputationTests.cs
@@ -1,4 +1,7 @@
-﻿using Atlas.Common.GeneticData.PhenotypeInfo;
+﻿using System.Collections;
+using System.Collections.Generic;
+using Atlas.Common.GeneticData;
+using Atlas.Common.GeneticData.PhenotypeInfo;
 using Atlas.MatchPrediction.Services.GenotypeLikelihood;
 using FluentAssertions;
 using NUnit.Framework;
@@ -16,66 +19,223 @@ namespace Atlas.MatchPrediction.Test.Services.GenotypeLikelihood
             genotypeImputation = new GenotypeImputation();
         }
 
+
         [Test]
         public void GetPossibleDiplotypes_WhenGenotypeHasAllLociAndNoHomozygousCases_Returns16Diplotypes()
         {
             var genotype = new PhenotypeInfo<string>
             {
-                A = {Position1 = "*01:01", Position2 = "*03:01" },
-                B = { Position1 = "*08:01", Position2 = "*07:02" },
-                C = { Position1 = "*07:01", Position2 = "*07:02" },
-                Dqb1 = { Position1 = "*02:01", Position2 = "*06:02" },
-                Drb1 = { Position1 = "*03:01", Position2 = "*15:01" }
+                A = {Position1 = "A-1", Position2 = "A-2" },
+                B = { Position1 = "B-1", Position2 = "B-2" },
+                C = { Position1 = "C-1", Position2 = "C-2" },
+                Dqb1 = { Position1 = "Dqb1-1", Position2 = "Dqb1-2" },
+                Drb1 = { Position1 = "Drb1-1", Position2 = "Drb1-2" }
             };
 
-            var diplotypes = genotypeImputation.GetPossibleDiplotypes(genotype);
-            diplotypes.Count.Should().Be(16);
+            var diplotypesFromGenotype = genotypeImputation.GetPossibleDiplotypes(genotype);
+
+            var diplotypes = new List<PhenotypeInfo<string>>()
+            {
+                new PhenotypeInfo<string>
+                {
+                    A = {Position1 = "A-2", Position2 = "A-1" },
+                    B = { Position1 = "B-1", Position2 = "B-2" },
+                    C = { Position1 = "C-1", Position2 = "C-2" },
+                    Dqb1 = { Position1 = "Dqb1-1", Position2 = "Dqb1-2" },
+                    Drb1 = { Position1 = "Drb1-1", Position2 = "Drb1-2" }
+                },
+                new PhenotypeInfo<string>
+                {
+                    A = {Position1 = "A-2", Position2 = "A-1" },
+                    B = { Position1 = "B-1", Position2 = "B-2" },
+                    C = { Position1 = "C-1", Position2 = "C-2" },
+                    Dqb1 = { Position1 = "Dqb1-1", Position2 = "Dqb1-2" },
+                    Drb1 = { Position1 = "Drb1-2", Position2 = "Drb1-1" }
+                },
+                new PhenotypeInfo<string>
+                {
+                    A = {Position1 = "A-2", Position2 = "A-1" },
+                    B = { Position1 = "B-1", Position2 = "B-2" },
+                    C = { Position1 = "C-1", Position2 = "C-2" },
+                    Dqb1 = { Position1 = "Dqb1-2", Position2 = "Dqb1-1" },
+                    Drb1 = { Position1 = "Drb1-1", Position2 = "Drb1-2" }
+                },
+                new PhenotypeInfo<string>
+                {
+                    A = {Position1 = "A-2", Position2 = "A-1" },
+                    B = { Position1 = "B-1", Position2 = "B-2" },
+                    C = { Position1 = "C-1", Position2 = "C-2" },
+                    Dqb1 = { Position1 = "Dqb1-2", Position2 = "Dqb1-1" },
+                    Drb1 = { Position1 = "Drb1-2", Position2 = "Drb1-1" }
+                },
+                new PhenotypeInfo<string>
+                {
+                    A = {Position1 = "A-2", Position2 = "A-1" },
+                    B = { Position1 = "B-1", Position2 = "B-2" },
+                    C = { Position1 = "C-2", Position2 = "C-1" },
+                    Dqb1 = { Position1 = "Dqb1-1", Position2 = "Dqb1-2" },
+                    Drb1 = { Position1 = "Drb1-1", Position2 = "Drb1-2" }
+                },
+                new PhenotypeInfo<string>
+                {
+                    A = {Position1 = "A-2", Position2 = "A-1" },
+                    B = { Position1 = "B-1", Position2 = "B-2" },
+                    C = { Position1 = "C-2", Position2 = "C-1" },
+                    Dqb1 = { Position1 = "Dqb1-1", Position2 = "Dqb1-2" },
+                    Drb1 = { Position1 = "Drb1-2", Position2 = "Drb1-1" }
+                },
+                new PhenotypeInfo<string>
+                {
+                    A = {Position1 = "A-2", Position2 = "A-1" },
+                    B = { Position1 = "B-1", Position2 = "B-2" },
+                    C = { Position1 = "C-2", Position2 = "C-1" },
+                    Dqb1 = { Position1 = "Dqb1-2", Position2 = "Dqb1-1" },
+                    Drb1 = { Position1 = "Drb1-1", Position2 = "Drb1-2" }
+                },
+                new PhenotypeInfo<string>
+                {
+                    A = {Position1 = "A-2", Position2 = "A-1" },
+                    B = { Position1 = "B-1", Position2 = "B-2" },
+                    C = { Position1 = "C-2", Position2 = "C-1" },
+                    Dqb1 = { Position1 = "Dqb1-2", Position2 = "Dqb1-1" },
+                    Drb1 = { Position1 = "Drb1-2", Position2 = "Drb1-1" }
+                },
+                new PhenotypeInfo<string>
+                {
+                    A = {Position1 = "A-2", Position2 = "A-1" },
+                    B = { Position1 = "B-2", Position2 = "B-1" },
+                    C = { Position1 = "C-1", Position2 = "C-2" },
+                    Dqb1 = { Position1 = "Dqb1-1", Position2 = "Dqb1-2" },
+                    Drb1 = { Position1 = "Drb1-1", Position2 = "Drb1-2" }
+                },
+                new PhenotypeInfo<string>
+                {
+                    A = {Position1 = "A-2", Position2 = "A-1" },
+                    B = { Position1 = "B-2", Position2 = "B-1" },
+                    C = { Position1 = "C-1", Position2 = "C-2" },
+                    Dqb1 = { Position1 = "Dqb1-1", Position2 = "Dqb1-2" },
+                    Drb1 = { Position1 = "Drb1-2", Position2 = "Drb1-1" }
+                },
+                new PhenotypeInfo<string>
+                {
+                    A = {Position1 = "A-2", Position2 = "A-1" },
+                    B = { Position1 = "B-2", Position2 = "B-1" },
+                    C = { Position1 = "C-1", Position2 = "C-2" },
+                    Dqb1 = { Position1 = "Dqb1-2", Position2 = "Dqb1-1" },
+                    Drb1 = { Position1 = "Drb1-1", Position2 = "Drb1-2" }
+                },
+                new PhenotypeInfo<string>
+                {
+                    A = {Position1 = "A-2", Position2 = "A-1" },
+                    B = { Position1 = "B-2", Position2 = "B-1" },
+                    C = { Position1 = "C-1", Position2 = "C-2" },
+                    Dqb1 = { Position1 = "Dqb1-2", Position2 = "Dqb1-1" },
+                    Drb1 = { Position1 = "Drb1-2", Position2 = "Drb1-1" }
+                },
+                new PhenotypeInfo<string>
+                {
+                    A = {Position1 = "A-2", Position2 = "A-1" },
+                    B = { Position1 = "B-2", Position2 = "B-1" },
+                    C = { Position1 = "C-2", Position2 = "C-1" },
+                    Dqb1 = { Position1 = "Dqb1-1", Position2 = "Dqb1-2" },
+                    Drb1 = { Position1 = "Drb1-1", Position2 = "Drb1-2" }
+                },
+                new PhenotypeInfo<string>
+                {
+                    A = {Position1 = "A-2", Position2 = "A-1" },
+                    B = { Position1 = "B-2", Position2 = "B-1" },
+                    C = { Position1 = "C-2", Position2 = "C-1" },
+                    Dqb1 = { Position1 = "Dqb1-1", Position2 = "Dqb1-2" },
+                    Drb1 = { Position1 = "Drb1-2", Position2 = "Drb1-1" }
+                },
+                new PhenotypeInfo<string>
+                {
+                    A = {Position1 = "A-2", Position2 = "A-1" },
+                    B = { Position1 = "B-2", Position2 = "B-1" },
+                    C = { Position1 = "C-2", Position2 = "C-1" },
+                    Dqb1 = { Position1 = "Dqb1-2", Position2 = "Dqb1-1" },
+                    Drb1 = { Position1 = "Drb1-1", Position2 = "Drb1-2" }
+                },
+                new PhenotypeInfo<string>
+                {
+                    A = {Position1 = "A-2", Position2 = "A-1" },
+                    B = { Position1 = "B-2", Position2 = "B-1" },
+                    C = { Position1 = "C-2", Position2 = "C-1" },
+                    Dqb1 = { Position1 = "Dqb1-2", Position2 = "Dqb1-1" },
+                    Drb1 = { Position1 = "Drb1-2", Position2 = "Drb1-1" }
+                },
+            };
+
+            diplotypes.Should().BeEquivalentTo(diplotypesFromGenotype);
         }
 
-        [Test]
-        public void GetPossibleDiplotypes_WhenGenotypeHasNoCLocusAndNoHomozygousCases_Returns8Diplotypes()
+        [TestCase(Locus.C)]
+        [TestCase(Locus.Dqb1)]
+        public void GetPossibleDiplotypes_WhenGenotypeHasEmptyLocus_ReturnsDiplotypes(Locus locusToIgnore)
         {
             var genotype = new PhenotypeInfo<string>
             {
-                A = { Position1 = "*01:01", Position2 = "*03:01" },
-                B = { Position1 = "*08:01", Position2 = "*07:02" },
-                Dqb1 = { Position1 = "*02:01", Position2 = "*06:02" },
-                Drb1 = { Position1 = "*03:01", Position2 = "*15:01" }
+                A = { Position1 = "A-1", Position2 = "A-2" },
+                B = { Position1 = "B-1", Position2 = "B-2" },
+                C = { Position1 = "C-1", Position2 = "C-2" },
+                Dqb1 = { Position1 = "Dqb1-1", Position2 = "Dqb1-2" },
+                Drb1 = { Position1 = "Drb1-1", Position2 = "Drb1-2" }
             };
+
+            genotype.SetLocus(locusToIgnore, new LocusInfo<string> { Position1 = null, Position2 = null });
 
             var diplotypes = genotypeImputation.GetPossibleDiplotypes(genotype);
             diplotypes.Count.Should().Be(8);
         }
 
-        [Test]
-        public void GetPossibleDiplotypes_WhenGenotypeHasAllLociAndHomozygousCase_Returns8Diplotypes()
+        [TestCase(1, 8)]
+        [TestCase(2, 4)]
+        [TestCase(3, 2)]
+        [TestCase(4, 1)]
+        [TestCase(5, 1)]
+        public void GetPossibleDiplotypes_WhenGenotypeHasHomozygousCases_ReturnsDiplotypes(int numberOfHomozygousLoci, int expectedDiplotypeCount)
         {
             var genotype = new PhenotypeInfo<string>
             {
-                A = { Position1 = "*01:01", Position2 = "*01:01" },
-                B = { Position1 = "*08:01", Position2 = "*07:02" },
-                C = { Position1 = "*07:01", Position2 = "*07:02" },
-                Dqb1 = { Position1 = "*02:01", Position2 = "*06:02" },
-                Drb1 = { Position1 = "*03:01", Position2 = "*15:01" }
+                A = { Position1 = "A-1", Position2 = "A-2" },
+                B = { Position1 = "B-1", Position2 = "B-2" },
+                C = { Position1 = "C-1", Position2 = "C-2" },
+                Dqb1 = { Position1 = "Dqb1-1", Position2 = "Dqb1-2" },
+                Drb1 = { Position1 = "Drb1-1", Position2 = "Drb1-2" }
             };
+
+            genotype.EachLocus((locus, locusInfo) =>
+            {
+                if (numberOfHomozygousLoci <= 0 || locus == Locus.Dpb1) return;
+                genotype.SetLocus(locus,
+                    new LocusInfo<string> {Position1 = "homozygous", Position2 = "homozygous"});
+                numberOfHomozygousLoci += -1;
+            });
+
+            var diplotypes = genotypeImputation.GetPossibleDiplotypes(genotype);
+            diplotypes.Count.Should().Be(expectedDiplotypeCount);
+        }
+
+        [TestCase(Locus.A)]
+        [TestCase(Locus.C)]
+        [TestCase(Locus.B)]
+        [TestCase(Locus.Dqb1)]
+        [TestCase(Locus.Drb1)]
+        public void GetPossibleDiplotypes_WhenGenotypeHasHomozygousCase_Returns4Diplotypes(Locus homozygousLocus)
+        {
+            var genotype = new PhenotypeInfo<string>
+            {
+                A = { Position1 = "A-1", Position2 = "A-2" },
+                B = { Position1 = "B-1", Position2 = "B-2" },
+                C = { Position1 = "C-1", Position2 = "C-2" },
+                Dqb1 = { Position1 = "Dqb1-1", Position2 = "Dqb1-2" },
+                Drb1 = { Position1 = "Drb1-1", Position2 = "Drb1-2" }
+            };
+
+            genotype.SetLocus(homozygousLocus, new LocusInfo<string> { Position1 = "homozygous", Position2 = "homozygous" });
 
             var diplotypes = genotypeImputation.GetPossibleDiplotypes(genotype);
             diplotypes.Count.Should().Be(8);
-        }
-
-        [Test]
-        public void GetPossibleDiplotypes_WhenGenotypeHasNoCLocusAndHomozygousCases_Returns4Diplotypes()
-        {
-            var genotype = new PhenotypeInfo<string>
-            {
-                A = { Position1 = "*01:01", Position2 = "*01:01" },
-                B = { Position1 = "*08:01", Position2 = "*07:02" },
-                Dqb1 = { Position1 = "*02:01", Position2 = "*06:02" },
-                Drb1 = { Position1 = "*03:01", Position2 = "*15:01" }
-            };
-
-            var diplotypes = genotypeImputation.GetPossibleDiplotypes(genotype);
-            diplotypes.Count.Should().Be(4);
         }
 
         [Test, Repeat(10000), Ignore("Only used for manual benchmarking. Ran in ~521ms")]
@@ -83,33 +243,33 @@ namespace Atlas.MatchPrediction.Test.Services.GenotypeLikelihood
         {
             genotypeImputation.GetPossibleDiplotypes(new PhenotypeInfo<string>
             {
-                A = { Position1 = "*01:01", Position2 = "*03:01" },
-                B = { Position1 = "*08:01", Position2 = "*07:02" },
-                C = { Position1 = "*07:01", Position2 = "*07:02" },
-                Dqb1 = { Position1 = "*02:01", Position2 = "*06:02" },
-                Drb1 = { Position1 = "*03:01", Position2 = "*15:01" }
+                A = { Position1 = "A-1", Position2 = "A-2" },
+                B = { Position1 = "B-1", Position2 = "B-2" },
+                C = { Position1 = "C-1", Position2 = "C-2" },
+                Dqb1 = { Position1 = "Dqb1-1", Position2 = "Dqb1-2" },
+                Drb1 = { Position1 = "Drb1-1", Position2 = "Drb1-2" }
             });
             genotypeImputation.GetPossibleDiplotypes(new PhenotypeInfo<string>
             {
-                A = { Position1 = "*01:01", Position2 = "*03:01" },
-                B = { Position1 = "*08:01", Position2 = "*07:02" },
-                Dqb1 = { Position1 = "*02:01", Position2 = "*06:02" },
-                Drb1 = { Position1 = "*03:01", Position2 = "*15:01" }
+                A = { Position1 = "A-1", Position2 = "A-2" },
+                B = { Position1 = "B-1", Position2 = "B-2" },
+                Dqb1 = { Position1 = "Dqb1-1", Position2 = "Dqb1-2" },
+                Drb1 = { Position1 = "Drb1-1", Position2 = "Drb1-2" }
             });
             genotypeImputation.GetPossibleDiplotypes(new PhenotypeInfo<string>
             {
-                A = { Position1 = "*01:01", Position2 = "*01:01" },
-                B = { Position1 = "*08:01", Position2 = "*07:02" },
-                C = { Position1 = "*07:01", Position2 = "*07:02" },
-                Dqb1 = { Position1 = "*02:01", Position2 = "*06:02" },
-                Drb1 = { Position1 = "*03:01", Position2 = "*15:01" }
+                A = { Position1 = "homozygous", Position2 = "homozygous" },
+                B = { Position1 = "B-1", Position2 = "B-2" },
+                C = { Position1 = "C-1", Position2 = "C-2" },
+                Dqb1 = { Position1 = "Dqb1-1", Position2 = "Dqb1-2" },
+                Drb1 = { Position1 = "Drb1-1", Position2 = "Drb1-2" }
             });
             genotypeImputation.GetPossibleDiplotypes(new PhenotypeInfo<string>
             {
-                A = { Position1 = "*01:01", Position2 = "*01:01" },
-                B = { Position1 = "*08:01", Position2 = "*07:02" },
-                Dqb1 = { Position1 = "*02:01", Position2 = "*06:02" },
-                Drb1 = { Position1 = "*03:01", Position2 = "*15:01" }
+                A = { Position1 = "homozygous", Position2 = "homozygous" },
+                B = { Position1 = "B-1", Position2 = "B-2" },
+                Dqb1 = { Position1 = "Dqb1-1", Position2 = "Dqb1-2" },
+                Drb1 = { Position1 = "Drb1-1", Position2 = "Drb1-2" }
             });
         }
     }
