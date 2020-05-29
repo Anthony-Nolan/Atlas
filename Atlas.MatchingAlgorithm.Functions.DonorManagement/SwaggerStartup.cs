c54f78793dc10776bac9d1a4c1f3813247bd32c6
Mike.McLean <Mike.McLean@softwire.com>
2020-05-29 11:50:12 +0100

    feature: ATLAS-231: Add Swagger to MatchAlg.DonMgmt

---
+++ Atlas.MatchingAlgorithm.Functions.DonorManagement/SwaggerStartup.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using Atlas.Common.Utils;
+using Atlas.MatchingAlgorithm.Functions.DonorManagement;
+using AzureFunctions.Extensions.Swashbuckle;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Hosting;
+
+[assembly: FunctionsStartup(typeof(SwashBuckleStartup))]
+namespace Atlas.MatchingAlgorithm.Functions.DonorManagement
+{
+    internal class SwashBuckleStartup : IWebJobsStartup
+    {
+        public void Configure(IWebJobsBuilder builder)
+        {
+            builder.AddSwashBuckle(Assembly.GetExecutingAssembly());
+            UrlOpener.OpenInDefaultBrowser("http://localhost:7071/api/swagger/ui"); //'7071' is the default AzureFunctions port for all Azure functions.
+        }
+    }
+}
