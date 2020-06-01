ddc8257d82c7c2c6d69feedf58b17fd1a0804aa9
Tom Johnson <tom.johnson@softwire.com>
2020-06-01 18:37:58 +0100

    review: ATLAS-39: Updated structure of UnorderedPair class

--- Atlas.Common/GeneticData/UnorderedPair.cs
+++ Atlas.Common/GeneticData/UnorderedPair.cs
@@ -1,16 +1,15 @@
 ﻿namespace Atlas.Common.GeneticData
 {
     /// <summary>
-    /// Data type to hold an instance of T and T2 as a pair of items.
+    /// Data type to hold an instance of T as a pair of items.
     /// "Item1" and "Item2" are arbitrary.
     /// 
     /// </summary>
-    /// <typeparam name="T">The type of the information that is required at Item1.</typeparam>
-    /// <typeparam name="T2">The type of the information that is required at item2.</typeparam>
-    public class UnorderedPair<T, T2>
+    /// <typeparam name="T">The type of the information that is required at each item.</typeparam>
+    public class UnorderedPair<T>
     {
         public T Item1 { get; set; }
-        public T2 Item2 { get; set; }
+        public T Item2 { get; set; }
 
         #region Equality
 
@@ -34,10 +33,10 @@
                 return false;
             }
 
-            return Equals((UnorderedPair<T, T2>)other);
+            return Equals((UnorderedPair<T>)other);
         }
 
-        protected bool Equals(UnorderedPair<T, T2> other)
+        protected bool Equals(UnorderedPair<T> other)
         {
             return Item1.Equals(other.Item2) && Item2.Equals(other.Item1)
                    || Item1.Equals(other.Item1) && Item2.Equals(other.Item2);
