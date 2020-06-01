2b83e27ade5ee87881d241f521a41f07ec4f57ea
Ben Below <benjamin.below@softwire.com>
2020-06-01 18:11:48 +0100

    feature: ATLAS-186: Pull donors from donor import repository during matching algorithm data refresh

--- Atlas.DonorImport/ExternalInterface/DependencyInjection/ServiceConfiguration.cs
+++ Atlas.DonorImport/ExternalInterface/DependencyInjection/ServiceConfiguration.cs
@@ -10,9 +10,15 @@ namespace Atlas.DonorImport.ExternalInterface.DependencyInjection
             this IServiceCollection services,
             Func<IServiceProvider, string> fetchDonorImportDatabaseConnectionString)
         {
+            services.RegisterServices();
             services.RegisterDatabaseTypes(fetchDonorImportDatabaseConnectionString);
         }
 
+        private static void RegisterServices(this IServiceCollection services)
+        {
+            services.AddScoped<IDonorReader, DonorReader>();
+        }
+        
         private static void RegisterDatabaseTypes(
             this IServiceCollection services,
             Func<IServiceProvider, string> fetchDonorImportDatabaseConnectionString)
