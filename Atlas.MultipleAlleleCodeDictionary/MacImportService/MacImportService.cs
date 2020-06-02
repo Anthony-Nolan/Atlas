dfeb223eb234aab375c390ef672f72c6af372a79
Josh Powell <josh.powell@softwire.com>
2020-06-02 15:09:38 +0100

    chore ATLAS-336: Add logging to macImporter

--- Atlas.MultipleAlleleCodeDictionary/MacImportService/MacImportService.cs
+++ Atlas.MultipleAlleleCodeDictionary/MacImportService/MacImportService.cs
@@ -1,5 +1,8 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Atlas.MultipleAlleleCodeDictionary.utils;
+using Atlas.Common.ApplicationInsights;
+using Atlas.MultipleAlleleCodeDictionary.ApplicationInsights;
 
 namespace Atlas.MultipleAlleleCodeDictionary.MacImportService
 {
@@ -12,18 +15,31 @@ namespace Atlas.MultipleAlleleCodeDictionary.MacImportService
     {
         private readonly IMacRepository macRepository;
         private readonly IMacParser macParser;
+        private readonly ILogger logger;
         
-        public MacImporter(IMacRepository macRepository, IMacParser macParser)
+        public MacImporter(IMacRepository macRepository, IMacParser macParser, ILogger logger)
         {
             this.macRepository = macRepository;
             this.macParser = macParser;
+            this.logger = logger;
         }
 
         public async Task ImportLatestMultipleAlleleCodes()
         {
-            var lastEntryBeforeInsert = await macRepository.GetLastMacEntry();
-            var newMacs = await macParser.GetMacsSinceLastEntry(lastEntryBeforeInsert);
-            await macRepository.InsertMacs(newMacs);
+            logger.SendEvent(new MacImportEventModel("Mac Import started"));
+            try
+            {
+                var lastEntryBeforeInsert = await macRepository.GetLastMacEntry();
+                logger.SendEvent(new MacImportEventModel($"The last MAC entry found was: {lastEntryBeforeInsert}"));
+                var newMacs = await macParser.GetMacsSinceLastEntry(lastEntryBeforeInsert);
+                logger.SendEvent(new MacImportEventModel($"Attempting to insert {newMacs.Count} new MACs"));
+                await macRepository.InsertMacs(newMacs);
+            }
+            catch (Exception e)
+            {
+                logger.SendEvent(new MacImportEventModel(e, "Failed to finish MAC Import"));
+            }
+            logger.SendEvent(new MacImportEventModel("Successfully finished MAC Import"));
         }
     }
 }
