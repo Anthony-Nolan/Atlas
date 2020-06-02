a2aa9bb2116fd675c96f32a210ecdfe274ccbc48
Ben Below <benjamin.below@softwire.com>
2020-06-02 16:53:21 +0100

    chore: ATLAS-186: Add new database connection string (matching -> donor import) to terraform/readme

--- terraform/modules/donor_import/outputs.tf
+++ terraform/modules/donor_import/outputs.tf
@@ -3,3 +3,9 @@ output "general" {
     updated_searchable_donors_servicebus_topic = azurerm_servicebus_topic.updated-searchable-donors
   }
 }
+
+output "sql_database" {
+  value = {
+    name = azurerm_sql_database.atlas-donor-import.name
+  }
+}
