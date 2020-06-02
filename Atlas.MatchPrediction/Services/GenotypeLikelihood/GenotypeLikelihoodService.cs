10015e3ce90ed9de8128926d811acc9cd87ba1bc
Tom Johnson <tom.johnson@softwire.com>
2020-06-02 16:20:34 +0100

    feature: ATLAS-39: Created test for service

--- Atlas.MatchPrediction/Services/GenotypeLikelihood/GenotypeLikelihoodService.cs
+++ Atlas.MatchPrediction/Services/GenotypeLikelihood/GenotypeLikelihoodService.cs
@@ -10,25 +10,31 @@ namespace Atlas.MatchPrediction.Services.GenotypeLikelihood
 {
     public interface IGenotypeLikelihoodService
     {
-        public GenotypeLikelihoodResponse CalculateLikelihood(GenotypeLikelihoodInput genotypeLikelihood);
+        public Task<GenotypeLikelihoodResponse> CalculateLikelihood(GenotypeLikelihoodInput genotypeLikelihood);
     }
 
     public class GenotypeLikelihoodService : IGenotypeLikelihoodService
     {
+        private readonly IHaplotypeFrequencySetRepository setRepository;
+        private readonly IHaplotypeFrequenciesRepository frequencyRepository;
         private readonly IGenotypeImputer genotypeImputer;
 
-        public GenotypeLikelihoodService(IGenotypeImputer genotypeImputer)
+        public GenotypeLikelihoodService(
+            IHaplotypeFrequencySetRepository setRepository,
+            IHaplotypeFrequenciesRepository frequencyRepository,
+            IGenotypeImputer genotypeImputer)
         {
+            this.setRepository = setRepository;
+            this.frequencyRepository = frequencyRepository;
             this.genotypeImputer = genotypeImputer;
         }
 
-        public GenotypeLikelihoodResponse CalculateLikelihood(GenotypeLikelihoodInput genotypeLikelihood)
+        public async Task<GenotypeLikelihoodResponse> CalculateLikelihood(GenotypeLikelihoodInput genotypeLikelihood)
         {
             var diplotypes = genotypeImputer.GetPossibleDiplotypes(genotypeLikelihood.Genotype);
             var haplotypes = GetHaplotypes(diplotypes).ToList();
-            var haplotypesWithFrequencies = await haplotypeFrequencies.GetDiplotypeFrequencies(haplotypes);
-
-            GetFrequenciesForDiplotype(haplotypesWithFrequencies, diplotypes);
+            var frequencySet = await setRepository.GetActiveSet(null, null);
+            var haplotypesWithFrequencies = await frequencyRepository.GetDiplotypeFrequencies(haplotypes, frequencySet.Id);
 
             return new GenotypeLikelihoodResponse() {Likelihood = 1};
         }
@@ -37,16 +43,6 @@ namespace Atlas.MatchPrediction.Services.GenotypeLikelihood
         {
             return diplotypes.SelectMany(diplotype => new List<LociInfo<string>>
                 {diplotype.Item1.Hla, diplotype.Item2.Hla});
-        }
-
-        private static void GetFrequenciesForDiplotype(
-            Dictionary<LociInfo<string>, decimal> haplotypesWithFrequencies, IEnumerable<Diplotype> diplotypes)
-        {
-            foreach (var diplotype in diplotypes)
-            {
-                diplotype.Item1.Frequency = haplotypesWithFrequencies[diplotype.Item1.Hla];
-                diplotype.Item2.Frequency = haplotypesWithFrequencies[diplotype.Item2.Hla];
-            }
         }
     }
 }
