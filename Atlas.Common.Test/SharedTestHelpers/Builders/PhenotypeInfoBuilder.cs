8f038d01b7183d5428f8c0febf8891543b83a4f2
Tom Johnson <tom.johnson@softwire.com>
2020-06-04 12:38:47 +0100

    feature: ATLAS-39: Added integration tests

--- Atlas.Common.Test/SharedTestHelpers/Builders/PhenotypeInfoBuilder.cs
+++ Atlas.Common.Test/SharedTestHelpers/Builders/PhenotypeInfoBuilder.cs
@@ -7,7 +7,7 @@ namespace Atlas.Common.Test.SharedTestHelpers.Builders
     public static class PhenotypeInfoBuilder
     {
         public static Builder<PhenotypeInfo<string>> New => Builder<PhenotypeInfo<string>>.New
-            .With(d => d.A , new LocusInfo<string> { Position1 = "A-1", Position2 = "A-2" })
+            .With(d => d.A, new LocusInfo<string> { Position1 = "A-1", Position2 = "A-2" })
             .With(d => d.B, new LocusInfo<string> { Position1 = "B-1", Position2 = "B-2" })
             .With(d => d.C, new LocusInfo<string> { Position1 = "C-1", Position2 = "C-2" })
             .With(d => d.Dpb1, new LocusInfo<string> { Position1 = "Dpb1-1", Position2 = "Dpb1-2" })
