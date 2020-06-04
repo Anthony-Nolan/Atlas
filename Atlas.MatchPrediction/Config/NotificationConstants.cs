12e7f283361f13d77eb2fba0f0362b76d0da0e76
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2020-06-04 16:27:45 +0100

    feature: ATLAS-15: Notify on import success.

---
+++ Atlas.MatchPrediction/Config/NotificationConstants.cs
@@ -0,0 +1,7 @@
+namespace Atlas.MatchPrediction.Config
+{
+    internal static class NotificationConstants
+    {
+        public const string OriginatorName = "Atlas.MatchingPrediction";
+    }
+}
--- Atlas.MatchPrediction/Services/HaplotypeFrequencies/FrequencySetService.cs
+++ Atlas.MatchPrediction/Services/HaplotypeFrequencies/FrequencySetService.cs
@@ -1,9 +1,10 @@
-using System;
-using System.IO;
-using System.Threading.Tasks;
 using Atlas.Common.ApplicationInsights;
 using Atlas.Common.Notifications;
 using Atlas.Common.Notifications.MessageModels;
+using Atlas.MatchPrediction.Config;
+using System;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace Atlas.MatchPrediction.Services.HaplotypeFrequencies
 {
@@ -33,21 +34,32 @@ namespace Atlas.MatchPrediction.Services.HaplotypeFrequencies
 
         public async Task ImportFrequencySet(Stream stream, string fileName)
         {
-            const string errorName = "Haplotype Frequency Set Import Failure in the Match Prediction component";
+            const string supportSummaryPrefix = "Haplotype Frequency Set Import";
 
             try
             {
                 var metaData = metadataExtractor.GetMetadataFromFileName(fileName);
+
                 await importer.Import(metaData, stream);
+
+                await notificationsClient.SendNotification(new Notification(
+                    $"{supportSummaryPrefix} Succeeded",
+                    $"Import of file, '{fileName}', has completed successfully.",
+                    NotificationConstants.OriginatorName));
             }
             catch (Exception ex)
             {
+                var errorName = $"{supportSummaryPrefix} Failure in the Match Prediction component";
+
                 logger.SendEvent(new ErrorEventModel(errorName, ex));
+
                 await notificationsClient.SendAlert(new Alert(
                     errorName,
                     $"Import of file, '{fileName}', failed with the following exception message: {ex.Message} " +
                     "Full exception info has been logged to Application Insights.",
-                    Priority.High));
+                    Priority.High,
+                    NotificationConstants.OriginatorName));
+
                 throw;
             }
         }
