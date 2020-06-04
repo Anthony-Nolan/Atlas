2a298481a133b13229370d32bcea19d6571649b9
Mike.McLean <Mike.McLean@softwire.com>
2020-06-04 13:02:16 +0100

    fix: ATLAS-357: Fix handling of empty strings in Donor Imports

--- Atlas.DonorImport/Models/FileSchema/Hla.cs
+++ Atlas.DonorImport/Models/FileSchema/Hla.cs
@@ -22,23 +22,34 @@ namespace Atlas.DonorImport.Models.FileSchema
         [Obsolete("Access via Field1 and Field2, not directly - this property is only for deserialization purposes.")]
         public DnaLocus Dna { get; set; }
 
+        [Obsolete("Access via Field1 and Field2, not directly - this property is only for deserialization purposes.")]
         [JsonProperty(PropertyName = "ser")]
         public SerologyLocus Serology { get; set; }
 
-#pragma warning disable 618 // Dna is not Obsolete, but would be considered private if not fr deserialization to this class  
+#pragma warning disable 618 // Dna & Serology are not Obsolete, but would be considered private if not for deserialization to this class
+        [JsonIgnore]
         public string Field1 => Dna?.Field1 ?? Serology?.Field1;
+        [JsonIgnore] 
         public string Field2 => Dna?.Field2 ?? Serology?.Field2;
     }
 
     internal class DnaLocus
     {
-        public string Field1 { get; set; }
-        public string Field2 { get; set; }
+        [JsonIgnore]
+        private string raw1;
+        public string Field1 { get => raw1; set => raw1 = string.IsNullOrEmpty(value) ? null : value; }
+        [JsonIgnore]
+        private string raw2;
+        public string Field2 { get => raw2; set => raw2 = string.IsNullOrEmpty(value) ? null : value; }
     }
 
     internal class SerologyLocus
     {
-        public string Field1 { get; set; }
-        public string Field2 { get; set; }
+        [JsonIgnore]
+        private string raw1;
+        public string Field1 { get => raw1; set => raw1 = string.IsNullOrEmpty(value) ? null : value; }
+        [JsonIgnore]
+        private string raw2;
+        public string Field2 { get => raw2; set => raw2 = string.IsNullOrEmpty(value) ? null : value; }
     }
 }
