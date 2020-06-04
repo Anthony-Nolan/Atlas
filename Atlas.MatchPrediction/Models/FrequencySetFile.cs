c049b3d33c98f530fde9b852e3b9d8939134527e
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2020-06-04 17:39:28 +0100

    feature: ATLAS-15: Log time taken to complete import.

---
+++ Atlas.MatchPrediction/Models/FrequencySetFile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace Atlas.MatchPrediction.Models
+{
+    public class FrequencySetFile
+    {
+        public Stream Contents { get; set; }
+        public string FileName { get; set; }
+        public DateTimeOffset? UploadedDateTime { get; set; }
+        public DateTimeOffset? ImportedDateTime { get; set; }
+    }
+}
--- Atlas.MatchPrediction/Services/HaplotypeFrequencies/FrequencySetService.cs
+++ Atlas.MatchPrediction/Services/HaplotypeFrequencies/FrequencySetService.cs
@@ -1,20 +1,23 @@
 using Atlas.Common.ApplicationInsights;
 using Atlas.Common.Notifications;
 using Atlas.Common.Notifications.MessageModels;
+using Atlas.MatchPrediction.ApplicationInsights;
 using Atlas.MatchPrediction.Config;
+using Atlas.MatchPrediction.Models;
 using System;
-using System.IO;
 using System.Threading.Tasks;
 
 namespace Atlas.MatchPrediction.Services.HaplotypeFrequencies
 {
     public interface IFrequencySetService
     {
-        public Task ImportFrequencySet(Stream stream, string fileName);
+        public Task ImportFrequencySet(FrequencySetFile file);
     }
 
     internal class FrequencySetService : IFrequencySetService
     {
+        const string SummaryPrefix = "Haplotype Frequency Set Import";
+
         private readonly IFrequencySetMetadataExtractor metadataExtractor;
         private readonly IFrequencySetImporter importer;
         private readonly INotificationsClient notificationsClient;
@@ -32,36 +35,49 @@ namespace Atlas.MatchPrediction.Services.HaplotypeFrequencies
             this.logger = logger;
         }
 
-        public async Task ImportFrequencySet(Stream stream, string fileName)
+        public async Task ImportFrequencySet(FrequencySetFile file)
         {
-            const string supportSummaryPrefix = "Haplotype Frequency Set Import";
+            logger.SendTrace($"{SummaryPrefix}: request initiated for file, {file.FileName}.", LogLevel.Info);
 
             try
             {
-                var metaData = metadataExtractor.GetMetadataFromFileName(fileName);
+                var metaData = metadataExtractor.GetMetadataFromFileName(file.FileName);
+                await importer.Import(metaData, file.Contents);
+                file.ImportedDateTime = DateTimeOffset.Now;
 
-                await importer.Import(metaData, stream);
-
-                await notificationsClient.SendNotification(new Notification(
-                    $"{supportSummaryPrefix} Succeeded",
-                    $"Import of file, '{fileName}', has completed successfully.",
-                    NotificationConstants.OriginatorName));
+                await SendSuccessNotification(file);
             }
             catch (Exception ex)
             {
-                var errorName = $"{supportSummaryPrefix} Failure in the Match Prediction component";
-
-                logger.SendEvent(new ErrorEventModel(errorName, ex));
-
-                await notificationsClient.SendAlert(new Alert(
-                    errorName,
-                    $"Import of file, '{fileName}', failed with the following exception message: {ex.Message} " +
-                    "Full exception info has been logged to Application Insights.",
-                    Priority.High,
-                    NotificationConstants.OriginatorName));
-
+                await SendErrorAlert(file, ex);
                 throw;
             }
+        }
+
+        private async Task SendSuccessNotification(FrequencySetFile file)
+        {
+            var successName = $"{SummaryPrefix} Succeeded";
+
+            logger.SendEvent(new HaplotypeFrequencySetImportEventModel(successName, file));
+
+            await notificationsClient.SendNotification(new Notification(
+                successName,
+                $"Import of file, '{file.FileName}', has completed successfully.",
+                NotificationConstants.OriginatorName));
+        }
+
+        private async Task SendErrorAlert(FrequencySetFile file, Exception ex)
+        {
+            var errorName = $"{SummaryPrefix} Failure in the Match Prediction component";
+
+            logger.SendEvent(new ErrorEventModel(errorName, ex));
+
+            await notificationsClient.SendAlert(new Alert(
+                errorName,
+                $"Import of file, '{file.FileName}', failed with the following exception message: {ex.Message} " +
+                "Full exception info has been logged to Application Insights.",
+                Priority.High,
+                NotificationConstants.OriginatorName));
         }
     }
 }
