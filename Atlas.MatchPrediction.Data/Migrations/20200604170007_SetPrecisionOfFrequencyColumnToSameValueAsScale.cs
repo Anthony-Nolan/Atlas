2d39dcf004a2d39bc08e69b5dd8e6f5487c9fa58
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2020-06-04 18:04:25 +0100

    review: ATLAS-15: Set precision of frequency column to scale to prevent values greater or equal to 1 being inserted.

---
+++ Atlas.MatchPrediction.Data/Migrations/20200604170007_SetPrecisionOfFrequencyColumnToSameValueAsScale.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Atlas.MatchPrediction.Data.Migrations
+{
+    public partial class SetPrecisionOfFrequencyColumnToSameValueAsScale : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Frequency",
+                table: "HaplotypeFrequencies",
+                type: "decimal(20,20)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(21,20)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Frequency",
+                table: "HaplotypeFrequencies",
+                type: "decimal(21,20)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(20,20)");
+        }
+    }
+}
