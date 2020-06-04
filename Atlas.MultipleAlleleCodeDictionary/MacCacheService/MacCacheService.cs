422ac559c8774321918c349110cfac56b2e705ca
Josh Powell <josh.powell@softwire.com>
2020-06-04 16:12:20 +0100

    feat: atlas-47: add getting mac codes from cache

---
+++ Atlas.MultipleAlleleCodeDictionary/MacCacheService/MacCacheService.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Atlas.Common.ApplicationInsights;
+using Atlas.Common.Caching;
+using Atlas.Common.GeneticData;
+using Atlas.MultipleAlleleCodeDictionary.MacImportService;
+using Atlas.MultipleAlleleCodeDictionary.Models;
+using LazyCache;
+
+namespace Atlas.MultipleAlleleCodeDictionary.MacCacheService
+{
+
+    internal interface IMacCachingService
+    {
+        Task GenerateMacCache();
+    }
+    
+    internal interface IMacCache
+    {
+        Task<MultipleAlleleCodeEntity> GetMacCode(string macCode);
+    }
+    
+    internal class MacCache: IMacCache, IMacCachingService
+    {
+        
+        private readonly IAppCache cache;
+        private readonly ILogger logger;
+        private readonly IMacRepository macRepository;
+
+        public MacCache(ILogger logger, IPersistentCacheProvider cacheProvider, IMacRepository macRepository)
+        {
+            this.logger = logger;
+            this.cache = cacheProvider.Cache;
+            this.macRepository = macRepository;
+        }
+
+        public async Task<MultipleAlleleCodeEntity> GetMacCode(string macCode)
+        {
+            return await cache.GetOrAddAsync(macCode, () => macRepository.GetMac(macCode));
+        }
+        
+        public async Task GenerateMacCache()
+        {
+         
+            
+        }
+    }
+}
