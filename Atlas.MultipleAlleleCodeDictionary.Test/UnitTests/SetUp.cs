b80b000f4930a3fbe6410d3df342881c8c8d1de0
Josh Powell <josh.powell@softwire.com>
2020-06-05 17:42:10 +0100

    feat: atlas-362: unit test for mac import

---
+++ Atlas.MultipleAlleleCodeDictionary.Test/UnitTests/SetUp.cs
@@ -0,0 +1,20 @@
+﻿using Atlas.Common.Test.SharedTestHelpers;
+using Atlas.MultipleAlleleCodeDictionary.Test.DependencyInjection;
+using NUnit.Framework;
+
+namespace Atlas.MultipleAlleleCodeDictionary.Test.UnitTests
+{
+    [SetUpFixture]
+    public class UnitTestSetUp
+    {
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            TestStackTraceHelper.CatchAndRethrowWithStackTraceInExceptionMessage(() =>
+            {
+                DependencyInjection.DependencyInjection.Provider = ServiceConfiguration.CreateProvider();
+            });
+        }
+        
+    }
+}
