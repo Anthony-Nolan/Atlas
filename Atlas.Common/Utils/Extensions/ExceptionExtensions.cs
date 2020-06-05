fd78c310999610fa8c9ef72d8dabdb527ec28e41
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2020-06-05 11:36:08 +0100

    review: ATLAS-15: Review markups.

---
+++ Atlas.Common/Utils/Extensions/ExceptionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Atlas.Common.Utils.Extensions
+{
+    public static class ExceptionExtensions
+    {
+        /// <returns>Inner exception message when available, else the outer exception message.</returns>
+        public static string InnermostMessage(this Exception ex)
+        {
+            return ex.InnerException != null ? ex.InnerException.Message : ex.Message;
+        }
+    }
+}
--- Atlas.MatchPrediction/Models/FrequencySetFile.cs
+++ Atlas.MatchPrediction/Models/FrequencySetFile.cs
@@ -3,11 +3,16 @@ using System.IO;
 
 namespace Atlas.MatchPrediction.Models
 {
-    public class FrequencySetFile
+    public class FrequencySetFile : IDisposable
     {
         public Stream Contents { get; set; }
         public string FileName { get; set; }
         public DateTimeOffset? UploadedDateTime { get; set; }
         public DateTimeOffset? ImportedDateTime { get; set; }
+
+        public void Dispose()
+        {
+            Contents?.Dispose();
+        }
     }
 }
