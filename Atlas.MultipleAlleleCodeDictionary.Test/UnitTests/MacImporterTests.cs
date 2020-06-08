31c453e35be8900f8c8581036c729572e3e1dc31
Josh Powell <josh.powell@softwire.com>
2020-06-08 11:27:45 +0100

    review: atlas-362 remove dependency injection from unit tests

--- Atlas.MultipleAlleleCodeDictionary.Test/UnitTests/MacImporterTests.cs
+++ Atlas.MultipleAlleleCodeDictionary.Test/UnitTests/MacImporterTests.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Atlas.Common.Test.SharedTestHelpers;
 using Atlas.MultipleAlleleCodeDictionary.AzureStorage.Models;
 using Atlas.MultipleAlleleCodeDictionary.AzureStorage.Repositories;
@@ -7,40 +8,39 @@ using Atlas.MultipleAlleleCodeDictionary.MacImportServices;
 using Atlas.MultipleAlleleCodeDictionary.MacImportServices.SourceData;
 using Atlas.MultipleAlleleCodeDictionary.Test.TestHelpers.Builders;
 using Atlas.MultipleAlleleCodeDictionary.utils;
-using Microsoft.Extensions.DependencyInjection;
+using Atlas.Common.ApplicationInsights;
 using NSubstitute;
 using NUnit.Framework;
 
 namespace Atlas.MultipleAlleleCodeDictionary.Test.UnitTests
 {
     [TestFixture]
-    internal class Tests
+    internal class MacImporterTests
     {
         private IMacImporter macImporter;
         private IMacCodeDownloader mockDownloader;
-        private IMacParser macParser;
         private IMacRepository mockRepository;
+        private IMacParser macParser;
+        private ILogger mockLogger;
 
         [SetUp]
         public void Setup()
         {
             TestStackTraceHelper.CatchAndRethrowWithStackTraceInExceptionMessage(() =>
             {
-                macImporter = DependencyInjection.DependencyInjection.Provider.GetService<IMacImporter>();
-                mockDownloader = DependencyInjection.DependencyInjection.Provider.GetService<IMacCodeDownloader>();
-                mockRepository = DependencyInjection.DependencyInjection.Provider.GetService<IMacRepository>();
+                mockDownloader = Substitute.For<IMacCodeDownloader>();
+                mockRepository = Substitute.For<IMacRepository>();
+                mockLogger = Substitute.For<ILogger>();
+                
+                macParser = new MacLineParser(mockDownloader, mockLogger);
+                macImporter = new MacImporter(mockRepository, macParser, mockLogger, mockDownloader);
             });
         }
 
         [Test]
-        public void TokenTest()
+        public async Task ImportMacs_WillNotReplaceExistingMacs()
         {
-            Assert.Pass();
-        }
-        [Test]
-        public void ImportMacs_WillNotReplaceExistingMacs()
-        {
-            const int numberOfOldMacs = 50;
+            // Arrange
             var shorterEarlyMac = MacEntityBuilder.New.With(m => m.RowKey, "AA").Build();
             var shorterLateMac = MacEntityBuilder.New.With(m => m.RowKey, "ZZ").Build();
             var lastMac = MacEntityBuilder.New.With(m => m.RowKey, "ZZZ").Build();
@@ -50,18 +50,17 @@ namespace Atlas.MultipleAlleleCodeDictionary.Test.UnitTests
                 shorterLateMac,
                 lastMac
             };
-
             var lastOldMac = lastMac.RowKey;
-            
             const int numberOfNewMacs = 50;
-            
             var newMacs = Enumerable.Range(0, numberOfNewMacs).Select(i => MacEntityBuilder.New.Build()).ToList();
             mockDownloader.DownloadAndUnzipStream().Returns(MacSourceFileBuilder.BuildMacFile(oldMacs.Concat(newMacs)));
-            
-            
             mockRepository.GetLastMacEntry().Returns(lastOldMac);
-            macImporter.ImportLatestMultipleAlleleCodes();
-            mockRepository.Received().InsertMacs(Arg.Is<List<MultipleAlleleCodeEntity>>(x => x.Count == 50));
+            
+            // Act
+            await macImporter.ImportLatestMultipleAlleleCodes();
+            
+            // Assert
+            await mockRepository.Received().InsertMacs(Arg.Is<List<MultipleAlleleCodeEntity>>(x => x.Count == numberOfNewMacs));
             
         }
     }
