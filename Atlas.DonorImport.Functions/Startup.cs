d8c0cce9b977f8074a97824b31ffe409bcd7cf8a
Mike.McLean <Mike.McLean@softwire.com>
2020-06-08 17:16:45 +0100

    feature: ATLAS-169: Standardise HLA representations during import into Master Donor Database

--- Atlas.DonorImport.Functions/Startup.cs
+++ Atlas.DonorImport.Functions/Startup.cs
@@ -1,6 +1,10 @@
+using Atlas.Common.ApplicationInsights;
+using Atlas.Common.Utils.Extensions;
 using Atlas.DonorImport.ExternalInterface.DependencyInjection;
 using Atlas.DonorImport.Functions;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.DependencyInjection;
 
 [assembly: FunctionsStartup(typeof(Startup))]
 
@@ -11,7 +15,8 @@ namespace Atlas.DonorImport.Functions
         public override void Configure(IFunctionsHostBuilder builder)
         {
             // TODO: ATLAS-327: Inject settings
-            builder.Services.RegisterDonorImport();
+            builder.Services.RegisterOptions<ApplicationInsightsSettings>("ApplicationInsights");
+            builder.Services.RegisterDonorImport(sp => sp.GetService<IOptions<ApplicationInsightsSettings>>().Value);
         }
     }
 }
