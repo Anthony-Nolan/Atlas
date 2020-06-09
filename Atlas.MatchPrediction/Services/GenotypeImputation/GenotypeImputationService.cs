201db71abf2602272404843bf6c5b6f9fc196544
Tom Johnson <tom.johnson@softwire.com>
2020-06-09 14:18:22 +0100

    feature: ATLAS-20: Created new function and service to call HlaMetadataFactory

---
+++ Atlas.MatchPrediction/Services/GenotypeImputation/GenotypeImputationService.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Atlas.MatchPrediction.Client.Models.GenotypeImputation;
+using Atlas.HlaMetadataDictionary.ExternalInterface;
+
+namespace Atlas.MatchPrediction.Services.GenotypeImputation
+{
+    public interface IGenotypeImputationService
+    {
+        public Task<GenotypeImputationResponse> ImputeGenotype(GenotypeImputationInput phenotype);
+    }
+
+    public class GenotypeImputationService : IGenotypeImputationService
+    {
+        private readonly IHlaMetadataDictionary hlaMetadataDictionary;
+
+        public GenotypeImputationService(IHlaMetadataDictionaryFactory factory)
+        {
+            this.hlaMetadataDictionary = factory.BuildDictionary();
+        }
+
+        public async Task<GenotypeImputationResponse> ImputeGenotype(GenotypeImputationInput phenotype)
+        {
+            var allelesPerLocus = await phenotype.Phenotype.MapAsync(async (locus, position, hla) =>
+                await hlaMetadataDictionary.GetTwoFieldAllelesForAmbiguousHla(locus, hla));
+
+            return new GenotypeImputationResponse();
+        }
+    }
+}
