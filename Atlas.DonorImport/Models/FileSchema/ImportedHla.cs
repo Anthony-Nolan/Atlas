1549c0201dea473b743829f1b4ca00335ff3ab70
Mike.McLean <Mike.McLean@softwire.com>
2020-06-10 15:07:33 +0100

    refactor: ATLAS-169: Re-write LocusInterpretter in a sensible way now that we're doing it in one pass.

--- Atlas.DonorImport/Models/FileSchema/ImportedHla.cs
+++ Atlas.DonorImport/Models/FileSchema/ImportedHla.cs
@@ -1,11 +1,9 @@
-using System;
 using Newtonsoft.Json;
 
 namespace Atlas.DonorImport.Models.FileSchema
 {
     // ReSharper disable InconsistentNaming
     // ReSharper disable ClassNeverInstantiated.Global - Instantiated by JSON parser
-    // ReSharper disable UnusedAutoPropertyAccessor.Global
     internal class ImportedHla
     {
         public ImportedLocus A { get; set; }
@@ -18,11 +16,8 @@ namespace Atlas.DonorImport.Models.FileSchema
 
     internal class ImportedLocus
     {
-        // ReSharper disable once MemberCanBePrivate.Global - Needed for JSON parsing
-        [Obsolete("Access via ReadField1 and ReadField2, not directly - this property is only for deserialization purposes.")]
         public TwoFieldStringData Dna { get; set; }
 
-        [Obsolete("Access via ReadField1 and ReadField2, not directly - this property is only for deserialization purposes.")]
         [JsonProperty(PropertyName = "ser")]
         public TwoFieldStringData Serology { get; set; }
     }
