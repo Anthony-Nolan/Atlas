26e61a55fcef22ebd46be4fd67304e7de8292a4d
Josh Powell <josh.powell@softwire.com>
2020-06-11 16:38:14 +0100

    feat: atlas-384: use alleleNameSplitter

--- Atlas.MultipleAlleleCodeDictionary/Utils/MacExpander.cs
+++ Atlas.MultipleAlleleCodeDictionary/Utils/MacExpander.cs
@@ -1,17 +1,48 @@
 ﻿using Atlas.MultipleAlleleCodeDictionary.ExternalInterface.Models;
+using Atlas.Common.GeneticData.Hla.Models.MolecularHlaTyping;
+using Atlas.Common.GeneticData.Hla.Services;
+using Atlas.Common.Helpers;
+using Atlas.Common.GeneticData.Hla.Services.AlleleStringSplitters;
 
 namespace Atlas.MultipleAlleleCodeDictionary.utils
 {
     internal interface IMacExpander
     {
-        public string GetSpecificMacFromGeneric(MultipleAlleleCode mac);
+        MolecularAlleleDetails ExpandMac(MultipleAlleleCode mac, string firstField);
     }
     
     internal class MacExpander : IMacExpander
     {
-        public string GetSpecificMacFromGeneric(MultipleAlleleCode mac)
+        private const char AlleleDelimiter = '/';
+        private const char FieldDelimiter = ':';
+        private AlleleStringSplitterBase stringSplitter;
+
+        public MacExpander(AlleleStringSplitterBase stringSplitter)
         {
-            throw new System.NotImplementedException();
+            this.stringSplitter = stringSplitter;
+        }
+        
+        public MolecularAlleleDetails ExpandMac(MultipleAlleleCode mac, string firstField)
+        {
+            return mac.IsGeneric ? ExpandMac(mac, firstField) : GetSpecificMac(mac);
+        }
+        private MolecularAlleleDetails GetGenericMac(MultipleAlleleCode mac, string firstField)
+        {
+            var secondFields = mac.Hla.Split(AlleleDelimiter);
+            var combinedFields = new string[secondFields.Length];
+            for (var i = 0; i < secondFields.Length; i++)
+            {
+                combinedFields[i] = $"{firstField}:{secondFields[i]}";
+            }
+
+            var alleles = string.Join(FieldDelimiter, combinedFields);
+            return new MolecularAlleleDetails(alleles);
+        }
+
+        private MolecularAlleleDetails GetSpecificMac(MultipleAlleleCode mac)
+        {
+            return stringSplitter.GetAlleleNamesFromAlleleString(mac.Hla);
+            return new MolecularAlleleDetails(mac.Hla);
         }
     }
 }
