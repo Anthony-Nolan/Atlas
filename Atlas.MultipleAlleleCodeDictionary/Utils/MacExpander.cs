c2513771e57dda9bfcae517991d7743fbb115ff2
Josh Powell <josh.powell@softwire.com>
2020-06-11 12:32:30 +0100

    feat: atlas-384: add method to get generic code

--- Atlas.MultipleAlleleCodeDictionary/Utils/MacExpander.cs
+++ Atlas.MultipleAlleleCodeDictionary/Utils/MacExpander.cs
@@ -1,11 +1,17 @@
-﻿namespace Atlas.MultipleAlleleCodeDictionary.utils
+﻿using Atlas.MultipleAlleleCodeDictionary.ExternalInterface.Models;
+
+namespace Atlas.MultipleAlleleCodeDictionary.utils
 {
     internal interface IMacExpander
     {
+        public string GetSpecificMacFromGeneric(MultipleAlleleCode mac);
     }
     
-    internal class MacExpander
+    internal class MacExpander : IMacExpander
     {
-        
+        public string GetSpecificMacFromGeneric(MultipleAlleleCode mac)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
