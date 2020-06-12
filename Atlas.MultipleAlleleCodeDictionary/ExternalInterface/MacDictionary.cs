52d72dd6af8f5716bafb418af4e134111c3a39ec
Josh Powell <josh.powell@softwire.com>
2020-06-12 11:49:37 +0100

    review: atlas-54: fix minor markups

--- Atlas.MultipleAlleleCodeDictionary/ExternalInterface/MacDictionary.cs
+++ Atlas.MultipleAlleleCodeDictionary/ExternalInterface/MacDictionary.cs
@@ -10,39 +10,38 @@ namespace Atlas.MultipleAlleleCodeDictionary.ExternalInterface
         public Task ImportLatestMacs();
         public Task<Mac> GetMac(string macCode);
         public Task GenerateMacCache();
-         public Task<string> GetHlaFromMac(string macCode);
+        public Task<string> GetHlaFromMac(string macCode);
     }
-    
+
     public class MacDictionary : IMacDictionary
     {
         private readonly IMacImporter macImporter;
-        private readonly IMacCache macCache;
+        private readonly IMacCacheService macCacheService;
 
-        public MacDictionary(IMacImporter macImporter, IMacCache macCache)
+        public MacDictionary(IMacImporter macImporter, IMacCacheService macCacheService)
         {
             this.macImporter = macImporter;
-            this.macCache = macCache;
+            this.macCacheService = macCacheService;
         }
-        
+
         public async Task ImportLatestMacs()
         {
             await macImporter.ImportLatestMacs();
         }
-        
+
         public async Task<Mac> GetMac(string macCode)
         {
-            return await macCache.GetMacCode(macCode);
+            return await macCacheService.GetMacCode(macCode);
         }
-        
+
         public async Task GenerateMacCache()
         {
-            await macCache.GenerateMacCache();
+            await macCacheService.GenerateMacCache();
         }
 
         public async Task<string> GetHlaFromMac(string macCode)
         {
-            return await macCache.GetHlaFromMac(macCode);
+            return await macCacheService.GetHlaFromMac(macCode);
         }
-        
     }
 }
