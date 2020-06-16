5e62741c5c5e6f68d60f59b3ce5398dcb34147ee
Josh Powell <josh.powell@softwire.com>
2020-06-16 16:42:16 +0100

    feature: Atlas-384: add generic mac expander and tests

---
+++ Atlas.MultipleAlleleCodeDictionary.Test/TestHelpers/Builders/MolecularAlleleDetailsBuilder.cs
@@ -0,0 +1,12 @@
+﻿using Atlas.Common.GeneticData.Hla.Models.MolecularHlaTyping;
+using Atlas.MultipleAlleleCodeDictionary.ExternalInterface.Models;
+using LochNessBuilder;
+
+namespace Atlas.MultipleAlleleCodeDictionary.Test.TestHelpers.Builders
+{
+    [Builder]
+    internal class MolecularAlleleDetailsBuilder
+    {
+        public static Builder<MolecularAlleleDetails> New => Builder<MolecularAlleleDetails>.New;
+    }
+}
