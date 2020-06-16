bcf0f040173aef4f1eb532dd2e6af2649b0375be
Ben Below <benjamin.below@softwire.com>
2020-06-16 16:22:11 +0100

    chore: ATLAS-20: Add MPA HLA client variables to terraform

--- terraform/core/modules/match_prediction/function.tf
+++ terraform/core/modules/match_prediction/function.tf
@@ -16,6 +16,8 @@ resource "azurerm_function_app" "atlas_match_prediction_function" {
     "ApplicationInsights:LogLevel"                      = var.APPLICATION_INSIGHTS_LOG_LEVEL
     "AzureStorage:ConnectionString"                     = var.azure_storage.primary_connection_string
     "AzureStorage:HaplotypeFrequencySetImportContainer" = azurerm_storage_container.haplotype_frequency_set_blob_container.name
+    "Client:HlaService:ApiKey"                          = var.HLA_SERVICE_APIKEY
+    "Client:HlaService:BaseUrl"                         = var.HLA_SERVICE_BASEURL
     "NotificationsServiceBus:ConnectionString"          = var.servicebus_namespace_authorization_rules.write-only.primary_connection_string
     "NotificationsServiceBus:AlertsTopic"               = var.servicebus_topics.alerts.name
     "NotificationsServiceBus:NotificationsTopic"        = var.servicebus_topics.notifications.name
--- terraform/core/modules/match_prediction/variables_release.tf
+++ terraform/core/modules/match_prediction/variables_release.tf
@@ -3,15 +3,22 @@
 variable "APPLICATION_INSIGHTS_LOG_LEVEL" {
   type = string
 }
-
-variable "WEBSITE_RUN_FROM_PACKAGE" {
-  type = string
-}
-
 variable "DATABASE_PASSWORD" {
   type = string
 }
 
 variable "DATABASE_USERNAME" {
+  type = string
+}
+
+variable "HLA_SERVICE_APIKEY" {
+  type = string
+}
+
+variable "HLA_SERVICE_BASEURL" {
+  type = string
+}
+
+variable "WEBSITE_RUN_FROM_PACKAGE" {
   type = string
 }
