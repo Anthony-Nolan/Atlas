6cb37bd1a27227914f67cb49cd9b7e5ef785ed88
Ben Below <benjamin.below@softwire.com>
2020-06-16 09:17:21 +0100

    feature: ATLAS-332: Allow (some) stages in data refresh to be skipped, if they were previously completed in an interrupted refresh

--- Atlas.MatchingAlgorithm.Test/TestHelpers/Builders/DataRefresh/DataRefreshSettingsBuilder.cs
+++ Atlas.MatchingAlgorithm.Test/TestHelpers/Builders/DataRefresh/DataRefreshSettingsBuilder.cs
@@ -4,17 +4,11 @@ using LochNessBuilder;
 namespace Atlas.MatchingAlgorithm.Test.TestHelpers.Builders.DataRefresh
 {
     [Builder]
-    public static class DataRefreshSettingsBuilder
+    internal static class DataRefreshSettingsBuilder
     {
-        public static Builder<DataRefreshSettings> New
-        {
-            get
-            {
-                return Builder<DataRefreshSettings>.New
-                    .With(s => s.DormantDatabaseSize, "S0")
-                    .With(s => s.ActiveDatabaseSize, "S4")
-                    .With(s => s.RefreshDatabaseSize, "P15");
-            }
-        }
+        public static Builder<DataRefreshSettings> New => Builder<DataRefreshSettings>.New
+            .With(s => s.DormantDatabaseSize, "S0")
+            .With(s => s.ActiveDatabaseSize, "S4")
+            .With(s => s.RefreshDatabaseSize, "P15");
     }
 }
