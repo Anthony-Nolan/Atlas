7be5647658d2dfe4ec5fd0a9fb956123749386ed
Tom Johnson <tom.johnson@softwire.com>
2020-06-17 17:33:20 +0100

    feature: ATLAS-217: Moved over tests and re-wrote existing class

--- Atlas.MatchingAlgorithm.Test/Services/Search/Matching/DonorMatchCalculatorTests.cs
+++ Atlas.MatchingAlgorithm.Test/Services/Search/Matching/DonorMatchCalculatorTests.cs
@@ -3,9 +3,7 @@ using System.Collections.Generic;
 using Atlas.Common.GeneticData.PhenotypeInfo;
 using Atlas.Common.Matching.Services;
 using Atlas.MatchingAlgorithm.Common.Models;
-using Atlas.MatchingAlgorithm.Data.Repositories.DonorRetrieval;
 using Atlas.MatchingAlgorithm.Services.Search.Matching;
-using FluentAssertions;
 using NSubstitute;
 using NUnit.Framework;
 
@@ -21,9 +19,6 @@ namespace Atlas.MatchingAlgorithm.Test.Services.Search.Matching
         // ReSharper disable once InconsistentNaming
         private const string PatientPGroup1_2 = "p-group1-2";
         private const string PatientPGroup2 = "p-group2";
-        private const string PatientPGroupHomozygous = "p-group-shared";
-
-        private const string NonMatchingPGroup = "p-group-that-does-not-match-either-patient-p-group";
         private const string ArbitraryPGroup = "arbitrary-p-group";
 
         // Use constant patient hla data to make tests shorter
@@ -31,18 +26,6 @@ namespace Atlas.MatchingAlgorithm.Test.Services.Search.Matching
         private readonly AlleleLevelLocusMatchCriteria defaultCriteria = new AlleleLevelLocusMatchCriteria()
         {
             PGroupsToMatchInPositionOne = new List<string> {PatientPGroup1_1, PatientPGroup1_2},
-            PGroupsToMatchInPositionTwo = new List<string> {PatientPGroup2},
-        };
-        
-        private readonly AlleleLevelLocusMatchCriteria homozygousPatientCriteria = new AlleleLevelLocusMatchCriteria()
-        {
-            PGroupsToMatchInPositionOne = new List<string> {PatientPGroupHomozygous},
-            PGroupsToMatchInPositionTwo = new List<string> {PatientPGroupHomozygous},
-        };    
-        
-        private readonly AlleleLevelLocusMatchCriteria patientWithNoPGroupsAtPositionOneCriteria = new AlleleLevelLocusMatchCriteria()
-        {
-            PGroupsToMatchInPositionOne = new List<string> {},
             PGroupsToMatchInPositionTwo = new List<string> {PatientPGroup2},
         };
 
@@ -53,212 +36,6 @@ namespace Atlas.MatchingAlgorithm.Test.Services.Search.Matching
             donorMatchCalculator = new DonorMatchCalculator(alleleGroupsMatchingCount);
         }
 
-        #region MatchCount Tests
-        
-        [Test]
-        public void CalculateMatchesForDonors_WhenNoPGroupsMatch_ReturnsMatchCountOfZero()
-        {
-            var donorPGroups = new List<string>{NonMatchingPGroup};
-            var donorHla = new LocusInfo<IEnumerable<string>>(donorPGroups);
-
-            var matchDetails = donorMatchCalculator.CalculateMatchDetailsForDonorHla(defaultCriteria, donorHla);
-
-            matchDetails.MatchCount.Should().Be(0);
-        }
-
-        [Test]
-        public void CalculateMatchesForDonors_WhenDonorNotTypedAtLocus_ReturnsMatchCountOfTwo()
-        {
-            var donorHla = new LocusInfo<IEnumerable<string>>(null);
-
-            var matchDetails = donorMatchCalculator.CalculateMatchDetailsForDonorHla(defaultCriteria, donorHla);
-
-            matchDetails.MatchCount.Should().Be(2);
-        }
-        
-        [Test]
-        public void CalculateMatchesForDonors_ForDoubleDirectMatch_ReturnsMatchCountOfTwo()
-        {
-            var donorPGroups1 = new List<string>{PatientPGroup1_1};
-            var donorPGroups2 = new List<string>{PatientPGroup2};
-            var donorHla = new LocusInfo<IEnumerable<string>>(donorPGroups1, donorPGroups2);
-
-            var matchDetails = donorMatchCalculator.CalculateMatchDetailsForDonorHla(defaultCriteria, donorHla);
-
-            matchDetails.MatchCount.Should().Be(2);
-        }
-        
-        [Test]
-        public void CalculateMatchesForDonors_ForDoubleCrossMatch_ReturnsMatchCountOfTwo()
-        {
-            var donorPGroups1 = new List<string>{PatientPGroup2};
-            var donorPGroups2 = new List<string>{PatientPGroup1_1};
-            var donorHla = new LocusInfo<IEnumerable<string>>(donorPGroups1, donorPGroups2);
-
-            var matchDetails = donorMatchCalculator.CalculateMatchDetailsForDonorHla(defaultCriteria, donorHla);
-
-            matchDetails.MatchCount.Should().Be(2);
-        }
-        
-        [Test]
-        public void CalculateMatchesForDonors_ForSingleDirectMatchAtPositionOne_ReturnsMatchCountOfOne()
-        {
-            var donorPGroups1 = new List<string>{PatientPGroup1_1};
-            var donorPGroups2 = new List<string>{NonMatchingPGroup};
-            var donorHla = new LocusInfo<IEnumerable<string>>(donorPGroups1, donorPGroups2);
-
-            var matchDetails = donorMatchCalculator.CalculateMatchDetailsForDonorHla(defaultCriteria, donorHla);
-
-            matchDetails.MatchCount.Should().Be(1);
-        }
-        
-        [Test]
-        public void CalculateMatchesForDonors_ForSingleDirectMatchAtPositionTwo_ReturnsMatchCountOfOne()
-        {
-            var donorPGroups1 = new List<string>{NonMatchingPGroup};
-            var donorPGroups2 = new List<string>{PatientPGroup2};
-            var donorHla = new LocusInfo<IEnumerable<string>>(donorPGroups1, donorPGroups2);
-
-            var matchDetails = donorMatchCalculator.CalculateMatchDetailsForDonorHla(defaultCriteria, donorHla);
-
-            matchDetails.MatchCount.Should().Be(1);
-        }
-        
-        [Test]
-        public void CalculateMatchesForDonors_WhenDonorPositionOneMatchesPatientPositionTwo_ReturnsMatchCountOfOne()
-        {
-            var donorPGroups1 = new List<string>{PatientPGroup2};
-            var donorPGroups2 = new List<string>{NonMatchingPGroup};
-            var donorHla = new LocusInfo<IEnumerable<string>>(donorPGroups1, donorPGroups2);
-
-            var matchDetails = donorMatchCalculator.CalculateMatchDetailsForDonorHla(defaultCriteria, donorHla);
-
-            matchDetails.MatchCount.Should().Be(1);
-        }
-        
-        [Test]
-        public void CalculateMatchesForDonors_WhenDonorPositionTwoMatchesPatientPositionOne_ReturnsMatchCountOfOne()
-        {
-            var donorPGroups1 = new List<string>{NonMatchingPGroup};
-            var donorPGroups2 = new List<string>{PatientPGroup1_2};
-            var donorHla = new LocusInfo<IEnumerable<string>>(donorPGroups1, donorPGroups2);
-
-            var matchDetails = donorMatchCalculator.CalculateMatchDetailsForDonorHla(defaultCriteria, donorHla);
-
-            matchDetails.MatchCount.Should().Be(1);
-        }
-        
-        [Test]
-        public void CalculateMatchesForDonors_WhenOneDonorPositionMatchesBothPatientPositions_ReturnsMatchCountOfOne()
-        {
-            var donorPGroups1 = new List<string>{NonMatchingPGroup};
-            var donorPGroups2 = new List<string>{PatientPGroup1_2, PatientPGroup2};
-            var donorHla = new LocusInfo<IEnumerable<string>>(donorPGroups1, donorPGroups2);
-
-            var matchDetails = donorMatchCalculator.CalculateMatchDetailsForDonorHla(defaultCriteria, donorHla);
-
-            matchDetails.MatchCount.Should().Be(1);
-        }
-        
-        [Test]
-        public void CalculateMatchesForDonors_WhenBothDonorPositionMatchesOnePatientPosition_ReturnsMatchCountOfOne()
-        {
-            var donorPGroups1 = new List<string>{PatientPGroup2};
-            var donorPGroups2 = new List<string>{PatientPGroup2};
-            var donorHla = new LocusInfo<IEnumerable<string>>(donorPGroups1, donorPGroups2);
-
-            var matchDetails = donorMatchCalculator.CalculateMatchDetailsForDonorHla(defaultCriteria, donorHla);
-
-            matchDetails.MatchCount.Should().Be(1);
-        }
-        
-        [Test]
-        public void CalculateMatchesForDonors_WhenMultiplePGroupsMatchForASinglePosition_ReturnsMatchCountOfOne()
-        {
-            var donorPGroups1 = new List<string>{NonMatchingPGroup};
-            var donorPGroups2 = new List<string>{PatientPGroup1_1, PatientPGroup1_2};
-            var donorHla = new LocusInfo<IEnumerable<string>>(donorPGroups1, donorPGroups2);
-
-            var matchDetails = donorMatchCalculator.CalculateMatchDetailsForDonorHla(defaultCriteria, donorHla);
-
-            matchDetails.MatchCount.Should().Be(1);
-        }
-
-        [Test]
-        public void CalculateMatchesForDonors_ForHomozygousPatientMatchingNeitherPosition_ReturnsMatchCountOfZero()
-        {
-            var donorPGroups1 = new List<string>{NonMatchingPGroup};
-            var donorPGroups2 = new List<string>{NonMatchingPGroup};
-            var donorHla = new LocusInfo<IEnumerable<string>>(donorPGroups1, donorPGroups2);
-
-            var matchDetails = donorMatchCalculator.CalculateMatchDetailsForDonorHla(homozygousPatientCriteria, donorHla);
-
-            matchDetails.MatchCount.Should().Be(0);
-        }
-        
-        [Test]
-        public void CalculateMatchesForDonors_ForHomozygousPatientMatchingPositionOne_ReturnsMatchCountOfOne()
-        {
-            var donorPGroups1 = new List<string>{PatientPGroupHomozygous};
-            var donorPGroups2 = new List<string>{NonMatchingPGroup};
-            var donorHla = new LocusInfo<IEnumerable<string>>(donorPGroups1, donorPGroups2);
-
-            var matchDetails = donorMatchCalculator.CalculateMatchDetailsForDonorHla(homozygousPatientCriteria, donorHla);
-
-            matchDetails.MatchCount.Should().Be(1);
-        }
-        
-        [Test]
-        public void CalculateMatchesForDonors_ForHomozygousPatientMatchingPositionTwo_ReturnsMatchCountOfOne()
-        {
-            var donorPGroups1 = new List<string>{NonMatchingPGroup};
-            var donorPGroups2 = new List<string>{PatientPGroupHomozygous};
-            var donorHla = new LocusInfo<IEnumerable<string>>(donorPGroups1, donorPGroups2);
-
-            var matchDetails = donorMatchCalculator.CalculateMatchDetailsForDonorHla(homozygousPatientCriteria, donorHla);
-
-            matchDetails.MatchCount.Should().Be(1);
-        }
-        
-        [Test]
-        public void CalculateMatchesForDonors_ForHomozygousPatientMatchingBothPositions_ReturnsMatchCountOfTwo()
-        {
-            var donorPGroups1 = new List<string>{PatientPGroupHomozygous};
-            var donorPGroups2 = new List<string>{PatientPGroupHomozygous};
-            var donorHla = new LocusInfo<IEnumerable<string>>(donorPGroups1, donorPGroups2);
-
-            var matchDetails = donorMatchCalculator.CalculateMatchDetailsForDonorHla(homozygousPatientCriteria, donorHla);
-
-            matchDetails.MatchCount.Should().Be(2);
-        }
-
-        [Test]
-        public void CalculateMatchesForDonors_WhenDonorPositionHasNoPGroups_DoesNotMatchAtThatPosition()
-        {
-            // This can happen in the case of a null allele
-            var donorPGroups1 = new List<string>{};
-            var donorPGroups2 = new List<string>{PatientPGroupHomozygous};
-            var donorHla = new LocusInfo<IEnumerable<string>>(donorPGroups1, donorPGroups2);
-
-            var matchDetails = donorMatchCalculator.CalculateMatchDetailsForDonorHla(homozygousPatientCriteria, donorHla);
-
-            matchDetails.MatchCount.Should().Be(1);
-        }          
-        
-        [Test]
-        public void CalculateMatchesForDonors_WhenPatientPositionHasNoPGroups_DoesNotMatchAtThatPosition()
-        {
-            var donorPGroups1 = new List<string>{PatientPGroup2};
-            var donorPGroups2 = new List<string>{PatientPGroup2};
-            var donorHla = new LocusInfo<IEnumerable<string>>(donorPGroups1, donorPGroups2);
-
-            var matchDetails = donorMatchCalculator.CalculateMatchDetailsForDonorHla(patientWithNoPGroupsAtPositionOneCriteria, donorHla);
-
-            matchDetails.MatchCount.Should().Be(1);
-        }
-
-        #endregion
-        
         [Test]
         public void CalculateMatchesForDonors_WhenOnlyDonorPositionOneNull_ThrowsException()
         {
--- Atlas.MatchingAlgorithm/Services/Search/Matching/DonorMatchCalculator.cs
+++ Atlas.MatchingAlgorithm/Services/Search/Matching/DonorMatchCalculator.cs
@@ -1,90 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using Atlas.Common.GeneticData.PhenotypeInfo;
+using Atlas.Common.Matching.Services;
 using Atlas.MatchingAlgorithm.Common.Models;
 using Atlas.MatchingAlgorithm.Common.Models.SearchResults;
-using PGroup = System.Collections.Generic.List<string>;
 
 namespace Atlas.MatchingAlgorithm.Services.Search.Matching
 {
     public interface IDonorMatchCalculator
     {
-        LocusMatchDetails CalculateMatchDetailsForDonorHla(AlleleLevelLocusMatchCriteria locusMatchCriteria, LocusInfo<IEnumerable<string>> pGroups);
+        LocusMatchDetails CalculateMatchDetailsForDonorHla(AlleleLevelLocusMatchCriteria locusMatchCriteria,
+            LocusInfo<IEnumerable<string>> pGroups);
     }
 
     public class DonorMatchCalculator : IDonorMatchCalculator
     {
+        private readonly IAlleleGroupsMatchingCount alleleGroupsMatchingCount;
+
+        public DonorMatchCalculator(IAlleleGroupsMatchingCount alleleGroupsMatchingCount)
+        {
+            this.alleleGroupsMatchingCount = alleleGroupsMatchingCount;
+        }
+
         public LocusMatchDetails CalculateMatchDetailsForDonorHla(
             AlleleLevelLocusMatchCriteria locusMatchCriteria,
             LocusInfo<IEnumerable<string>> pGroups)
         {
-            return CalculateMatchDetailsForDonorHla(
-                locusMatchCriteria,
-                new LocusInfo<PGroup>(pGroups.Position1?.ToList(), pGroups.Position2?.ToList())
-            );
-        }
+            var expandedHla = new LocusInfo<IEnumerable<string>>(pGroups.Position1, pGroups.Position2);
+            var expandedLocusMatchCriteria = 
+                new LocusInfo<IEnumerable<string>>(locusMatchCriteria.PGroupsToMatchInPositionOne, locusMatchCriteria.PGroupsToMatchInPositionTwo);
 
-        private static LocusMatchDetails CalculateMatchDetailsForDonorHla(
-            AlleleLevelLocusMatchCriteria locusMatchCriteria,
-            LocusInfo<PGroup> expandedHla)
-        {
             if (expandedHla.Position1 == null ^ expandedHla.Position2 == null)
             {
                 throw new ArgumentException(
                     "Locus cannot be partially typed. Either both positions should have data, or both should be null - check the validity of the matching data.");
             }
 
+            var matchCount = alleleGroupsMatchingCount.MatchCount(expandedLocusMatchCriteria, expandedHla);
+
             return new LocusMatchDetails
             {
-                MatchCount = CalculateMatchCount(locusMatchCriteria, expandedHla),
+                MatchCount = matchCount
             };
-        }
-
-        private static int CalculateMatchCount(AlleleLevelLocusMatchCriteria locusMatchCriteria, LocusInfo<PGroup> expandedHla)
-        {
-            var hla1 = expandedHla.Position1;
-            var hla2 = expandedHla.Position2;
-
-            // Assume a match until we know otherwise - untyped loci should count as a potential match
-            var matchCount = 2;
-            if (locusMatchCriteria != null && hla1 != null && hla2 != null)
-            {
-                // We have typed search and donor hla to compare
-                matchCount = 0;
-
-                var atLeastOneMatch = locusMatchCriteria.PGroupsToMatchInPositionOne.Any(pg => hla1.Union(hla2).Contains(pg)) ||
-                                      locusMatchCriteria.PGroupsToMatchInPositionTwo.Any(pg => hla1.Union(hla2).Contains(pg));
-
-                if (atLeastOneMatch)
-                {
-                    matchCount++;
-                }
-
-                var twoMatches = DirectMatch(locusMatchCriteria, expandedHla) || CrossMatch(locusMatchCriteria, expandedHla);
-                if (twoMatches)
-                {
-                    matchCount++;
-                }
-            }
-
-            return matchCount;
-        }
-
-        private static bool DirectMatch(AlleleLevelLocusMatchCriteria locusMatchCriteria, LocusInfo<PGroup> expandedHla)
-        {
-            var hla1 = expandedHla.Position1;
-            var hla2 = expandedHla.Position2;
-            return locusMatchCriteria.PGroupsToMatchInPositionOne.Any(pg => hla1.Contains(pg)) &&
-                   locusMatchCriteria.PGroupsToMatchInPositionTwo.Any(pg => hla2.Contains(pg));
-        }
-
-        private static bool CrossMatch(AlleleLevelLocusMatchCriteria locusMatchCriteria, LocusInfo<PGroup> expandedHla)
-        {
-            var hla1 = expandedHla.Position1;
-            var hla2 = expandedHla.Position2;
-            return locusMatchCriteria.PGroupsToMatchInPositionOne.Any(pg => hla2.Contains(pg)) &&
-                   locusMatchCriteria.PGroupsToMatchInPositionTwo.Any(pg => hla1.Contains(pg));
         }
     }
 }
