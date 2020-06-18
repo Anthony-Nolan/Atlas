96bf175ea2fe5227cbb72b9c2d7a08c089c3d6bf
Tom Johnson <tom.johnson@softwire.com>
2020-06-18 16:47:36 +0100

    feature: ATLAS-414: Created debug endpoint and service for genotype matching

---
+++ Atlas.MatchPrediction.Client.Models/MatchCalculation/MatchCalculationInput.cs
@@ -0,0 +1,10 @@
+﻿using Atlas.Common.GeneticData.PhenotypeInfo;
+
+namespace Atlas.MatchPrediction.Client.Models.MatchCalculation
+{
+    public class MatchCalculationInput
+    {
+        public PhenotypeInfo<string> DonorHla { get; set; }
+        public PhenotypeInfo<string> PatientHla { get; set; }
+    }
+}
---
+++ Atlas.MatchPrediction.Functions/Functions/MatchCalculationFunctions.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Atlas.MatchPrediction.Client.Models.MatchCalculation;
+using Atlas.MatchPrediction.Services.MatchCalculation;
+using AzureFunctions.Extensions.Swashbuckle.Attribute;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Newtonsoft.Json;
+
+namespace Atlas.MatchPrediction.Functions.Functions
+{
+    public class MatchCalculationFunctions
+    {
+        private readonly IMatchCalculationService matchCalculatorService;
+
+        public MatchCalculationFunctions(IMatchCalculationService matchCalculatorService)
+        {
+            this.matchCalculatorService = matchCalculatorService;
+        }
+
+        [FunctionName(nameof(MatchAtGGroupLevelCalculation))]
+        public async Task<IActionResult> MatchAtGGroupLevelCalculation(
+            [HttpTrigger(AuthorizationLevel.Function, "post")] [RequestBodyType(typeof(MatchCalculationInput), "match calculation")]
+            HttpRequest request)
+        {
+            var matchCalculationInput = JsonConvert.DeserializeObject<MatchCalculationInput>(await new StreamReader(request.Body).ReadToEndAsync());
+
+            var likelihood = await matchCalculatorService.MatchAtGGroupLevel(matchCalculationInput.PatientHla, matchCalculationInput.DonorHla);
+            return new JsonResult(likelihood);
+        }
+    }
+}
