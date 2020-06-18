5d60c857f436cb9e827b88aa597499cbafb110ed
Josh Powell <josh.powell@softwire.com>
2020-06-18 16:12:29 +0100

    refactor:L atlas-386: remove matchingalgorithm cache function

--- Atlas.MatchingAlgorithm.Functions/Functions/CachingFunctions.cs
+++ Atlas.MatchingAlgorithm.Functions/Functions/CachingFunctions.cs
@@ -3,31 +3,18 @@ using System.Threading.Tasks;
 using Atlas.Common.Utils;
 using Atlas.HlaMetadataDictionary.ExternalInterface;
 using Atlas.MultipleAlleleCodeDictionary;
+using Atlas.MultipleAlleleCodeDictionary.ExternalInterface;
 using Microsoft.Azure.WebJobs;
 
 namespace Atlas.MatchingAlgorithm.Functions.Functions
 {
     public class CachingFunctions
     {
-        private readonly IAntigenCachingService antigenCachingService;
         private readonly IHlaMetadataCacheControl hlaMetadataCacheControl;
 
-        public CachingFunctions(
-            IAntigenCachingService antigenCachingService,
-            IHlaMetadataCacheControl hlaMetadataCacheControl
-        )
+        public CachingFunctions(IHlaMetadataCacheControl hlaMetadataCacheControl)
         {
-            this.antigenCachingService = antigenCachingService;
             this.hlaMetadataCacheControl = hlaMetadataCacheControl;
-        }
-
-        [SuppressMessage(null, SuppressMessage.UnusedParameter, Justification = SuppressMessage.UsedByAzureTrigger)]
-        [FunctionName(nameof(UpdateHlaCache))]
-        public async Task UpdateHlaCache(
-            [TimerTrigger("00 00 02 * * *", RunOnStartup = true)]
-            TimerInfo timerInfo)
-        {
-            await antigenCachingService.GenerateAntigenCache();
         }
 
         [SuppressMessage(null, SuppressMessage.UnusedParameter, Justification = SuppressMessage.UsedByAzureTrigger)]
