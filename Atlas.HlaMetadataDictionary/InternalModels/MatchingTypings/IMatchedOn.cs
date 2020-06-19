1dc75fa76c9f6158f8e7751f562fe3fc76ffaa57
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2020-06-19 16:26:19 +0100

    docs: ATLAS-368: Document purpose of the TypingUsedInMatching property.

--- Atlas.HlaMetadataDictionary/InternalModels/MatchingTypings/IMatchedOn.cs
+++ Atlas.HlaMetadataDictionary/InternalModels/MatchingTypings/IMatchedOn.cs
@@ -5,6 +5,13 @@ namespace Atlas.HlaMetadataDictionary.InternalModels.MatchingTypings
     internal interface IMatchedOn
     {
         HlaTyping HlaTyping { get; }
+
+        /// <summary>
+        /// There are times where the value held in <see cref="HlaTyping"/> HLA typing is not the one
+        /// that should be used in HMD pre-calculations.
+        /// E.g., Where an allele or serology has been deemed "identical to" a second typing within the same HLA nomenclature version.
+        /// This property is therefore used to hold the typing name that should actually be used in HMD pre-calculations.
+        /// </summary>
         HlaTyping TypingUsedInMatching { get; }
     }
 }
