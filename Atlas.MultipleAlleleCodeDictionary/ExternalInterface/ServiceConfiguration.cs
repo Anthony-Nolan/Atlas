62a955170d66d23d935973ab98419b5be3af00a8
Josh Powell <josh.powell@softwire.com>
2020-06-19 10:51:28 +0100

    wip: atlas-54 remove hla service client

--- Atlas.MultipleAlleleCodeDictionary/ExternalInterface/ServiceConfiguration.cs
+++ Atlas.MultipleAlleleCodeDictionary/ExternalInterface/ServiceConfiguration.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using Atlas.Common.ApplicationInsights;
 using Atlas.Common.Caching;
-using Atlas.Common.NovaHttpClient.Client;
 using Atlas.MultipleAlleleCodeDictionary.AzureStorage.Repositories;
-using Atlas.MultipleAlleleCodeDictionary.HlaService;
 using Atlas.MultipleAlleleCodeDictionary.MacCacheService;
 using Atlas.MultipleAlleleCodeDictionary.MacImportServices;
 using Atlas.MultipleAlleleCodeDictionary.MacImportServices.SourceData;
@@ -12,7 +10,6 @@ using Atlas.MultipleAlleleCodeDictionary.Settings;
 using Atlas.MultipleAlleleCodeDictionary.utils;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
-using Newtonsoft.Json;
 
 namespace Atlas.MultipleAlleleCodeDictionary.ExternalInterface
 {
@@ -47,31 +44,6 @@ namespace Atlas.MultipleAlleleCodeDictionary.ExternalInterface
             services.AddScoped<IMacCacheService, MacCacheService.MacCacheService>();
             services.AddScoped<IMacDictionary, MacDictionary>();
             services.AddScoped<IMacExpander, MacExpander>();
-        }
-
-        private static IHlaServiceClient GetHlaServiceClient(
-            string hlaClientApiKey,
-            string hlaClientBaseUrl,
-            ILogger logger)
-        {
-            var clientSettings = new HttpClientSettings
-            {
-                ApiKey = hlaClientApiKey,
-                BaseUrl = hlaClientBaseUrl,
-                ClientName = "hla_service_client",
-                JsonSettings = new JsonSerializerSettings()
-            };
-            
-            try
-            {
-                return new HlaServiceClient(clientSettings, logger);
-            }
-            // When running on startup, the client setup will often throw a NullReferenceException.
-            // This appears to go away when running not immediately after startup, so we retry once to circumvent
-            catch (NullReferenceException)
-            {
-                return new HlaServiceClient(clientSettings, logger);
-            }
         }
     }
 }
