7784a94a758eabe178ef5cfbd7aeb05dfc9c49e1
Josh Powell <josh.powell@softwire.com>
2020-06-19 14:42:17 +0100

    review: atlas-386: fix misnamed methopd params

--- Atlas.MultipleAlleleCodeDictionary/ExternalInterface/MacDictionary.cs
+++ Atlas.MultipleAlleleCodeDictionary/ExternalInterface/MacDictionary.cs
@@ -34,8 +34,11 @@ namespace Atlas.MultipleAlleleCodeDictionary.ExternalInterface
         /// This is the case for specific MACs with the incorrect first field. Technically only one first field is permitted per-specific MAC,
         /// but this dictionary will expand specific MACs ignoring the given first field.
         /// </remarks>
-        public Task<IEnumerable<string>> GetHlaFromMac(string macCode, string mac);
+        public Task<IEnumerable<string>> GetHlaFromMac(string macCode, string firstField);
 
+        /// <param name="macWithFirstField">
+        /// Should be in the format "01:AB". Where 01 is the first field.
+        /// </param>
         public Task<IEnumerable<string>> GetHlaFromMac(string macWithFirstField);
     }
 
@@ -66,15 +69,12 @@ namespace Atlas.MultipleAlleleCodeDictionary.ExternalInterface
         }
 
         /// <inheritdoc />
-        public async Task<IEnumerable<string>> GetHlaFromMac(string firstField, string mac)
+        public async Task<IEnumerable<string>> GetHlaFromMac(string mac, string firstField)
         {
-            return await macCacheService.GetHlaFromMac(mac, firstField);
+            return await macCacheService.GetHlaFromMac(firstField, mac);
         }
         
-
-        /// <param name="macWithFirstField">
-        /// Should be in the format "01:AB". Where 01 is the first field.
-        /// </param>
+        /// <inheritdoc />
         public async Task<IEnumerable<string>> GetHlaFromMac(string macWithFirstField)
         {
             var parts = macWithFirstField.Split(MolecularTypingNameConstants.FieldDelimiter);
