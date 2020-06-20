36b9b979343eed9d5e132d3317122d4078a120a1
Mike.McLean <Mike.McLean@softwire.com>
2020-06-20 18:37:58 +0100

    tests: ATLAS-423: Fix broken tests relating to Index Steps

--- Atlas.MatchingAlgorithm.Test/TestHelpers/Builders/DataRefresh/DataRefreshRecordBuilder.cs
+++ Atlas.MatchingAlgorithm.Test/TestHelpers/Builders/DataRefresh/DataRefreshRecordBuilder.cs
@@ -22,9 +22,11 @@ namespace Atlas.MatchingAlgorithm.Test.TestHelpers.Builders.DataRefresh
             {
                 DataRefreshStage.MetadataDictionaryRefresh => builder.With(r => r.MetadataDictionaryRefreshCompleted, DateTime.UtcNow),
                 DataRefreshStage.DataDeletion => builder.With(r => r.DataDeletionCompleted, DateTime.UtcNow),
+                DataRefreshStage.IndexRemoval => builder.With(r => r.IndexDeletionCompleted, DateTime.UtcNow),
                 DataRefreshStage.DatabaseScalingSetup => builder.With(r => r.DatabaseScalingSetupCompleted, DateTime.UtcNow),
                 DataRefreshStage.DonorImport => builder.With(r => r.DonorImportCompleted, DateTime.UtcNow),
                 DataRefreshStage.DonorHlaProcessing => builder.With(r => r.DonorHlaProcessingCompleted, DateTime.UtcNow),
+                DataRefreshStage.IndexRecreation => builder.With(r => r.IndexRecreationCompleted, DateTime.UtcNow),
                 DataRefreshStage.DatabaseScalingTearDown => builder.With(r => r.DatabaseScalingTearDownCompleted, DateTime.UtcNow),
                 DataRefreshStage.QueuedDonorUpdateProcessing => builder.With(r => r.QueuedDonorUpdatesCompleted, DateTime.UtcNow),
                 _ => throw new ArgumentOutOfRangeException(nameof(stage))
