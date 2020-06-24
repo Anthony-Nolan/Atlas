da4566f5390cdfd205601c0ed4f8d813cc63eec4
Ben Below <benjamin.below@softwire.com>
2020-06-24 13:03:28 +0100

    refactor: ATLAS-327: Pull settings up to entry points - Matching Algorithm, All remaining settings

--- Atlas.MatchingAlgorithm.Functions.DonorManagement/Startup.cs
+++ Atlas.MatchingAlgorithm.Functions.DonorManagement/Startup.cs
@@ -1,9 +1,12 @@
 using Atlas.Common.ApplicationInsights;
+using Atlas.Common.Notifications;
 using Atlas.Common.Utils.Extensions;
 using Atlas.HlaMetadataDictionary.ExternalInterface.Settings;
 using Atlas.MatchingAlgorithm.DependencyInjection;
 using Atlas.MatchingAlgorithm.Functions.DonorManagement;
+using Atlas.MatchingAlgorithm.Settings;
 using Atlas.MatchingAlgorithm.Settings.Azure;
+using Atlas.MatchingAlgorithm.Settings.ServiceBus;
 using Atlas.MultipleAlleleCodeDictionary.Settings;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection;
@@ -20,17 +23,23 @@ namespace Atlas.MatchingAlgorithm.Functions.DonorManagement
             builder.Services.RegisterMatchingAlgorithmDonorManagement(
                 DependencyInjectionUtils.OptionsReaderFor<ApplicationInsightsSettings>(),
                 DependencyInjectionUtils.OptionsReaderFor<AzureStorageSettings>(),
+                DependencyInjectionUtils.OptionsReaderFor<DonorManagementSettings>(),
                 DependencyInjectionUtils.OptionsReaderFor<HlaMetadataDictionarySettings>(),
-                DependencyInjectionUtils.OptionsReaderFor<MacDictionarySettings>()
+                DependencyInjectionUtils.OptionsReaderFor<MacDictionarySettings>(),
+                DependencyInjectionUtils.OptionsReaderFor<MessagingServiceBusSettings>(),
+                DependencyInjectionUtils.OptionsReaderFor<NotificationsServiceBusSettings>()
             );
         }
 
         private static void RegisterSettings(IServiceCollection services)
         {
             services.RegisterOptions<ApplicationInsightsSettings>("ApplicationInsights");
-            services.RegisterOptions<ApplicationInsightsSettings>("AzureStorage");
+            services.RegisterOptions<AzureStorageSettings>("AzureStorage");
+            services.RegisterOptions<DonorManagementSettings>("MessagingServiceBus:DonorManagement");
             services.RegisterOptions<HlaMetadataDictionarySettings>("HlaMetadataDictionary");
             services.RegisterOptions<MacDictionarySettings>("MacDictionary");
+            services.RegisterOptions<MessagingServiceBusSettings>("MessagingServiceBus");
+            services.RegisterOptions<NotificationsServiceBusSettings>("NotificationsServiceBus");
         }
     }
 }
