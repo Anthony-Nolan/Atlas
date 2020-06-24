4039b42519e9e7e92d13487f636fe0a653c922a6
Tom Johnson <tom.johnson@softwire.com>
2020-06-24 15:08:50 +0100

    feature: ATLAS-29: Expand ambiguous hla

--- Atlas.MatchPrediction/Services/MatchProbability/MatchProbabilityService.cs
+++ Atlas.MatchPrediction/Services/MatchProbability/MatchProbabilityService.cs
@@ -1,5 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
 using Atlas.MatchPrediction.Client.Models.MatchProbability;
+using Atlas.MatchPrediction.Services.ExpandAmbiguousPhenotype;
 
 namespace Atlas.MatchPrediction.Services.MatchProbability
 {
@@ -10,8 +12,23 @@ namespace Atlas.MatchPrediction.Services.MatchProbability
 
     public class MatchProbabilityService : IMatchProbabilityService
     {
+        private readonly ICompressedPhenotypeExpander compressedPhenotypeExpander;
+
+        public MatchProbabilityService(ICompressedPhenotypeExpander compressedPhenotypeExpander)
+        {
+            this.compressedPhenotypeExpander = compressedPhenotypeExpander;
+        }
+
         public async Task<MatchProbabilityResponse> CalculateMatchProbability(MatchProbabilityInput matchProbabilityInput)
         {
+            var patientGenotypes =
+                await compressedPhenotypeExpander.ExpandCompressedPhenotype(
+                    matchProbabilityInput.PatientHla,
+                    matchProbabilityInput.HlaNomenclatureVersion);
+            var donorGenotypes =
+                await compressedPhenotypeExpander.ExpandCompressedPhenotype(
+                    matchProbabilityInput.DonorHla,
+                    matchProbabilityInput.HlaNomenclatureVersion);
 
             return new MatchProbabilityResponse();
         }
