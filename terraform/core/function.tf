93961f4e957c0bf48f581d712154f5cbf91753be
Ben Below <benjamin.below@softwire.com>
2020-06-24 13:39:46 +0100

    review: ATLAS-327: Apply missed rename of MacImport -> MacDictionary

--- terraform/core/function.tf
+++ terraform/core/function.tf
@@ -12,15 +12,15 @@ resource "azurerm_function_app" "atlas_function" {
   app_settings = {
     // APPINSIGHTS_INSTRUMENTATIONKEY
     //      The azure functions dashboard requires the instrumentation key with this name to integrate with application insights.
-    "APPINSIGHTS_INSTRUMENTATIONKEY"            = azurerm_application_insights.atlas.instrumentation_key
-    "ApplicationInsights:LogLevel"              = var.APPLICATION_INSIGHTS_LOG_LEVEL
-    "MacDictionary:ConnectionString"            = azurerm_storage_account.azure_storage.primary_connection_string
-    "MacDictionary:TableName"                   = module.multiple_allele_code_lookup.storage_table.name,
-    "MacDictionary:MacSourceUrl"                = var.MAC_SOURCE
-    "MessagingServiceBus:ConnectionString"      = azurerm_servicebus_namespace_authorization_rule.read-write.primary_connection_string
-    "MessagingServiceBus:SearchRequestsQueue"   = module.matching_algorithm.general.search_requests_queue
-    "MessagingServiceBus:SearchResultsTopic"    = module.matching_algorithm.general.search_results_topic
-    "WEBSITE_MAX_DYNAMIC_APPLICATION_SCALE_OUT" = "1"
-    "WEBSITE_RUN_FROM_PACKAGE"                  = var.WEBSITE_RUN_FROM_PACKAGE
+    "APPINSIGHTS_INSTRUMENTATIONKEY"             = azurerm_application_insights.atlas.instrumentation_key
+    "ApplicationInsights:LogLevel"               = var.APPLICATION_INSIGHTS_LOG_LEVEL
+    "MacDictionary:AzureStorageConnectionString" = azurerm_storage_account.azure_storage.primary_connection_string
+    "MacDictionary:TableName"                    = module.multiple_allele_code_lookup.storage_table.name,
+    "MacDictionary:MacSourceUrl"                 = var.MAC_SOURCE
+    "MessagingServiceBus:ConnectionString"       = azurerm_servicebus_namespace_authorization_rule.read-write.primary_connection_string
+    "MessagingServiceBus:SearchRequestsQueue"    = module.matching_algorithm.general.search_requests_queue
+    "MessagingServiceBus:SearchResultsTopic"     = module.matching_algorithm.general.search_results_topic
+    "WEBSITE_MAX_DYNAMIC_APPLICATION_SCALE_OUT"  = "1"
+    "WEBSITE_RUN_FROM_PACKAGE"                   = var.WEBSITE_RUN_FROM_PACKAGE
   }
 }
