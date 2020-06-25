1c878babbf7dbebf90f6d54d66032b706bf8eb96
Ben Below <benjamin.below@softwire.com>
2020-06-25 13:13:02 +0100

    fix: ATLAS-327: Add HMD azure connection string to MPA settings

--- terraform/core/modules/match_prediction/function.tf
+++ terraform/core/modules/match_prediction/function.tf
@@ -12,18 +12,19 @@ resource "azurerm_function_app" "atlas_match_prediction_function" {
   app_settings = {
     // APPINSIGHTS_INSTRUMENTATIONKEY
     //      The azure functions dashboard requires the instrumentation key with this name to integrate with application insights.
-    "APPINSIGHTS_INSTRUMENTATIONKEY"                    = var.application_insights.instrumentation_key
-    "ApplicationInsights:LogLevel"                      = var.APPLICATION_INSIGHTS_LOG_LEVEL
-    "AzureStorage:ConnectionString"                     = var.azure_storage.primary_connection_string
-    "AzureStorage:HaplotypeFrequencySetImportContainer" = azurerm_storage_container.haplotype_frequency_set_blob_container.name
-    "MacDictionary:AzureStorageConnectionString"        = var.azure_storage.primary_connection_string
-    "MacDictionary:MacSourceUrl"                        = var.MAC_SOURCE
-    "MacDictionary:TableName"                           = var.mac_import_table.name,
-    "NotificationsServiceBus:ConnectionString"          = var.servicebus_namespace_authorization_rules.write-only.primary_connection_string
-    "NotificationsServiceBus:AlertsTopic"               = var.servicebus_topics.alerts.name
-    "NotificationsServiceBus:NotificationsTopic"        = var.servicebus_topics.notifications.name
-    "WEBSITE_MAX_DYNAMIC_APPLICATION_SCALE_OUT"         = "1"
-    "WEBSITE_RUN_FROM_PACKAGE"                          = var.WEBSITE_RUN_FROM_PACKAGE
+    "APPINSIGHTS_INSTRUMENTATIONKEY"                     = var.application_insights.instrumentation_key
+    "ApplicationInsights:LogLevel"                       = var.APPLICATION_INSIGHTS_LOG_LEVEL
+    "AzureStorage:ConnectionString"                      = var.azure_storage.primary_connection_string
+    "AzureStorage:HaplotypeFrequencySetImportContainer"  = azurerm_storage_container.haplotype_frequency_set_blob_container.name
+    "HlaMetadataDictionary:AzureStorageConnectionString" = var.azure_storage.primary_connection_string
+    "MacDictionary:AzureStorageConnectionString"         = var.azure_storage.primary_connection_string
+    "MacDictionary:MacSourceUrl"                         = var.MAC_SOURCE
+    "MacDictionary:TableName"                            = var.mac_import_table.name,
+    "NotificationsServiceBus:ConnectionString"           = var.servicebus_namespace_authorization_rules.write-only.primary_connection_string
+    "NotificationsServiceBus:AlertsTopic"                = var.servicebus_topics.alerts.name
+    "NotificationsServiceBus:NotificationsTopic"         = var.servicebus_topics.notifications.name
+    "WEBSITE_MAX_DYNAMIC_APPLICATION_SCALE_OUT"          = "1"
+    "WEBSITE_RUN_FROM_PACKAGE"                           = var.WEBSITE_RUN_FROM_PACKAGE
   }
 
   connection_string {
