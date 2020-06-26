26f42339d9963a52fa19dbd21273b226504b02cc
Ben Below <benjamin.below@softwire.com>
2020-06-26 21:59:03 +0100

    feature: ATLAS-236: Run match prediction on search results

--- Atlas.Functions/DurableFunctions/Search/Orchestration/SearchOrchestrationFunctions.cs
+++ Atlas.Functions/DurableFunctions/Search/Orchestration/SearchOrchestrationFunctions.cs
@@ -1,10 +1,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Atlas.Common.GeneticData.PhenotypeInfo;
+using Atlas.Common.Utils.Extensions;
 using Atlas.Functions.DurableFunctions.Search.Activity;
 using Atlas.MatchingAlgorithm.Client.Models.SearchRequests;
 using Atlas.MatchingAlgorithm.Client.Models.SearchResults;
 using Atlas.MatchingAlgorithm.Common.Models;
+using Atlas.MatchingAlgorithm.Extensions;
+using Atlas.MatchPrediction.Client.Models.MatchProbability;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.DurableTask;
 
@@ -17,7 +21,7 @@ namespace Atlas.Functions.DurableFunctions.Search.Orchestration
     public static class SearchOrchestrationFunctions
     {
         [FunctionName(nameof(SearchOrchestrator))]
-        public static async Task<List<string>> SearchOrchestrator([OrchestrationTrigger] IDurableOrchestrationContext context)
+        public static async Task<List<object>> SearchOrchestrator([OrchestrationTrigger] IDurableOrchestrationContext context)
         {
             var searchRequest = context.GetInput<SearchRequest>();
             var searchResults = await context.CallActivityAsync<SearchResultSet>(
@@ -25,7 +29,42 @@ namespace Atlas.Functions.DurableFunctions.Search.Orchestration
                 new IdentifiedSearchRequest {Id = context.InstanceId, SearchRequest = searchRequest}
             );
 
-            return searchResults.SearchResults.Select(r => r.DonorId.ToString()).ToList();
+            var matchPredictionResults = (await Task.WhenAll(
+                searchResults.SearchResults.Select(r => RunMatchPrediction(context, searchRequest, r))
+            )).ToDictionary();
+
+           return searchResults.SearchResults
+                .Select(r => new {MatchResult = r, MPAResult = matchPredictionResults[r.DonorId]} as object)
+                .ToList();
+        }
+
+        /// <returns>A Task returning a Key Value pair of Atlas Donor ID, and match prediction response.</returns>
+        private static async Task<KeyValuePair<int, MatchProbabilityResponse>> RunMatchPrediction(
+            IDurableOrchestrationContext context,
+            SearchRequest searchRequest,
+            SearchResult matchingResult)
+        {
+            var matchPredictionResult = await context.CallActivityAsync<MatchProbabilityResponse>(
+                nameof(SearchActivityFunctions.RunMatchPrediction),
+                new MatchProbabilityInput
+                {
+                    // TODO: ATLAS-236: Get donor HLA from result model
+                    DonorHla = new PhenotypeInfo<string>
+                    {
+                        // TODO: ATLAS-236: Send all 5 loci, only 5 loci supported just now.
+                        A = new LocusInfo<string>("*01:01:01"),
+                        B = new LocusInfo<string>("*08:182"),
+                        C = new LocusInfo<string>("*07:02:80"),
+                        Dqb1 = new LocusInfo<string>("*06:01:03"),
+                        Drb1 = new LocusInfo<string>("*11:129"),
+                    },
+                    // TODO: ATLAS-236: Just use PhenotypeInfo directly on client model
+                    PatientHla = searchRequest.SearchHlaData.ToPhenotypeInfo(),
+                    // TODO: ATLAS-236: Get nomenclature version from search results
+                    HlaNomenclatureVersion = "3400"
+                }
+            );
+            return new KeyValuePair<int, MatchProbabilityResponse>(matchingResult.DonorId, matchPredictionResult);
         }
     }
 }
