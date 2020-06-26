9d9fc32061e7ec7785789c305a1eb11eecf8e20e
Ben Below <benjamin.below@softwire.com>
2020-06-26 21:59:03 +0100

    feature: ATLAS-236: Run match prediction on search results

--- Atlas.Functions/DurableFunctions/Search/Activity/SearchActivityFunctions.cs
+++ Atlas.Functions/DurableFunctions/Search/Activity/SearchActivityFunctions.cs
@@ -2,6 +2,8 @@ using System.Threading.Tasks;
 using Atlas.MatchingAlgorithm.Client.Models.SearchResults;
 using Atlas.MatchingAlgorithm.Common.Models;
 using Atlas.MatchingAlgorithm.Services.Search;
+using Atlas.MatchPrediction.Client.Models.MatchProbability;
+using Atlas.MatchPrediction.Services.MatchProbability;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.DurableTask;
 
@@ -10,16 +12,24 @@ namespace Atlas.Functions.DurableFunctions.Search.Activity
     public class SearchActivityFunctions
     {
         private readonly ISearchRunner searchRunner;
+        private readonly IMatchProbabilityService matchProbabilityService;
 
-        public SearchActivityFunctions(ISearchRunner searchRunner)
+        public SearchActivityFunctions(ISearchRunner searchRunner, IMatchProbabilityService matchProbabilityService)
         {
             this.searchRunner = searchRunner;
+            this.matchProbabilityService = matchProbabilityService;
         }
         
         [FunctionName(nameof(RunMatchingAlgorithm))]
         public async Task<SearchResultSet> RunMatchingAlgorithm([ActivityTrigger] IdentifiedSearchRequest searchRequest)
         {
             return await searchRunner.RunSearch(searchRequest);
+        }
+        
+        [FunctionName(nameof(RunMatchPrediction))]
+        public async Task<MatchProbabilityResponse> RunMatchPrediction([ActivityTrigger] MatchProbabilityInput matchProbabilityInput)
+        {
+            return await matchProbabilityService.CalculateMatchProbability(matchProbabilityInput);
         }
     }
 }
--- Atlas.MatchPrediction.Data/Repositories/HaplotypeFrequencySetRepository.cs
+++ Atlas.MatchPrediction.Data/Repositories/HaplotypeFrequencySetRepository.cs
@@ -1,8 +1,8 @@
-﻿using Atlas.MatchPrediction.Data.Context;
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Atlas.MatchPrediction.Data.Context;
 using Atlas.MatchPrediction.Data.Models;
 using Microsoft.EntityFrameworkCore;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Atlas.MatchPrediction.Data.Repositories
 {
@@ -15,44 +15,57 @@ namespace Atlas.MatchPrediction.Data.Repositories
 
     public class HaplotypeFrequencySetRepository : IHaplotypeFrequencySetRepository
     {
-        private readonly MatchPredictionContext context;
+        private readonly string connectionString;
+        private readonly ContextFactory contextFactory;
 
-        public HaplotypeFrequencySetRepository(MatchPredictionContext context)
+        private MatchPredictionContext NewContext() => contextFactory.Create(connectionString);
+
+        public HaplotypeFrequencySetRepository(string connectionString, ContextFactory contextFactory)
         {
-            this.context = context;
+            this.connectionString = connectionString;
+            this.contextFactory = contextFactory;
         }
 
         public async Task<HaplotypeFrequencySet> GetActiveSet(string registry, string ethnicity)
         {
-            return await context.HaplotypeFrequencySets
-                .Where(set => set.Active == true && set.RegistryCode == registry && set.EthnicityCode == ethnicity)
-                .SingleOrDefaultAsync();
+            await using (var context = NewContext())
+            {
+                return await context.HaplotypeFrequencySets
+                    .Where(set => set.Active == true && set.RegistryCode == registry && set.EthnicityCode == ethnicity)
+                    .SingleOrDefaultAsync();
+            }
         }
 
         public async Task<HaplotypeFrequencySet> AddSet(HaplotypeFrequencySet set)
         {
-            await context.HaplotypeFrequencySets.AddAsync(set);
-            await context.SaveChangesAsync();
-            return set;
+            await using (var context = NewContext())
+            {
+                await context.HaplotypeFrequencySets.AddAsync(set);
+                await context.SaveChangesAsync();
+                return set;
+            }
         }
 
         public async Task ActivateSet(int setId)
         {
-            var set = await context.HaplotypeFrequencySets.SingleAsync(s => s.Id == setId);
-            set.Active = true;
-
-            var otherMatchingSets = context.HaplotypeFrequencySets.Where(s =>
-                s.Id != setId
-                && s.EthnicityCode == set.EthnicityCode
-                && s.RegistryCode == set.RegistryCode
-            );
-
-            foreach (var otherMatchingSet in otherMatchingSets)
+            await using (var context = NewContext())
             {
-                otherMatchingSet.Active = false;
-            }
+                var set = await context.HaplotypeFrequencySets.SingleAsync(s => s.Id == setId);
+                set.Active = true;
 
-            await context.SaveChangesAsync();
+                var otherMatchingSets = context.HaplotypeFrequencySets.Where(s =>
+                    s.Id != setId
+                    && s.EthnicityCode == set.EthnicityCode
+                    && s.RegistryCode == set.RegistryCode
+                );
+
+                foreach (var otherMatchingSet in otherMatchingSets)
+                {
+                    otherMatchingSet.Active = false;
+                }
+
+                await context.SaveChangesAsync();
+            }
         }
     }
 }
