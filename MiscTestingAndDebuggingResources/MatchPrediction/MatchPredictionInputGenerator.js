9caeb24b2b9d1f8cfeb28166f36dcf219b1c8f21
Ben Below <benjamin.below@softwire.com>
2020-07-01 11:59:01 +0100

    chore: ATLAS-29: Add match prediction input generator

---
+++ MiscTestingAndDebuggingResources/MatchPrediction/MatchPredictionInputGenerator.js
@@ -0,0 +1,66 @@
+var fs = require('fs');
+
+const config = {
+    fileName: "imputation-input"
+}
+
+// USAGE INSTRUCTIONS
+// This method is designed for hla copied directly from the donor database. 
+// The expected locus order is A-B-C-DQB1-DRB1
+// The easiest way to use is to copy these columns directly from a `SELECT * FROM DONORS` query of the donor store
+// You can also copy data directly from the HF Set database - it will treat a 5-value input as homozygous at all loci
+// The output is the JSON to POST to the match prediction endpoints
+const fileContent = generateInputFromHlaData(
+    "01:01:01G	08:01:01G	07:01:01G	02:01:01G	03:01:17",
+    "01:01:01G	08:01:01G	07:01:01G	02:01:01G	03:01:17"
+);
+
+fs.writeFile(`${config.fileName}.json`, fileContent, () => {})
+
+function generateInputFromHlaData(donorRawData, patientRawData) {
+    const initialDonorHlaValues = donorRawData.split(/\s/);
+    const initialPatientHlaValues = patientRawData.split(/\s/);
+
+
+    const donorHlaValues = initialDonorHlaValues.length === 10 ? initialDonorHlaValues : duplicateAllValues(initialDonorHlaValues);
+    const patientHlaValues = initialPatientHlaValues.length === 10 ? initialPatientHlaValues : duplicateAllValues(initialPatientHlaValues);
+
+    const hlaNomenclatureVersion = "3400";
+
+    return `{
+    "HlaNomenclatureVersion": "${hlaNomenclatureVersion}",
+    "DonorHla": ${buildPhenotypeInfo(donorHlaValues)},
+    "PatientHla": ${buildPhenotypeInfo(patientHlaValues)},
+}`
+}
+
+function buildPhenotypeInfo(hlaValues) {
+    return `{
+        "a": {
+            "position1": "${hlaValues[0]}",
+            "position2": "${hlaValues[1]}"
+        },
+        "b": {
+            "position1": "${hlaValues[2]}",
+            "position2": "${hlaValues[3]}"
+        },
+        "c": {
+            "position1": "${hlaValues[4]}",
+            "position2": "${hlaValues[5]}"
+        },
+        "dqb1": {
+            "position1": "${hlaValues[6]}",
+            "position2": "${hlaValues[7]}"
+        },
+        "drb1": {
+            "position1": "${hlaValues[8]}",
+            "position2": "${hlaValues[9]}"
+        }
+    }`
+}
+
+function duplicateAllValues(array) {
+    return array.reduce(function (res, current, index, array) {
+        return res.concat([current, current]);
+    }, []);
+}
