659e0ae0ca1e287055f5e2b727542fdce672f0c2
Josh Powell <josh.powell@softwire.com>
2020-07-01 14:24:55 +0100

    atlas-23: add individual meta data to match prediction request

--- Atlas.MatchPrediction/ExternalInterface/Models/MatchProbability/MatchProbabilityInput.cs
+++ Atlas.MatchPrediction/ExternalInterface/Models/MatchProbability/MatchProbabilityInput.cs
@@ -1,11 +1,14 @@
 ﻿using Atlas.Common.GeneticData.PhenotypeInfo;
+using Atlas.MatchPrediction.Models;
 
 namespace Atlas.MatchPrediction.ExternalInterface.Models.MatchProbability
 {
     public class MatchProbabilityInput
     {
         public PhenotypeInfo<string> DonorHla { get; set; }
+        public IndividualMetaData DonorMetaData { get; set; }
         public PhenotypeInfo<string> PatientHla { get; set; }
+        public IndividualMetaData PatientMetaData { get; set; }
         public string HlaNomenclatureVersion { get; set; }
     }
 }
