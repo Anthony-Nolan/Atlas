ed38b11b7a8589868baafefeb993d725d70cbf9a
Josh Powell <josh.powell@softwire.com>
2020-07-01 14:22:04 +0100

    feature: atlas-23: add haplotype frequency set service

---
+++ Atlas.MatchPrediction/Services/HaplotypeFrequencySets/HaplotypeFrequencySetService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using Atlas.MatchPrediction.Data.Models;
+using Atlas.MatchPrediction.Data.Repositories;
+using Atlas.MatchPrediction.Models;
+
+namespace Atlas.MatchPrediction.Services.HaplotypeFrequencySets
+{
+    internal interface IHaplotypeFrequencySetService
+    {
+        Task<HaplotypeFrequencySet> GetHaplotypeFrequencySetId(IndividualMetaData donorInfo, IndividualMetaData patientInfo);
+    }
+    
+    internal class HaplotypeFrequencySetService : IHaplotypeFrequencySetService
+    {
+        private readonly IHaplotypeFrequencySetRepository repository;
+
+        public HaplotypeFrequencySetService(IHaplotypeFrequencySetRepository repository)
+        {
+            this.repository = repository;
+        }
+        
+        public async Task<HaplotypeFrequencySet> GetHaplotypeFrequencySetId(IndividualMetaData donorInfo, IndividualMetaData patientInfo)
+        {
+            return await repository.GetActiveSet(donorInfo.RegistryId, donorInfo.EthnicityId);
+        }
+    }
+}
