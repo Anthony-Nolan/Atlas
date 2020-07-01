b47722f4ec13688b76bad2c9345ef5d715827a8c
Josh Powell <josh.powell@softwire.com>
2020-07-01 14:21:38 +0100

    feature atlas-23: add external models for halpotype frequency sets

---
+++ Atlas.MatchPrediction/Models/IndividualMetaData.cs
@@ -0,0 +1,13 @@
+﻿namespace Atlas.MatchPrediction.Models
+{
+    public class IndividualMetaData
+    {
+        public IndividualMetaData(string ethnicityId, string registryId)
+        { 
+            EthnicityId = ethnicityId;
+            RegistryId = registryId;
+        }
+        public string EthnicityId { get; set; }
+        public string RegistryId { get; set; }
+    }
+}
