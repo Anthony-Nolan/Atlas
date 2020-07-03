753a15c8cd12bd6d21226d1394156a420f05a346
Josh Powell <josh.powell@softwire.com>
2020-07-03 14:35:58 +0100

    review: atlas-23: fix test naming in haplotype frequency set tests

--- Atlas.MatchPrediction/ExternalInterface/Models/HaplotypeFrequencySet/HaplotypeFrequencySet.cs
+++ Atlas.MatchPrediction/ExternalInterface/Models/HaplotypeFrequencySet/HaplotypeFrequencySet.cs
@@ -4,13 +4,6 @@ namespace Atlas.MatchPrediction.ExternalInterface.Models.HaplotypeFrequencySet
 {
     public class HaplotypeFrequencySet
     {
-        public HaplotypeFrequencySet(Data.Models.HaplotypeFrequencySet set)
-        {
-            Id = set.Id;
-            RegistryCode = set.RegistryCode;
-            EthnicityCode = set.EthnicityCode;
-            Name = set.Name;
-        }
         public int Id { get; set; }
         public string RegistryCode { get; set; }
         public string EthnicityCode { get; set; }
