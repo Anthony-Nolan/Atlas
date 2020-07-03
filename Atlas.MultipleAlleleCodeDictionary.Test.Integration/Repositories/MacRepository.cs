b61311a098e4b2d9e6ebde69099a7a5ea776bb43
Mike.McLean <Mike.McLean@softwire.com>
2020-07-03 16:36:48 +0100

    tests: ATLAS-455: Prevent spurious test failures.

--- Atlas.MultipleAlleleCodeDictionary.Test.Integration/Repositories/MacRepository.cs
+++ Atlas.MultipleAlleleCodeDictionary.Test.Integration/Repositories/MacRepository.cs
@@ -1,4 +1,5 @@
 using System.Threading.Tasks;
+using Atlas.Common.AzureStorage.TableStorage;
 using Atlas.MultipleAlleleCodeDictionary.AzureStorage.Models;
 using Atlas.MultipleAlleleCodeDictionary.AzureStorage.Repositories;
 using Atlas.MultipleAlleleCodeDictionary.Settings;
@@ -24,12 +25,18 @@ namespace Atlas.MultipleAlleleCodeDictionary.Test.Integration.Repositories
             await Table.CreateIfNotExistsAsync();
         }
 
-        /// <inheritdoc />
+        /// <remarks>
+        /// Don't bother with the underlying Repo's "GetAllMacs" method, as that converts to the domain object first.
+        /// If there's ever invalid data stored in the Table from a prior test, then the conversion may b0rk and the
+        /// tests will spuriously fail.
+        /// </remarks>
         public async Task DeleteAllMacs()
         {
-            foreach (var mac in await GetAllMacs())
+            var query = new TableQuery<MacEntity>();
+            var existingMacs = await Table.ExecuteQueryAsync(query);
+
+            foreach (var macToDelete in existingMacs)
             {
-                var macToDelete = new MacEntity(mac);
                 // A TableEntities ETag property must be set to "*" to allow overwrites.
                 macToDelete.ETag = "*";
                 var delete = TableOperation.Delete(macToDelete);
