68998c63308a2fbf7bbc831ecbd950c82e9cf6b3
Tom Johnson <tom.johnson@softwire.com>
2020-07-06 17:59:36 +0100

    refactor: ATLAS-490: Commonised and cleaned up match probability tests

--- Atlas.MatchPrediction.Test.Integration/IntegrationTests/MatchProbability/MatchProbabilityTests.cs
+++ Atlas.MatchPrediction.Test.Integration/IntegrationTests/MatchProbability/MatchProbabilityTests.cs
@@ -1,16 +1,20 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using Atlas.Common.GeneticData;
 using Atlas.Common.GeneticData.PhenotypeInfo;
+using Atlas.Common.Test.SharedTestHelpers.Builders;
 using Atlas.HlaMetadataDictionary.Test.IntegrationTests;
 using Atlas.MatchPrediction.Data.Models;
 using Atlas.MatchPrediction.ExternalInterface.Models.MatchProbability;
 using Atlas.MatchPrediction.Services.HaplotypeFrequencies;
 using Atlas.MatchPrediction.Services.MatchProbability;
+using Atlas.MatchPrediction.Test.Integration.Resources;
 using Atlas.MatchPrediction.Test.Integration.TestHelpers.Builders.FrequencySetFile;
 using FluentAssertions;
-using LochNessBuilder;
 using Microsoft.Extensions.DependencyInjection;
 using NUnit.Framework;
+
+// ReSharper disable InconsistentNaming - want to avoid calling "G groups" "gGroup", as "g" groups are a distinct thing 
 
 namespace Atlas.MatchPrediction.Test.Integration.IntegrationTests.MatchProbability
 {
@@ -21,27 +25,16 @@ namespace Atlas.MatchPrediction.Test.Integration.IntegrationTests.MatchProbabili
 
         private const string HlaNomenclatureVersion = Constants.SnapshotHlaNomenclatureVersion;
 
-        private const string A1 = "02:09";
-        private const string A2 = "11:03";
-        private const string B1 = "15:12";
-        private const string B2 = "08:182";
-        private const string C1 = "01:03";
-        private const string C2 = "03:05";
-        private const string Dqb11 = "03:09";
-        private const string Dqb12 = "02:04";
-        private const string Drb11 = "03:124";
-        private const string Drb12 = "11:129";
-
-        private const string GGroupA1 = "02:01:01G";
-        private const string GGroupA2 = "11:03:01G";
-        private const string GGroupB1 = "15:12:01G";
-        private const string GGroupB2 = "08:01:01G";
-        private const string GGroupC1 = "01:03:01G";
-        private const string GGroupC2 = "03:05:01G";
-        private const string GGroupDqb11 = "03:01:01G";
-        private const string GGroupDqb12 = "02:01:01G";
-        private const string GGroupDrb11 = "03:01:01G";
-        private const string GGroupDrb12 = "11:06:01G";
+        private readonly string GGroupA1 = Alleles.UnambiguousAlleleDetails.A.Position1.GGroup;
+        private readonly string GGroupA2 = Alleles.UnambiguousAlleleDetails.A.Position2.GGroup;
+        private readonly string GGroupB1 = Alleles.UnambiguousAlleleDetails.B.Position1.GGroup;
+        private readonly string GGroupB2 = Alleles.UnambiguousAlleleDetails.B.Position2.GGroup;
+        private readonly string GGroupC1 = Alleles.UnambiguousAlleleDetails.C.Position1.GGroup;
+        private readonly string GGroupC2 = Alleles.UnambiguousAlleleDetails.C.Position2.GGroup;
+        private readonly string GGroupDqb11 = Alleles.UnambiguousAlleleDetails.Dqb1.Position1.GGroup;
+        private readonly string GGroupDqb12 = Alleles.UnambiguousAlleleDetails.Dqb1.Position2.GGroup;
+        private readonly string GGroupDrb11 = Alleles.UnambiguousAlleleDetails.Drb1.Position1.GGroup;
+        private readonly string GGroupDrb12 = Alleles.UnambiguousAlleleDetails.Drb1.Position2.GGroup;
 
         [SetUp]
         public void SetUp()
@@ -55,8 +48,8 @@ namespace Atlas.MatchPrediction.Test.Integration.IntegrationTests.MatchProbabili
         {
             var matchProbabilityInput = new MatchProbabilityInput
             {
-                PatientHla = NewHla,
-                DonorHla = NewHla,
+                PatientHla = DefaultUnambiguousAllelesBuilder.Build(),
+                DonorHla = DefaultUnambiguousAllelesBuilder.Build(),
                 HlaNomenclatureVersion = HlaNomenclatureVersion
             };
 
@@ -71,11 +64,12 @@ namespace Atlas.MatchPrediction.Test.Integration.IntegrationTests.MatchProbabili
         [Test]
         public async Task CalculateMatchProbability_WhenGenotypesAreNonMatching_ZeroPercentProbability()
         {
-            var donorHla = NewHla.With(h => h.A, new LocusInfo<string> {Position1 = "23:17", Position2 = "23:18"});
+            var patientHla = DefaultUnambiguousAllelesBuilder.WithDataAt(Locus.A, "02:09", "11:03").Build();
+            var donorHla = DefaultUnambiguousAllelesBuilder.WithDataAt(Locus.A, "23:17", "23:18").Build();
 
             var matchProbabilityInput = new MatchProbabilityInput
             {
-                PatientHla = NewHla,
+                PatientHla = patientHla,
                 DonorHla = donorHla,
                 HlaNomenclatureVersion = HlaNomenclatureVersion
             };
@@ -91,32 +85,42 @@ namespace Atlas.MatchPrediction.Test.Integration.IntegrationTests.MatchProbabili
         [Test]
         public async Task CalculateMatchProbability_WhenAmbiguousHla_ReturnsProbability()
         {
+            const string alleleStringA = "01:37";
+            const string GGroupA = "01:01:01G";
+            const string alleleStringB = "08:182";
+            const string GGroupB = "08:01:01G";
+            const string alleleStringC = "04:82";
+            const string GGroupC = "04:01:01G";
+            const string alleleStringDqb1 = "06:39";
+            const string GGroupDqb1 = "06:04:01G";
+            const string alleleStringDrb1 = "11:129";
+            const string GGroupDrb1 = "11:06:01G";
+
             var allPossibleHaplotypes = new List<HaplotypeFrequency>
             {
-                new HaplotypeFrequency {A = "01:01:01G", B = GGroupB1, C = GGroupC1, DQB1 = GGroupDqb11, DRB1 = GGroupDrb11, Frequency = 0.7m},
-                new HaplotypeFrequency {A = GGroupA1, B = "08:01:01G", C = GGroupC1, DQB1 = GGroupDqb11, DRB1 = GGroupDrb11, Frequency = 0.6m},
-                new HaplotypeFrequency {A = GGroupA1, B = GGroupB1, C = "04:01:01G", DQB1 = GGroupDqb11, DRB1 = GGroupDrb11, Frequency = 0.5m},
-                new HaplotypeFrequency {A = GGroupA1, B = GGroupB1, C = GGroupC1, DQB1 = "06:04:01G", DRB1 = GGroupDrb11, Frequency = 0.4m},
-                new HaplotypeFrequency {A = GGroupA1, B = GGroupB1, C = GGroupC1, DQB1 = GGroupDqb11, DRB1 = "11:06:01G", Frequency = 0.3m},
+                new HaplotypeFrequency {A = GGroupA, B = GGroupB1, C = GGroupC1, DQB1 = GGroupDqb11, DRB1 = GGroupDrb11, Frequency = 0.7m},
+                new HaplotypeFrequency {A = GGroupA1, B = GGroupB, C = GGroupC1, DQB1 = GGroupDqb11, DRB1 = GGroupDrb11, Frequency = 0.6m},
+                new HaplotypeFrequency {A = GGroupA1, B = GGroupB1, C = GGroupC, DQB1 = GGroupDqb11, DRB1 = GGroupDrb11, Frequency = 0.5m},
+                new HaplotypeFrequency {A = GGroupA1, B = GGroupB1, C = GGroupC1, DQB1 = GGroupDqb1, DRB1 = GGroupDrb11, Frequency = 0.4m},
+                new HaplotypeFrequency {A = GGroupA1, B = GGroupB1, C = GGroupC1, DQB1 = GGroupDqb11, DRB1 = GGroupDrb1, Frequency = 0.3m},
                 new HaplotypeFrequency {A = GGroupA1, B = GGroupB1, C = GGroupC1, DQB1 = GGroupDqb11, DRB1 = GGroupDrb11, Frequency = 0.2m},
                 new HaplotypeFrequency {A = GGroupA2, B = GGroupB2, C = GGroupC2, DQB1 = GGroupDqb12, DRB1 = GGroupDrb12, Frequency = 0.1m}
             };
 
             await ImportFrequencies(allPossibleHaplotypes);
 
-            var patientHla = NewHla
-                .With(h => h.A, new LocusInfo<string> {Position1 = $"{A1}/01:37", Position2 = A2})
-                .With(h => h.B, new LocusInfo<string> {Position1 = $"{B1}/08:182", Position2 = B2})
-                .With(h => h.C, new LocusInfo<string> {Position1 = $"{C1}/04:82", Position2 = C2})
-                .With(h => h.Dqb1, new LocusInfo<string> {Position1 = $"{Dqb11}/06:39", Position2 = Dqb12})
-                .With(h => h.Drb1, new LocusInfo<string> {Position1 = $"{Drb11}/11:129", Position2 = Drb12});
-
-            var donorHla = NewHla;
+            var patientHla = DefaultUnambiguousAllelesBuilder
+                    .WithDataAt(Locus.A, LocusPosition.One, $"{Alleles.UnambiguousAlleleDetails.A.Position1.Allele}/{alleleStringA}")
+                    .WithDataAt(Locus.B, LocusPosition.One, $"{Alleles.UnambiguousAlleleDetails.B.Position1.Allele}/{alleleStringB}")
+                    .WithDataAt(Locus.C, LocusPosition.One, $"{Alleles.UnambiguousAlleleDetails.C.Position1.Allele}/{alleleStringC}")
+                    .WithDataAt(Locus.Dqb1, LocusPosition.One, $"{Alleles.UnambiguousAlleleDetails.Dqb1.Position1.Allele}/{alleleStringDqb1}")
+                    .WithDataAt(Locus.Drb1, LocusPosition.One, $"{Alleles.UnambiguousAlleleDetails.Drb1.Position1.Allele}/{alleleStringDrb1}")
+                    .Build();
 
             var matchProbabilityInput = new MatchProbabilityInput
             {
                 PatientHla = patientHla,
-                DonorHla = donorHla,
+                DonorHla = DefaultUnambiguousAllelesBuilder.Build(),
                 HlaNomenclatureVersion = HlaNomenclatureVersion
             };
 
@@ -136,18 +140,13 @@ namespace Atlas.MatchPrediction.Test.Integration.IntegrationTests.MatchProbabili
             matchDetails.ZeroMismatchProbabilityPerLocus.Should().Be(expectedProbabilityPerLocus);
         }
 
-        private static Builder<PhenotypeInfo<string>> NewHla => Builder<PhenotypeInfo<string>>.New
-            .With(h => h.A, new LocusInfo<string> {Position1 = A1, Position2 = A2})
-            .With(h => h.B, new LocusInfo<string> {Position1 = B1, Position2 = B2})
-            .With(h => h.C, new LocusInfo<string> {Position1 = C1, Position2 = C2})
-            .With(h => h.Dpb1, new LocusInfo<string> {Position1 = null, Position2 = null })
-            .With(h => h.Dqb1, new LocusInfo<string> {Position1 = Dqb11, Position2 = Dqb12})
-            .With(h => h.Drb1, new LocusInfo<string> {Position1 = Drb11, Position2 = Drb12});
-
         private async Task ImportFrequencies(IEnumerable<HaplotypeFrequency> haplotypes)
         {
             using var file = FrequencySetFileBuilder.New(haplotypes).Build();
             await importService.ImportFrequencySet(file);
         }
+
+        private static PhenotypeInfoBuilder<string> DefaultUnambiguousAllelesBuilder =>
+            new PhenotypeInfoBuilder<string>(Alleles.UnambiguousAlleleDetails.Alleles());
     }
 }
