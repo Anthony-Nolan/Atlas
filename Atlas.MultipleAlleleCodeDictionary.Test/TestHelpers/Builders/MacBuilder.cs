047a09a3aec7949cbe0aa72ddeea3d220d7e7e0e
Ben Below <benjamin.below@softwire.com>
2020-07-06 13:43:05 +0100

    fix: ATLAS-455: Do not throw when there are no new MACs to import

--- Atlas.MultipleAlleleCodeDictionary.Test/TestHelpers/Builders/MacBuilder.cs
+++ Atlas.MultipleAlleleCodeDictionary.Test/TestHelpers/Builders/MacBuilder.cs
@@ -4,14 +4,14 @@ using LochNessBuilder;
 namespace Atlas.MultipleAlleleCodeDictionary.Test.TestHelpers.Builders
 {
     [Builder]
-    internal class MacBuilder
+    internal static class MacBuilder
     {
         private static int macId = 0;
 
         private static int NextMacId => ++macId;
         
         public static Builder<Mac> New => Builder<Mac>.New
-            .With<string>(mac => mac.Hla, "default-hla")
-            .With<string>(mac => mac.Code, $"default-mac-{NextMacId}");
+            .With(mac => mac.Hla, "default-hla")
+            .WithFactory(mac => mac.Code, () => $"default-mac-{NextMacId}");
     }
 }
