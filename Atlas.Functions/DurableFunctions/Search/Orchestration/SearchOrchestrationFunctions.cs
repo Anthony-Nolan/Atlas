a56d650cfad94f7e5b640b3b060621b683d3c604
Ben Below <benjamin.below@softwire.com>
2020-07-07 18:45:43 +0100

    docs: ATLAS-328: Add comment explaining current accuracy limitation

--- Atlas.Functions/DurableFunctions/Search/Orchestration/SearchOrchestrationFunctions.cs
+++ Atlas.Functions/DurableFunctions/Search/Orchestration/SearchOrchestrationFunctions.cs
@@ -119,6 +119,8 @@ namespace Atlas.Functions.DurableFunctions.Search.Orchestration
             IDictionary<int, MatchProbabilityResponse> matchPredictionResults,
             Dictionary<int, Donor> donorInformation)
         {
+            // Note that this serializes the match prediction results, using default settings - which rounds any decimals to 15dp.
+            // TODO: ATLAS-492: Determine whether 15dp is enough here. Work around this limitation if not.
             await context.CallActivityAsync(
                 nameof(SearchActivityFunctions.PersistSearchResults),
                 new SearchActivityFunctions.PersistSearchResultsParameters
