0c08cde9445594e38cdb622dd0b17ed75cd6e355
Ben Below <benjamin.below@softwire.com>
2020-07-08 17:25:10 +0100

    feature: ATLAS-400: Enforce haplotype uniqueness within a set in the database

---
+++ Atlas.MatchPrediction.Data/Migrations/20200708161934_EnforceHaplotypeUniquenessWithinSet.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Atlas.MatchPrediction.Data.Migrations
+{
+    public partial class EnforceHaplotypeUniquenessWithinSet : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_HaplotypeFrequencies_A_B_C_DQB1_DRB1_Set_Id",
+                table: "HaplotypeFrequencies");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HaplotypeFrequencies_A_B_C_DQB1_DRB1_Set_Id",
+                table: "HaplotypeFrequencies",
+                columns: new[] { "A", "B", "C", "DQB1", "DRB1", "Set_Id" },
+                unique: true)
+                .Annotation("SqlServer:Include", new[] { "Frequency" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_HaplotypeFrequencies_A_B_C_DQB1_DRB1_Set_Id",
+                table: "HaplotypeFrequencies");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HaplotypeFrequencies_A_B_C_DQB1_DRB1_Set_Id",
+                table: "HaplotypeFrequencies",
+                columns: new[] { "A", "B", "C", "DQB1", "DRB1", "Set_Id" })
+                .Annotation("SqlServer:Include", new[] { "Frequency" });
+        }
+    }
+}
