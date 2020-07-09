8083c2ffb91f4894a912fe4951afb3317faa73c2
Josh Powell <josh.powell@softwire.com>
2020-07-09 16:16:36 +0100

    tests: Atlas-23: Add tests for insuring we get the correct HF set

--- Atlas.MatchPrediction.Test.Integration/IntegrationTests/MatchPrediction/MatchProbability/MatchProbabilityTests.cs
+++ Atlas.MatchPrediction.Test.Integration/IntegrationTests/MatchPrediction/MatchProbability/MatchProbabilityTests.cs
@@ -203,14 +203,26 @@ namespace Atlas.MatchPrediction.Test.Integration.IntegrationTests.MatchPredictio
 
             var genericHaplotypeSet = new List<HaplotypeFrequency>
             {
-                new HaplotypeFrequency {A = GGroupA, B = GGroupB, C = GGroupC, DQB1 = GGroupDqb1, DRB1 = GGroupDrb1, Frequency = 0.2m},
+                new HaplotypeFrequency {A = GGroupA, B = GGroupB1, C = GGroupC1, DQB1 = GGroupDqb11, DRB1 = GGroupDrb11, Frequency = 0.9m},
+                new HaplotypeFrequency {A = GGroupA1, B = GGroupB, C = GGroupC1, DQB1 = GGroupDqb11, DRB1 = GGroupDrb11, Frequency = 0.9m},
+                new HaplotypeFrequency {A = GGroupA1, B = GGroupB1, C = GGroupC, DQB1 = GGroupDqb11, DRB1 = GGroupDrb11, Frequency = 0.9m},
+                new HaplotypeFrequency {A = GGroupA1, B = GGroupB1, C = GGroupC1, DQB1 = GGroupDqb1, DRB1 = GGroupDrb11, Frequency = 0.9m},
+                new HaplotypeFrequency {A = GGroupA1, B = GGroupB1, C = GGroupC1, DQB1 = GGroupDqb11, DRB1 = GGroupDrb1, Frequency = 0.9m},
+                new HaplotypeFrequency {A = GGroupA1, B = GGroupB1, C = GGroupC1, DQB1 = GGroupDqb11, DRB1 = GGroupDrb11, Frequency = 0.9m},
+                new HaplotypeFrequency {A = GGroupA2, B = GGroupB2, C = GGroupC2, DQB1 = GGroupDqb12, DRB1 = GGroupDrb12, Frequency = 0.9m}
             };
             
             await ImportFrequencies(genericHaplotypeSet, DefaultEthnicityCode, DefaultRegistryCode);
             
             var specificHaplotypeSet = new List<HaplotypeFrequency>
             {
-                new HaplotypeFrequency {A = GGroupA, B = GGroupB, C = GGroupC, DQB1 = GGroupDqb1, DRB1 = GGroupDrb1, Frequency = 0.8m},
+                new HaplotypeFrequency {A = GGroupA, B = GGroupB1, C = GGroupC1, DQB1 = GGroupDqb11, DRB1 = GGroupDrb11, Frequency = 0.1m},
+                new HaplotypeFrequency {A = GGroupA1, B = GGroupB, C = GGroupC1, DQB1 = GGroupDqb11, DRB1 = GGroupDrb11, Frequency = 0.1m},
+                new HaplotypeFrequency {A = GGroupA1, B = GGroupB1, C = GGroupC, DQB1 = GGroupDqb11, DRB1 = GGroupDrb11, Frequency = 0.1m},
+                new HaplotypeFrequency {A = GGroupA1, B = GGroupB1, C = GGroupC1, DQB1 = GGroupDqb1, DRB1 = GGroupDrb11, Frequency = 0.1m},
+                new HaplotypeFrequency {A = GGroupA1, B = GGroupB1, C = GGroupC1, DQB1 = GGroupDqb11, DRB1 = GGroupDrb1, Frequency = 0.1m},
+                new HaplotypeFrequency {A = GGroupA1, B = GGroupB1, C = GGroupC1, DQB1 = GGroupDqb11, DRB1 = GGroupDrb11, Frequency = 0.1m},
+                new HaplotypeFrequency {A = GGroupA2, B = GGroupB2, C = GGroupC2, DQB1 = GGroupDqb12, DRB1 = GGroupDrb12, Frequency = 0.1m}
             };
 
             await ImportFrequencies(specificHaplotypeSet, specificEthnicityCode, specificRegistryCode);
@@ -225,27 +237,159 @@ namespace Atlas.MatchPrediction.Test.Integration.IntegrationTests.MatchPredictio
 
             var matchProbabilityInput = new MatchProbabilityInput
             {
-                PatientHla = DefaultUnambiguousAllelesBuilder.Build(),
+                PatientHla = patientHla,
                 DonorHla = DefaultUnambiguousAllelesBuilder.Build(),
                 HlaNomenclatureVersion = HlaNomenclatureVersion,
                 DonorFrequencySetMetadata = new FrequencySetMetadata { EthnicityCode = specificEthnicityCode, RegistryCode = specificRegistryCode},
                 PatientFrequencySetMetadata = new FrequencySetMetadata { EthnicityCode = specificEthnicityCode, RegistryCode = specificRegistryCode}
             };
 
-            var expectedProbabilityPerLocus = new LociInfo<decimal?>
+            var matchDetails = await matchProbabilityService.CalculateMatchProbability(matchProbabilityInput);
+
+            matchDetails.ZeroMismatchProbability.Should().Be(0.1666666666666666666666666667m);
+        }
+        
+        [Test]
+        public async Task CalculateMatchProbability_WhenUsingRegistryButNotEthnicityMatchedHaplotypeSet_ReturnsProbability()
+        {
+            const string alleleStringA = "01:37";
+            const string GGroupA = "01:01:01G";
+            const string alleleStringB = "08:182";
+            const string GGroupB = "08:01:01G";
+            const string alleleStringC = "04:82";
+            const string GGroupC = "04:01:01G";
+            const string alleleStringDqb1 = "06:39";
+            const string GGroupDqb1 = "06:04:01G";
+            const string alleleStringDrb1 = "11:129";
+            const string GGroupDrb1 = "11:06:01G";
+
+            const string specificRegistryCode = "specific-registry-code";
+            const string specificEthnicityCode = "specific-ethnicity-code";
+
+            var genericHaplotypeSet = new List<HaplotypeFrequency>
             {
-                A = 0.7407407407407407407407407407m,
-                B = 0.7777777777777777777777777778m,
-                C = 0.8148148148148148148148148148m,
-                Dpb1 = null,
-                Dqb1 = 0.8518518518518518518518518519m, 
-                Drb1 = 0.8888888888888888888888888889m
+                new HaplotypeFrequency {A = GGroupA, B = GGroupB1, C = GGroupC1, DQB1 = GGroupDqb11, DRB1 = GGroupDrb11, Frequency = 0.9m},
+                new HaplotypeFrequency {A = GGroupA1, B = GGroupB, C = GGroupC1, DQB1 = GGroupDqb11, DRB1 = GGroupDrb11, Frequency = 0.9m},
+                new HaplotypeFrequency {A = GGroupA1, B = GGroupB1, C = GGroupC, DQB1 = GGroupDqb11, DRB1 = GGroupDrb11, Frequency = 0.9m},
+                new HaplotypeFrequency {A = GGroupA1, B = GGroupB1, C = GGroupC1, DQB1 = GGroupDqb1, DRB1 = GGroupDrb11, Frequency = 0.9m},
+                new HaplotypeFrequency {A = GGroupA1, B = GGroupB1, C = GGroupC1, DQB1 = GGroupDqb11, DRB1 = GGroupDrb1, Frequency = 0.9m},
+                new HaplotypeFrequency {A = GGroupA1, B = GGroupB1, C = GGroupC1, DQB1 = GGroupDqb11, DRB1 = GGroupDrb11, Frequency = 0.9m},
+                new HaplotypeFrequency {A = GGroupA2, B = GGroupB2, C = GGroupC2, DQB1 = GGroupDqb12, DRB1 = GGroupDrb12, Frequency = 0.9m}
+            };
+            
+            await ImportFrequencies(genericHaplotypeSet, DefaultEthnicityCode, DefaultRegistryCode);
+            
+            var genericEthnicitySet = new List<HaplotypeFrequency>
+            {
+                new HaplotypeFrequency {A = GGroupA, B = GGroupB1, C = GGroupC1, DQB1 = GGroupDqb11, DRB1 = GGroupDrb11, Frequency = 0.1m},
+                new HaplotypeFrequency {A = GGroupA1, B = GGroupB, C = GGroupC1, DQB1 = GGroupDqb11, DRB1 = GGroupDrb11, Frequency = 0.1m},
+                new HaplotypeFrequency {A = GGroupA1, B = GGroupB1, C = GGroupC, DQB1 = GGroupDqb11, DRB1 = GGroupDrb11, Frequency = 0.1m},
+                new HaplotypeFrequency {A = GGroupA1, B = GGroupB1, C = GGroupC1, DQB1 = GGroupDqb1, DRB1 = GGroupDrb11, Frequency = 0.1m},
+                new HaplotypeFrequency {A = GGroupA1, B = GGroupB1, C = GGroupC1, DQB1 = GGroupDqb11, DRB1 = GGroupDrb1, Frequency = 0.1m},
+                new HaplotypeFrequency {A = GGroupA1, B = GGroupB1, C = GGroupC1, DQB1 = GGroupDqb11, DRB1 = GGroupDrb11, Frequency = 0.1m},
+                new HaplotypeFrequency {A = GGroupA2, B = GGroupB2, C = GGroupC2, DQB1 = GGroupDqb12, DRB1 = GGroupDrb12, Frequency = 0.1m}
+            };
+
+            await ImportFrequencies(genericEthnicitySet, null, specificRegistryCode);
+            
+            var specificEthnicitySet = new List<HaplotypeFrequency>
+            {
+                new HaplotypeFrequency {A = GGroupA, B = GGroupB1, C = GGroupC1, DQB1 = GGroupDqb11, DRB1 = GGroupDrb11, Frequency = 0.9m},
+                new HaplotypeFrequency {A = GGroupA1, B = GGroupB, C = GGroupC1, DQB1 = GGroupDqb11, DRB1 = GGroupDrb11, Frequency = 0.9m},
+                new HaplotypeFrequency {A = GGroupA1, B = GGroupB1, C = GGroupC, DQB1 = GGroupDqb11, DRB1 = GGroupDrb11, Frequency = 0.9m},
+                new HaplotypeFrequency {A = GGroupA1, B = GGroupB1, C = GGroupC1, DQB1 = GGroupDqb1, DRB1 = GGroupDrb11, Frequency = 0.9m},
+                new HaplotypeFrequency {A = GGroupA1, B = GGroupB1, C = GGroupC1, DQB1 = GGroupDqb11, DRB1 = GGroupDrb1, Frequency = 0.9m},
+                new HaplotypeFrequency {A = GGroupA1, B = GGroupB1, C = GGroupC1, DQB1 = GGroupDqb11, DRB1 = GGroupDrb11, Frequency = 0.9m},
+                new HaplotypeFrequency {A = GGroupA2, B = GGroupB2, C = GGroupC2, DQB1 = GGroupDqb12, DRB1 = GGroupDrb12, Frequency = 0.9m}
+            };
+
+            await ImportFrequencies(specificEthnicitySet, specificEthnicityCode, specificRegistryCode);
+
+            var patientHla = DefaultUnambiguousAllelesBuilder
+                    .WithDataAt(Locus.A, LocusPosition.One, $"{Alleles.UnambiguousAlleleDetails.A.Position1.Allele}/{alleleStringA}")
+                    .WithDataAt(Locus.B, LocusPosition.One, $"{Alleles.UnambiguousAlleleDetails.B.Position1.Allele}/{alleleStringB}")
+                    .WithDataAt(Locus.C, LocusPosition.One, $"{Alleles.UnambiguousAlleleDetails.C.Position1.Allele}/{alleleStringC}")
+                    .WithDataAt(Locus.Dqb1, LocusPosition.One, $"{Alleles.UnambiguousAlleleDetails.Dqb1.Position1.Allele}/{alleleStringDqb1}")
+                    .WithDataAt(Locus.Drb1, LocusPosition.One, $"{Alleles.UnambiguousAlleleDetails.Drb1.Position1.Allele}/{alleleStringDrb1}")
+                    .Build();
+
+            var matchProbabilityInput = new MatchProbabilityInput
+            {
+                PatientHla = patientHla,
+                DonorHla = DefaultUnambiguousAllelesBuilder.Build(),
+                HlaNomenclatureVersion = HlaNomenclatureVersion,
+                DonorFrequencySetMetadata = new FrequencySetMetadata { EthnicityCode = "unrepresented-donor-code", RegistryCode = specificRegistryCode},
+                PatientFrequencySetMetadata = new FrequencySetMetadata { EthnicityCode = "unrepresented-patient-code", RegistryCode = specificRegistryCode}
             };
 
             var matchDetails = await matchProbabilityService.CalculateMatchProbability(matchProbabilityInput);
 
-            matchDetails.ZeroMismatchProbability.Should().Be(0.0740740740740740740740740741m);
-            matchDetails.ZeroMismatchProbabilityPerLocus.Should().Be(expectedProbabilityPerLocus);
+            matchDetails.ZeroMismatchProbability.Should().Be(0.1666666666666666666666666667m);
+        }
+        
+                [Test]
+        public async Task CalculateMatchProbability_WhenUsingRegistryAndEthnicityCodesAreNotRepresented_ReturnsProbability()
+        {
+            const string alleleStringA = "01:37";
+            const string GGroupA = "01:01:01G";
+            const string alleleStringB = "08:182";
+            const string GGroupB = "08:01:01G";
+            const string alleleStringC = "04:82";
+            const string GGroupC = "04:01:01G";
+            const string alleleStringDqb1 = "06:39";
+            const string GGroupDqb1 = "06:04:01G";
+            const string alleleStringDrb1 = "11:129";
+            const string GGroupDrb1 = "11:06:01G";
+
+            const string specificRegistryCode = "specific-registry-code";
+            const string specificEthnicityCode = "specific-ethnicity-code";
+
+            var globalHaplotypeSet = new List<HaplotypeFrequency>
+            {
+                new HaplotypeFrequency {A = GGroupA, B = GGroupB1, C = GGroupC1, DQB1 = GGroupDqb11, DRB1 = GGroupDrb11, Frequency = 0.1m},
+                new HaplotypeFrequency {A = GGroupA1, B = GGroupB, C = GGroupC1, DQB1 = GGroupDqb11, DRB1 = GGroupDrb11, Frequency = 0.1m},
+                new HaplotypeFrequency {A = GGroupA1, B = GGroupB1, C = GGroupC, DQB1 = GGroupDqb11, DRB1 = GGroupDrb11, Frequency = 0.1m},
+                new HaplotypeFrequency {A = GGroupA1, B = GGroupB1, C = GGroupC1, DQB1 = GGroupDqb1, DRB1 = GGroupDrb11, Frequency = 0.1m},
+                new HaplotypeFrequency {A = GGroupA1, B = GGroupB1, C = GGroupC1, DQB1 = GGroupDqb11, DRB1 = GGroupDrb1, Frequency = 0.1m},
+                new HaplotypeFrequency {A = GGroupA1, B = GGroupB1, C = GGroupC1, DQB1 = GGroupDqb11, DRB1 = GGroupDrb11, Frequency = 0.1m},
+                new HaplotypeFrequency {A = GGroupA2, B = GGroupB2, C = GGroupC2, DQB1 = GGroupDqb12, DRB1 = GGroupDrb12, Frequency = 0.1m}
+            };
+            
+            await ImportFrequencies(globalHaplotypeSet, null, null);
+            
+            var genericEthnicitySet = new List<HaplotypeFrequency>
+            {
+                new HaplotypeFrequency {A = GGroupA, B = GGroupB1, C = GGroupC1, DQB1 = GGroupDqb11, DRB1 = GGroupDrb11, Frequency = 0.9m},
+                new HaplotypeFrequency {A = GGroupA1, B = GGroupB, C = GGroupC1, DQB1 = GGroupDqb11, DRB1 = GGroupDrb11, Frequency = 0.9m},
+                new HaplotypeFrequency {A = GGroupA1, B = GGroupB1, C = GGroupC, DQB1 = GGroupDqb11, DRB1 = GGroupDrb11, Frequency = 0.9m},
+                new HaplotypeFrequency {A = GGroupA1, B = GGroupB1, C = GGroupC1, DQB1 = GGroupDqb1, DRB1 = GGroupDrb11, Frequency = 0.9m},
+                new HaplotypeFrequency {A = GGroupA1, B = GGroupB1, C = GGroupC1, DQB1 = GGroupDqb11, DRB1 = GGroupDrb1, Frequency = 0.9m},
+                new HaplotypeFrequency {A = GGroupA1, B = GGroupB1, C = GGroupC1, DQB1 = GGroupDqb11, DRB1 = GGroupDrb11, Frequency = 0.9m},
+                new HaplotypeFrequency {A = GGroupA2, B = GGroupB2, C = GGroupC2, DQB1 = GGroupDqb12, DRB1 = GGroupDrb12, Frequency = 0.9m}
+            };
+
+            await ImportFrequencies(genericEthnicitySet, null, DefaultRegistryCode);
+
+            var patientHla = DefaultUnambiguousAllelesBuilder
+                    .WithDataAt(Locus.A, LocusPosition.One, $"{Alleles.UnambiguousAlleleDetails.A.Position1.Allele}/{alleleStringA}")
+                    .WithDataAt(Locus.B, LocusPosition.One, $"{Alleles.UnambiguousAlleleDetails.B.Position1.Allele}/{alleleStringB}")
+                    .WithDataAt(Locus.C, LocusPosition.One, $"{Alleles.UnambiguousAlleleDetails.C.Position1.Allele}/{alleleStringC}")
+                    .WithDataAt(Locus.Dqb1, LocusPosition.One, $"{Alleles.UnambiguousAlleleDetails.Dqb1.Position1.Allele}/{alleleStringDqb1}")
+                    .WithDataAt(Locus.Drb1, LocusPosition.One, $"{Alleles.UnambiguousAlleleDetails.Drb1.Position1.Allele}/{alleleStringDrb1}")
+                    .Build();
+
+            var matchProbabilityInput = new MatchProbabilityInput
+            {
+                PatientHla = patientHla,
+                DonorHla = DefaultUnambiguousAllelesBuilder.Build(),
+                HlaNomenclatureVersion = HlaNomenclatureVersion,
+                DonorFrequencySetMetadata = new FrequencySetMetadata { EthnicityCode = "unrepresented-donor-code", RegistryCode = "unrepresented-donor-code"},
+                PatientFrequencySetMetadata = new FrequencySetMetadata { EthnicityCode = "unrepresented-patient-code", RegistryCode = "unrepresented-patient-code"}
+            };
+
+            var matchDetails = await matchProbabilityService.CalculateMatchProbability(matchProbabilityInput);
+
+            matchDetails.ZeroMismatchProbability.Should().Be(0.1666666666666666666666666667m);
         }
 
         private async Task ImportFrequencies(IEnumerable<HaplotypeFrequency> haplotypes, string ethnicityCode, string registryCode)
