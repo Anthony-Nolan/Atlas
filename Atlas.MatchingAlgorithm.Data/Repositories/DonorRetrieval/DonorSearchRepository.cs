f1937be6677410687f8bfd2583854f102489e97d
Mike.McLean <Mike.McLean@softwire.com>
2020-07-10 11:03:27 +0100

    chore: ATLAS-491: Remove unnecessary separate interface files

--- Atlas.MatchingAlgorithm.Data/Repositories/DonorRetrieval/DonorSearchRepository.cs
+++ Atlas.MatchingAlgorithm.Data/Repositories/DonorRetrieval/DonorSearchRepository.cs
@@ -17,6 +17,18 @@ using System.Threading.Tasks;
 
 namespace Atlas.MatchingAlgorithm.Data.Repositories.DonorRetrieval
 {
+    public interface IDonorSearchRepository
+    {
+        /// <summary>
+        /// Returns donor matches at a given locus matching the search criteria
+        /// </summary>
+        Task<IEnumerable<PotentialHlaMatchRelation>> GetDonorMatchesAtLocus(
+            Locus locus,
+            LocusSearchCriteria criteria,
+            MatchingFilteringOptions filteringOptions
+        );
+    }
+
     public class DonorSearchRepository : Repository, IDonorSearchRepository
     {
         private readonly ILogger logger;
