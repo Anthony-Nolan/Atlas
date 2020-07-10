282b79eef5b75591830eb974031cdfea696c2796
Mike.McLean <Mike.McLean@softwire.com>
2020-07-10 11:03:27 +0100

    chore: ATLAS-491: Remove unnecessary separate interface files

--- Atlas.MatchingAlgorithm.Data/Repositories/DonorRetrieval/DonorInspectionRepository.cs
+++ Atlas.MatchingAlgorithm.Data/Repositories/DonorRetrieval/DonorInspectionRepository.cs
@@ -16,6 +16,16 @@ using Atlas.Common.Utils.Extensions;
 
 namespace Atlas.MatchingAlgorithm.Data.Repositories.DonorRetrieval
 {
+    /// <summary>
+    /// Provides methods for retrieving donor data used when returning search results. Does not perform any filtering of donors
+    /// </summary>
+    public interface IDonorInspectionRepository
+    {
+        Task<DonorInfo> GetDonor(int donorId);
+        Task<Dictionary<int, DonorInfo>> GetDonors(IEnumerable<int> donorIds);
+        Task<IEnumerable<DonorIdWithPGroupNames>> GetPGroupsForDonors(IEnumerable<int> donorIds);
+    }
+
     public class DonorInspectionRepository : Repository, IDonorInspectionRepository
     {
         public DonorInspectionRepository(IConnectionStringProvider connectionStringProvider) : base(connectionStringProvider)
