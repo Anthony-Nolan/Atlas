88d82eb139da34c9bbb3bd51a4cdb9338b4467a1
Ben Below <benjamin.below@softwire.com>
2020-07-13 14:40:17 +0100

    refactor: ATLAS-400: Reorder existing LocusInfo class
    No meaningful changes.
    
    - re-ordering and grouping of methods with regions
    - disable unhelpful Resharper rules
    - some R# suggested cleanup

--- Atlas.Common/GeneticData/PhenotypeInfo/LocusInfo.cs
+++ Atlas.Common/GeneticData/PhenotypeInfo/LocusInfo.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections.Generic;
 
+// ReSharper disable InconsistentNaming - want to use T/R to easily distinguish contained type and target type(s)
+// ReSharper disable MemberCanBeInternal
+
 namespace Atlas.Common.GeneticData.PhenotypeInfo
 {
     /// <summary>
@@ -12,6 +15,8 @@ namespace Atlas.Common.GeneticData.PhenotypeInfo
     {
         public T Position1 { get; set; }
         public T Position2 { get; set; }
+
+        #region Constructors
 
         public LocusInfo()
         {
@@ -34,21 +39,107 @@ namespace Atlas.Common.GeneticData.PhenotypeInfo
             return (LocusInfo<T>) MemberwiseClone();
         }
         
+        #endregion
+        
+        public T GetAtPosition(LocusPosition position)
+        {
+            return position switch
+            {
+                LocusPosition.One => Position1,
+                LocusPosition.Two => Position2,
+                _ => throw new ArgumentOutOfRangeException(nameof(position), position, null)
+            };
+        }
+
+        public void SetAtPosition(LocusPosition position, T value)
+        {
+            switch (position)
+            {
+                case LocusPosition.One:
+                    Position1 = value;
+                    break;
+                case LocusPosition.Two:
+                    Position2 = value;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(position), position, null);
+            }
+        }
+        
+        #region Functional Methods
+
         public LocusInfo<R> Map<R>(Func<T, R> mapping)
         {
             return new LocusInfo<R>(mapping(Position1), mapping(Position2));
         }
 
-        public bool Position1And2NotNull()
+        #endregion
+
+        public IEnumerable<T> ToEnumerable()
+        {
+            return new List<T> {Position1, Position2};
+        }
+
+        internal bool Position1And2NotNull()
         {
             return Position1 != null && Position2 != null;
         }
 
-        public bool SinglePositionNull()
+        internal bool SinglePositionNull()
         {
             return Position1 == null ^ Position2 == null;
         }
 
+        #region Equality Operations
+        
+        public static bool operator ==(LocusInfo<T> left, LocusInfo<T> right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(LocusInfo<T> left, LocusInfo<T> right)
+        {
+            return !Equals(left, right);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            if (obj.GetType() != this.GetType())
+            {
+                return false;
+            }
+
+            return Equals((LocusInfo<T>) obj);
+        }
+
+        private bool Equals(LocusInfo<T> other)
+        {
+            return EqualityComparer<T>.Default.Equals(Position1, other.Position1) &&
+                   EqualityComparer<T>.Default.Equals(Position2, other.Position2);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                // TODO: ATLAS-499: Ensure that genetic data types are immutable
+                var hashCode = EqualityComparer<T>.Default.GetHashCode(Position1);
+                hashCode = (hashCode * 397) ^ EqualityComparer<T>.Default.GetHashCode(Position2);
+                return hashCode;
+            }
+        }
+
+        #endregion
         public T GetAtPosition(LocusPosition position)
         {
             switch (position)
