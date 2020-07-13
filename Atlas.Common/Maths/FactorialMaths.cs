8dea8e3a6755e538f37cf5c19f299515c96dff3e
Ben Below <benjamin.below@softwire.com>
2020-07-13 16:10:49 +0100

    refactor: ATLAS-522: Split up and add more tests for combination/factorial helper code.

---
+++ Atlas.Common/Maths/FactorialMaths.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace Atlas.Common.Maths
+{
+    internal static class FactorialMaths
+    {
+        /// <remarks>
+        /// Expected use case is when numerator is larger than denominator.
+        /// If the inverse is true, returns 0, rather than a fractional value.
+        /// </remarks>>
+        internal static long FactorialDivision(int topFactorial, int divisorFactorial)
+        {
+            long result = 1;
+            for (var i = topFactorial; i > divisorFactorial; i--)
+            {
+                result *= i;
+            }
+
+            return result;
+        }
+
+        internal static long Factorial(int i)
+        {
+            if (i < 0)
+            {
+                throw new InvalidOperationException("Cannot calculate factorial of negative number");
+            }
+            return i <= 1 ? 1 : i * Factorial(i - 1);
+        }
+    }
+}
