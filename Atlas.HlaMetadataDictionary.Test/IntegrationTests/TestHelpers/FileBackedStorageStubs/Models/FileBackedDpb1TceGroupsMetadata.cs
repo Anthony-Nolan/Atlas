92bcdc9f726e2d66a53b979dffec2f46ae9a87e7
Mike.McLean <Mike.McLean@softwire.com>
2020-07-14 16:04:32 +0100

    feature: ATLAS-531: Clearer code for making new file-baed dictionaries

--- Atlas.HlaMetadataDictionary.Test/IntegrationTests/TestHelpers/FileBackedStorageStubs/Models/FileBackedDpb1TceGroupsMetadata.cs
+++ Atlas.HlaMetadataDictionary.Test/IntegrationTests/TestHelpers/FileBackedStorageStubs/Models/FileBackedDpb1TceGroupsMetadata.cs
@@ -1,6 +1,7 @@
 using Atlas.Common.GeneticData;
 using Atlas.Common.GeneticData.Hla.Models;
 using Atlas.HlaMetadataDictionary.InternalModels.Metadata;
+using Newtonsoft.Json;
 
 namespace Atlas.HlaMetadataDictionary.Test.IntegrationTests.TestHelpers.FileBackedStorageStubs.Models
 {
@@ -10,11 +11,23 @@ namespace Atlas.HlaMetadataDictionary.Test.IntegrationTests.TestHelpers.FileBack
         public string LookupName { get; }
         public TypingMethod TypingMethod => TypingMethod.Molecular;
         public string TceGroup { get; }
-        public object HlaInfoToSerialise => TceGroup;
         public string SerialisedHlaInfoType { get; }
+        [JsonIgnore] // This property isn't actually used anywhere for the FileBacked objects, but it's on the interface. But we don't want to write these to file when we regenerate the FileBacked HMD's file.
+        public object HlaInfoToSerialise => TceGroup;
 
-        public FileBackedDpb1TceGroupsMetadata(string lookupName, string tceGroup, string serialisedHlaInfoType)
-        {
+        /// <remarks>
+        /// ********************* WARNING! *********************
+        /// The names of these parameters MUST stay in sync
+        /// with the property names of the JSON objects found in
+        /// the IntegrationTest MetadataDictionary test file.
+        /// i.e. `all_hla_metadata.json`.
+        /// ********************* WARNING! *********************
+        /// </remarks>
+        public FileBackedDpb1TceGroupsMetadata(              //*******************
+            string lookupName,                               //****    See    ****
+            string tceGroup,                                 //****  warning  ****
+            string serialisedHlaInfoType)                    //****   above   ****
+        {                                                    //*******************
             LookupName = lookupName;
             TceGroup = tceGroup;
             SerialisedHlaInfoType = serialisedHlaInfoType;
