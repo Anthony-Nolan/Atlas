953f7379a89be287779871aa60d992d1b7c13b2f
Ben Below <benjamin.below@softwire.com>
2020-07-16 10:25:52 +0100

    tests: ATLAS-525: Add automated performance benchmark tests

--- Atlas.MatchPrediction.Test.Integration/TestHelpers/Builders/FrequencySetFile/FrequencySetFileBuilder.cs
+++ Atlas.MatchPrediction.Test.Integration/TestHelpers/Builders/FrequencySetFile/FrequencySetFileBuilder.cs
@@ -6,6 +6,7 @@ using System.Text;
 using Atlas.Common.Utils.Extensions;
 using Atlas.MatchPrediction.Data.Models;
 using LochNessBuilder;
+using Builder = LochNessBuilder.Builder<Atlas.MatchPrediction.Test.Integration.TestHelpers.Builders.FrequencySetFile.TestFrequencySetFile>;
 
 namespace Atlas.MatchPrediction.Test.Integration.TestHelpers.Builders.FrequencySetFile
 {
@@ -14,38 +15,36 @@ namespace Atlas.MatchPrediction.Test.Integration.TestHelpers.Builders.FrequencyS
     {
         private const string CsvHeader = "a;b;c;drb1;dqb1;freq";
 
-        internal static Builder<TestFrequencySetFile> New(
-            string registryCode,
-            string ethnicityCode,
-            int haplotypeCount = 1,
-            decimal frequencyValue = 0.00001m)
+        internal static Builder FileWithoutContents(string registryCode, string ethnicityCode)
+        {
+            var uniqueFileName = $"file-{DateTime.Now:HHmmssffff}.csv";
+
+            return Builder.New
+                .With(x => x.FileName, uniqueFileName)
+                .With(x => x.FullPath, BuildFilePath(uniqueFileName, registryCode, ethnicityCode));
+        }
+
+        internal static Builder New(string registryCode, string ethnicityCode, int haplotypeCount = 1, decimal frequencyValue = 0.00001m)
         {
             return FileWithoutContents(registryCode, ethnicityCode)
                 .With(x => x.Contents, GetStream(BuildCsvFile(haplotypeCount, frequencyValue)));
         }
 
-        internal static Builder<TestFrequencySetFile> New(
-            string registryCode,
-            string ethnicityCode,
-            IEnumerable<HaplotypeFrequency> haplotypeFrequencies)
+        internal static Builder New(string registryCode, string ethnicityCode, IEnumerable<HaplotypeFrequency> haplotypeFrequencies)
         {
             return FileWithoutContents(registryCode, ethnicityCode)
                 .With(x => x.Contents, GetStream(BuildCsvFile(haplotypeFrequencies)));
         }
 
-        internal static Builder<TestFrequencySetFile> New(IEnumerable<HaplotypeFrequency> haplotypeFrequencies)
+        internal static Builder New(IEnumerable<HaplotypeFrequency> haplotypeFrequencies)
         {
             return FileWithoutContents(null, null)
                 .With(x => x.Contents, GetStream(BuildCsvFile(haplotypeFrequencies)));
         }
 
-        private static Builder<TestFrequencySetFile> FileWithoutContents(string registryCode, string ethnicityCode)
+        internal static Builder WithHaplotypeFrequencyFileStream(this Builder builder, Stream stream)
         {
-            var uniqueFileName = $"file-{DateTime.Now:HHmmssffff}.csv";
-
-            return Builder<TestFrequencySetFile>.New
-                .With(x => x.FileName, uniqueFileName)
-                .With(x => x.FullPath, BuildFilePath(uniqueFileName, registryCode, ethnicityCode));
+            return builder.With(f => f.Contents, stream);
         }
 
         /// <param name="registryCode">Path will only contain file name if this is null.</param>
