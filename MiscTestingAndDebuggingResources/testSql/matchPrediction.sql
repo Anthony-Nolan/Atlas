2c32045022dc4ac49ebea8c4dc6fcf1dcf6e1d03
Josh Powell <josh.powell@softwire.com>
2020-07-17 11:06:05 +0100

    feat: add individual SQL files for each db requiring user migration
    refactor: Atlas-360: Move sql folder into terraform core
    
    test: Atlas-360: Add backtick to sql command
    
    wip atlas-360: use string var instead iof backtick
    
    wip- atlas-360: use nvarchar
    
    wip Atlas-260: Add test ps
    
    feat: Atlas-360: add sql checking

---
+++ MiscTestingAndDebuggingResources/testSql/matchPrediction.sql
@@ -0,0 +1,13 @@
+IF USER_ID('$(matchPredictionUsername)') IS NULL
+BEGIN 
+	CREATE USER $(matchPredictionUsername) WITH PASSWORD = $(matchPredictionPassword)
+  ALTER ROLE db_datareader ADD MEMBER $(matchPredictionUsername)
+  ALTER ROLE db_datawriter ADD MEMBER $(matchPredictionUsername)
+END
+
+ELSE
+
+BEGIN
+	ALTER USER $(matchPredictionUsername) WITH PASSWORD = $(matchPredictionPassword)
+END
+
---
+++ MiscTestingAndDebuggingResources/testSql/matchingPersistent.sql
@@ -0,0 +1,14 @@
+IF USER_ID('$(matchingUsername)') IS NULL
+BEGIN 
+	CREATE USER $(matchingUsername) WITH PASSWORD = $(matchingPassword)
+  ALTER ROLE db_datareader ADD MEMBER $(matchingUsername)
+  ALTER ROLE db_datawriter ADD MEMBER $(matchingUsername)
+  ALTER ROLE db_owner ADD MEMBER $(matchingUsername)
+END
+
+ELSE
+
+BEGIN
+	ALTER USER $(matchingUsername) WITH PASSWORD = $(matchingPassword)
+END
+
