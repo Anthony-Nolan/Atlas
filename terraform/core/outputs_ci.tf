4d6c28750a96140da3417a91ca9b1a356a5663c3
Ben Below <benjamin.below@softwire.com>
2020-07-20 12:20:48 +0100

    chore: ATLAS-187: Rename + re-order CI output variables
    - better naming for transient databases
    - consistent database naming between transient/persistent
    - alphabetise

--- terraform/core/outputs_ci.tf
+++ terraform/core/outputs_ci.tf
@@ -2,7 +2,7 @@
 // For outputs that are to be read by other terraform scripts, use outputs.tf
 
 output "donor-import-database-name" {
-  value= module.donor_import.sql_database.name
+  value = module.donor_import.sql_database.name
 }
 
 output "donor-import-function-name" {
@@ -13,6 +13,10 @@ output "donor-matching-function-name" {
   value = module.matching_algorithm.function_app.donor_matching_app_name
 }
 
+output "function-app-name" {
+  value = local.atlas_function_app_name
+}
+
 output "match-prediction-database-name" {
   value = module.match_prediction.sql_database.name
 }
@@ -21,32 +25,32 @@ output "match-prediction-function-name" {
   value = module.match_prediction.function_app.app_name
 }
 
-output "matching-algorithm-a-conn-string" {
+output "matching-algorithm-database-persistent-conn-string" {
+  value = module.matching_algorithm.sql_database.persistent_database_connection_string
+}
+
+output "matching-algorithm-database-persistent-name" {
+  value = module.matching_algorithm.sql_database.persistent_database_name
+}
+
+output "matching-algorithm-database-transient-a-conn-string" {
   value = module.matching_algorithm.sql_database.transient_a_database_connection_string
 }
 
-output "matching-algorithm-a-name" {
+output "matching-algorithm-database-transient-a-name" {
   value = module.matching_algorithm.sql_database.transient_a_database_name
 }
 
-output "matching-algorithm-b-conn-string" {
+output "matching-algorithm-database-transient-b-conn-string" {
   value = module.matching_algorithm.sql_database.transient_b_database_connection_string
 }
 
-output "matching-algorithm-b-name" {
+output "matching-algorithm-database-transient-b-name" {
   value = module.matching_algorithm.sql_database.transient_b_database_name
 }
 
 output "matching-algorithm-function-name" {
   value = module.matching_algorithm.function_app.app_name
-}
-
-output "matching-algorithm-persistent-db-conn-string" {
-  value = module.matching_algorithm.sql_database.persistent_database_connection_string
-}
-
-output "matching-algorithm-persistent-db-name" {
-  value = module.matching_algorithm.sql_database.persistent_database_name
 }
 
 output "sql-server" {
@@ -59,8 +63,4 @@ output "sql-server-admin-login" {
 
 output "sql-server-admin-login-password" {
   value = var.DATABASE_SERVER_ADMIN_LOGIN_PASSWORD
-}
-
-output "function-app-name" {
-  value = local.atlas_function_app_name
 }
