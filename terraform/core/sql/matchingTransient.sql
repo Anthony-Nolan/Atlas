ac03597633a74d770173beeae4fcfc8d20fe4f09
Josh Powell <josh.powell@softwire.com>
2020-07-21 16:52:05 +0100

    review: Atlas-360: Alter roles if user already exist

--- terraform/core/sql/matchingTransient.sql
+++ terraform/core/sql/matchingTransient.sql
@@ -10,5 +10,8 @@ ELSE
 
 BEGIN
 	ALTER USER $(matchingUsername) WITH PASSWORD = '$(matchingPassword)'
+  ALTER ROLE db_datareader ADD MEMBER $(matchingUsername)
+  ALTER ROLE db_datawriter ADD MEMBER $(matchingUsername)
+  ALTER ROLE db_owner ADD MEMBER $(matchingUsername)
 END
 
