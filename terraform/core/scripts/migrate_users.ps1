762bd3d4b56cef5fb17828ddbb58eb214bd25c73
Josh Powell <josh.powell@softwire.com>
2020-07-21 16:59:11 +0100

    docs: Atlas-360: add documentation for db user release

--- terraform/core/scripts/migrate_users.ps1
+++ terraform/core/scripts/migrate_users.ps1
@@ -8,10 +8,10 @@ $variablesArray =
 
 Write-Host $variablesArray
 
-Invoke-Sqlcmd -InputFile "sql/matchPrediction.sql" -ServerInstance $env:sqlServer -Database $env:matchPredictionDatabaseName -Username $env:sqlServerLogin -Password $env:sqlServerLoginPassword -Variable $variablesArray
+Invoke-Sqlcmd -InputFile "sql/createUsers/matchPrediction.sql" -ServerInstance $env:sqlServer -Database $env:matchPredictionDatabaseName -Username $env:sqlServerLogin -Password $env:sqlServerLoginPassword -Variable $variablesArray
 
-Invoke-Sqlcmd -InputFile "sql/matchingTransient.sql" -ServerInstance $env:sqlServer -Database $env:matchingAlgorithmDatabaseTransientAName -Username $env:sqlServerLogin -Password $env:sqlServerLoginPassword -Variable $variablesArray
-Invoke-Sqlcmd -InputFile "sql/matchingTransient.sql" -ServerInstance $env:sqlServer -Database $env:matchingAlgorithmDatabaseTransientBName -Username $env:sqlServerLogin -Password $env:sqlServerLoginPassword -Variable $variablesArray
-Invoke-Sqlcmd -InputFile "sql/matchingPersistent.sql" -ServerInstance $env:sqlServer -Database $env:matchingAlgorithmDatabasePersistentName -Username $env:sqlServerLogin -Password $env:sqlServerLoginPassword -Variable $variablesArray
+Invoke-Sqlcmd -InputFile "sql/createUsers/matchingTransient.sql" -ServerInstance $env:sqlServer -Database $env:matchingAlgorithmDatabaseTransientAName -Username $env:sqlServerLogin -Password $env:sqlServerLoginPassword -Variable $variablesArray
+Invoke-Sqlcmd -InputFile "sql/createUsers/matchingTransient.sql" -ServerInstance $env:sqlServer -Database $env:matchingAlgorithmDatabaseTransientBName -Username $env:sqlServerLogin -Password $env:sqlServerLoginPassword -Variable $variablesArray
+Invoke-Sqlcmd -InputFile "sql/createUsers/matchingPersistent.sql" -ServerInstance $env:sqlServer -Database $env:matchingAlgorithmDatabasePersistentName -Username $env:sqlServerLogin -Password $env:sqlServerLoginPassword -Variable $variablesArray
 
-Invoke-Sqlcmd -InputFile "sql/donorImport.sql" -ServerInstance $env:sqlServer -Database $env:donorImportDatabase -Username $env:sqlServerLogin -Password $env:sqlServerLoginPassword -Variable $variablesArray
+Invoke-Sqlcmd -InputFile "sql/createUsers/donorImport.sql" -ServerInstance $env:sqlServer -Database $env:donorImportDatabase -Username $env:sqlServerLogin -Password $env:sqlServerLoginPassword -Variable $variablesArray
