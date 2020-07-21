2e755c7a3871dcec82e5adc9e69118b45eb04f2c
Josh Powell <josh.powell@softwire.com>
2020-07-21 12:44:52 +0100

    fix: Atlas-360: Use updated tf db names
    fix: Atlas-360: Use double dollars to use azure vars not ps ones
    
    Fix: Atlas-360: remove double dollars from ps1
    
    wip: Atlas-360: use alternate way of formatting strings
    
    wip: Atlas-360: Remove brackets where they might not be useful
    
    wip: Atlas-360: Use env vars to be passed in from azure vars
    
    wip: Atlas-360: remove single quotes
    
    wip: Atlas-360: bring env vars back into string interpolatioN

--- terraform/core/sql/matchPrediction.sql
+++ terraform/core/sql/matchPrediction.sql
@@ -1,6 +1,6 @@
 IF USER_ID('$(matchPredictionUsername)') IS NULL
 BEGIN 
-	CREATE USER $(matchPredictionUsername) WITH PASSWORD = $(matchPredictionPassword)
+	CREATE USER $(matchPredictionUsername) WITH PASSWORD = '$(matchPredictionPassword)'
   ALTER ROLE db_datareader ADD MEMBER $(matchPredictionUsername)
   ALTER ROLE db_datawriter ADD MEMBER $(matchPredictionUsername)
 END
@@ -8,6 +8,6 @@ END
 ELSE
 
 BEGIN
-	ALTER USER $(matchPredictionUsername) WITH PASSWORD = $(matchPredictionPassword)
+	ALTER USER $(matchPredictionUsername) WITH PASSWORD = '$(matchPredictionPassword)'
 END
 
