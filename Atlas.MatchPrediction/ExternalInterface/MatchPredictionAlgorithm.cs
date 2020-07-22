f2fe68290f7b2ca54d4d355f91db55e0179d71ee
Mike.McLean <Mike.McLean@softwire.com>
2020-07-22 19:41:34 +0100

    refactor: ATLAS-NONE: Migrate Logging code to new `using` structure

--- Atlas.MatchPrediction/ExternalInterface/MatchPredictionAlgorithm.cs
+++ Atlas.MatchPrediction/ExternalInterface/MatchPredictionAlgorithm.cs
@@ -1,5 +1,6 @@
 using System.Threading.Tasks;
 using Atlas.Common.ApplicationInsights;
+using Atlas.Common.ApplicationInsights.Timing;
 using Atlas.MatchPrediction.ExternalInterface.Models.HaplotypeFrequencySet;
 using Atlas.MatchPrediction.ExternalInterface.Models.MatchProbability;
 using Atlas.MatchPrediction.Services.HaplotypeFrequencies;
@@ -33,12 +34,11 @@ namespace Atlas.MatchPrediction.ExternalInterface
         /// <inheritdoc />
         public async Task<MatchProbabilityResponse> RunMatchPredictionAlgorithm(MatchProbabilityInput matchProbabilityInput)
         {
-            var result = await logger.RunTimedAsync(
-                async () => await matchProbabilityService.CalculateMatchProbability(matchProbabilityInput),
-                "Match Prediction Algorithm Completed"
-            );
-
-            return result.Round(4);
+            using (logger.RunTimed("Run Match Prediction Algorithm"))
+            {
+                var result = await matchProbabilityService.CalculateMatchProbability(matchProbabilityInput);
+                return result.Round(4);
+            }
         }
 
         public async Task<HaplotypeFrequencySetResponse> GetHaplotypeFrequencySet(HaplotypeFrequencySetInput haplotypeFrequencySetInput)
