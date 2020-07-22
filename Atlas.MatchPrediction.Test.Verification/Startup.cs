375196f89be2631c5a89ec612bea0e6b64894cf5
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2020-07-22 19:06:42 +0100

    chore: ATLAS-560: Add empty MPA Verification API project with single Token test.

---
+++ Atlas.MatchPrediction.Test.Verification/Startup.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Atlas.MatchPrediction.Test.Verification
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.ConfigureSwaggerService();
+
+            services.AddMvc(options => { options.EnableEndpointRouting = false; });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.ConfigureSwagger();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            
+            app.UseMvc();
+        }
+    }
+}
