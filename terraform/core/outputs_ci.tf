028930dca6c2b26a7c7da5f84a2bd3a72958f58a
Josh Powell <josh.powell@softwire.com>
2020-07-22 15:27:18 +0100

    fix: Atlas-360: add username as output vars from terraform

--- terraform/core/outputs_ci.tf
+++ terraform/core/outputs_ci.tf
@@ -1,6 +1,10 @@
 // The outputs in this file are flat so that they can be read by azure Devops
 // For outputs that are to be read by other terraform scripts, use outputs.tf
 
+output "donor-database-username" {
+  value = var.DONOR_DATABASE_USERNAME
+}
+
 output "donor-import-database-name" {
   value = module.donor_import.sql_database.name
 }
@@ -25,6 +29,10 @@ output "match-prediction-function-name" {
   value = module.match_prediction.function_app.app_name
 }
 
+output "match-prediction-database-username" {
+  value = var.MATCH_PREDICTION_DATABASE_USERNAME
+}
+
 output "matching-algorithm-database-persistent-name" {
   value = module.matching_algorithm.sql_database.persistent_database_name
 }
@@ -39,6 +47,14 @@ output "matching-algorithm-database-transient-b-name" {
 
 output "matching-algorithm-function-name" {
   value = module.matching_algorithm.function_app.app_name
+}
+
+output "matching-database-username" {
+  value = var.MATCHING_DATABASE_USERNAME
+}
+
+output "matching-username-for-donor-import-db" {
+  value = var.MATCHING_USERNAME_FOR_DONOR_IMPORT_DATABASE
 }
 
 output "sql-server" {
