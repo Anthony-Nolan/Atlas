5026d725708074559e21cb34e683ffb4983a1400
Josh Powell <josh.powell@softwire.com>
2020-07-27 17:11:12 +0100

    refactor: Atlas-NONE: Remove unused fields in test

--- Atlas.DonorImport.Test.Integration/IntegrationTests/Import/ExceptionHandling/ExceptionHandlingTests.cs
+++ Atlas.DonorImport.Test.Integration/IntegrationTests/Import/ExceptionHandling/ExceptionHandlingTests.cs
@@ -1,15 +1,11 @@
 ﻿using System.Threading.Tasks;
 using Atlas.Common.Test.SharedTestHelpers;
-using Atlas.DonorImport.Clients;
 using Atlas.DonorImport.ExternalInterface.Models;
-using Atlas.DonorImport.Models.FileSchema;
 using Atlas.DonorImport.Services;
-using Atlas.DonorImport.Test.Integration.TestHelpers;
 using Atlas.DonorImport.Test.TestHelpers.Builders;
 using Atlas.DonorImport.Test.TestHelpers.Builders.ExternalModels;
 using Atlas.Common.ApplicationInsights;
 using Atlas.Common.Notifications;
-using LochNessBuilder;
 using Microsoft.Extensions.DependencyInjection;
 using NSubstitute;
 using NUnit.Framework;
@@ -19,25 +15,15 @@ namespace Atlas.DonorImport.Test.Integration.IntegrationTests.Import.ExceptionHa
     [TestFixture]
     public class ExceptionHandlingTests
     {
-        private IDonorInspectionRepository donorRepository;
-        private IMessagingServiceBusClient serviceBusClient;
         private IDonorFileImporter donorFileImporter;
         private ILogger mockLogger;
         private INotificationSender mockNotificationSender;
-        private readonly Builder<DonorImportFile> fileBuilder = DonorImportFileBuilder.NewWithoutContents;
-
-        private readonly Builder<DonorUpdate> donorCreationBuilder =
-            DonorUpdateBuilder.New
-                .WithRecordIdPrefix("external-donor-code-")
-                .With(upd => upd.ChangeType, ImportDonorChangeType.Create);
 
         [OneTimeSetUp]
         public void OneTimeSetUp()
         {
             TestStackTraceHelper.CatchAndRethrowWithStackTraceInExceptionMessage(() =>
             {
-                donorRepository = DependencyInjection.DependencyInjection.Provider.GetService<IDonorInspectionRepository>();
-                serviceBusClient = DependencyInjection.DependencyInjection.Provider.GetService<IMessagingServiceBusClient>();
                 donorFileImporter = DependencyInjection.DependencyInjection.Provider.GetService<IDonorFileImporter>();
                 mockLogger = DependencyInjection.DependencyInjection.Provider.GetService<ILogger>();
                 mockNotificationSender = DependencyInjection.DependencyInjection.Provider.GetService<INotificationSender>();
