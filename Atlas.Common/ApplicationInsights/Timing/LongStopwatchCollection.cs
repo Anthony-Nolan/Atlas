14064dfa070a9ea675c3c36344c46440bd34df80
Mike.McLean <Mike.McLean@softwire.com>
2020-07-28 02:14:09 +0100

    feature: ATLAS-NONE: A StopwatchCollection to make it easy to modify logging deep in nested code

---
+++ Atlas.Common/ApplicationInsights/Timing/LongStopwatchCollection.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LoggingStopwatch
+{
+    public class LongStopwatchCollection
+    {
+        private readonly Dictionary<string, ILongOperationLoggingStopwatch> watches = new Dictionary<string, ILongOperationLoggingStopwatch>();
+
+        private readonly Action<string> defaultLoggingAction;
+        private readonly LongLoggingSettings defaultLoggingSettings;
+
+        public LongStopwatchCollection(Action<string> defaultLoggingAction = null, LongLoggingSettings defaultLoggingSettings = null)
+        {
+            this.defaultLoggingAction = defaultLoggingAction;
+            this.defaultLoggingSettings = defaultLoggingSettings;
+        }
+
+        public IDisposable InitialiseDisabledStopwatch(string watchKey, string watchDescription = null, Action<string> loggingAction = null, LongLoggingSettings loggingSettings = null)
+        {
+            var fakeStopwatch = new FastFakeLongOperationLoggingStopwatch();
+            watches.Add(watchKey, fakeStopwatch);
+            return fakeStopwatch;
+        }
+
+        public IDisposable InitialiseStopwatch(string watchKey, string watchDescription = null, Action<string> loggingAction = null, LongLoggingSettings loggingSettings = null)
+        {
+            var initialisedStopwatch = new LongOperationLoggingStopwatch(
+                watchDescription ?? watchKey,
+                loggingAction ?? defaultLoggingAction,
+                loggingSettings ?? defaultLoggingSettings);
+            
+            watches.Add(watchKey, initialisedStopwatch);
+            return initialisedStopwatch;
+        }
+
+        public IDisposable TimeInnerOperation(string watchKey)
+        {
+            return watches[watchKey].TimeInnerOperation();
+        }
+
+        public ILongOperationLoggingStopwatch GetStopwatch(string watchKey)
+        {
+            return watches[watchKey];
+        }
+    }
+
+}
