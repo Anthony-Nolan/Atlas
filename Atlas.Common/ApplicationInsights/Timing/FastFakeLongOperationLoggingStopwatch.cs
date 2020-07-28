8d48e316661ce4f31b6075f8a69798f280e5c68e
Mike.McLean <Mike.McLean@softwire.com>
2020-07-28 02:14:09 +0100

    feature: ATLAS-NONE: A StopwatchCollection to make it easy to modify logging deep in nested code

---
+++ Atlas.Common/ApplicationInsights/Timing/FastFakeLongOperationLoggingStopwatch.cs
@@ -0,0 +1,24 @@
+﻿/* ******************************
+   **  Copyright Softwire 2020 ** 
+   ****************************** */
+// This was taken from a Softwire shareable Repo. At soem point it may get nugetified, in which case we might want
+// migrate to that. Worth checking whether we've diverged, from the original code, though.
+
+using System;
+
+namespace LoggingStopwatch
+{
+    /// <summary>
+    /// Defined in case you ever want something that looks like a ILongOperationLoggingStopwatch but does nothing, and has no overhead.
+    /// </summary>
+    public class FastFakeLongOperationLoggingStopwatch : FakeDisposable, ILongOperationLoggingStopwatch
+    {
+        private readonly IDisposable inner = new FakeDisposable();
+        public IDisposable TimeInnerOperation() => inner;
+    }
+
+    public class FakeDisposable : IDisposable
+    {
+        public void Dispose() { /*Do Nothing*/ }
+    }
+}
