755bb19e8dc9508b102914c4752719a1e238d65c
Tom Johnson <tom.johnson@softwire.com>
2020-07-29 17:55:15 +0100

    feature: ATLAS-251: Created fixed sized queues

---
+++ Atlas.Common.Test/Helpers/FixedSizedQueueTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using Atlas.Common.Helpers;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Atlas.Common.Test.Helpers
+{
+    public class FixedSizedQueueTests
+    {
+        [Test]
+        public void FixedSizedQueue_DropsFirstItemInQueueUponNewInsertion()
+        {
+            var fixedQueue = new FixedSizedQueue<int>(3);
+            fixedQueue.Enqueue(1);
+            fixedQueue.Enqueue(2);
+            fixedQueue.Enqueue(3);
+            fixedQueue.Enqueue(4);
+
+            fixedQueue.Size.Should().Be(3);
+            fixedQueue.ElementAt(0).Should().Be(2);
+            fixedQueue.ElementAt(1).Should().Be(3);
+            fixedQueue.ElementAt(2).Should().Be(4);
+        }
+    }
+}
