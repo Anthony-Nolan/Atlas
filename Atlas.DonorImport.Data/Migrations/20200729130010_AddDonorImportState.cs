7d5876cc9227283c7776cf6f81ed10cdf0c5eba6
Josh Powell <josh.powell@softwire.com>
2020-07-29 14:02:26 +0100

    feature: Atlas-389: Add donor import history migrations

---
+++ Atlas.DonorImport.Data/Migrations/20200729130010_AddDonorImportState.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Atlas.DonorImport.Data.Migrations
+{
+    public partial class AddDonorImportState : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DonorImportHistory",
+                columns: table => new
+                {
+                    Filename = table.Column<string>(nullable: false),
+                    UploadTime = table.Column<DateTime>(nullable: false),
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    State = table.Column<int>(nullable: false),
+                    LastUpdated = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DonorImportHistory", x => new { x.Filename, x.UploadTime });
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DonorImportHistory");
+        }
+    }
+}
