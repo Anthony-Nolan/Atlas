d99561dbe172c430dd78445a95970aea5698918a
Tom Johnson <tom.johnson@softwire.com>
2020-07-29 09:59:01 +0100

    feature: ATLAS-251: Added new column to DataRefreshHistory table

---
+++ Atlas.MatchingAlgorithm.Data.Persistent/Migrations/20200729084843_LastDonorWithProcessedHla.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Atlas.MatchingAlgorithm.Data.Persistent.Migrations
+{
+    public partial class LastDonorWithProcessedHla : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "LastDonorWithProcessedHla",
+                table: "DataRefreshHistory",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastDonorWithProcessedHla",
+                table: "DataRefreshHistory");
+        }
+    }
+}
