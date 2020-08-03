fb76641bb9c22ea2f1149598c3e189a5f179b3d0
Ben Below <benjamin.below@softwire.com>
2020-08-03 12:54:22 +0100

    feature: ATLAS-598: Add validation for Match Prediction requests

---
+++ Atlas.MatchPrediction.Test/Validators/MatchProbabilityInputValidatorTests.cs
@@ -0,0 +1,52 @@
+using Atlas.Common.GeneticData.PhenotypeInfo;
+using Atlas.MatchPrediction.Test.TestHelpers.Builders;
+using Atlas.MatchPrediction.Validators;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Atlas.MatchPrediction.Test.Validators
+{
+    [TestFixture]
+    internal class MatchProbabilityInputValidatorTests
+    {
+        [Test]
+        public void Validator_ForValidInput_ValidationPasses()
+        {
+            var input = MatchProbabilityInputBuilder.New.Build();
+
+            var result = new MatchProbabilityInputValidator().Validate(input);
+
+            result.IsValid.Should().BeTrue();
+        }
+
+        [Test]
+        public void Validator_WhenPatientHlaNotProvided_ValidationFails()
+        {
+            var input = MatchProbabilityInputBuilder.New.With(i => i.PatientHla, null as PhenotypeInfo<string>).Build();
+
+            var result = new MatchProbabilityInputValidator().Validate(input);
+
+            result.IsValid.Should().BeFalse();
+        }
+
+        [Test]
+        public void Validator_WhenDonorHlaNotProvided_ValidationFails()
+        {
+            var input = MatchProbabilityInputBuilder.New.With(i => i.DonorHla, null as PhenotypeInfo<string>).Build();
+
+            var result = new MatchProbabilityInputValidator().Validate(input);
+
+            result.IsValid.Should().BeFalse();
+        }
+
+        [Test]
+        public void Validator_WhenHlaNomenclatureNotProvided_ValidationFails()
+        {
+            var input = MatchProbabilityInputBuilder.New.With(i => i.HlaNomenclatureVersion, null as string).Build();
+
+            var result = new MatchProbabilityInputValidator().Validate(input);
+
+            result.IsValid.Should().BeFalse();
+        }
+    }
+}
