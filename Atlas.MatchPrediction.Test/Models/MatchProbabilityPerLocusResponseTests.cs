4fc63462a335d58d2d546c128fc42e6cf0aa8a37
Tom Johnson <tom.johnson@softwire.com>
2020-08-07 13:15:31 +0100

    review: ATLAS-65: Fixed algorithm and completed review mark-up's

--- Atlas.MatchPrediction.Test/Models/MatchProbabilityPerLocusResponseTests.cs
+++ Atlas.MatchPrediction.Test/Models/MatchProbabilityPerLocusResponseTests.cs
@@ -10,7 +10,7 @@ namespace Atlas.MatchPrediction.Test.Models
     public class MatchProbabilityPerLocusResponseTests
     {
         [Test]
-        public void PredictiveMatchCategory_WhenMatchCategoryIsExact()
+        public void PredictiveMatchCategory_WhenGuranteedNoMismatches_IsExact()
         {
             var matchProbability = MatchProbabilitiesBuilder.New.WithProbabilityValuesSetTo(1m, 0m, 0m).Build();
 
@@ -23,7 +23,7 @@ namespace Atlas.MatchPrediction.Test.Models
         }
 
         [Test]
-        public void PredictiveMatchCategory_WhenMatchCategoryIsMismatchAndTwoMismatchProbabilityIsZeroPercent()
+        public void PredictiveMatchCategory_WhenMatchCategoryIsMismatchAndGuranteedSingleMismatch_IsExact()
         {
             var matchProbability = MatchProbabilitiesBuilder.New.WithProbabilityValuesSetTo(0m, 1m, 0m).Build();
 
@@ -36,7 +36,7 @@ namespace Atlas.MatchPrediction.Test.Models
         }
 
         [Test]
-        public void PredictiveMatchCategory_WhenMatchCategoryIsMismatchAndOneMismatchProbabilityIsZeroPercent()
+        public void PredictiveMatchCategory_WhenMatchCategoryIsMismatchAndGuranteedDoubleMismatch_IsMismatch()
         {
             var matchProbability = MatchProbabilitiesBuilder.New.WithProbabilityValuesSetTo(0m, 0m, 1m).Build();
 
@@ -49,7 +49,7 @@ namespace Atlas.MatchPrediction.Test.Models
         }
 
         [Test]
-        public void PredictiveMatchCategory_WhenMatchCategoryIsMismatchAndNeitherOneOrTwoMismatchProbabilityIsZeroPercent()
+        public void PredictiveMatchCategory_WhenMatchCategoryIsMismatchAndSingleAndDoubleMismatchNotGuranteed_IsPotential()
         {
             var matchProbability = MatchProbabilitiesBuilder.New.WithProbabilityValuesSetTo(0m, 0.5m, 0.5m).Build();
 
@@ -61,27 +61,27 @@ namespace Atlas.MatchPrediction.Test.Models
             matchProbabilityPerLocusResponse.PositionalMatchCategories.Should().BeEquivalentTo(expectedPositionalMatchCategories);
         }
 
-        [Test] public void PredictiveMatchCategory_WhenMatchCategoryIsPotentialAndTwoMismatchProbabilityIsZeroPercent()
+        [Test] public void PredictiveMatchCategory_WhenMatchCategoryIsPotentialAndNotDoubleMismatchGuranteed_IsPotential()
         {
             var matchProbability = MatchProbabilitiesBuilder.New.WithProbabilityValuesSetTo(0.5m, 0.5m, 0m).Build();
 
             var matchProbabilityPerLocusResponse = new MatchProbabilityPerLocusResponse(matchProbability);
 
             var expectedPositionalMatchCategories =
-                new LocusInfo<PredictiveMatchCategory>(PredictiveMatchCategory.Potential, PredictiveMatchCategory.Potential);
+                new LocusInfo<PredictiveMatchCategory>(PredictiveMatchCategory.Potential, PredictiveMatchCategory.Exact);
 
             matchProbabilityPerLocusResponse.PositionalMatchCategories.Should().BeEquivalentTo(expectedPositionalMatchCategories);
         }
 
         [Test]
-        public void PredictiveMatchCategory_WhenMatchCategoryIsPotentialAndTwoMismatchProbabilityIsNotZeroPercent()
+        public void PredictiveMatchCategory_WhenMatchCategoryIsPotentialAndNoMismatchGuranteedOrNot_IsExact()
         {
             var matchProbability = MatchProbabilitiesBuilder.New.WithProbabilityValuesSetTo(0.2m, 0.6m, 0.2m).Build();
 
             var matchProbabilityPerLocusResponse = new MatchProbabilityPerLocusResponse(matchProbability);
 
             var expectedPositionalMatchCategories =
-                new LocusInfo<PredictiveMatchCategory>(PredictiveMatchCategory.Potential, PredictiveMatchCategory.Exact);
+                new LocusInfo<PredictiveMatchCategory>(PredictiveMatchCategory.Potential, PredictiveMatchCategory.Potential);
 
             matchProbabilityPerLocusResponse.PositionalMatchCategories.Should().BeEquivalentTo(expectedPositionalMatchCategories);
         }
