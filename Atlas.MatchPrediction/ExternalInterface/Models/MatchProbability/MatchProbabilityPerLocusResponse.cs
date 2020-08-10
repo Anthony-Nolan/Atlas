f740b22c160db3049b86419eba2142a862905ea8
Tom Johnson <tom.johnson@softwire.com>
2020-08-10 11:51:15 +0100

    review: ATLAS-65: Completed refactor and review mark-up's

--- Atlas.MatchPrediction/ExternalInterface/Models/MatchProbability/MatchProbabilityPerLocusResponse.cs
+++ Atlas.MatchPrediction/ExternalInterface/Models/MatchProbability/MatchProbabilityPerLocusResponse.cs
@@ -8,14 +8,14 @@ namespace Atlas.MatchPrediction.ExternalInterface.Models.MatchProbability
     {
         public MatchProbabilities MatchProbabilities { get; set; }
 
+        /// <summary>
+        /// Match grades are being assigned arbitrarily to pos1 and pos2.
+        /// </summary>
         public LocusInfo<PredictiveMatchCategory> PositionalMatchCategories => MatchProbabilities.MatchCategory switch
         {
             PredictiveMatchCategory.Exact => new LocusInfo<PredictiveMatchCategory>(PredictiveMatchCategory.Exact),
-            PredictiveMatchCategory.Mismatch => new LocusInfo<PredictiveMatchCategory>(PredictiveMatchCategory.Mismatch,
-                MatchProbabilities.TwoMismatchProbability.Decimal == 0 ? PredictiveMatchCategory.Exact :
-                MatchProbabilities.OneMismatchProbability.Decimal == 0 ? PredictiveMatchCategory.Mismatch : PredictiveMatchCategory.Potential),
-            PredictiveMatchCategory.Potential => new LocusInfo<PredictiveMatchCategory>(PredictiveMatchCategory.Potential,
-                MatchProbabilities.TwoMismatchProbability.Decimal == 0 ? PredictiveMatchCategory.Exact : PredictiveMatchCategory.Potential),
+            PredictiveMatchCategory.Mismatch => new LocusInfo<PredictiveMatchCategory>(PredictiveMatchCategory.Mismatch, GetMismatchSecondMatchCategory()),
+            PredictiveMatchCategory.Potential => new LocusInfo<PredictiveMatchCategory>(PredictiveMatchCategory.Potential, GetPotentialSecondMatchCategory()),
             _ => throw new ArgumentOutOfRangeException()
         };
 
@@ -39,6 +39,35 @@ namespace Atlas.MatchPrediction.ExternalInterface.Models.MatchProbability
             {
                 MatchProbabilities = MatchProbabilities?.Round(decimalPlaces)
             };
+        }
+
+        /// <summary>
+        /// If zero percent chance of two mismatches, other match category must be exact.
+        /// If zero percent chance of one mismatch, can't be either exact or potential so must be mismatch.
+        /// All other cases the match category will be potential.
+        /// </summary>
+        private PredictiveMatchCategory GetMismatchSecondMatchCategory()
+        {
+            if (MatchProbabilities.TwoMismatchProbability.Decimal == 0)
+            {
+                return PredictiveMatchCategory.Exact;
+            }
+
+            if (MatchProbabilities.OneMismatchProbability.Decimal == 0)
+            {
+                return PredictiveMatchCategory.Mismatch;
+            }
+
+            return PredictiveMatchCategory.Potential;
+        }
+
+        /// <summary>
+        /// If zero percent chance of two mismatches, other match category must be exact.
+        /// If There is a chance of two mismatches then neither of them can be exact so match category is potential.
+        /// </summary>
+        private PredictiveMatchCategory GetPotentialSecondMatchCategory()
+        {
+            return MatchProbabilities.TwoMismatchProbability.Decimal == 0 ? PredictiveMatchCategory.Exact : PredictiveMatchCategory.Potential;
         }
     }
 }
