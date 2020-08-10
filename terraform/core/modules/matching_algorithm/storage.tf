91dacc2006b2ddc53544fd0f6580077cf29070fb
Ben Below <benjamin.below@softwire.com>
2020-08-10 15:12:58 +0100

    fix: ATLAS-646: Update to 2.22.0 of azurerm terraform provider

--- terraform/core/modules/matching_algorithm/storage.tf
+++ terraform/core/modules/matching_algorithm/storage.tf
@@ -1,6 +1,5 @@
 resource "azurerm_storage_container" "search_matching_results_blob_container" {
   name                  = "matching-algorithm-results"
   storage_account_name  = var.azure_storage.name
-  resource_group_name   = var.app_service_plan.resource_group_name
   container_access_type = "private"
 }
--- terraform/core/modules/multiple_allele_code_lookup/storage.tf
+++ terraform/core/modules/multiple_allele_code_lookup/storage.tf
@@ -1,5 +1,4 @@
 resource "azurerm_storage_table" "multiple_allele_code_storage" {
   name                 = "AtlasMultipleAlleleCodes"
   storage_account_name = var.azure_storage.name
-  resource_group_name  = var.app_service_plan.resource_group_name
 }
