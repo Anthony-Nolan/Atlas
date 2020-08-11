4408dd8c8a738104912ee27cce69cb6e8cb6d024
Tom Johnson <tom.johnson@softwire.com>
2020-08-11 14:40:31 +0100

    fix: ATLAS-584: Pass in variables to modules

--- terraform/core/modules/match_prediction/variables_release.tf
+++ terraform/core/modules/match_prediction/variables_release.tf
@@ -11,6 +11,13 @@ variable "DATABASE_USERNAME" {
   type = string
 }
 
+variable "IP_RESTRICTION_SETTINGS" {
+  type = list(object({
+    ip_address = string
+    subnet_mask = string
+  }))
+}
+
 variable "MAC_SOURCE" {
   type = string
 }
