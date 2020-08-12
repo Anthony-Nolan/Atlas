63895bbe0d1e7c2ea1d35bab2c5fee3a56965057
Josh Powell <josh.powell@softwire.com>
2020-08-12 15:05:20 +0100

    feature: Atlas-510: return sync 400 error if search request invalid

--- Atlas.Functions/DurableFunctions/Search/Client/SearchClientFunctions.cs
+++ Atlas.Functions/DurableFunctions/Search/Client/SearchClientFunctions.cs
@@ -1,9 +1,12 @@
+using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Atlas.Common.ApplicationInsights;
 using Atlas.Functions.DurableFunctions.Search.Orchestration;
 using Atlas.Functions.Models.Search.Requests;
+using Atlas.MatchingAlgorithm.Validators.SearchRequest;
 using AzureFunctions.Extensions.Swashbuckle.Attribute;
+using FluentValidation;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.DurableTask;
 using Microsoft.Azure.WebJobs.Extensions.Http;
@@ -36,6 +39,14 @@ namespace Atlas.Functions.DurableFunctions.Search.Client
         {
             var searchRequest = JsonConvert.DeserializeObject<SearchRequest>(await request.Content.ReadAsStringAsync());
 
+            var matchingRequest = searchRequest.ToMatchingRequest();
+            
+            var validationResult = new SearchRequestValidator().Validate(matchingRequest);
+            if (!validationResult.IsValid)
+            {
+                return new HttpResponseMessage( HttpStatusCode.BadRequest ) {Content =  new StringContent(JsonConvert.SerializeObject(validationResult.Errors), System.Text.Encoding.UTF8, "application/json" ) };
+            }
+            
             // Function input comes from the request content.
             var instanceId = await starter.StartNewAsync(nameof(SearchOrchestrationFunctions.SearchOrchestrator), searchRequest);
             
