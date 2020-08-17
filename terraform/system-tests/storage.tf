e5f2361fbbfca2ea0ed7338c6a5f2fd3883e556a
Josh Powell <josh.powell@softwire.com>
2020-08-17 15:43:52 +0100

    review: Atlas-314: Add storage account to system tests terraform

---
+++ terraform/system-tests/storage.tf
@@ -0,0 +1,13 @@
+resource "azurerm_storage_account" "system_test_storage" {
+  name                     = lower("ATLAS-SYSTEM-TEST-STORAGE-ACCOUNT")
+  resource_group_name      = azurerm_resource_group.atlas_resource_group.name
+  location                 = local.location
+  tags                     = local.common_tags
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_table" "multiple_allele_code_storage" {
+  name                 = "AtlasMultipleAlleleCodes"
+  storage_account_name = azurerm_storage_account.system_test_storage.name
+}
