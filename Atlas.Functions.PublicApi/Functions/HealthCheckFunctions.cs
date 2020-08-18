893580f59bd622d67f3abc185ba2521f697e6f49
Ben Below <benjamin.below@softwire.com>
2020-08-18 12:16:27 +0100

    chore: ATLAS-665: Add new empty functions project for public api

---
+++ Atlas.Functions.PublicApi/Functions/HealthCheckFunctions.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+
+namespace Atlas.Functions.PublicApi.Functions
+{
+    public static class HealthCheckFunctions
+    {
+        [FunctionName(nameof(HealthCheck))]
+        public static OkObjectResult HealthCheck([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequest req)
+        {
+            const string responseMessage = "This HTTP triggered function executed successfully";
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
