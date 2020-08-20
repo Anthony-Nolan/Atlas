c94cd4da357cac71257085822b93d46ad5a11d92
Ben Below <benjamin.below@softwire.com>
2020-08-20 15:41:31 +0100

    fix: ATLAS-665: Get subscription and topic the right way round

--- terraform/core/function.tf
+++ terraform/core/function.tf
@@ -33,8 +33,8 @@ resource "azurerm_function_app" "atlas_function" {
     "AtlasFunction:AzureStorage:MatchingResultsBlobContainer"       = module.matching_algorithm.azure_storage.search_results_container
     "AtlasFunction:AzureStorage:SearchResultsBlobContainer"         = azurerm_storage_container.search_results_blob_container.name
     "AtlasFunction:MessagingServiceBus:ConnectionString"            = azurerm_servicebus_namespace_authorization_rule.write-only.primary_connection_string
-    "AtlasFunction:MessagingServiceBus:MatchingResultsSubscription" = module.matching_algorithm.service_bus.matching_results_topic
-    "AtlasFunction:MessagingServiceBus:MatchingResultsTopic"        = azurerm_servicebus_subscription.match-prediction-orchestration-search-results-ready.name
+    "AtlasFunction:MessagingServiceBus:MatchingResultsSubscription" = azurerm_servicebus_subscription.match-prediction-orchestration-search-results-ready.name
+    "AtlasFunction:MessagingServiceBus:MatchingResultsTopic"        = module.matching_algorithm.service_bus.matching_results_topic
     "AtlasFunction:MessagingServiceBus:SearchResultsTopic"          = azurerm_servicebus_topic.search-results-ready.name
     "AtlasFunction:Orchestration:MatchPredictionBatchSize"          = var.ORCHESTRATION_MATCH_PREDICTION_BATCH_SIZE
 
