d6806367920431fe8ed6cf057553b04ccb81ba2a
Ben Below <benjamin.below@softwire.com>
2020-08-26 15:19:53 +0100

    feature: ATLAS-692: Replace webhook donor import subscription with one pointing to service bus topic

--- terraform/core/modules/donor_import/servicebus.tf
+++ terraform/core/modules/donor_import/servicebus.tf
@@ -19,3 +19,25 @@ resource "azurerm_servicebus_subscription" "audit-updated-searchable-donors" {
   max_delivery_count                   = var.default_servicebus_settings.default-message-retries
   dead_lettering_on_message_expiration = false
 }
+
+resource "azurerm_servicebus_topic" "donor-import-file-uploads" {
+  name                  = "donor-import-file-uploads"
+  resource_group_name   = var.app_service_plan.resource_group_name
+  namespace_name        = var.servicebus_namespace.name
+  auto_delete_on_idle   = var.default_servicebus_settings.long-expiry
+  default_message_ttl   = var.default_servicebus_settings.long-expiry
+  max_size_in_megabytes = var.default_servicebus_settings.default-bus-size
+  support_ordering      = true
+}
+
+resource "azurerm_servicebus_subscription" "donor-import-file-processor" {
+  name                                 = "donor-import"
+  resource_group_name                  = var.app_service_plan.resource_group_name
+  namespace_name                       = var.servicebus_namespace.name
+  topic_name                           = azurerm_servicebus_topic.donor-import-file-uploads.name
+  auto_delete_on_idle                  = var.default_servicebus_settings.audit-subscription-idle-delete
+  default_message_ttl                  = var.default_servicebus_settings.long-expiry
+  lock_duration                        = var.default_servicebus_settings.default-read-lock
+  max_delivery_count                   = var.default_servicebus_settings.default-message-retries
+  dead_lettering_on_message_expiration = false
+}
--- terraform/webhooks/event_grid.tf
+++ terraform/webhooks/event_grid.tf
@@ -6,31 +6,17 @@ locals {
 }
 
 module "donor_import_function_key" {
-  source = "./modules/fetch_key"
+  source          = "./modules/fetch_key"
   function_app_id = data.terraform_remote_state.atlas.outputs.donor_import.function_app.id
-  client_id = var.AZURE_CLIENT_ID
-  client_secret = var.AZURE_CLIENT_SECRET
+  client_id       = var.AZURE_CLIENT_ID
+  client_secret   = var.AZURE_CLIENT_SECRET
 }
 
 module "match_prediction_function_key" {
-  source = "./modules/fetch_key"
+  source          = "./modules/fetch_key"
   function_app_id = data.terraform_remote_state.atlas.outputs.match_prediction.function_app.id
-  client_id = var.AZURE_CLIENT_ID
-  client_secret = var.AZURE_CLIENT_SECRET
-}
-
-resource "azurerm_eventgrid_event_subscription" "donor-file-upload" {
-  name                 = "${lower(var.ENVIRONMENT)}-donor-file-upload-subscription"
-  scope                = data.terraform_remote_state.atlas.outputs.storage_account.id
-  included_event_types = ["Microsoft.Storage.BlobCreated"]
-
-  subject_filter {
-    subject_begins_with = "/blobServices/default/containers/${data.terraform_remote_state.atlas.outputs.donor_import.storage.donor_container_name}"
-  }
-
-  webhook_endpoint {
-    url = "https://${data.terraform_remote_state.atlas.outputs.donor_import.function_app.hostname}/runtime/webhooks/EventGrid?functionName=${local.import_donor_file_function_name}&code=${module.donor_import_function_key.function_key.key}"
-  }
+  client_id       = var.AZURE_CLIENT_ID
+  client_secret   = var.AZURE_CLIENT_SECRET
 }
 
 resource "azurerm_eventgrid_event_subscription" "haplotype-frequency-set-upload" {
