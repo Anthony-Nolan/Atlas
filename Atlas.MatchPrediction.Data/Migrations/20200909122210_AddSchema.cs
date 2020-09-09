d65bad3dde3a702807e6745555d5b5e6130cad24
Ben Below <benjamin.below@softwire.com>
2020-09-09 13:28:54 +0100

    refactor: ATLAS-747: Move match prediction database to a schema on a shared DB (local only)

---
+++ Atlas.MatchPrediction.Data/Migrations/20200909122210_AddSchema.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Atlas.MatchPrediction.Data.Migrations
+{
+    public partial class AddSchema : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "MatchPrediction");
+
+            migrationBuilder.RenameTable(
+                name: "HaplotypeFrequencySets",
+                newName: "HaplotypeFrequencySets",
+                newSchema: "MatchPrediction");
+
+            migrationBuilder.RenameTable(
+                name: "HaplotypeFrequencies",
+                newName: "HaplotypeFrequencies",
+                newSchema: "MatchPrediction");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "HaplotypeFrequencySets",
+                schema: "MatchPrediction",
+                newName: "HaplotypeFrequencySets");
+
+            migrationBuilder.RenameTable(
+                name: "HaplotypeFrequencies",
+                schema: "MatchPrediction",
+                newName: "HaplotypeFrequencies");
+        }
+    }
+}
