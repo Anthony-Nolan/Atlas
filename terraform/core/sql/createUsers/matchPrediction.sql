ced18ea36bc570b6da00f8d43ccc9d3f65dd8974
Ben Below <benjamin.below@softwire.com>
2020-09-09 15:10:41 +0100

    chore: ATLAS-747: Apply auto-formatting to SQL user scripts

--- terraform/core/sql/createUsers/matchPrediction.sql
+++ terraform/core/sql/createUsers/matchPrediction.sql
@@ -1,15 +1,15 @@
 IF USER_ID('$(matchPredictionUsername)') IS NULL
-BEGIN 
-	CREATE USER $(matchPredictionUsername) WITH PASSWORD = '$(matchPredictionPassword)'
-  ALTER ROLE db_datareader ADD MEMBER $(matchPredictionUsername)
-  ALTER ROLE db_datawriter ADD MEMBER $(matchPredictionUsername)
-END
+    BEGIN
+        CREATE USER $(matchPredictionUsername) WITH PASSWORD = '$(matchPredictionPassword)'
+        ALTER ROLE db_datareader ADD MEMBER $(matchPredictionUsername)
+        ALTER ROLE db_datawriter ADD MEMBER $(matchPredictionUsername)
+    END
 
 ELSE
 
-BEGIN
-	ALTER USER $(matchPredictionUsername) WITH PASSWORD = '$(matchPredictionPassword)'
-  ALTER ROLE db_datareader ADD MEMBER $(matchPredictionUsername)
-  ALTER ROLE db_datawriter ADD MEMBER $(matchPredictionUsername)
-END
+    BEGIN
+        ALTER USER $(matchPredictionUsername) WITH PASSWORD = '$(matchPredictionPassword)'
+        ALTER ROLE db_datareader ADD MEMBER $(matchPredictionUsername)
+        ALTER ROLE db_datawriter ADD MEMBER $(matchPredictionUsername)
+    END
 
