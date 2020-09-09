b11880f3c51c13bf63874069cb8a1df24e777a45
Ben Below <benjamin.below@softwire.com>
2020-09-09 12:57:38 +0100

    refactor: ATLAS-747: Move donor database to a schema on a shared DB (local only)

---
+++ Atlas.DonorImport.Data/Migrations/20200909113616_AddSchema.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Atlas.DonorImport.Data.Migrations
+{
+    public partial class AddSchema : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "Donors");
+
+            migrationBuilder.RenameTable(
+                name: "Donors",
+                newName: "Donors",
+                newSchema: "Donors");
+
+            migrationBuilder.RenameTable(
+                name: "DonorLogs",
+                newName: "DonorLogs",
+                newSchema: "Donors");
+
+            migrationBuilder.RenameTable(
+                name: "DonorImportHistory",
+                newName: "DonorImportHistory",
+                newSchema: "Donors");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "Donors",
+                schema: "Donors",
+                newName: "Donors");
+
+            migrationBuilder.RenameTable(
+                name: "DonorLogs",
+                schema: "Donors",
+                newName: "DonorLogs");
+
+            migrationBuilder.RenameTable(
+                name: "DonorImportHistory",
+                schema: "Donors",
+                newName: "DonorImportHistory");
+        }
+    }
+}
