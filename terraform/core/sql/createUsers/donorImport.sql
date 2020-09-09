86f046dc52f94bdf272666538f0f09431d5e3ee4
Ben Below <benjamin.below@softwire.com>
2020-09-09 15:10:41 +0100

    chore: ATLAS-747: Apply auto-formatting to SQL user scripts

--- terraform/core/sql/createUsers/donorImport.sql
+++ terraform/core/sql/createUsers/donorImport.sql
@@ -1,30 +1,29 @@
 IF USER_ID('$(matchingUsernameForDonorDB)') IS NULL
-BEGIN 
-	CREATE USER $(matchingUsernameForDonorDB) WITH PASSWORD = '$(matchingPasswordForDonorDB)';
-  ALTER ROLE db_datareader ADD MEMBER $(matchingUsernameForDonorDB)
-END
+    BEGIN
+        CREATE USER $(matchingUsernameForDonorDB) WITH PASSWORD = '$(matchingPasswordForDonorDB)';
+        ALTER ROLE db_datareader ADD MEMBER $(matchingUsernameForDonorDB)
+    END
 
 ELSE
 
-BEGIN
-	ALTER USER $(matchingUsernameForDonorDB) WITH PASSWORD = '$(matchingPasswordForDonorDB)';
-	ALTER ROLE db_datareader ADD MEMBER $(matchingUsernameForDonorDB)
-END
-
+    BEGIN
+        ALTER USER $(matchingUsernameForDonorDB) WITH PASSWORD = '$(matchingPasswordForDonorDB)';
+        ALTER ROLE db_datareader ADD MEMBER $(matchingUsernameForDonorDB)
+    END
 
 
 IF USER_ID('$(donorImportUsername)') IS NULL
-BEGIN 
-	CREATE USER $(donorImportUsername) WITH PASSWORD = '$(donorImportPassword)'
-  ALTER ROLE db_datareader ADD MEMBER $(donorImportUsername)
-  ALTER ROLE db_datawriter ADD MEMBER $(donorImportUsername)
-END
+    BEGIN
+        CREATE USER $(donorImportUsername) WITH PASSWORD = '$(donorImportPassword)'
+        ALTER ROLE db_datareader ADD MEMBER $(donorImportUsername)
+        ALTER ROLE db_datawriter ADD MEMBER $(donorImportUsername)
+    END
 
 ELSE
 
-BEGIN
-	ALTER USER $(donorImportUsername) WITH PASSWORD = '$(donorImportPassword)'
-  ALTER ROLE db_datareader ADD MEMBER $(donorImportUsername)
-  ALTER ROLE db_datawriter ADD MEMBER $(donorImportUsername)
-END
+    BEGIN
+        ALTER USER $(donorImportUsername) WITH PASSWORD = '$(donorImportPassword)'
+        ALTER ROLE db_datareader ADD MEMBER $(donorImportUsername)
+        ALTER ROLE db_datawriter ADD MEMBER $(donorImportUsername)
+    END
 
--- terraform/core/sql/createUsers/matchingPersistent.sql
+++ terraform/core/sql/createUsers/matchingPersistent.sql
@@ -1,17 +1,17 @@
 IF USER_ID('$(matchingUsername)') IS NULL
-BEGIN 
-	CREATE USER $(matchingUsername) WITH PASSWORD = '$(matchingPassword)'
-  ALTER ROLE db_datareader ADD MEMBER $(matchingUsername)
-  ALTER ROLE db_datawriter ADD MEMBER $(matchingUsername)
-  ALTER ROLE db_owner ADD MEMBER $(matchingUsername)
-END
+    BEGIN
+        CREATE USER $(matchingUsername) WITH PASSWORD = '$(matchingPassword)'
+        ALTER ROLE db_datareader ADD MEMBER $(matchingUsername)
+        ALTER ROLE db_datawriter ADD MEMBER $(matchingUsername)
+        ALTER ROLE db_owner ADD MEMBER $(matchingUsername)
+    END
 
 ELSE
 
-BEGIN
-	ALTER USER $(matchingUsername) WITH PASSWORD = '$(matchingPassword)'
-  ALTER ROLE db_datareader ADD MEMBER $(matchingUsername)
-  ALTER ROLE db_datawriter ADD MEMBER $(matchingUsername)
-  ALTER ROLE db_owner ADD MEMBER $(matchingUsername)
-END
+    BEGIN
+        ALTER USER $(matchingUsername) WITH PASSWORD = '$(matchingPassword)'
+        ALTER ROLE db_datareader ADD MEMBER $(matchingUsername)
+        ALTER ROLE db_datawriter ADD MEMBER $(matchingUsername)
+        ALTER ROLE db_owner ADD MEMBER $(matchingUsername)
+    END
 
