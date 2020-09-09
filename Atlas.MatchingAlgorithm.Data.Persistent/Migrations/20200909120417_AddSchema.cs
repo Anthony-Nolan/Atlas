d5b315e8776a5803a30d67a6a1a09d03888977e2
Ben Below <benjamin.below@softwire.com>
2020-09-09 13:09:38 +0100

    refactor: ATLAS-747: Move persistent matching database to a schema on a shared DB (local only)

---
+++ Atlas.MatchingAlgorithm.Data.Persistent/Migrations/20200909120417_AddSchema.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Atlas.MatchingAlgorithm.Data.Persistent.Migrations
+{
+    public partial class AddSchema : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "MatchingAlgorithmPersistent");
+
+            migrationBuilder.RenameTable(
+                name: "GradeWeightings",
+                newName: "GradeWeightings",
+                newSchema: "MatchingAlgorithmPersistent");
+
+            migrationBuilder.RenameTable(
+                name: "DataRefreshHistory",
+                newName: "DataRefreshHistory",
+                newSchema: "MatchingAlgorithmPersistent");
+
+            migrationBuilder.RenameTable(
+                name: "ConfidenceWeightings",
+                newName: "ConfidenceWeightings",
+                newSchema: "MatchingAlgorithmPersistent");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "GradeWeightings",
+                schema: "MatchingAlgorithmPersistent",
+                newName: "GradeWeightings");
+
+            migrationBuilder.RenameTable(
+                name: "DataRefreshHistory",
+                schema: "MatchingAlgorithmPersistent",
+                newName: "DataRefreshHistory");
+
+            migrationBuilder.RenameTable(
+                name: "ConfidenceWeightings",
+                schema: "MatchingAlgorithmPersistent",
+                newName: "ConfidenceWeightings");
+        }
+    }
+}
