be9d79d5aad2d7de5803687f5520883365279c0b
Tom Johnson <tom.johnson@softwire.com>
2020-09-10 17:13:49 +0100

    chore: ATLAS-484: Removed deprecated package from common and removed un-needed file

--- Atlas.Common/AzureStorage/TableStorage/AzureTableBatchInsertException.cs
+++ Atlas.Common/AzureStorage/TableStorage/AzureTableBatchInsertException.cs
@@ -2,23 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using Atlas.Common.Utils.Extensions;
-using CosmosEntity = Microsoft.Azure.Cosmos.Table.TableEntity;
-using WindowsAzureEntity = Microsoft.WindowsAzure.Storage.Table.TableEntity;
+using Microsoft.Azure.Cosmos.Table;
 
 namespace Atlas.Common.AzureStorage.TableStorage
 {
     internal class AzureTableBatchInsertException : Exception
     {
-        public AzureTableBatchInsertException(IEnumerable<CosmosEntity> tableEntities, Exception inner)
+        public AzureTableBatchInsertException(IEnumerable<TableEntity> tableEntities, Exception inner)
             : base(GetErrorMessage(tableEntities), inner)
         { }
 
-        public AzureTableBatchInsertException(IEnumerable<WindowsAzureEntity> tableEntities, Exception inner)
-            : base(GetErrorMessage(tableEntities), inner)
-        { }
-
-        private static string GetErrorMessage(IEnumerable<CosmosEntity> entities) => GetErrorMessage(entities, e => e.PartitionKey, e => e.RowKey);
-        private static string GetErrorMessage(IEnumerable<WindowsAzureEntity> entities) => GetErrorMessage(entities, e => e.PartitionKey, e => e.RowKey);
+        private static string GetErrorMessage(IEnumerable<TableEntity> entities) => GetErrorMessage(entities, e => e.PartitionKey, e => e.RowKey);
 
         private static string GetErrorMessage<T>(IEnumerable<T> entities, Func<T, string> partitionKeyFor, Func<T, string> rowKeyFor)
         {
