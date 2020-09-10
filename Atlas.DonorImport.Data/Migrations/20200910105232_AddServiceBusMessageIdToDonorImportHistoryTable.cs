875923abc833f515da98bcd5f41c4b4df89722ad
Tom Johnson <tom.johnson@softwire.com>
2020-09-10 11:53:21 +0100

    feature: ATLAS-751: Created new migration for ServiceBusMessageId

---
+++ Atlas.DonorImport.Data/Migrations/20200910105232_AddServiceBusMessageIdToDonorImportHistoryTable.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Atlas.DonorImport.Data.Migrations
+{
+    public partial class AddServiceBusMessageIdToDonorImportHistoryTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ServiceBusMessageId",
+                schema: "Donors",
+                table: "DonorImportHistory",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ServiceBusMessageId",
+                schema: "Donors",
+                table: "DonorImportHistory");
+        }
+    }
+}
