daccad95f4ff5085ab72dfedbb722636caa9f9ca
Tom Johnson <tom.johnson@softwire.com>
2020-09-10 12:14:16 +0100

    chore: ATLAS-751: Updated integration tests for new change

--- Atlas.DonorImport.Test.Integration/IntegrationTests/Import/DonorTypeParsing/DonorTypeParsingTests.cs
+++ Atlas.DonorImport.Test.Integration/IntegrationTests/Import/DonorTypeParsing/DonorTypeParsingTests.cs
@@ -110,7 +110,8 @@ namespace Atlas.DonorImport.Test.Integration.IntegrationTests.Import.DonorTypePa
             var donorTestFilePath = $"{typeof(DonorTypeParsingTests).Namespace}.{fileName}";
             await using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(donorTestFilePath))
             {
-                await donorFileImporter.ImportDonorFile(new DonorImportFile {Contents = stream, FileLocation = donorTestFilePath, UploadTime = DateTime.Now});
+                await donorFileImporter.ImportDonorFile(
+                    new DonorImportFile {Contents = stream, FileLocation = donorTestFilePath, UploadTime = DateTime.Now, MessageId = "message-id"});
             }
         }
     }
