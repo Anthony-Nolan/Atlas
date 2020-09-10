69db272f79aef6fb6d37fdfea2715644f654a384
Ben Below <benjamin.below@softwire.com>
2020-09-10 11:40:24 +0100

    chore: ATLAS-747: Update system test shared DB to S1

--- terraform/system-tests/sql_database.tf
+++ terraform/system-tests/sql_database.tf
@@ -13,7 +13,7 @@ resource "azurerm_sql_database" "atlas-system-tests-shared" {
 
   edition                          = "Standard"
   max_size_bytes                   = "32212254720"
-  requested_service_objective_name = "S0"
+  requested_service_objective_name = "S1"
 }
 
 resource "azurerm_sql_database" "atlas-matching" {
