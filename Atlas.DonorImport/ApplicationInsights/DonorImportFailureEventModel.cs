058ecea5f8abcda9a52a0f1eb1402f9e2b2f814f
Tom Johnson <tom.johnson@softwire.com>
2020-09-11 16:20:09 +0100

    feature: ATLAS-764: Review mark ups

---
+++ Atlas.DonorImport/ApplicationInsights/DonorImportFailureEventModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Atlas.Common.ApplicationInsights;
+using Atlas.DonorImport.ExternalInterface.Models;
+using Atlas.DonorImport.Services;
+
+namespace Atlas.DonorImport.ApplicationInsights
+{
+    internal class DonorImportFailureEventModel : EventModel
+    {
+        public DonorImportFailureEventModel(DonorImportFile file, Exception exception, int importedDonorCount, LazilyParsingDonorFile lazyFile) : base("Donor Import Failed")
+        {
+            Level = LogLevel.Warn;
+            Properties.Add(nameof(file.FileLocation), file.FileLocation);
+            Properties.Add("Exception", exception.ToString());
+            Properties.Add("ImportedDonorCount", importedDonorCount.ToString());
+            Properties.Add("ParsedDonorCount", lazyFile?.ParsedDonorCount.ToString());
+            Properties.Add("LastDonorCodeParsed", lazyFile?.LastSuccessfullyParsedDonorCode);
+        }
+    }
+}
