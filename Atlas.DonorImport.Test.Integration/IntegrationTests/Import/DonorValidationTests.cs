de757d25415c85724bbedef6b3453e9c012f2783
Ben Below <benjamin.below@softwire.com>
2020-09-14 15:47:12 +0100

    fix: ATLAS-767: Fix donor validation in case of homozygous loci

---
+++ Atlas.DonorImport.Test.Integration/IntegrationTests/Import/DonorValidationTests.cs
@@ -0,0 +1,226 @@
+using System.Threading.Tasks;
+using Atlas.Common.Test.SharedTestHelpers;
+using Atlas.DonorImport.ExternalInterface.Models;
+using Atlas.DonorImport.Models.FileSchema;
+using Atlas.DonorImport.Services;
+using Atlas.DonorImport.Test.Integration.TestHelpers;
+using Atlas.DonorImport.Test.TestHelpers.Builders;
+using Atlas.DonorImport.Test.TestHelpers.Builders.ExternalModels;
+using FluentAssertions;
+using LochNessBuilder;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+
+namespace Atlas.DonorImport.Test.Integration.IntegrationTests.Import
+{
+    [TestFixture]
+    public class DonorValidationTests
+    {
+        private IDonorInspectionRepository donorRepository;
+        private IDonorFileImporter donorFileImporter;
+        private readonly Builder<DonorImportFile> fileBuilder = DonorImportFileBuilder.NewWithoutContents;
+
+        private Builder<DonorUpdate> DonorCreationBuilder =>
+            DonorUpdateBuilder.New
+                .WithRecordIdPrefix("external-donor-code-")
+                .With(upd => upd.ChangeType, ImportDonorChangeType.Create);
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            TestStackTraceHelper.CatchAndRethrowWithStackTraceInExceptionMessage(() =>
+            {
+                donorRepository = DependencyInjection.DependencyInjection.Provider.GetService<IDonorInspectionRepository>();
+                donorFileImporter = DependencyInjection.DependencyInjection.Provider.GetService<IDonorFileImporter>();
+            });
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            TestStackTraceHelper.CatchAndRethrowWithStackTraceInExceptionMessage(DatabaseManager.ClearDatabases);
+        }
+
+        [Test]
+        public async Task ImportDonors_IfMissingMandatoryHlaTypings_DoesNotAddToDatabase()
+        {
+            var donorUpdate = DonorCreationBuilder.Build();
+
+            donorUpdate.Hla.A = null;
+            var file = fileBuilder.WithDonors(donorUpdate).Build();
+
+            await donorFileImporter.ImportDonorFile(file);
+
+            var result = await donorRepository.GetDonor(donorUpdate.RecordId);
+            result.Should().BeNull();
+        }
+
+        [Test]
+        public async Task ImportDonors_IfRequiredLocusHasNullHlaValues_DoesNotAddToDatabase()
+        {
+            var donorUpdate = DonorCreationBuilder.Build();
+
+            donorUpdate.Hla.A = new ImportedLocus
+            {
+                Dna = new TwoFieldStringData
+                {
+                    Field1 = null,
+                    Field2 = null
+                }
+            };
+            var file = fileBuilder.WithDonors(donorUpdate).Build();
+
+            await donorFileImporter.ImportDonorFile(file);
+
+            var result = await donorRepository.GetDonor(donorUpdate.RecordId);
+            result.Should().BeNull();
+        }
+
+        [Test]
+        public async Task ImportDonors_IfRequiredLocusHasEmptyValues_DoesNotAddToDatabase()
+        {
+            var donorUpdate = DonorCreationBuilder.Build();
+
+            donorUpdate.Hla.A = new ImportedLocus
+            {
+                Dna = new TwoFieldStringData
+                {
+                    Field1 = "",
+                    Field2 = ""
+                }
+            };
+            var file = fileBuilder.WithDonors(donorUpdate).Build();
+
+            await donorFileImporter.ImportDonorFile(file);
+
+            var result = await donorRepository.GetDonor(donorUpdate.RecordId);
+            result.Should().BeNull();
+        }
+
+        [Test]
+        public async Task ImportDonors_IfRequiredLocusIsMissingFirstPositionHla_DoesNotAddToDatabase()
+        {
+            var donorUpdate = DonorCreationBuilder.Build();
+
+            donorUpdate.Hla.A = new ImportedLocus
+            {
+                Dna = new TwoFieldStringData
+                {
+                    Field1 = "",
+                    Field2 = "*01:01"
+                }
+            };
+            var file = fileBuilder.WithDonors(donorUpdate).Build();
+
+            await donorFileImporter.ImportDonorFile(file);
+
+            var result = await donorRepository.GetDonor(donorUpdate.RecordId);
+            result.Should().BeNull();
+        }
+
+        [Test]
+        public async Task ImportDonors_IfRequiredLocusIsMissingSecondPositionHla_AddsToDatabaseAsHomozygous()
+        {
+            var donorUpdate = DonorCreationBuilder.Build();
+
+            const string hla = "*01:01";
+            donorUpdate.Hla.A = new ImportedLocus
+            {
+                Dna = new TwoFieldStringData
+                {
+                    Field1 = hla,
+                    Field2 = ""
+                }
+            };
+            var file = fileBuilder.WithDonors(donorUpdate).Build();
+
+            await donorFileImporter.ImportDonorFile(file);
+
+            var result = await donorRepository.GetDonor(donorUpdate.RecordId);
+            result.Should().NotBeNull();
+            result.A_1.Should().Be(hla);
+            result.A_2.Should().Be(hla);
+        }
+
+        [Test]
+        public async Task ImportDonors_ForDonorDeletion_WithInvalidHla_DonorIsStillDeleted()
+        {
+            var donorUpdate = DonorCreationBuilder.Build();
+            var validFile = fileBuilder.WithDonors(donorUpdate).Build();
+            await donorFileImporter.ImportDonorFile(validFile);
+            var importedDonor = await donorRepository.GetDonor(donorUpdate.RecordId);
+            importedDonor.ExternalDonorCode.Should().Be(donorUpdate.RecordId);
+
+            var donorDeletion = DonorUpdateBuilder
+                .New
+                .With(upd => upd.ChangeType, ImportDonorChangeType.Delete)
+                .With(upd => upd.RecordId, donorUpdate.RecordId)
+                .Build();
+            donorDeletion.Hla.A = null;
+            var invalidFile = fileBuilder.WithDonors(donorDeletion).Build();
+
+            await donorFileImporter.ImportDonorFile(invalidFile);
+
+            var result = await donorRepository.GetDonor(donorUpdate.RecordId);
+            result.Should().BeNull();
+        }
+
+        [Test]
+        public async Task ImportDonors_WhenOptionalHlaNotIncluded_AddsToDatabase()
+        {
+            var donorUpdate = DonorCreationBuilder.Build();
+            donorUpdate.Hla.DQB1 = new ImportedLocus();
+
+            var file = fileBuilder.WithDonors(donorUpdate).Build();
+
+            await donorFileImporter.ImportDonorFile(file);
+
+            var result = await donorRepository.GetDonor(donorUpdate.RecordId);
+            result.Should().NotBeNull();
+        }
+
+        [Test]
+        public async Task ImportDonors_WhenOptionalHlaIncludedButEmpty_AddsToDatabase()
+        {
+            var donorUpdate = DonorCreationBuilder.Build();
+            donorUpdate.Hla.DQB1 = new ImportedLocus {Dna = new TwoFieldStringData {Field1 = "", Field2 = ""}};
+
+            var file = fileBuilder.WithDonors(donorUpdate).Build();
+
+            await donorFileImporter.ImportDonorFile(file);
+
+            var result = await donorRepository.GetDonor(donorUpdate.RecordId);
+            result.Should().NotBeNull();
+        }
+
+        [Test]
+        public async Task ImportDonors_WhenOptionalHlaIncluded_AndOnlyPosition1IsPresent_AddsToDatabaseAsHomozygous()
+        {
+            var donorUpdate = DonorCreationBuilder.Build();
+            donorUpdate.Hla.DQB1 = new ImportedLocus {Dna = new TwoFieldStringData {Field1 = "01:01", Field2 = ""}};
+
+            var file = fileBuilder.WithDonors(donorUpdate).Build();
+
+            await donorFileImporter.ImportDonorFile(file);
+
+            var result = await donorRepository.GetDonor(donorUpdate.RecordId);
+            result.Should().NotBeNull();
+            result.DQB1_2.Should().Be(result.DQB1_1);
+            result.DQB1_2.Should().NotBeNullOrEmpty();
+        }
+
+        [Test]
+        public async Task ImportDonors_WhenOptionalHlaIncluded_AndOnlyPosition2IsPresent_DoesNotAddToDatabase()
+        {
+            var donorUpdate = DonorCreationBuilder.Build();
+            donorUpdate.Hla.DQB1 = new ImportedLocus {Dna = new TwoFieldStringData {Field1 = "", Field2 = "01:01"}};
+
+            var file = fileBuilder.WithDonors(donorUpdate).Build();
+
+            await donorFileImporter.ImportDonorFile(file);
+
+            var result = await donorRepository.GetDonor(donorUpdate.RecordId);
+            result.Should().BeNull();
+        }
+    }
+}
--- Atlas.DonorImport/Validators/SearchableDonorValidator.cs
+++ Atlas.DonorImport/Validators/SearchableDonorValidator.cs
@@ -1,4 +1,4 @@
-﻿using System.Data;
+﻿using Atlas.Common.Utils.Extensions;
 using Atlas.DonorImport.Models.FileSchema;
 using FluentValidation;
 
@@ -21,6 +21,10 @@ namespace Atlas.DonorImport.Validators
             RuleFor(h => h.A).NotEmpty().SetValidator(new RequiredImportedLocusValidator());
             RuleFor(h => h.B).NotEmpty().SetValidator(new RequiredImportedLocusValidator());
             RuleFor(h => h.DRB1).NotEmpty().SetValidator(new RequiredImportedLocusValidator());
+
+            RuleFor(h => h.C).SetValidator(new OptionalImportedLocusValidator());
+            RuleFor(h => h.DPB1).SetValidator(new OptionalImportedLocusValidator());
+            RuleFor(h => h.DQB1).SetValidator(new OptionalImportedLocusValidator());
         }
     }
 
@@ -33,12 +37,30 @@ namespace Atlas.DonorImport.Validators
                 .Unless(l => l.Serology != null && new RequiredTwoFieldStringValidator().Validate(l.Serology).IsValid);
         }   
     }
+    
     internal class RequiredTwoFieldStringValidator : AbstractValidator<TwoFieldStringData>
     {
         public RequiredTwoFieldStringValidator()
         {
             RuleFor(d => d.Field1).NotEmpty();
-            RuleFor(d => d.Field2).NotEmpty();
+        }   
+    }
+
+    internal class OptionalImportedLocusValidator : AbstractValidator<ImportedLocus>
+    {
+        public OptionalImportedLocusValidator()
+        {
+            RuleFor(l => l)
+                .Must(l => l.Dna == null || new OptionalTwoFieldStringValidator().Validate(l.Dna).IsValid)
+                .Must(l => l.Serology == null || new OptionalTwoFieldStringValidator().Validate(l.Serology).IsValid);
+        }   
+    }
+    
+    internal class OptionalTwoFieldStringValidator : AbstractValidator<TwoFieldStringData>
+    {
+        public OptionalTwoFieldStringValidator()
+        {
+            RuleFor(d => d.Field1).NotEmpty().When(d => !d.Field2.IsNullOrEmpty());
         }   
     }
     
