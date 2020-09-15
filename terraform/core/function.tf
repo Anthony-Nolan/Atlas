6e174e8e7846b34bb51b81ec0e33291c0a32f24f
Ben Below <benjamin.below@softwire.com>
2020-09-15 17:05:17 +0100

    chore: ATLAS-NONE: Reduce pre-warmed instance count of top level function to 1

--- terraform/core/function.tf
+++ terraform/core/function.tf
@@ -16,7 +16,7 @@ resource "azurerm_function_app" "atlas_function" {
   tags = local.common_tags
 
   site_config {
-    pre_warmed_instance_count = 2
+    pre_warmed_instance_count = 1
     ip_restriction = [for ip in var.IP_RESTRICTION_SETTINGS : {
       ip_address = ip
       subnet_id  = null
