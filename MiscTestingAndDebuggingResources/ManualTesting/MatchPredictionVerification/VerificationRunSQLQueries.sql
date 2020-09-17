b84b787993af4adac0ad6397a71387cf59674af9
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2020-09-17 11:46:05 +0100

    chore: ATLAS-NONE: Add match prediction time query to verification run sql.

--- MiscTestingAndDebuggingResources/ManualTesting/MatchPredictionVerification/VerificationRunSQLQueries.sql
+++ MiscTestingAndDebuggingResources/ManualTesting/MatchPredictionVerification/VerificationRunSQLQueries.sql
@@ -5,26 +5,30 @@
  */
 
  -- Replace with value of interest
- DECLARE @testHarnessId INT = (SELECT TestHarness_Id FROM TestDonorExportRecords WHERE Id = (
-	SELECT MAX(id)
-	FROM TestDonorExportRecords
-	WHERE WasDataRefreshSuccessful = 1))
+ DECLARE @runId INT = (SELECT TOP 1 Id FROM VerificationRuns WHERE TestHarness_Id = (
+		SELECT TOP 1 TestHarness_Id
+		FROM TestDonorExportRecords
+		WHERE WasDataRefreshSuccessful = 1
+		ORDER BY Id DESC)
+	ORDER BY Id DESC)
 
 /*
  *	VERIFICATION RUN RECORD
  */
 
-  SELECT * FROM VerificationRuns WHERE Id = @testHarnessId
+  SELECT * FROM VerificationRuns WHERE Id = @runId
 
 /*
  *	SEARCH REQUESTS AWAITING THEIR SEARCH RESULTS
  */
 
-SELECT r.*
+SELECT s.SimulatedHlaTypingCategory as cat, r.*
 FROM VerificationRuns v
 JOIN SearchRequests r
 ON v.Id = r.VerificationRun_Id
-WHERE v.TestHarness_Id = @testHarnessId AND r.SearchResultsRetrieved = 0
+join Simulants s
+on r.PatientSimulant_Id = s.Id
+WHERE v.Id = @runId AND r.SearchResultsRetrieved = 0
 
 /*
  *	FAILED SEARCH REQUESTS
@@ -34,4 +38,17 @@ SELECT r.*
 FROM VerificationRuns v
 JOIN SearchRequests r
 ON v.Id = r.VerificationRun_Id
-WHERE v.TestHarness_Id = @testHarnessId AND r.SearchResultsRetrieved = 1 AND r.WasSuccessful = 0
+WHERE v.Id = @runId AND r.SearchResultsRetrieved = 1 AND r.WasSuccessful = 0
+
+/*
+ *	MATCH PREDICTION TIMES
+ */
+
+SELECT
+	CAST(MIN(r.MatchPredictionTimeInMs/60000) AS DECIMAL(10,1)) AS MinTimeInMins,
+	CAST(MAX(r.MatchPredictionTimeInMs/60000) AS DECIMAL(10,1)) AS MaxTimeInMins,
+	CAST(AVG(r.MatchPredictionTimeInMs/60000) AS DECIMAL(10,1)) AS MeanAvgTimeInMins
+FROM VerificationRuns v
+JOIN SearchRequests r
+ON v.Id = r.VerificationRun_Id
+WHERE v.Id = @runId AND r.WasSuccessful = 1
