290d5d7b4c7f922a4d215923a49c2cdd7fc971a1
Ben Below <benjamin.below@softwire.com>
2020-09-22 15:28:19 +0100

    performance: ATLAS-825: Ensure MPA Set_Id index contains all necessary columns, to avoid clustered index scan on fetching set

--- Atlas.MatchPrediction.Data/Migrations/20200922141136_UpdateMpaIndex.Designer.cs
+++ Atlas.MatchPrediction.Data/Migrations/20200922141136_UpdateMpaIndex.Designer.cs
@@ -4,14 +4,16 @@ using Atlas.MatchPrediction.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Atlas.MatchPrediction.Data.Migrations
 {
     [DbContext(typeof(MatchPredictionContext))]
-    partial class MatchPredictionContextModelSnapshot : ModelSnapshot
+    [Migration("20200922141136_UpdateMpaIndex")]
+    partial class UpdateMpaIndex
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Atlas.MatchPrediction.Data/Migrations/20200922141136_UpdateMpaIndex.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Atlas.MatchPrediction.Data.Migrations
+{
+    public partial class UpdateMpaIndex : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_HaplotypeFrequencies_Set_Id",
+                schema: "MatchPrediction",
+                table: "HaplotypeFrequencies");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HaplotypeFrequencies_Set_Id",
+                schema: "MatchPrediction",
+                table: "HaplotypeFrequencies",
+                column: "Set_Id")
+                .Annotation("SqlServer:Include", new[] { "Id", "A", "B", "C", "DQB1", "DRB1", "Frequency", "TypingCategory" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_HaplotypeFrequencies_Set_Id",
+                schema: "MatchPrediction",
+                table: "HaplotypeFrequencies");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HaplotypeFrequencies_Set_Id",
+                schema: "MatchPrediction",
+                table: "HaplotypeFrequencies",
+                column: "Set_Id");
+        }
+    }
+}
