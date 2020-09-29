555538864034694b9e987741f2f5bb941511d53d
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2020-09-29 13:17:25 +0100

    chore: ATLAS-824: Extract search timings from notifications.

--- Atlas.Functions.PublicApi.Test.Manual/Services/SearchResultNotificationsPeeker.cs
+++ Atlas.Functions.PublicApi.Test.Manual/Services/SearchResultNotificationsPeeker.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Atlas.Client.Models.Search.Results;
@@ -67,7 +68,38 @@ namespace Atlas.Functions.PublicApi.Test.Manual.Services
                     .GroupBy(n => n.FailureMessage)
                     .ToDictionary(grp => grp.Key, grp => grp.Count()),
                 UniqueSearchRequestIdCount = notifications.Select(n => n.SearchRequestId).Distinct().Count(),
+                SearchTimesInSeconds = ExtractSearchTimes(notifications),
                 PeekedNotifications = notifications
+            };
+        }
+
+        private static SearchTimes ExtractSearchTimes(IReadOnlyCollection<SearchResultsNotification> notifications)
+        {
+            return new SearchTimes
+            {
+                MatchingAlgorithm = CalculateSearchTimesInSeconds(notifications, n => n.MatchingAlgorithmTime),
+                MatchPrediction = CalculateSearchTimesInSeconds(notifications,n => n.MatchPredictionTime),
+                Overall = CalculateSearchTimesInSeconds(notifications, n => n.OverallSearchTime)
+            };
+        }
+
+        private static SearchTimesInSeconds CalculateSearchTimesInSeconds(IReadOnlyCollection<SearchResultsNotification> notifications,
+            Func<SearchResultsNotification, TimeSpan> getTiming)
+        {
+            var timings = notifications.Select(getTiming)
+                .Select(t => t.TotalSeconds)
+                .OrderBy(s => s)
+                .ToList();
+
+            var midPoint = timings.Count/2;
+
+            return new SearchTimesInSeconds
+            {
+                AllTimings = timings,
+                Mean = timings.Average(),
+                Min = timings.Min(),
+                Median = timings.Count%2 == 0 ? (timings[midPoint-1] + timings[midPoint+1])/2 : timings[midPoint],
+                Max = timings.Max()
             };
         }
     }
