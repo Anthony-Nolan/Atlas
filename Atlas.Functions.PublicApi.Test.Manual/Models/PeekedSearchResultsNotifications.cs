5428920f0ff6940df2b1a79954212fd4380129c9
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2020-09-29 13:17:25 +0100

    chore: ATLAS-824: Extract search timings from notifications.

--- Atlas.Functions.PublicApi.Test.Manual/Models/PeekedSearchResultsNotifications.cs
+++ Atlas.Functions.PublicApi.Test.Manual/Models/PeekedSearchResultsNotifications.cs
@@ -10,6 +10,23 @@ namespace Atlas.Functions.PublicApi.Test.Manual.Models
         public int WasSuccessfulCount { get; set; }
         public int WasNotSuccessfulCount => TotalNotificationCount - WasSuccessfulCount;
         public IDictionary<string, int> FailureInfo { get; set; }
+        public SearchTimes SearchTimesInSeconds { get; set; }
         public IEnumerable<SearchResultsNotification> PeekedNotifications { get; set; }
+    }
+
+    public class SearchTimes
+    {
+        public SearchTimesInSeconds MatchingAlgorithm { get; set; }
+        public SearchTimesInSeconds MatchPrediction { get; set; }
+        public SearchTimesInSeconds Overall { get; set; }
+    }
+
+    public class SearchTimesInSeconds
+    {
+        public double Mean { get; set; }
+        public double Min { get; set; }
+        public double Median { get; set; }
+        public double Max { get; set; }
+        public IEnumerable<double> AllTimings { get; set; }
     }
 }
