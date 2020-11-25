43f4b90c40a08d631f91f797ed82abbfbe6c1270
Ben Below <benjamin.below@softwire.com>
2020-11-25 12:14:15 +0000

    chore: ATLAS-877: Add skeleton repeat search component projects

---
+++ Atlas.RepeatSearch.Test/ExampleTest.cs
@@ -0,0 +1,15 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Atlas.RepeatSearch.Test
+{
+    [TestFixture]
+    internal class ExampleTest
+    {
+        [Test]
+        public void ExampleTest_Passes()
+        {
+            true.Should().BeTrue();
+        }
+    }
+}
---
+++ Atlas.RepeatSearch/ExternalInterface/DependencyInjection/ServiceConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Atlas.Common.ApplicationInsights;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Atlas.RepeatSearch.ExternalInterface.DependencyInjection
+{
+    public static class ServiceConfiguration
+    {
+        public static void RegisterRepeatSearch(
+            this IServiceCollection services,
+            Func<IServiceProvider, ApplicationInsightsSettings> optionsReaderFor,
+            Func<IServiceProvider, string> connectionStringReader)
+        {
+        }
+    }
+}
