2fc30f3b4c29d5ab95b809f6b2f496c2f9008484
Ben Below <benjamin.below@softwire.com>
2020-12-01 09:48:42 +0000

    fix: ATLAS-749: Fix single-locus based matching integration tests

---
+++ Atlas.MatchingAlgorithm.Test/TestHelpers/Builders/SearchRequests/AlleleLevelLocusMatchCriteriaBuilder.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using LochNessBuilder;
+using Builder = LochNessBuilder.Builder<Atlas.MatchingAlgorithm.Common.Models.AlleleLevelLocusMatchCriteria>;
+
+namespace Atlas.MatchingAlgorithm.Test.TestHelpers.Builders.SearchRequests
+{
+    [Builder]
+    public static class AlleleLevelLocusMatchCriteriaBuilder
+    {
+        public static Builder New => Builder.New;
+
+        public static Builder WithMismatchCount(this Builder builder, int mismatchCount) => builder
+            .With(c => c.MismatchCount, mismatchCount);
+
+        public static Builder WithPGroups(this Builder builder, IEnumerable<string> pGroups1, IEnumerable<string> pGroups2) => builder
+            .With(c => c.PGroupsToMatchInPositionOne, pGroups1)
+            .With(c => c.PGroupsToMatchInPositionTwo, pGroups2);
+    }
+}
