2522e68223d9a12a309a13bd0c96553e0295418a
Ben Below <benjamin.below@softwire.com>
2020-12-16 17:42:24 +0000

    fix: ATLAS-749: Make all table truncations in validation tests conditional on tables existing

--- Atlas.MatchingAlgorithm.Test.Validation/TestData/Repositories/TestDataRepository.cs
+++ Atlas.MatchingAlgorithm.Test.Validation/TestData/Repositories/TestDataRepository.cs
@@ -7,6 +7,7 @@ using Atlas.MatchingAlgorithm.Data.Context;
 using Atlas.MatchingAlgorithm.Data.Models.Entities;
 using Atlas.MatchingAlgorithm.Data.Persistent.Context;
 using Atlas.MatchingAlgorithm.Data.Persistent.Models;
+using Dapper;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Storage;
@@ -79,32 +80,37 @@ namespace Atlas.MatchingAlgorithm.Test.Validation.TestData.Repositories
 
         private void RemoveTestData()
         {
-            if (TransientDatabaseExists() && DonorTableExists())
+            // HlaNames and PGroups are not deleted between test runs, as these stay constant - and not needing to add them every test run saves test runtime.
+            // If any issues arise from this in future, consider deleting them here also. 
+            if (TransientDatabaseExists())
             {
-                // HlaNames and PGroups are not deleted between test runs, as these stay constant - and not needing to add them every test run saves test runtime.
-                // If any issues arise from this in future, consider deleting them here also. 
-                
-                context.Database.ExecuteSqlRaw(@"
-                TRUNCATE TABLE [HlaNamePGroupRelationAtA]
-                TRUNCATE TABLE [HlaNamePGroupRelationAtB]
-                TRUNCATE TABLE [HlaNamePGroupRelationAtC]
-                TRUNCATE TABLE [HlaNamePGroupRelationAtDrb1]
-                TRUNCATE TABLE [HlaNamePGroupRelationAtDqb1]
-                
-                TRUNCATE TABLE [MatchingHlaAtA]
-                TRUNCATE TABLE [MatchingHlaAtB]
-                TRUNCATE TABLE [MatchingHlaAtC]
-                TRUNCATE TABLE [MatchingHlaAtDrb1]
-                TRUNCATE TABLE [MatchingHlaAtDqb1]
-                
-                TRUNCATE TABLE [Donors]
-                ");
-                
+                TruncateTableIfExists("HlaNamePGroupRelationAtA");
+                TruncateTableIfExists("HlaNamePGroupRelationAtB");
+                TruncateTableIfExists("HlaNamePGroupRelationAtC");
+                TruncateTableIfExists("HlaNamePGroupRelationAtDrb1");
+                TruncateTableIfExists("HlaNamePGroupRelationAtDqb1");
+
+                TruncateTableIfExists("MatchingHlaAtA");
+                TruncateTableIfExists("MatchingHlaAtB");
+                TruncateTableIfExists("MatchingHlaAtC");
+                TruncateTableIfExists("MatchingHlaAtDrb1");
+                TruncateTableIfExists("MatchingHlaAtDqb1");
+
+                TruncateTableIfExists("Donors");
+            }
+        }
+
+        private void TruncateTableIfExists(string tableName)
+        {
+            if (TableExists(tableName))
+            {
+                // SQL injection is not a risk here, as the tableName is not user provided, but hardcoded earlier in this class
+                context.Database.ExecuteSqlRaw($"TRUNCATE TABLE {tableName}");
                 context.SaveChanges();
             }
         }
 
-        private bool DonorTableExists()
+        private bool TableExists(string tableName)
         {
             var conn = context.Database.GetDbConnection();
             if (conn.State.Equals(ConnectionState.Closed))
@@ -112,14 +118,11 @@ namespace Atlas.MatchingAlgorithm.Test.Validation.TestData.Repositories
                 conn.Open();
             }
 
-            using (var command = conn.CreateCommand())
-            {
-                command.CommandText = @"
+            const string tableExistsSql = @"
     SELECT 1 FROM sys.tables AS T 
         INNER JOIN sys.schemas AS S ON T.schema_id = S.schema_id
-    WHERE T.Name = 'Donors'";
-                return command.ExecuteScalar() != null;
-            }
+    WHERE T.Name = @tableName";
+            return conn.QuerySingleOrDefault(tableExistsSql, new {tableName}) != null;
         }
 
         private bool TransientDatabaseExists()
