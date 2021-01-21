78015aa54d3f9cd9fdf51f958922be3d4566ca2b
Ben Below <benjamin.below@softwire.com>
2021-01-21 19:22:45 +0000

    fix: ATLAS-912: Do not render empty individual allele serology arrays in HMD

--- Atlas.HlaMetadataDictionary.Test/UnitTests/Models/ScoringInfoTests.cs
+++ Atlas.HlaMetadataDictionary.Test/UnitTests/Models/ScoringInfoTests.cs
@@ -59,10 +59,23 @@ namespace Atlas.HlaMetadataDictionary.Test.UnitTests.Models
             entity.SerialisedHlaInfoType.Should().Be("SingleAlleleScoringInfo");
             entity.SerialisedHlaInfo.Should().Contain(infoAsString);
 
-            infoAsString.Length.Should().BeLessThan(125);        // **This is really critical**. If you are exceeding this, you MUST do an update test and look at the payload size of A*02:01
-            infoAsString.Length.Should().Be(87);                 // This is just the current length. Adjusting this is fine, but you should be actively thinking about why you're doing it and whether it's necessary / safe.
+            // This maximum has been created using the expected number of single allele scoring info's serialised at 02:01. 
+            // (<single allele length> * <number of alleles> + <the rest of a serialised multiple allele hla>) must be less than the Azure storage max of 32,000 characters 
+            const int otherDataLength = 171;
+            const int azureMax = 32_000;
+            
+            // In practice, this number increases at A:02:01 with every nomenclature release.
+            // When the actual value of this number grows above the maximum in this test, the HMD will be rendered inoperable until the test can be fixed by shrinking the data size of each allele. 
+            // This shrinking process is finite - eventually we will reach a point where this test will always fail - and the code itself will need redesigning.
+            const int expectedMaximumSingleAlleles = 40;
+            const int maximumSerialisedLength = (azureMax-otherDataLength)/expectedMaximumSingleAlleles;
+            
+            infoAsString.Length.Should().BeLessThan(maximumSerialisedLength);        // **This is really critical**. If you are exceeding this, you MUST do an update test and look at the payload size of A*02:01
+            
+            // Snapshots
+            infoAsString.Length.Should().Be(78);                 // This is just the current length. Adjusting this is fine, but you should be actively thinking about why you're doing it and whether it's necessary / safe.
             infoAsString.Should().Be(                                   // This is just the current string. Adjusting this is fine, but you should be actively thinking about why you're doing it and whether it's necessary.
-                @"{""name"":""02:01"",""status"":{""seq"":1,""dna"":1},""pGrp"":""02:01P"",""gGrp"":""02:01:01G"",""ser"":[]}"
+                @"{""name"":""02:01"",""status"":{""seq"":1,""dna"":1},""pGrp"":""02:01P"",""gGrp"":""02:01:01G""}"
             );
         }
     }
