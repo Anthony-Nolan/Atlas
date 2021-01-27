4ef010c56d9c5378251d469aa545df1905f31429
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2021-01-27 12:44:38 +0000

    chore: ATLAS-843: Add index to management logs table to improve performance when querying by modified date.

---
+++ Atlas.MatchingAlgorithm.Data/Migrations/20210127124241_AddDateIndexToDonorManagementLogs.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Atlas.MatchingAlgorithm.Data.Migrations
+{
+    public partial class AddDateIndexToDonorManagementLogs : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_DonorManagementLogs_DonorId_LastUpdateDateTime",
+                table: "DonorManagementLogs",
+                columns: new[] { "DonorId", "LastUpdateDateTime" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_DonorManagementLogs_DonorId_LastUpdateDateTime",
+                table: "DonorManagementLogs");
+        }
+    }
+}
