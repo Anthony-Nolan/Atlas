8c5a7452be06e4d451ff4ec2fb88d2ef38855012
Tim O'Grady <Tim.OGrady@anthonynolan.org>
2021-02-03 14:33:56 +0000

    ATLAS-843: Add repeat search orchestration

---
+++ Atlas.Functions/DurableFunctions/RepeatSearch/RepeatSearchClientFunctions.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Atlas.Client.Models.Search.Results.Matching;
+using Atlas.Common.ApplicationInsights;
+using Atlas.MatchPrediction.ExternalInterface;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Newtonsoft.Json;
+
+namespace Atlas.Functions.DurableFunctions.RepeatSearch
+{
+    /// <summary>
+    /// See https://docs.microsoft.com/en-us/azure/azure-functions/durable/durable-functions-types-features-overview for the types of durable functions available
+    /// We are using:
+    ///     - Client = entry points.
+    ///     - Orchestrator = orchestration. DETERMINISTIC (https://docs.microsoft.com/en-us/azure/azure-functions/durable/durable-functions-code-constraints)
+    ///     - Activity = business logic. IDEMPOTENT (may be called multiple times, only at least once execution is guaranteed by Azure)
+    /// </summary>
+    public class RepeatSearchClientFunctions
+    {
+        private readonly ILogger logger;
+        private readonly IMatchPredictionAlgorithm matchPredictionAlgorithm;
+
+        public RepeatSearchClientFunctions(IMatchPredictionAlgorithm matchPredictionAlgorithm, ILogger logger)
+        {
+            this.matchPredictionAlgorithm = matchPredictionAlgorithm;
+            this.logger = logger;
+        }
+
+        [FunctionName(nameof(RepeatSearch))]
+        public async Task RepeatSearch(
+            [ServiceBusTrigger(
+                "%AtlasFunction:MessagingServiceBus:MatchingResultsTopic%",
+                "%AtlasFunction:MessagingServiceBus:MatchingResultsSubscription%",
+                Connection = "AtlasFunction:MessagingServiceBus:ConnectionString"
+            )]
+            MatchingResultsNotification resultsNotification,
+            [DurableClient] IDurableOrchestrationClient starter)
+        {
+            var searchId = resultsNotification.SearchRequestId;
+            if (!resultsNotification.SearchRequest?.RunMatchPrediction ?? false)
+            {
+                logger.SendTrace($"Match prediction for search request '{searchId}' was not requested, so will not be run.");
+                // TODO: ATLAS-493: Make the matching only results usable. 
+                return;
+            }
+
+            var instanceId = await starter.StartNewAsync(nameof(RepeatSearchOrchestrationFunctions.RepeatSearchOrchestrator), resultsNotification);
+
+            try
+            {
+                logger.SendTrace($"Started match prediction orchestration with ID = '{searchId}'. Orchestration Instance: {instanceId}");
+                var statusCheck = await GetStatusCheckEndpoints(starter, instanceId);
+                logger.SendTrace(statusCheck.StatusQueryGetUri);
+            }
+            catch (Exception)
+            {
+                // This function cannot be allowed to fail post-orchestration scheduling, as it would then retry, and we cannot schedule more than one orchestrator with the same id.
+                // We are only doing logging past this point, so if it fails we just swallow exceptions.
+            }
+        }
+
+        private static async Task<StatusCheckEndpoints> GetStatusCheckEndpoints(IDurableOrchestrationClient orchestrationClient, string instanceId)
+        {
+            // Log status check endpoints for convenience of debugging long search requests
+            var checkStatusResponse = orchestrationClient.CreateCheckStatusResponse(new HttpRequestMessage(), instanceId);
+            return JsonConvert.DeserializeObject<StatusCheckEndpoints>(await checkStatusResponse.Content.ReadAsStringAsync());
+        }
+
+        private class StatusCheckEndpoints
+        {
+            // ReSharper disable once UnusedAutoPropertyAccessor.Local
+            public string StatusQueryGetUri { get; set; }
+        }
+    }
+}
---
+++ Atlas.Functions/DurableFunctions/RepeatSearch/RepeatSearchOrchestrationFunctions.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Atlas.Client.Models.Search.Results.Matching;
+using Atlas.Common.ApplicationInsights;
+using Atlas.Common.Utils.Extensions;
+using Atlas.Functions.DurableFunctions.Search.Activity;
+using Atlas.Functions.Exceptions;
+using Atlas.Functions.Models;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+
+namespace Atlas.Functions.DurableFunctions.RepeatSearch
+{
+    /// <summary>
+    /// Note that orchestration triggered functions will be run multiple times per-request scope, so need to follow the code constraints
+    /// as documented by Microsoft https://docs.microsoft.com/en-us/azure/azure-functions/durable/durable-functions-code-constraints.
+    /// Logging should be avoided in this function due to this.
+    /// Any expensive or non-deterministic code should be called from an Activity function.
+    /// </summary>
+    // ReSharper disable once MemberCanBeInternal
+    // ReSharper disable once ClassNeverInstantiated.Global
+    public class RepeatSearchOrchestrationFunctions
+    {
+        private static readonly RetryOptions RetryOptions = new RetryOptions(TimeSpan.FromSeconds(5), 5) { BackoffCoefficient = 2 };
+
+        private readonly ILogger logger;
+
+        public RepeatSearchOrchestrationFunctions(ILogger logger)
+        {
+            this.logger = logger;
+        }
+
+        [FunctionName(nameof(RepeatSearchOrchestrator))]
+        public async Task<SearchOrchestrationOutput> RepeatSearchOrchestrator([OrchestrationTrigger] IDurableOrchestrationContext context)
+        {
+            var notification = context.GetInput<MatchingResultsNotification>();
+            var searchId = notification.SearchRequestId;
+
+            try
+            {
+                var orchestrationInitiated = context.CurrentUtcDateTime;
+                if (!notification.WasSuccessful)
+                {
+                    await SendFailureNotification(context, "Matching Algorithm", searchId);
+                    return null;
+                }
+
+                var matchPredictionRequestLocations = await PrepareMatchPrediction(context, notification);
+                var matchPredictionResultLocations = await RunMatchPredictionAlgorithm(context, searchId, matchPredictionRequestLocations);
+                await PersistSearchResults(context, new SearchActivityFunctions.PersistSearchResultsParameters
+                {
+                    SearchInitiated = orchestrationInitiated,
+                    MatchingResultsNotification = notification,
+                    MatchPredictionResultLocations = matchPredictionResultLocations
+                });
+
+                // "return" populates the "output" property on the status check GET endpoint set up by the durable functions framework
+                return new SearchOrchestrationOutput
+                {
+                    TotalSearchTime = context.CurrentUtcDateTime.Subtract(orchestrationInitiated),
+                    MatchingDonorCount = notification.NumberOfResults ?? -1,
+                };
+            }
+            catch (HandledOrchestrationException)
+            {
+                // Exceptions wrapper in "HandleOrchestrationException" have already been handled, and failure notifications sent.
+                // In this case we should just re-throw so the function is tracked as a failure.
+                throw;
+            }
+            catch (Exception e)
+            {
+                logger.SendTrace($"Failure during orchestration. Exception: {e.Message}, {e.InnerException?.Message}");
+                // An unexpected exception occurred in the *orchestration* code. Ensure we send a failure notification
+                await SendFailureNotification(context, "Orchestrator", searchId);
+                throw;
+            }
+        }
+
+        private async Task<TimedResultSet<IList<string>>> PrepareMatchPrediction(
+            IDurableOrchestrationContext context,
+            MatchingResultsNotification notification)
+        {
+            var batchIds = await RunStageAndHandleFailures(async () =>
+                    await context.CallActivityWithRetryAsync<TimedResultSet<IList<string>>>(
+                        nameof(SearchActivityFunctions.PrepareMatchPredictionBatches),
+                        RetryOptions,
+                        notification
+                    ),
+                context,
+                nameof(SearchActivityFunctions.PrepareMatchPredictionBatches),
+                notification.SearchRequestId
+            );
+
+            context.SetCustomStatus(new OrchestrationStatus
+            {
+                LastCompletedStage = "Prepare Match Prediction Batches",
+                ElapsedTimeOfStage = notification.ElapsedTime,
+            });
+
+            return batchIds;
+        }
+
+        private async Task<TimedResultSet<IReadOnlyDictionary<int, string>>> RunMatchPredictionAlgorithm(
+            IDurableOrchestrationContext context,
+            string searchId,
+            TimedResultSet<IList<string>> matchPredictionRequestLocations
+        )
+        {
+            var matchPredictionTasks = matchPredictionRequestLocations.ResultSet.Select(r => RunMatchPredictionForDonorBatch(context, r)).ToList();
+            var matchPredictionResultLocations = (await RunStageAndHandleFailures(
+                    async () => await Task.WhenAll(matchPredictionTasks),
+                    context,
+                    nameof(RunMatchPredictionAlgorithm),
+                    searchId
+                ))
+                .SelectMany(x => x)
+                .ToDictionary();
+
+
+            // We cannot use a stopwatch, as orchestration functions must be deterministic, and may be called multiple times.
+            // Results of activity functions are constant across multiple invocations, so we can trust that finished time of the previous stage will remain constant 
+            TimeSpan? totalElapsedTime = default;
+            if (matchPredictionRequestLocations.FinishedTimeUtc.HasValue)
+            {
+                var now = context.CurrentUtcDateTime;
+                totalElapsedTime = now.Subtract(matchPredictionRequestLocations.FinishedTimeUtc.Value);
+            }
+
+            context.SetCustomStatus(new OrchestrationStatus
+            {
+                LastCompletedStage = nameof(RunMatchPredictionAlgorithm),
+                ElapsedTimeOfStage = totalElapsedTime,
+            });
+
+            return new TimedResultSet<IReadOnlyDictionary<int, string>>
+            {
+                ResultSet = matchPredictionResultLocations,
+                // If the previous stage did not successfully report a timespan, we do not want to report an error - so we allow it to be null.
+                // TimedResultSet promises a non-null timestamp, so return MaxValue to be clear that this timing was not successful
+                ElapsedTime = totalElapsedTime ?? TimeSpan.MaxValue
+            };
+        }
+
+        /// <returns>A Task a list of locations in which MPA results (per donor) can be found.</returns>
+        private static async Task<IReadOnlyDictionary<int, string>> RunMatchPredictionForDonorBatch(
+            IDurableOrchestrationContext context,
+            string requestLocation
+        )
+        {
+            // Do not add error handling to this, as we will then see multiple failure notifications with multiple batches
+            return await context.CallActivityWithRetryAsync<IReadOnlyDictionary<int, string>>(
+                nameof(SearchActivityFunctions.RunMatchPredictionBatch),
+                RetryOptions,
+                requestLocation
+            );
+        }
+
+        private async Task PersistSearchResults(
+            IDurableOrchestrationContext context,
+            SearchActivityFunctions.PersistSearchResultsParameters persistSearchResultsParameters
+        )
+        {
+            await RunStageAndHandleFailures(
+                async () => await context.CallActivityWithRetryAsync(nameof(SearchActivityFunctions.PersistSearchResults), RetryOptions,
+                    persistSearchResultsParameters),
+                context,
+                nameof(PersistSearchResults),
+                persistSearchResultsParameters.MatchingResultsNotification.SearchRequestId
+            );
+
+            context.SetCustomStatus(new OrchestrationStatus
+            {
+                LastCompletedStage = nameof(PersistSearchResults),
+                ElapsedTimeOfStage = null,
+            });
+        }
+
+        private async Task RunStageAndHandleFailures(
+            Func<Task> runStage,
+            IDurableOrchestrationContext context,
+            string stageName,
+            string searchId) =>
+            await RunStageAndHandleFailures(async () =>
+            {
+                await runStage();
+                return true;
+            }, context, stageName, searchId);
+
+        private async Task<T> RunStageAndHandleFailures<T>(
+            Func<Task<T>> runStage,
+            IDurableOrchestrationContext context,
+            string stageName,
+            string searchId)
+        {
+            try
+            {
+                return await runStage();
+            }
+            catch (Exception e)
+            {
+                logger.SendTrace($"Failure at stage: {stageName}. Exception: {e.Message}, {e.InnerException?.Message}");
+                await SendFailureNotification(context, stageName, searchId);
+                throw new HandledOrchestrationException(e);
+            }
+        }
+
+        private static async Task SendFailureNotification(IDurableOrchestrationContext context, string failedStage, string searchId)
+        {
+            await context.CallActivityWithRetryAsync(
+                nameof(SearchActivityFunctions.SendFailureNotification),
+                RetryOptions,
+                (searchId, failedStage)
+            );
+
+            context.SetCustomStatus($"Repeat Search failed, during stage: {failedStage}");
+        }
+    }
+}
