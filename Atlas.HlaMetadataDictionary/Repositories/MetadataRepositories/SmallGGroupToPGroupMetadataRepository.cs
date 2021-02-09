b32029566ce23afcd27174e75309eef254b8ae73
Ben Below <benjamin.below@softwire.com>
2021-02-09 16:06:08 +0000

    feature: ATLAS-881: Add validation pathway for small-g typed HLA data

--- Atlas.HlaMetadataDictionary/Repositories/MetadataRepositories/SmallGGroupToPGroupMetadataRepository.cs
+++ Atlas.HlaMetadataDictionary/Repositories/MetadataRepositories/SmallGGroupToPGroupMetadataRepository.cs
@@ -1,15 +1,19 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Atlas.Common.Caching;
 using Atlas.Common.GeneticData;
 using Atlas.Common.GeneticData.Hla.Models;
 using Atlas.HlaMetadataDictionary.Repositories.AzureStorage;
 using Atlas.Common.ApplicationInsights;
+using Atlas.Common.ApplicationInsights.Timing;
 using Atlas.HlaMetadataDictionary.InternalModels.Metadata;
 
 namespace Atlas.HlaMetadataDictionary.Repositories.MetadataRepositories
 {
     internal interface ISmallGGroupToPGroupMetadataRepository : IHlaMetadataRepository
     {
+        Task<IDictionary<Locus, ISet<string>>> GetAllSmallGGroups(string hlaNomenclatureVersion);
         Task<IMolecularTypingToPGroupMetadata> GetPGroupBySmallGGroupIfExists(Locus locus, string smallGGroup, string hlaNomenclatureVersion);
     }
 
@@ -27,13 +31,39 @@ namespace Atlas.HlaMetadataDictionary.Repositories.MetadataRepositories
         {
         }
 
-        public async Task<IMolecularTypingToPGroupMetadata> GetPGroupBySmallGGroupIfExists(Locus locus, string smallGGroup, string hlaNomenclatureVersion)
+        public async Task<IMolecularTypingToPGroupMetadata> GetPGroupBySmallGGroupIfExists(
+            Locus locus,
+            string smallGGroup,
+            string hlaNomenclatureVersion)
         {
             var row = await GetHlaMetadataRowIfExists(locus, smallGGroup, TypingMethod.Molecular, hlaNomenclatureVersion);
 
             return row == null
                 ? null
                 : new MolecularTypingToPGroupMetadata(row.Locus, row.LookupName, row.GetHlaInfo<string>());
+        }
+        
+        public async Task<IDictionary<Locus, ISet<string>>> GetAllSmallGGroups(string hlaNomenclatureVersion)
+        {
+            return await Cache.GetOrAddAsync(
+                $"All-small-g-Groups:{hlaNomenclatureVersion}",
+                async _ => await CalculateAllSmallGGroups(hlaNomenclatureVersion)
+            );
+        }
+
+        private async Task<IDictionary<Locus, ISet<string>>> CalculateAllSmallGGroups(string hlaNomenclatureVersion)
+        {
+            var tableData = await TableData(hlaNomenclatureVersion);
+            using (Logger.RunTimed("Calculate all small g-groups"))
+            {
+                var byLocus = tableData.Values.GroupBy(v => v.Locus);
+                return byLocus.ToDictionary(
+                    g => g.Key,
+                    g => g.Select(row => row.LookupName)
+                        .Where(x => x != null)
+                        .ToHashSet() as ISet<string>
+                );
+            }
         }
     }
 }
--- Atlas.HlaMetadataDictionary/Repositories/MetadataRepositories/SmallGGroupsMetadataRepository.cs
+++ Atlas.HlaMetadataDictionary/Repositories/MetadataRepositories/SmallGGroupsMetadataRepository.cs
@@ -4,6 +4,11 @@ using Atlas.HlaMetadataDictionary.Repositories.AzureStorage;
 
 namespace Atlas.HlaMetadataDictionary.Repositories.MetadataRepositories
 {
+    /// <summary>
+    /// <see cref="ISmallGGroupToPGroupMetadataRepository.GetAllSmallGGroups"/> for ability to fetch all small g groups - while that
+    /// functionality might be expected to be found here, this repository's data is structured based on efficient "hlaName:g-group" lookups,
+    /// and using the "g-group:p-group" lookup repository is a much more efficient way of enumerating all small g groups.
+    /// </summary>
     internal interface ISmallGGroupsMetadataRepository : IHlaMetadataRepository
     {
     }
