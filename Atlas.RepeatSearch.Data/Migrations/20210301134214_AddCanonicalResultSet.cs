07186103b6489d655dc9d0c4b1e53e5dba83da60
Ben Below <benjamin.below@softwire.com>
2021-03-01 13:46:30 +0000

    chore: ATLAS-939: Add canonical result set to database schema

---
+++ Atlas.RepeatSearch.Data/Migrations/20210301134214_AddCanonicalResultSet.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Atlas.RepeatSearch.Data.Migrations
+{
+    public partial class AddCanonicalResultSet : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "RepeatSearch");
+
+            migrationBuilder.CreateTable(
+                name: "CanonicalResultSets",
+                schema: "RepeatSearch",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OriginalSearchRequestId = table.Column<string>(maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CanonicalResultSets", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SearchResult",
+                schema: "RepeatSearch",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AtlasDonorId = table.Column<int>(nullable: false),
+                    CanonicalResultSetId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SearchResult", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SearchResult_CanonicalResultSets_CanonicalResultSetId",
+                        column: x => x.CanonicalResultSetId,
+                        principalSchema: "RepeatSearch",
+                        principalTable: "CanonicalResultSets",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CanonicalResultSets_OriginalSearchRequestId",
+                schema: "RepeatSearch",
+                table: "CanonicalResultSets",
+                column: "OriginalSearchRequestId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SearchResult_AtlasDonorId",
+                schema: "RepeatSearch",
+                table: "SearchResult",
+                column: "AtlasDonorId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SearchResult_CanonicalResultSetId",
+                schema: "RepeatSearch",
+                table: "SearchResult",
+                column: "CanonicalResultSetId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SearchResult",
+                schema: "RepeatSearch");
+
+            migrationBuilder.DropTable(
+                name: "CanonicalResultSets",
+                schema: "RepeatSearch");
+        }
+    }
+}
---
+++ Atlas.RepeatSearch.Data/Models/CanonicalResultSet.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Atlas.RepeatSearch.Data.Context;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Atlas.RepeatSearch.Data.Models
+{
+    /// <summary>
+    /// Represents the latest "canonical" result set for a given search request Id.
+    /// Each repeat search run returns the diff between the previous canonical set and the latest one.
+    /// The initial canonical result set is the original search result set. 
+    /// </summary>
+    [Table(TableName)]
+    public class CanonicalResultSet
+    {
+        private const string TableName = "CanonicalResultSets";
+        internal static readonly string QualifiedTableName = $"{RepeatSearchContext.Schema}.{TableName}";
+        
+        public int Id { get; set; }
+        
+        [MaxLength(200)]
+        [Required]
+        public string OriginalSearchRequestId { get; set; }
+        
+        /// <summary>
+        /// Atlas Ids (NOT external donor codes) of donors in the canonical result set.
+        /// </summary>
+        public ICollection<SearchResult> SearchResults { get; set; }
+    }
+
+    internal static class CanonicalResultSetBuilder
+    {
+        public static void SetUpResultSetModel(this EntityTypeBuilder<CanonicalResultSet> resultSet)
+        {
+            resultSet.HasIndex(s => s.OriginalSearchRequestId).IsUnique();
+        }
+    }
+}
