ee21a72fc73a12e821837269a5f4ec9ca00a8b34
Ben Below <benjamin.below@softwire.com>
2021-03-01 16:37:14 +0000

    feature: ATLAS-939: Store history records of repeat search requests

---
+++ Atlas.RepeatSearch.Data/Migrations/20210301162950_AddRepeatSearchHistory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Atlas.RepeatSearch.Data.Migrations
+{
+    public partial class AddRepeatSearchHistory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RepeatSearchHistoryRecords",
+                schema: "RepeatSearch",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OriginalSearchRequestId = table.Column<string>(maxLength: 200, nullable: false),
+                    RepeatSearchRequestId = table.Column<string>(maxLength: 200, nullable: false),
+                    SearchCutoffDate = table.Column<DateTimeOffset>(nullable: false),
+                    DateCreated = table.Column<DateTimeOffset>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RepeatSearchHistoryRecords", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RepeatSearchHistoryRecords",
+                schema: "RepeatSearch");
+        }
+    }
+}
---
+++ Atlas.RepeatSearch.Data/Models/RepeatSearchHistoryRecord.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Atlas.RepeatSearch.Data.Models
+{
+    [Table(TableName)]
+    public class RepeatSearchHistoryRecord
+    {
+        private const string TableName = "RepeatSearchHistoryRecords";
+        
+        public int Id { get; set; }
+        
+        [MaxLength(200)]
+        [Required]
+        public string OriginalSearchRequestId { get; set; }
+        
+        [MaxLength(200)]
+        [Required]
+        public string RepeatSearchRequestId { get; set; }
+        
+        public DateTimeOffset SearchCutoffDate { get; set; }
+        
+        public DateTimeOffset DateCreated { get; set; } 
+    }
+}
