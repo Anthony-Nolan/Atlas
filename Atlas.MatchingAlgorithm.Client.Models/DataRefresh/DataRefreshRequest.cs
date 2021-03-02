e4512a22744c5c098880e6be4554e4e3c8c1d711
Ben Below <benjamin.below@softwire.com>
2021-03-02 11:49:39 +0000

    docs: ATLAS-941: Add documentation on how to trigger refresh manually

--- Atlas.MatchingAlgorithm.Client.Models/DataRefresh/DataRefreshRequest.cs
+++ Atlas.MatchingAlgorithm.Client.Models/DataRefresh/DataRefreshRequest.cs
@@ -4,6 +4,7 @@
     {
         /// <summary>
         /// If true, the refresh will occur regardless of whether a new HLA Nomenclature version has been published.
+        /// Otherwise, the refresh will only run if a new nomenclature version is detected.
         /// </summary>
         public bool ForceDataRefresh { get; set; }
     }
