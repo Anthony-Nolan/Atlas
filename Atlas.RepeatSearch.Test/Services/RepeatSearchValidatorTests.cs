5b383765ff9c33fa77d11029202b856a2c25c3c5
Ben Below <benjamin.below@softwire.com>
2021-03-05 13:08:56 +0000

    feature: ATLAS-947: Add validation that repeat search request id has had canonical results stored before running

---
+++ Atlas.RepeatSearch.Test/Services/RepeatSearchValidatorTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Threading.Tasks;
+using Atlas.Client.Models.Search.Requests;
+using Atlas.MatchingAlgorithm.Test.TestHelpers.Builders.SearchRequests;
+using Atlas.RepeatSearch.Data.Models;
+using Atlas.RepeatSearch.Data.Repositories;
+using Atlas.RepeatSearch.Services.Search;
+using FluentAssertions;
+using FluentValidation;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Atlas.RepeatSearch.Test.Services
+{
+    [TestFixture]
+    public class RepeatSearchValidatorTests
+    {
+        private ICanonicalResultSetRepository canonicalResultSetRepository;
+
+        private IRepeatSearchValidator repeatSearchValidator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            canonicalResultSetRepository = Substitute.For<ICanonicalResultSetRepository>();
+
+            canonicalResultSetRepository.GetCanonicalResultSetSummary(default).ReturnsForAnyArgs(new CanonicalResultSet());
+            
+            repeatSearchValidator = new RepeatSearchValidator(canonicalResultSetRepository);
+        }
+
+        [Test]
+        public async Task ValidateRepeatSearchRequest_WhenRequiredDataMissing_ThrowsException()
+        {
+            var repeatSearch = new RepeatSearchRequest();
+
+            await repeatSearchValidator
+                .Invoking(v => v.ValidateRepeatSearchAndThrow(repeatSearch))
+                .Should().ThrowAsync<ValidationException>();
+        }
+
+        [Test]
+        public async Task ValidateRepeatSearchRequest_ForValidRequest_DoesNotThrowException()
+        {
+            var repeatSearch = new RepeatSearchRequest
+            {
+                SearchRequest = new SearchRequestBuilder().Build(),
+                OriginalSearchId = "123",
+                SearchCutoffDate = DateTimeOffset.Now
+            };
+
+            await repeatSearchValidator
+                .Invoking(async v => await v.ValidateRepeatSearchAndThrow(repeatSearch))
+                .Should().NotThrowAsync();
+        }
+
+        [Test]
+        public async Task ValidateRepeatSearchRequest_WithUnrecognisedSearchId_ThrowsException()
+        {
+            const string searchId = "invalid-search-request";
+            
+            var repeatSearch = new RepeatSearchRequest
+            {
+                SearchRequest = new SearchRequestBuilder().Build(),
+                OriginalSearchId = searchId,
+                SearchCutoffDate = DateTimeOffset.Now
+            };
+
+            canonicalResultSetRepository.GetCanonicalResultSetSummary(searchId).Returns(null as CanonicalResultSet);
+
+            await repeatSearchValidator
+                .Invoking(v => v.ValidateRepeatSearchAndThrow(repeatSearch))
+                .Should().ThrowAsync<ValidationException>();
+        }
+    }
+}
---
+++ Atlas.RepeatSearch/Services/Search/RepeatSearchValidator.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks;
+using Atlas.Client.Models.Search.Requests;
+using Atlas.RepeatSearch.Data.Repositories;
+using Atlas.RepeatSearch.Validators;
+using FluentValidation;
+
+namespace Atlas.RepeatSearch.Services.Search
+{
+    public interface IRepeatSearchValidator
+    {
+        /// <summary>
+        /// Will throw a validation exception if the provided request is not valid.
+        /// </summary>
+        public Task ValidateRepeatSearchAndThrow(RepeatSearchRequest repeatSearchRequest);
+    }
+
+    internal class RepeatSearchValidator : IRepeatSearchValidator
+    {
+        private readonly ICanonicalResultSetRepository canonicalResultSetRepository;
+
+        public RepeatSearchValidator(ICanonicalResultSetRepository canonicalResultSetRepository)
+        {
+            this.canonicalResultSetRepository = canonicalResultSetRepository;
+        }
+
+        public async Task ValidateRepeatSearchAndThrow(RepeatSearchRequest repeatSearchRequest)
+        {
+            var validator = new RepeatSearchRequestValidator();
+            await validator.ValidateAndThrowAsync(repeatSearchRequest);
+
+            var canonicalSet = await canonicalResultSetRepository.GetCanonicalResultSetSummary(repeatSearchRequest.OriginalSearchId);
+
+            if (canonicalSet == null)
+            {
+                throw new ValidationException(
+                    $"Can not run a repeat search for search request with Id: {repeatSearchRequest.OriginalSearchId}, as there is no record of the original search.");
+            }
+        }
+    }
+}
