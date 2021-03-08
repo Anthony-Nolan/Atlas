a166825f3e63af14b246d0f17e55a9a99a6c22d0
Ben Below <benjamin.below@softwire.com>
2021-03-08 11:40:45 +0000

    feature: ATLAS-861: Store external donor codes (not Atlas donor IDs) in repeat search store

--- Atlas.RepeatSearch.Data/Migrations/20210308112034_StoreExternalDonorCodeNotAtlasId.Designer.cs
+++ Atlas.RepeatSearch.Data/Migrations/20210308112034_StoreExternalDonorCodeNotAtlasId.Designer.cs
@@ -4,14 +4,16 @@ using Atlas.RepeatSearch.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Atlas.RepeatSearch.Data.Migrations
 {
     [DbContext(typeof(RepeatSearchContext))]
-    partial class RepeatSearchContextModelSnapshot : ModelSnapshot
+    [Migration("20210308112034_StoreExternalDonorCodeNotAtlasId")]
+    partial class StoreExternalDonorCodeNotAtlasId
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
