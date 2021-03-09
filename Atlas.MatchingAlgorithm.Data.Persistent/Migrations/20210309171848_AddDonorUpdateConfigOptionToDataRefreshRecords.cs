0b4bcdca571b0e63ece38ff38971ffeb6da1360d
Ben Below <benjamin.below@softwire.com>
2021-03-09 17:56:24 +0000

    feature: ATLAS-908: Treat data refresh as in need of amrking all donor records as updated if HLA nomenclature has changed

---
+++ Atlas.MatchingAlgorithm.Data.Persistent/Migrations/20210309171848_AddDonorUpdateConfigOptionToDataRefreshRecords.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Atlas.MatchingAlgorithm.Data.Persistent.Migrations
+{
+    public partial class AddDonorUpdateConfigOptionToDataRefreshRecords : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "ShouldMarkAllDonorsAsUpdated",
+                schema: "MatchingAlgorithmPersistent",
+                table: "DataRefreshHistory",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ShouldMarkAllDonorsAsUpdated",
+                schema: "MatchingAlgorithmPersistent",
+                table: "DataRefreshHistory");
+        }
+    }
+}
