0a124f1cc123131fb15323cb75371cde73482e5c
Ben Below <benjamin.below@softwire.com>
2021-03-10 11:18:50 +0000

    feature: ATLAS-908: Mark donors as updated during data refresh

--- Atlas.MatchingAlgorithm.Data/Models/DonorManagementInfo.cs
+++ Atlas.MatchingAlgorithm.Data/Models/DonorManagementInfo.cs
@@ -5,6 +5,8 @@ namespace Atlas.MatchingAlgorithm.Data.Models
     public class DonorManagementInfo
     {
         public int DonorId { get; set; }
+        
+        // TODO: ATLAS-972: Confirm this is unused and remove
         public long UpdateSequenceNumber { get; set; }
         public DateTimeOffset UpdateDateTime { get; set; }
     }
