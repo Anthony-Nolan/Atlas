4a5bb1a8881f4e639144ad17aea8fcbd0de5dc25
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2021-03-25 12:01:46 +0000

    refactor: ATLAS-838: Further commonise ResultsNotification.

--- Atlas.Client.Models/Search/Results/Matching/MatchingResultsNotification.cs
+++ Atlas.Client.Models/Search/Results/Matching/MatchingResultsNotification.cs
@@ -13,22 +13,12 @@ namespace Atlas.Client.Models.Search.Results.Matching
         /// </summary>
         public SearchRequest SearchRequest { get; set; }
 
-        /// <summary>
-        ///     In the case of an original search this ID will be null
-        /// </summary>
-        public string RepeatSearchRequestId { get; set; }
-
         public bool IsRepeatSearch => RepeatSearchRequestId != null;
 
         /// <summary>
         ///     The version of the deployed matching algorithm that ran the search request
         /// </summary>
         public string MatchingAlgorithmServiceVersion { get; set; }
-
-        /// <summary>
-        ///     The version of the HLA Nomenclature used to run the search request - used for analysing both donor and patient hla.
-        /// </summary>
-        public string HlaNomenclatureVersion { get; set; }
 
         public TimeSpan ElapsedTime { get; set; }
     }
