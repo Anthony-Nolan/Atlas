cd6c280e2840341eedc53f898f978e80a47c7a41
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2021-07-29 14:42:57 +0100

    tests: #636: Ensure HF set nomenclature versions are used during phenotype expansion and genotype conversion steps.

---
+++ Atlas.MatchPrediction/Services/MatchProbability/GenotypeConverter.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Atlas.Common.ApplicationInsights;
+using Atlas.Common.ApplicationInsights.Timing;
+using Atlas.HlaMetadataDictionary.ExternalInterface;
+using Atlas.MatchPrediction.ApplicationInsights;
+using Atlas.MatchPrediction.ExternalInterface.Models;
+using TypedGenotype = Atlas.Common.GeneticData.PhenotypeInfo.PhenotypeInfo<Atlas.MatchPrediction.ExternalInterface.Models.HlaAtKnownTypingCategory>;
+using StringGenotype = Atlas.Common.GeneticData.PhenotypeInfo.PhenotypeInfo<string>;
+
+namespace Atlas.MatchPrediction.Services.MatchProbability
+{
+    internal interface IGenotypeConverter
+    {
+        Task<ICollection<GenotypeAtDesiredResolutions>> ConvertGenotypes(
+            ISet<TypedGenotype> genotypes,
+            string subjectLogDescription,
+            IReadOnlyDictionary<StringGenotype, decimal> genotypeLikelihoods,
+            string hlaNomenclatureVersion);
+    }
+
+    internal class GenotypeConverter : IGenotypeConverter
+    {
+        private readonly ILogger logger;
+        private readonly IHlaMetadataDictionaryFactory hlaMetadataDictionaryFactory;
+
+        public GenotypeConverter(
+            // ReSharper disable once SuggestBaseTypeForParameter
+            IMatchPredictionLogger logger,
+            IHlaMetadataDictionaryFactory hlaMetadataDictionaryFactory)
+        {
+            this.logger = logger;
+            this.hlaMetadataDictionaryFactory = hlaMetadataDictionaryFactory;
+        }
+        
+        public async Task<ICollection<GenotypeAtDesiredResolutions>> ConvertGenotypes(
+            ISet<TypedGenotype> genotypes,
+            string subjectLogDescription,
+            IReadOnlyDictionary<StringGenotype, decimal> genotypeLikelihoods,
+            string hlaNomenclatureVersion)
+        {
+            var hlaMetadataDictionary = hlaMetadataDictionaryFactory.BuildDictionary(hlaNomenclatureVersion);
+
+            using (logger.RunTimed($"Convert genotypes for matching: {subjectLogDescription}", LogLevel.Verbose))
+            {
+                return (await Task.WhenAll(genotypes.Select(async g => await GenotypeAtDesiredResolutions.FromHaplotypeResolutions(
+                    g,
+                    hlaMetadataDictionary,
+                    genotypeLikelihoods[g.ToHlaNames()]
+                )))).ToList();
+            }
+        }
+    }
+}
