54565fd6fa00264213c9bac3fec8e7738f3ea6f6
JesseJongejanWMDA <jesse.jongejan@wmda.info>
2021-12-10 15:25:03 +0100

    Return value in LocusSearchResult

--- Atlas.Client.Models/Search/Results/Matching/PerLocus/LocusSearchResult.cs
+++ Atlas.Client.Models/Search/Results/Matching/PerLocus/LocusSearchResult.cs
@@ -49,5 +49,13 @@
         ///     If scoring has not been requested on the locus, then the value will be null.
         /// </summary>
         public LocusPositionScoreDetails ScoreDetailsAtPositionTwo { get; set; }
+        
+        /// <summary>
+        /// Indicates the direction of the DPB1 mismatch, when there is a DPB1 mismatch.
+        /// When the mismatch is permissive or there is no DPB1 mismatch, NotApplicable will be returned.
+        /// When the direction could not be calculated or the locus is non-DPB1, null will be returned.
+        /// </summary>
+        public Dpb1MismatchDirection? Dpb1MismatchDirection { get; set; }
+
     }
 }
