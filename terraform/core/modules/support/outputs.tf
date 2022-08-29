5ca09c90f628ec55cb2a250d74831b686b00e061
Daria Sorokina <daria.sorokina@dataart.com>
2022-08-29 14:34:31 +0400

    chore: NOVA-9281: Added ids of support topics to outputs

--- terraform/core/modules/support/outputs.tf
+++ terraform/core/modules/support/outputs.tf
@@ -1,6 +1,8 @@
 output "general" {
   value = {
-    alerts_servicebus_topic        = azurerm_servicebus_topic.alerts
-    notifications_servicebus_topic = azurerm_servicebus_topic.notifications
+    alerts_servicebus_topic_id        = azurerm_servicebus_topic.alerts.id
+    alerts_servicebus_topic           = azurerm_servicebus_topic.alerts
+    notifications_servicebus_topic_id = azurerm_servicebus_topic.notifications.id
+    notifications_servicebus_topic    = azurerm_servicebus_topic.notifications
   }
 }
