ea32ac19577c7043af418f0f2b7db63bda5804c9
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2022-09-27 11:46:01 +0100

    review: #805: Decision to suppress conversion errors during match prediction should not be configurable during release.
    It is important that these errors are always suppressed during search requests and not suppressed during match prediction requests - independent of release environment. Therefore it is valid to hard-code the settings values into the function definitions within the terraform scripts.

--- terraform/core/variables.tf
+++ terraform/core/variables.tf
@@ -135,12 +135,6 @@ variable "MATCH_PREDICTION_DATABASE_USERNAME" {
   default = "match_prediction"
 }
 
-variable "MATCH_PREDICTION_SUPPRESS_COMPRESSED_PHENOTYPE_CONVERSION_EXCEPTIONS" {
-  type        = bool
-  default     = false
-  description = "Compressed phenotype conversion exceptions should NOT be suppressed when running match prediction requests outside of search."
-}
-
 variable "MATCHING_BATCH_SIZE" {
   type        = number
   default     = 250000
@@ -305,12 +299,6 @@ variable "REPEAT_SEARCH_MATCHING_MAX_SCALE_OUT" {
   type        = number
   default     = 1
   description = "The maximum number of instances of the repeat search's matching algorithm that can be scaled out."
-}
-
-variable "SEARCH_SUPPRESS_COMPRESSED_PHENOTYPE_CONVERSION_EXCEPTIONS" {
-  type        = bool
-  default     = true
-  description = "Compressed phenotype conversion exceptions should be suppressed when running match prediction as part of search."
 }
 
 variable "SERVICE_PLAN_MAX_SCALE_OUT" {
