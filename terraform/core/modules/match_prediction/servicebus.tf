d242b6cb4681ffd6d4ddd170f0d62ec41c13c837
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2022-09-29 09:52:06 +0100

    fix: #805: Remove audit subscriptions from request and result topics as topic getting too large & refusing more messages.

--- terraform/core/modules/match_prediction/servicebus.tf
+++ terraform/core/modules/match_prediction/servicebus.tf
@@ -42,18 +42,6 @@ resource "azurerm_servicebus_topic" "match-prediction-requests" {
   support_ordering      = true
 }
 
-resource "azurerm_servicebus_subscription" "audit-match-prediction-request" {
-  name                                 = "audit"
-  resource_group_name                  = var.app_service_plan.resource_group_name
-  namespace_name                       = var.servicebus_namespace.name
-  topic_name                           = azurerm_servicebus_topic.match-prediction-requests.name
-  auto_delete_on_idle                  = var.default_servicebus_settings.long-expiry
-  default_message_ttl                  = var.default_servicebus_settings.long-expiry
-  lock_duration                        = var.default_servicebus_settings.default-read-lock
-  max_delivery_count                   = var.default_servicebus_settings.default-message-retries
-  dead_lettering_on_message_expiration = false
-}
-
 resource "azurerm_servicebus_subscription" "match-prediction-request-runner" {
   name                                 = "match-prediction"
   resource_group_name                  = var.app_service_plan.resource_group_name
@@ -74,16 +62,4 @@ resource "azurerm_servicebus_topic" "match-prediction-results" {
   default_message_ttl   = var.default_servicebus_settings.long-expiry
   max_size_in_megabytes = var.default_servicebus_settings.default-bus-size
   support_ordering      = true
-}
-
-resource "azurerm_servicebus_subscription" "audit-match-prediction-results" {
-  name                                 = "audit"
-  resource_group_name                  = var.app_service_plan.resource_group_name
-  namespace_name                       = var.servicebus_namespace.name
-  topic_name                           = azurerm_servicebus_topic.match-prediction-results.name
-  auto_delete_on_idle                  = var.default_servicebus_settings.long-expiry
-  default_message_ttl                  = var.default_servicebus_settings.long-expiry
-  lock_duration                        = var.default_servicebus_settings.default-read-lock
-  max_delivery_count                   = var.default_servicebus_settings.default-message-retries
-  dead_lettering_on_message_expiration = false
 }
