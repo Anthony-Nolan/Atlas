56c72912fbabdc3875b7d651b863f53561f2124a
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2022-10-11 13:35:20 +0100

    chore: Identify which donors are missing from the active matching database.

---
+++ Atlas.ManualTesting/Models/MissingDonor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Atlas.ManualTesting.Models
+{
+    public class MissingDonors
+    {
+        public int Count { get; set; }
+        public IEnumerable<PropertyCount> DonorTypeCounts { get; set; }
+        public IEnumerable<PropertyCount> RegistryCounts { get; set; }
+        public IEnumerable<PropertyCount> UpdateFileCounts { get; set; }
+        public IEnumerable<PropertyCount> LastUpdatedCounts { get; set; }
+        public IEnumerable<MissingDonor> Donors { get; set; }
+    }
+
+    public class PropertyCount
+    {
+        public string Value { get; set; }
+        public int Count { get; set; }
+    }
+
+    public class MissingDonor
+    {
+        public int AtlasId { get; set; }
+        public string ExternalDonorCode { get; set; }
+        public string DonorType { get; set; }
+        public string RegistryCode { get; set; }
+        public string UpdateFile { get; set; }
+        public DateTimeOffset LastUpdated { get; set; }
+    }
+
+    public static class MissingDonorExtensions
+    {
+        public static IEnumerable<PropertyCount> GroupDonorsBy(this IEnumerable<MissingDonor> missingDonors, Func<MissingDonor, string> keySelector)
+        {
+            return missingDonors.GroupBy(keySelector).Select(grp => new PropertyCount { Count = grp.Count(), Value = grp.Key });
+        }
+    }
+}
---
+++ Atlas.ManualTesting/Services/DonorStoresInspector.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Atlas.DonorImport.Data.Models;
+using Atlas.DonorImport.Data.Repositories;
+using Atlas.ManualTesting.Models;
+using Atlas.MatchingAlgorithm.Data.Repositories.DonorRetrieval;
+using MoreLinq.Extensions;
+
+namespace Atlas.ManualTesting.Services
+{
+    public interface IDonorStoresInspector
+    {
+        /// <summary>
+        /// Checks the active matching database for missing donors against the donor import database.
+        /// </summary>
+        /// <returns></returns>
+        Task<MissingDonors> GetDonorsMissingFromActiveMatchingDatabase();
+    }
+
+    internal class DonorStoresInspector : IDonorStoresInspector
+    {
+        private readonly IDonorReadRepository donorImportRepository;
+        private readonly IDonorInspectionRepository activeMatchingRepository;
+        private const int BatchSize = 10000;
+
+        public DonorStoresInspector(
+            IDonorReadRepository donorImportRepository,
+            // ReSharper disable once SuggestBaseTypeForParameterInConstructor
+            IActiveMatchingDatabaseConnectionStringProvider connectionStringProvider)
+        {
+            this.donorImportRepository = donorImportRepository;
+            activeMatchingRepository = new DonorInspectionRepository(connectionStringProvider);
+        }
+
+        /// <inheritdoc />
+        public async Task<MissingDonors> GetDonorsMissingFromActiveMatchingDatabase()
+        {
+            var missingDonors = new List<Donor>();
+
+            var donorsStream = donorImportRepository.StreamAllDonors();
+            foreach (var streamedDonorBatch in donorsStream.Batch(BatchSize))
+            {
+                var donorImportDonors = streamedDonorBatch.ToList();
+                var matchingDatabaseDonors = (await GetExistingDonorIdsFromActiveMatchingDatabase(donorImportDonors.Select(d => d.AtlasId))).ToList();
+
+                var missing = donorImportDonors.Where(d => !matchingDatabaseDonors.Contains(d.AtlasId)).ToList();
+                if (!missing.Any())
+                {
+                    continue;
+                }
+
+                missingDonors.AddRange(missing);
+            }
+
+            return BuildMissingDonors(missingDonors);
+        }
+
+        private async Task<IEnumerable<int>> GetExistingDonorIdsFromActiveMatchingDatabase(IEnumerable<int> donors)
+        {
+            var existingDonors = await activeMatchingRepository.GetDonors(donors);
+            return existingDonors.Keys;
+        }
+
+        private static MissingDonors BuildMissingDonors(IEnumerable<Donor> donors)
+        {
+            var missingDonors = donors.Select(BuildMissingDonor).ToList();
+
+            return new MissingDonors
+            {
+                Count = missingDonors.Count,
+                Donors = missingDonors,
+                DonorTypeCounts = missingDonors.GroupDonorsBy(d => d.DonorType),
+                RegistryCounts = missingDonors.GroupDonorsBy(d => d.RegistryCode),
+                UpdateFileCounts = missingDonors.GroupDonorsBy(d => d.UpdateFile),
+                LastUpdatedCounts = missingDonors.GroupDonorsBy(d => d.LastUpdated.ToString("yyyy/MM/dd"))
+            };
+        }
+
+        private static MissingDonor BuildMissingDonor(Donor d)
+        {
+            return new MissingDonor
+            {
+                AtlasId = d.AtlasId,
+                ExternalDonorCode = d.ExternalDonorCode,
+                DonorType = d.DonorType.ToString(),
+                RegistryCode = d.RegistryCode,
+                UpdateFile = d.UpdateFile,
+                LastUpdated = d.LastUpdated
+            };
+        }
+
+
+    }
+}
