04c47045f9a313ccdd206b89e043cc9ad1e246ce
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2022-11-16 18:43:26 +0000

    fix: #833: Assign predictive match categories using percentage probability value instead of decimal.

---
+++ Atlas.MatchPrediction.Test/Models/MatchProbabilitiesTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using Atlas.Client.Models.Search.Results.MatchPrediction;
+using Atlas.MatchPrediction.Test.TestHelpers.Builders;
+using FluentAssertions;
+
+namespace Atlas.MatchPrediction.Test.Models
+{
+    [TestFixture]
+    internal class MatchProbabilitiesTests
+    {
+        [TestCase(1, PredictiveMatchCategory.Exact)]
+        [TestCase(0.995, PredictiveMatchCategory.Exact, Description = "Zero mismatch percentage would round to 100%")]
+        [TestCase(0, PredictiveMatchCategory.Mismatch)]
+        [TestCase(0.0049, PredictiveMatchCategory.Mismatch, Description = "Zero mismatch percentage would round to 0%")]
+        [TestCase(0.6, PredictiveMatchCategory.Potential)]
+        public void MatchCategory_ReturnsExpectedMatchCategory(decimal zeroMismatchValue, PredictiveMatchCategory? expectedCategory)
+        {
+            var matchProbability = MatchProbabilitiesBuilder.New.WithZeroMismatchProbability(zeroMismatchValue).Build();
+
+            matchProbability.MatchCategory.Should().Be(expectedCategory);
+        }
+    }
+}
--- Atlas.MatchPrediction.Test/Models/MatchProbabilityPerLocusResponseTests.cs
+++ Atlas.MatchPrediction.Test/Models/MatchProbabilityPerLocusResponseTests.cs
@@ -1,6 +1,5 @@
 ﻿using Atlas.Client.Models.Search.Results.MatchPrediction;
 using Atlas.Common.GeneticData.PhenotypeInfo;
-using Atlas.MatchPrediction.ExternalInterface.Models.MatchProbability;
 using Atlas.MatchPrediction.Test.TestHelpers.Builders;
 using FluentAssertions;
 using NUnit.Framework;
@@ -10,11 +9,23 @@ namespace Atlas.MatchPrediction.Test.Models
     [TestFixture]
     public class MatchProbabilityPerLocusResponseTests
     {
+        /// <summary>
+        /// Test cases include examples where the probability is exactly 1m or 0m,
+        /// and values that are not exactly 1m or 0m, but would round to 100% and 0%, respectively.
+        /// </summary>
         [TestCase(1, 0, 0, PredictiveMatchCategory.Exact, PredictiveMatchCategory.Exact)]
-        [TestCase(0, 1, 0, PredictiveMatchCategory.Mismatch, PredictiveMatchCategory.Exact)]
+        [TestCase(0.995, 0.0025, 0.0025, PredictiveMatchCategory.Exact, PredictiveMatchCategory.Exact)]
+
         [TestCase(0, 0, 1, PredictiveMatchCategory.Mismatch, PredictiveMatchCategory.Mismatch)]
+        [TestCase(0.0025, 0.0025, 0.995, PredictiveMatchCategory.Mismatch, PredictiveMatchCategory.Mismatch)]
+
+        [TestCase(0, 1, 0, PredictiveMatchCategory.Mismatch, PredictiveMatchCategory.Exact)]
+        [TestCase(0.005, 0.99, 0.005, PredictiveMatchCategory.Mismatch, PredictiveMatchCategory.Exact)]
         [TestCase(0, 0.5, 0.5, PredictiveMatchCategory.Mismatch, PredictiveMatchCategory.Potential)]
+        [TestCase(0.005, 0.495, 0.5, PredictiveMatchCategory.Mismatch, PredictiveMatchCategory.Potential)]
+
         [TestCase(0.5, 0.5, 0, PredictiveMatchCategory.Potential, PredictiveMatchCategory.Exact)]
+        [TestCase(0.5, 0.495, 0.005, PredictiveMatchCategory.Potential, PredictiveMatchCategory.Exact)]
         [TestCase(0.2, 0.6, 0.2, PredictiveMatchCategory.Potential, PredictiveMatchCategory.Potential)]
         public void PredictiveMatchCategory_ReturnsExpectedMatchCategories(
             decimal zeroMismatchValue,
