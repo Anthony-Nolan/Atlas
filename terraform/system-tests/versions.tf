ef84beeae5879088e148a7735da8d8e72de95f38
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2022-12-19 14:18:31 +0000

    system-tests terraform 0.12 -> 0.13

--- terraform/system-tests/versions.tf
+++ terraform/system-tests/versions.tf
@@ -1,3 +1,9 @@
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.13, < 0.14"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.39.0"
+    }
+  }
 }
