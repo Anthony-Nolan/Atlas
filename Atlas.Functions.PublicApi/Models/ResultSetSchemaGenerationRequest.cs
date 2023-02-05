f48d8b756e414280bed7435a514310afd91a3d1e
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2023-02-05 19:12:09 +0000

    docs: #643: Added new API function that generates JSON schemas for result set client models.

---
+++ Atlas.Functions.PublicApi/Models/ResultSetSchemaGenerationRequest.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Atlas.Functions.PublicApi.Models
+{
+    public class ResultSetSchemaGenerationRequest
+    {
+        public ResultSetOptions ResultSet { get; set; }
+    }
+
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum ResultSetOptions
+    {
+        OriginalSearch,
+        RepeatSearch,
+        OriginalMatchingAlgorithm,
+        RepeatMatchingAlgorithm
+    }
+}
