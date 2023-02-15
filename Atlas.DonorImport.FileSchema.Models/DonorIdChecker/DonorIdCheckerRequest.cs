b8f4630fdfbab23bcbd6d8e400404e865817ee40
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2023-02-15 11:25:16 +0000

    chore: #878: Add schema models for donor id checker files.

---
+++ Atlas.DonorImport.FileSchema.Models/DonorIdChecker/DonorIdCheckerRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace Atlas.DonorImport.FileSchema.Models.DonorIdChecker
+{
+    /// <summary>
+    /// Request to check whether a set of donors exist in the donor store or not, by their record IDs.
+    /// </summary>
+    public class DonorIdCheckerRequest
+    {
+        /// <summary>
+        /// List of alphanumeric donor record Ids.
+        /// </summary>
+        public IEnumerable<string> RecordIds { get; set; }
+    }
+}
---
+++ Atlas.DonorImport.FileSchema.Models/DonorIdChecker/DonorIdCheckerResults.cs
@@ -0,0 +1,23 @@
+﻿namespace Atlas.DonorImport.FileSchema.Models.DonorIdChecker
+{
+    /// <summary>
+    /// Results for the donor ID checker
+    /// </summary>
+    public class DonorIdCheckerResults
+    {
+        public IEnumerable<DonorIdCheckerResult> Results { get; set; }
+    }
+
+    public class DonorIdCheckerResult
+    {
+        /// <summary>
+        /// Alphanumeric donor Id
+        /// </summary>
+        public string RecordId { get; set; }
+
+        /// <summary>
+        /// Is the donor present in the donor store or not
+        /// </summary>
+        public bool IsPresentInDonorStore { get; set; }
+    }
+}
