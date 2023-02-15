a8e581bae4aaae4fbf8d50e5c36b6d5501c4e670
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2023-02-15 15:18:41 +0000

    feature: #885: Add matching and scoring denominators to result set.

--- Atlas.Client.Models/CHANGELOG_Client.md
+++ Atlas.Client.Models/CHANGELOG_Client.md
@@ -20,6 +20,7 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ### 1.5.0
+* `ResultSet` model has been extended with new properties, `MatchCriteriaDenominator` and `ScoringCriteriaDenominator`, which represent the match count denominator for matching results and scoring results, respectively.
 * `MatchingResultsNotification` and `SearchResultsNotification` have both been extended with more failure information.
   * As part of this enhancement, the following properties have been deprecated:
     * `SearchResultsNotification.FailureMessage` has been superseded by a new property `SearchResultsNotification.FailureInfo`.
---
+++ Atlas.MatchingAlgorithm.Test/Models/SearchResults/DenominatorTests.cs
@@ -0,0 +1,81 @@
+﻿using Atlas.Client.Models.Search.Results.Matching.ResultSet;
+using Atlas.Client.Models.Search.Results.ResultSet;
+using Atlas.Common.Public.Models.GeneticData;
+using Atlas.MatchingAlgorithm.Test.TestHelpers.Builders.SearchRequests;
+using FluentAssertions;
+using LochNessBuilder;
+using NUnit.Framework;
+
+namespace Atlas.MatchingAlgorithm.Test.Models.SearchResults
+{
+    [TestFixture]
+    internal class DenominatorTests
+    {
+        /// <summary>
+        /// Using <see cref="OriginalMatchingAlgorithmResultSet"/> as concrete representation of underlying base class <see cref="ResultSet{TResult}"/>
+        /// to test base functionality of <see cref="ResultSet{TResult}.MatchCriteriaDenominator"/> and <see cref="ResultSet{TResult}.ScoringCriteriaDenominator"/>
+        /// </summary>
+        private static Builder<OriginalMatchingAlgorithmResultSet> ResultBuilder => Builder<OriginalMatchingAlgorithmResultSet>.New;
+
+        [Test]
+        public void MatchCriteriaDenominator_MatchAllLoci_Returns10([Range(0, 2)] int mismatchCount)
+        {
+            var request = new SearchRequestBuilder().WithMismatchCountAtLoci(new[] { Locus.A, Locus.B, Locus.C, Locus.Dqb1, Locus.Drb1 }, mismatchCount).Build();
+            var result = ResultBuilder.With(x => x.SearchRequest, request).Build();
+
+            result.MatchCriteriaDenominator.Should().Be(10);
+        }
+
+        [Test]
+        public void MatchCriteriaDenominator_MatchRequiredLociOnly_Returns6([Range(0, 2)] int mismatchCount)
+        {
+            var request = new SearchRequestBuilder().WithMismatchCountAtLoci(new[] { Locus.A, Locus.B, Locus.Drb1 }, mismatchCount).Build();
+            var result = ResultBuilder.With(x => x.SearchRequest, request).Build();
+
+            result.MatchCriteriaDenominator.Should().Be(6);
+        }
+
+        [Test]
+        public void ScoringCriteriaDenominator_ScoreAllLoci_Returns12()
+        {
+            var request = new SearchRequestBuilder().WithLociToScore(new[] { Locus.A, Locus.B, Locus.C, Locus.Dpb1, Locus.Dqb1, Locus.Drb1 }).Build();
+            var result = ResultBuilder.With(x => x.SearchRequest, request).Build();
+
+            result.ScoringCriteriaDenominator.Should().Be(12);
+        }
+
+        [Test]
+        public void ScoringCriteriaDenominator_ScoreAllLoci_ExcludeDpb1FromAggregation_Returns10()
+        {
+            var request = new SearchRequestBuilder()
+                .WithLociToScore(new[] { Locus.A, Locus.B, Locus.C, Locus.Dpb1, Locus.Dqb1, Locus.Drb1 })
+                .WithLociExcludedFromScoringAggregates(new[] { Locus.Dpb1 })
+                .Build();
+            var result = ResultBuilder.With(x => x.SearchRequest, request).Build();
+
+            result.ScoringCriteriaDenominator.Should().Be(10);
+        }
+
+        /// <summary>
+        /// It's possible that a consumer will only use the scoring feature for DPB1.
+        /// This test documents that the scoring denominator will only be `2` in this use case.
+        /// </summary>
+        [Test]
+        public void ScoringCriteriaDenominator_OnlyScoreDpb1_Returns2()
+        {
+            var request = new SearchRequestBuilder().WithLociToScore(new[] { Locus.Dpb1 }).Build();
+            var result = ResultBuilder.With(x => x.SearchRequest, request).Build();
+
+            result.ScoringCriteriaDenominator.Should().Be(2);
+        }
+
+        [Test]
+        public void ScoringCriteriaDenominator_LociToScoreIsEmpty_ReturnsNull()
+        {
+            var request = new SearchRequestBuilder().Build();
+            var result = ResultBuilder.With(x => x.SearchRequest, request).Build();
+
+            result.ScoringCriteriaDenominator.Should().BeNull();
+        }
+    }
+}
