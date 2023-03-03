81372e449bb40a3d2991ab4d8cd8386a03c2996d
Dmitry Isaev <disaev@NMDP.ORG>
2023-03-03 14:19:34 +0200

    chore: #878: update app settings to include donor id check results container name

--- terraform/core/modules/donor_import/storage.tf
+++ terraform/core/modules/donor_import/storage.tf
@@ -3,3 +3,7 @@ resource "azurerm_storage_container" "donor_blob_storage" {
   storage_account_name  = var.azure_storage.name
   container_access_type = "private"
 }
+
+locals {
+  donor_id_checker_results_container_name = "donor-id-checker/results"
+}
