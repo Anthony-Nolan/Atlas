4d58aa206c83fcbf4ce0253ae7cdc24de0306f55
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2023-05-22 14:31:54 +0100

    feature: #964: Report discrepant antigen mismatch counts for exercise 2.

---
+++ Atlas.ManualTesting/Services/WmdaConsensusResults/WmdaDiscrepantReportWriter.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Atlas.ManualTesting.Services.WmdaConsensusResults
+{
+    public interface IWmdaDiscrepantResultsWriter
+    {
+        Task WriteToFile(string discrepantResultsFilePath, DiscrepantResultsReport report);
+    }
+
+    internal class WmdaDiscrepantResultsWriter : IWmdaDiscrepantResultsWriter
+    {
+        public async Task WriteToFile(string discrepantResultsFilePath, DiscrepantResultsReport report)
+        {
+            var contents = BuildFileContents(report.AnalysedDiscrepancies);
+
+            var headerText =
+                             "Locus;" +
+                             "Patient HLA Typing;Donor HLA Typing;" +
+                             "Consensus Count;Atlas Count;" +
+                             $"([Patient HLA-Donor HLA] Common {report.CommonHlaCategory} [Common {report.CommonHlaCategory} Count]);" +
+                             "PDP Count;PDP Ids";
+
+            await File.WriteAllLinesAsync(discrepantResultsFilePath, new[] { headerText }.Concat(contents));
+        }
+
+        private static IEnumerable<string> BuildFileContents(IEnumerable<AnalysedDiscrepancy> analysedDiscrepancies)
+        {
+            static string HlaMetadataToString(CommonHlaMetadataInfo info)
+            {
+                if (!info.CommonHlaMetadata.Any())
+                {
+                    return "_";
+                }
+
+                const int maxLength = 24;
+                var concatHlaMetadata = ConcatenateStrings(info.CommonHlaMetadata);
+                var formattedString = concatHlaMetadata.Length > maxLength ? $"{concatHlaMetadata[..maxLength]}..." : concatHlaMetadata;
+                return $"([{info.PatientHla}-{info.DonorHla}] {formattedString} [{info.CommonHlaMetadata.Count}])";
+            }
+
+            static string ConcatenateStrings(IEnumerable<string> strings) => string.Join(",", strings);
+
+            return analysedDiscrepancies.Select(d =>
+                $"{d.Locus};" +
+                $"{d.PatientHla.Position1} + {d.PatientHla.Position2};" +
+                $"{d.DonorHla.Position1} + {d.DonorHla.Position2};" +
+                $"{d.MismatchedCounts.First().ConsensusMismatchCount};" +
+                $"{d.MismatchedCounts.First().AtlasMismatchCount};" +
+                $"{HlaMetadataToString(d.CommonHlaMetadata.Position1)} + {HlaMetadataToString(d.CommonHlaMetadata.Position2)};" +
+                $"{d.MismatchedCounts.Count};" +
+                $"{ConcatenateStrings(d.MismatchedCounts.Select(mc => $"{mc.PatientId}:{mc.DonorId}"))}"
+            );
+        }
+    }
+}
---
+++ Atlas.ManualTesting/Services/WmdaConsensusResults/WmdaResultsComparers.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using Atlas.Common.Public.Models.GeneticData;
+using Atlas.ManualTesting.Common;
+using Atlas.ManualTesting.Common.SubjectImport;
+using Atlas.ManualTesting.Models;
+
+namespace Atlas.ManualTesting.Services.WmdaConsensusResults
+{
+    public interface IWmdaResultsTotalMismatchComparer : IWmdaResultsComparer { }
+    internal class WmdaResultsTotalMismatchComparer : WmdaResultsComparerBase<WmdaConsensusResultsFile>, IWmdaResultsTotalMismatchComparer
+    {
+        public WmdaResultsTotalMismatchComparer(
+            IFileReader<WmdaConsensusResultsFile> resultsFileReader,
+            IFileReader<ImportedSubject> subjectFileReader) 
+            : base(resultsFileReader, subjectFileReader)
+        {
+        }
+
+        protected override IDictionary<Locus, string> SelectLocusMismatchCounts(WmdaConsensusResultsFile results)
+        {
+            return results.TotalMismatchCounts;
+        }
+    }
+
+    public interface IWmdaResultsAntigenMismatchComparer : IWmdaResultsComparer { }
+    internal class WmdaResultsAntigenMismatchComparer : WmdaResultsComparerBase<WmdaConsensusResultsFileSetTwo>, IWmdaResultsAntigenMismatchComparer
+    {
+        public WmdaResultsAntigenMismatchComparer(
+            IFileReader<WmdaConsensusResultsFileSetTwo> resultsFileReader,
+            IFileReader<ImportedSubject> subjectFileReader) 
+            : base(resultsFileReader, subjectFileReader)
+        {
+        }
+
+        protected override IDictionary<Locus, string> SelectLocusMismatchCounts(WmdaConsensusResultsFileSetTwo results)
+        {
+            return results.AntigenMismatchCounts;
+        }
+    }
+}
