89b1f94a5556bf69718f54d3dd6636868b5b451c
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2023-05-24 19:10:22 +0100

    fix: #978: Extend antigen match calculator logic to explicitly handle non-expressing typings.

--- Atlas.MatchingAlgorithm/Services/Search/Scoring/AntigenMatching/AntigenMatchCalculator.cs
+++ Atlas.MatchingAlgorithm/Services/Search/Scoring/AntigenMatching/AntigenMatchCalculator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using Atlas.Client.Models.Search.Results.Matching.PerLocus;
+using Atlas.Common.Utils.Extensions;
 using Atlas.HlaMetadataDictionary.ExternalInterface.Models.Metadata.ScoringMetadata;
 
 namespace Atlas.MatchingAlgorithm.Services.Search.Scoring.AntigenMatching
@@ -37,9 +38,24 @@ namespace Atlas.MatchingAlgorithm.Services.Search.Scoring.AntigenMatching
             var patientMatchingSerologies = patientMetadata.HlaScoringInfo.MatchingSerologies.Select(s => s.Name).ToList();
             var donorSerologyEquivalents = donorMetadata.HlaScoringInfo.MatchingSerologies.Where(s => s.IsDirectMapping).Select(s => s.Name).ToList();
 
-            return patientMatchingSerologies.Any() && donorSerologyEquivalents.Any()
-                ? patientMatchingSerologies.Intersect(donorSerologyEquivalents).Any()
-                : null;
+            // if both typings have assigned serologies, then determine if they are antigen-matched by checking for overlapping assignments
+            if (patientMatchingSerologies.Any() && donorSerologyEquivalents.Any())
+            {
+                return patientMatchingSerologies.Intersect(donorSerologyEquivalents).Any();
+            }
+
+            // else if either typing is a non-expressing typing, then return `false`
+            static bool IsNonExpressingTyping(IHlaScoringMetadata metadata) =>
+                metadata.HlaScoringInfo.MatchingPGroups.IsNullOrEmpty() && 
+                metadata.HlaScoringInfo.MatchingSerologies.IsNullOrEmpty();
+
+            if (IsNonExpressingTyping(patientMetadata) || IsNonExpressingTyping(donorMetadata))
+            {
+                return false;
+            }
+
+            // finally, default to `null` where antigen match cannot be determined, e.g., expressing allele with no serology assignment
+            return null;
         }
     }
 }
