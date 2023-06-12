223ccbb32a24d888153b2dd3865656ee007983b1
Igor Kupreychik <int100072@nmdp.org>
2023-06-12 10:20:22 +0200

    review: #990: rename UploadErrorEventModel to FileErrorEventModel

--- Atlas.Common/ApplicationInsights/FileErrorEventModel.cs
+++ Atlas.Common/ApplicationInsights/FileErrorEventModel.cs
@@ -2,9 +2,9 @@
 
 namespace Atlas.Common.ApplicationInsights
 {
-    public class UploadErrorEventModel : ErrorEventModel
+    public class FileErrorEventModel : ErrorEventModel
     {
-        public UploadErrorEventModel(string filename, string messageName, Exception exception)
+        public FileErrorEventModel(string filename, string messageName, Exception exception)
             : base(messageName, exception)
         {
             Properties.Add("FileName", filename);
