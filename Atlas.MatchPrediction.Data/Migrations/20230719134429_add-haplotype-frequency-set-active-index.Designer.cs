d1e4725238cc3e4a699b7edadeb2fa778586ff23
Igor Kupreychik <int100072@nmdp.org>
2023-07-19 15:49:23 +0200

    feature: #1017: Improve delete efficiency of inactive HF sets

--- Atlas.MatchPrediction.Data/Migrations/20230719134429_add-haplotype-frequency-set-active-index.Designer.cs
+++ Atlas.MatchPrediction.Data/Migrations/20230719134429_add-haplotype-frequency-set-active-index.Designer.cs
@@ -4,6 +4,7 @@ using Atlas.MatchPrediction.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Atlas.MatchPrediction.Data.Migrations
 {
     [DbContext(typeof(MatchPredictionContext))]
-    partial class MatchPredictionContextModelSnapshot : ModelSnapshot
+    [Migration("20230719134429_add-haplotype-frequency-set-active-index")]
+    partial class addhaplotypefrequencysetactiveindex
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
