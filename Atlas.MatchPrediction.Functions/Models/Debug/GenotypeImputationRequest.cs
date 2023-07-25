0bfa45f363d4443a5f33a839b0ab471d10c76aca
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2023-07-25 20:31:56 +0100

    chore: #1021: Add new debug function to expose genotype imputation service.

---
+++ Atlas.MatchPrediction.Functions/Models/Debug/GenotypeImputationRequest.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Atlas.Common.Public.Models.GeneticData;
+using Atlas.Common.Public.Models.GeneticData.PhenotypeInfo.TransferModels;
+using Atlas.MatchPrediction.ExternalInterface.Models.HaplotypeFrequencySet;
+
+namespace Atlas.MatchPrediction.Functions.Models.Debug
+{
+    public class GenotypeImputationRequest
+    {
+        public PhenotypeInfoTransfer<string> HlaTyping { get; set; }
+        public IEnumerable<Locus> AllowedLoci { get; set; }
+        public FrequencySetMetadata FrequencySetMetadata { get; set; }
+    }
+}
---
+++ Atlas.MatchPrediction.Functions/Models/Debug/GenotypeImputationResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Atlas.MatchPrediction.ExternalInterface.Models.HaplotypeFrequencySet;
+
+namespace Atlas.MatchPrediction.Functions.Models.Debug
+{
+    public class GenotypeImputationResponse
+    {
+        public HaplotypeFrequencySet HaplotypeFrequencySet { get; set; }
+        public IEnumerable<string> GenotypeLikelihoods { get; set; }
+    }
+}
