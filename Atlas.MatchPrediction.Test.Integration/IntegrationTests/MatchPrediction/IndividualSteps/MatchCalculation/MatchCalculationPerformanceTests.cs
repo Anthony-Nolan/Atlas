94f9ee0d0c8799847370fcecd81a018241896bd1
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2023-10-04 17:46:10 +0100

    chore: #637: Removed redundant match calculation debug endpoint and its corresponding service level code.

--- Atlas.MatchPrediction.Test.Integration/IntegrationTests/MatchPrediction/IndividualSteps/MatchCalculation/MatchCalculationPerformanceTests.cs
+++ Atlas.MatchPrediction.Test.Integration/IntegrationTests/MatchPrediction/IndividualSteps/MatchCalculation/MatchCalculationPerformanceTests.cs
@@ -1,12 +1,8 @@
 ﻿using System.Collections.Generic;
-using System.Threading.Tasks;
-using Atlas.Common.GeneticData;
-using Atlas.Common.GeneticData.PhenotypeInfo;
 using Atlas.Common.Public.Models.GeneticData;
 using Atlas.Common.Public.Models.GeneticData.PhenotypeInfo;
 using Atlas.Common.Test.SharedTestHelpers;
 using Atlas.Common.Test.SharedTestHelpers.Builders;
-using Atlas.HlaMetadataDictionary.Test.IntegrationTests.TestHelpers.FileBackedStorageStubs;
 using Atlas.MatchPrediction.Config;
 using Atlas.MatchPrediction.Services.MatchCalculation;
 using Atlas.MatchPrediction.Test.Integration.Resources.Alleles;
@@ -20,8 +16,6 @@ namespace Atlas.MatchPrediction.Test.Integration.IntegrationTests.MatchPredictio
     {
         private IMatchCalculationService matchCalculationService;
 
-        private const string HlaNomenclatureVersion = FileBackedHlaMetadataRepositoryBaseReader.OlderTestHlaVersion;
-
         private static readonly ISet<Locus> AllowedLoci = LocusSettings.MatchPredictionLoci;
 
         [OneTimeSetUp]
@@ -29,75 +23,10 @@ namespace Atlas.MatchPrediction.Test.Integration.IntegrationTests.MatchPredictio
         {
             matchCalculationService = DependencyInjection.DependencyInjection.Provider.GetService<IMatchCalculationService>();
         }
-        
-        [Test]
-        [Repeat(10_000)] // Each test run is 10 method calls.
-        [Ignore("This will not be called in production MPA (only by debug endpoints), so not worth running, even on CI. " +
-                "This test demonstrates the difference between this approach and the performance enhanced version." +
-                "10,000 repeats ~= 6 seconds.")]
-        public async Task CalculateMatchCounts_PerformanceTest()
-        {
-            Task<LociInfo<int?>> CalculateMatchCounts(PhenotypeInfo<string> patient, HashSet<Locus> allowedLoci = null)
-            {
-                return matchCalculationService.CalculateMatchCounts(
-                    patient,
-                    DefaultGGroupsBuilder.Build(),
-                    HlaNomenclatureVersion,
-                    allowedLoci ?? AllowedLoci);
-            }
 
-            // Match
-            await CalculateMatchCounts(DefaultGGroupsBuilder.Build());
-
-            // Mismatch at A
-            const string mismatchHlaA = "01:01:01G";
-            await CalculateMatchCounts(DefaultGGroupsBuilder.WithDataAt(Locus.A, mismatchHlaA).Build());
-            await CalculateMatchCounts(DefaultGGroupsBuilder.WithDataAt(Locus.A, LocusPosition.One, mismatchHlaA).Build());
-
-            // Mismatch at B
-            const string mismatchHlaB  = "38:01:01G";
-            await CalculateMatchCounts(DefaultGGroupsBuilder.WithDataAt(Locus.B, mismatchHlaB).Build());
-            await CalculateMatchCounts(DefaultGGroupsBuilder.WithDataAt(Locus.B, LocusPosition.Two, mismatchHlaB).Build());
-
-            // Mismatch at C
-            const string mismatchHlaC = "14:02:01G";
-            await CalculateMatchCounts(DefaultGGroupsBuilder.WithDataAt(Locus.C, mismatchHlaC).Build());
-
-            // Mismatch at DQB1
-            const string mismatchHlaDqb1 = "06:09:01G";
-            await CalculateMatchCounts(DefaultGGroupsBuilder.WithDataAt(Locus.Dqb1, mismatchHlaDqb1).Build());
-
-            // Mismatch at DRB1
-            const string mismatchHlaDrb1 = "14:02:01G";
-            await CalculateMatchCounts(DefaultGGroupsBuilder.WithDataAt(Locus.Drb1, mismatchHlaDrb1).Build());
-            
-            // Mismatch at all
-            await CalculateMatchCounts(DefaultGGroupsBuilder
-                .WithDataAt(Locus.A, mismatchHlaA)
-                .WithDataAt(Locus.B, mismatchHlaB)
-                .WithDataAt(Locus.C, mismatchHlaC)
-                .WithDataAt(Locus.Dqb1, mismatchHlaDqb1)
-                .WithDataAt(Locus.Drb1, mismatchHlaDrb1)
-                .Build()
-            );
-            
-            // Mismatch at all, excluded loci
-            await CalculateMatchCounts(DefaultGGroupsBuilder
-                .WithDataAt(Locus.A, mismatchHlaA)
-                .WithDataAt(Locus.B, mismatchHlaB)
-                .WithDataAt(Locus.C, mismatchHlaC)
-                .WithDataAt(Locus.Dqb1, mismatchHlaDqb1)
-                .WithDataAt(Locus.Drb1, mismatchHlaDrb1)
-                .Build(),
-                new HashSet<Locus> {Locus.C, Locus.Dqb1}
-            );
-        }
-        
         /// <summary>
         /// Note that while in practice this method should be called with P Group typed hla, under the hood the work done is string comparison.
         /// A performance test of this method does not care about biological correctness, so running with G AlleleGroups is sufficient here.
-        /// 
-        /// As such, we've used the same test data as for <see cref="CalculateMatchCounts_PerformanceTest"/>, to enable reasonable comparison. 
         /// </summary>
         [Test]
         [Repeat(100_000)] // Each test run is 10 method calls.
@@ -159,7 +88,6 @@ namespace Atlas.MatchPrediction.Test.Integration.IntegrationTests.MatchPredictio
             );
         }
 
-        private static PhenotypeInfoBuilder<string> DefaultGGroupsBuilder =>
-            new PhenotypeInfoBuilder<string>(Alleles.UnambiguousAlleleDetails.GGroups());
+        private static PhenotypeInfoBuilder<string> DefaultGGroupsBuilder => new(Alleles.UnambiguousAlleleDetails.GGroups());
     }
 }
