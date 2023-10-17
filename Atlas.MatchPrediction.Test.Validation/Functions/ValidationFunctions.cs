d3cc95e811066200506f2eba89303b7911fa0b95
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2023-10-17 19:22:02 +0100

    refactor: #1095: Move functions and docs specific to exercise 3 to their own space.

--- Atlas.MatchPrediction.Test.Validation/Functions/ValidationFunctions.cs
+++ Atlas.MatchPrediction.Test.Validation/Functions/ValidationFunctions.cs
@@ -1,6 +1,4 @@
-﻿using Atlas.Common.Utils;
-using Atlas.Common.Utils.Http;
-using Atlas.MatchPrediction.ExternalInterface.Models;
+﻿using Atlas.Common.Utils.Http;
 using Atlas.MatchPrediction.Test.Validation.Models;
 using Atlas.MatchPrediction.Test.Validation.Services;
 using AzureFunctions.Extensions.Swashbuckle.Attribute;
@@ -9,7 +7,6 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Newtonsoft.Json;
 using System;
-using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Net;
 using System.Threading.Tasks;
@@ -19,20 +16,11 @@ namespace Atlas.MatchPrediction.Test.Validation.Functions
     public class ValidationFunctions
     {
         private readonly ISubjectInfoImporter subjectInfoImporter;
-        private readonly IMatchPredictionRequester matchPredictionRequester;
-        private readonly IResultsProcessor resultsProcessor;
-        private readonly IMessageSender messageSender;
 
         public ValidationFunctions(
-            ISubjectInfoImporter subjectInfoImporter, 
-            IMatchPredictionRequester matchPredictionRequester, 
-            IResultsProcessor resultsProcessor, 
-            IMessageSender messageSender)
+            ISubjectInfoImporter subjectInfoImporter)
         {
             this.subjectInfoImporter = subjectInfoImporter;
-            this.matchPredictionRequester = matchPredictionRequester;
-            this.resultsProcessor = resultsProcessor;
-            this.messageSender = messageSender;
         }
 
         [FunctionName(nameof(ImportSubjects))]
@@ -49,72 +37,6 @@ namespace Atlas.MatchPrediction.Test.Validation.Functions
             catch (Exception ex)
             {
                 throw new AtlasHttpException(HttpStatusCode.InternalServerError, "Failed to import subjects.", ex);
-            }
-        }
-
-        [SuppressMessage(null, SuppressMessage.UnusedParameter, Justification = SuppressMessage.UsedByAzureTrigger)]
-        [FunctionName(nameof(SendMatchPredictionRequests))]
-        public async Task SendMatchPredictionRequests(
-            [HttpTrigger(AuthorizationLevel.Function, "post")]
-            HttpRequest request)
-        {
-            try
-            {
-                await matchPredictionRequester.SendMatchPredictionRequests();
-            }
-            catch (Exception ex)
-            {
-                throw new AtlasHttpException(HttpStatusCode.InternalServerError, "Failure whilst sending requests.", ex);
-            }
-        }
-
-        [SuppressMessage(null, SuppressMessage.UnusedParameter, Justification = SuppressMessage.UsedByAzureTrigger)]
-        [FunctionName(nameof(ResumeMatchPredictionRequests))]
-        public async Task ResumeMatchPredictionRequests(
-            [HttpTrigger(AuthorizationLevel.Function, "post")]
-            HttpRequest request)
-        {
-            try
-            {
-                await matchPredictionRequester.ResumeMatchPredictionRequests();
-            }
-            catch (Exception ex)
-            {
-                throw new AtlasHttpException(HttpStatusCode.InternalServerError, "Failure whilst resuming to send requests.", ex);
-            }
-        }
-
-        [SuppressMessage(null, SuppressMessage.UnusedParameter, Justification = SuppressMessage.UsedByAzureTrigger)]
-        [FunctionName(nameof(PromptDownloadOfMissingResults))]
-        public async Task PromptDownloadOfMissingResults(
-            [HttpTrigger(AuthorizationLevel.Function, "post")]
-            HttpRequest request)
-        {
-            try
-            {
-                await messageSender.SendNotificationsForMissingResults();
-            }
-            catch (Exception ex)
-            {
-                throw new AtlasHttpException(HttpStatusCode.InternalServerError, "Failure whilst downloading missing results.", ex);
-            }
-        }
-
-        [FunctionName(nameof(ProcessResults))]
-        public async Task ProcessResults(
-            [ServiceBusTrigger(
-                "%MatchPredictionRequests:ResultsTopic%",
-                "%MatchPredictionRequests:ResultsSubscription%",
-                Connection = "MessagingServiceBus:ConnectionString")]
-            MatchPredictionResultLocation[] locations)
-        {
-            try
-            {
-                await resultsProcessor.ProcessAndStoreResults(locations);
-            }
-            catch (Exception ex)
-            {
-                throw new AtlasHttpException(HttpStatusCode.InternalServerError, "Failed to process results.", ex);
             }
         }
     }
