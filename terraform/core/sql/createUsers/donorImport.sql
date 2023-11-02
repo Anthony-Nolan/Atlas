6ed8fd866dfda1dde1412b560ddb1bdabb8feff2
Igor Kupreychik <int100072@nmdp.org>
2023-11-02 13:56:52 +0100

    fix: #1110: Add db-writer permissions for matching user on Donors schema

--- terraform/core/sql/createUsers/donorImport.sql
+++ terraform/core/sql/createUsers/donorImport.sql
@@ -2,6 +2,7 @@ IF USER_ID('$(matchingUsernameForDonorDB)') IS NULL
     BEGIN
         CREATE USER $(matchingUsernameForDonorDB) WITH PASSWORD = '$(matchingPasswordForDonorDB)', DEFAULT_SCHEMA = $(donorImportSchema);
         ALTER ROLE db_datareader ADD MEMBER $(matchingUsernameForDonorDB)
+		ALTER ROLE db_datawriter ADD MEMBER $(matchingUsernameForDonorDB)
     END
 
 ELSE
@@ -9,6 +10,7 @@ ELSE
     BEGIN
         ALTER USER $(matchingUsernameForDonorDB) WITH PASSWORD = '$(matchingPasswordForDonorDB)', DEFAULT_SCHEMA = $(donorImportSchema);
         ALTER ROLE db_datareader ADD MEMBER $(matchingUsernameForDonorDB)
+		ALTER ROLE db_datawriter ADD MEMBER $(matchingUsernameForDonorDB)
     END
 
 
