a4009141fab61e6bf7e63eb3c0166feef722efdd
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2023-12-04 21:57:24 +0000

    chore: #1144: Added debug endpoint to retrieve random donors to help in generating test data.

---
+++ Atlas.DonorImport.Functions/Models/Debug/DonorRequest.cs
@@ -0,0 +1,17 @@
+﻿using Atlas.DonorImport.FileSchema.Models;
+
+namespace Atlas.DonorImport.Functions.Models.Debug
+{
+    public class DonorRequest
+    {
+        /// <summary>
+        /// Optional parameter if you wish to limit donors to specified <see cref="DonorType"/>
+        /// </summary>
+        public ImportDonorType? DonorType { get; set; }
+
+        /// <summary>
+        /// Optional parameter if you wish to limit donor to specified <see cref="RegistryCode"/>
+        /// </summary>
+        public string RegistryCode { get; set; }
+    }
+}
