3d961ace380ffab10f4eabf8ce35144e5b0c49a5
SergeyEzh <151528303+SergeyEzh@users.noreply.github.com>
2023-12-20 13:18:30 +0100

    chore #1116: Add recommended indexes to matching db

---
+++ Atlas.MatchingAlgorithm.Data/Migrations/20231218155540_AlterIndexOnDonorManagementLogTableIndexKeyIsOnlyLastUpdateDateTime.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Atlas.MatchingAlgorithm.Data.Migrations
+{
+    public partial class AlterIndexOnDonorManagementLogTableIndexKeyIsOnlyLastUpdateDateTime : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_DonorManagementLogs_DonorId_LastUpdateDateTime",
+                table: "DonorManagementLogs");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DonorManagementLogs_LastUpdateDateTime",
+                table: "DonorManagementLogs",
+                column: "LastUpdateDateTime")
+                .Annotation("SqlServer:Include", new[] { "DonorId" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_DonorManagementLogs_LastUpdateDateTime",
+                table: "DonorManagementLogs");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DonorManagementLogs_DonorId_LastUpdateDateTime",
+                table: "DonorManagementLogs",
+                columns: new[] { "DonorId", "LastUpdateDateTime" });
+        }
+    }
+}
