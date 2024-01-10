8b3e794a1fbfeb639eac66e1de2df015438799ab
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2024-01-10 21:31:28 +0000

    refactor: #1139: Move publish function from donor import functions class to donor update functions class.

--- Atlas.DonorImport.Functions/Functions/DonorUpdatesFunctions.cs
+++ Atlas.DonorImport.Functions/Functions/DonorUpdatesFunctions.cs
@@ -1,6 +1,12 @@
 ﻿using Microsoft.Azure.WebJobs;
 using System.Threading.Tasks;
 using Atlas.DonorImport.Services.DonorUpdates;
+using AzureFunctions.Extensions.Swashbuckle.Attribute;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace Atlas.DonorImport.Functions.Functions
 {
@@ -8,11 +14,16 @@ namespace Atlas.DonorImport.Functions.Functions
     {
         private readonly IDonorUpdatesPublisher updatesPublisher;
         private readonly IDonorUpdatesCleaner updatesCleaner;
+        private readonly IDonorUpdatesSaver donorUpdatesSaver;
 
-        public DonorUpdatesFunctions(IDonorUpdatesPublisher updatesPublisher, IDonorUpdatesCleaner updatesCleaner)
+        public DonorUpdatesFunctions(
+            IDonorUpdatesPublisher updatesPublisher,
+            IDonorUpdatesCleaner updatesCleaner,
+            IDonorUpdatesSaver donorUpdatesSaver)
         {
             this.updatesPublisher = updatesPublisher;
             this.updatesCleaner = updatesCleaner;
+            this.donorUpdatesSaver = donorUpdatesSaver;
         }
 
         [FunctionName(nameof(PublishSearchableDonorUpdates))]
@@ -25,6 +36,18 @@ namespace Atlas.DonorImport.Functions.Functions
         public async Task DeleteExpiredPublishedDonorUpdates([TimerTrigger("%PublishDonorUpdates:DeletionCronSchedule%")] TimerInfo timer)
         {
             await updatesCleaner.DeleteExpiredPublishedDonorUpdates();
+        }
+
+        [FunctionName(nameof(ManuallyPublishDonorUpdatesByDonorId))]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(500)]
+        public async Task<IActionResult> ManuallyPublishDonorUpdatesByDonorId([HttpTrigger(AuthorizationLevel.Function, "post")][RequestBodyType(typeof(int[]), "Donor ids")] HttpRequest request)
+        {
+            using var reader = new StreamReader(request.Body);
+            var ids = JsonConvert.DeserializeObject<int[]>(await reader.ReadToEndAsync());
+
+            await donorUpdatesSaver.GenerateAndSave(ids);
+            return new OkResult();
         }
     }
 }
