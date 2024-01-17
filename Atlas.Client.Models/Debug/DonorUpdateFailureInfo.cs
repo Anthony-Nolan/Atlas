97f42cf846804f98f8fb33c8415b7e1ac4c12199
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2024-01-17 15:33:23 +0000

    chore: #1174: Add debug endpoint to retrieve donor import failures by fileName.

---
+++ Atlas.Client.Models/Debug/DonorUpdateFailureInfo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Atlas.Client.Models.Debug
+{
+    /// <summary>
+    /// Information about why a donor update failed validation during donor import.
+    /// </summary>
+    public class DonorUpdateFailureInfo
+    {
+        public string DonorImportFileName { get; set; }
+        public DonorInfo Donor { get; set; }
+        public FailureInfo UpdateFailureInfo { get; set; }
+
+        public class DonorInfo
+        {
+            public string ExternalDonorCode { get; set; }
+            public string DonorType { get; set; }
+            public string EthnicityCode { get; set; }
+            public string RegistryCode { get; set; }
+        }
+
+        public class FailureInfo
+        {
+            public string PropertyName { get; set; }
+            public string Reason { get; set; }
+            public DateTimeOffset DateTime { get; set; }
+        }
+    }
+}
---
+++ Atlas.DonorImport.Functions/Functions/Debug/DonorUpdateFunctions.cs
@@ -0,0 +1,46 @@
+﻿using Atlas.Client.Models.Debug;
+using Atlas.Common.Utils.Http;
+using Atlas.DonorImport.Data.Repositories;
+using Atlas.DonorImport.Functions.Models.Debug;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Atlas.DonorImport.Functions.Functions.Debug
+{
+    public class DonorUpdateFunctions
+    {
+        private readonly IDonorImportFailureRepository donorImportFailureRepository;
+
+        public DonorUpdateFunctions(IDonorImportFailureRepository donorImportFailureRepository)
+        {
+            this.donorImportFailureRepository = donorImportFailureRepository;
+        }
+
+        /// <summary>
+        /// Retrieves donor import failures by file name.
+        /// File name must include the file extension, e.g., `.json`, but file path is optional.
+        /// </summary>
+        /// <param name="request">The HTTP request.</param>
+        /// <param name="fileName">The file name.</param>
+        /// <returns>The list of donor import failures. May be an empty list if no failures found.</returns>
+        [FunctionName(nameof(GetDonorImportFailuresByFileName))]
+        [ProducesResponseType(typeof(IEnumerable<DonorUpdateFailureInfo>), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetDonorImportFailuresByFileName(
+            [HttpTrigger(
+                AuthorizationLevel.Function,
+                "get",
+                Route = $"{RouteConstants.DebugRoutePrefix}/donorUpdates/failures/"+"{fileName}")]
+            HttpRequest request,
+            string fileName)
+        {
+            var failures = await donorImportFailureRepository.GetDonorImportFailuresByFileName(fileName);
+            return new JsonResult(failures.Select(f => f.ToDonorUpdateFailureInfo()));
+        }
+    }
+}
