4f07fabe8495b3a14759fd4afd0873ae4e8927fb
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2024-02-02 11:40:54 +0000

    chore: #1187: Add remaining donor import E2E related debug endpoints.

---
+++ Atlas.Debug.Client.Models/SupportMessages/SupportMessageHttpFunctionSettings.cs
@@ -0,0 +1,11 @@
+﻿using Atlas.Debug.Client.Models.HttpFunctions;
+
+namespace Atlas.Debug.Client.Models.SupportMessages
+{
+    /// <summary>
+    /// Settings for calling the support message debug functions.
+    /// </summary>
+    public class SupportMessageHttpFunctionSettings : HttpFunctionSettings
+    {
+    }
+}
