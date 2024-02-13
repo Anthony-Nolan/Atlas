7bf15a2f2557895db3c2c70f0aa870fff9d3c720
Sergey Ezhov <sezhov@nmdp.org>
2024-02-13 16:42:00 +0100

    chore: #1208: PoC ability to profile altlas function apps by changing service plan

--- terraform/core/function.tf
+++ terraform/core/function.tf
@@ -1,5 +1,6 @@
 locals {
   atlas_function_app_name            = "${local.environment}-ATLAS-FUNCTIONS"
+  atlas_temporary_function_app_name  = "${local.environment}-ATLAS-FUNCTIONS-TEMP"
   atlas_public_api_function_app_name = "${local.environment}-ATLAS-API"
 }
 
@@ -13,6 +14,9 @@ resource "azurerm_windows_function_app" "atlas_function" {
   functions_extension_version = "~4"
   storage_account_access_key  = azurerm_storage_account.atlas_durable_function_storage.primary_access_key
   storage_account_name        = azurerm_storage_account.atlas_durable_function_storage.name
+
+  # Disable for profiling
+  enabled = false
 
   tags = local.common_tags
 
@@ -111,6 +115,125 @@ resource "azurerm_windows_function_app" "atlas_function" {
     value = module.match_prediction.sql_database.connection_string
   }
 }
+
+
+resource "azurerm_windows_function_app" "atlas_function_temporary" {
+  name                        = local.atlas_temporary_function_app_name
+  resource_group_name         = azurerm_resource_group.atlas_resource_group.name
+  location                    = local.location
+  service_plan_id             = azurerm_service_plan.atlas-non-elastic-plan.id
+  client_certificate_mode     = "Required"
+  https_only                  = true
+  functions_extension_version = "~4"
+  storage_account_access_key  = azurerm_storage_account.atlas_durable_function_storage.primary_access_key
+  storage_account_name        = azurerm_storage_account.atlas_durable_function_storage.name
+
+
+  tags = local.common_tags
+
+  site_config {
+    always_on                 = true
+    worker_count              = 1
+    application_insights_key  = azurerm_application_insights.atlas.instrumentation_key
+    pre_warmed_instance_count = 1
+    use_32_bit_worker         = false
+    ftps_state                = "AllAllowed"
+    scm_minimum_tls_version   = "1.0"
+    cors {
+      support_credentials = false
+    }
+    application_stack {
+      dotnet_version = "v6.0"
+    }
+    dynamic "ip_restriction" {
+      for_each = var.IP_RESTRICTION_SETTINGS
+      content {
+        ip_address = ip_restriction
+      }
+    }
+  }
+
+  app_settings = {
+    "ApplicationInsights:LogLevel" = var.APPLICATION_INSIGHTS_LOG_LEVEL
+
+    "AzureFunctionsJobHost__extensions__durableTask__maxConcurrentActivityFunctions" = var.MAX_CONCURRENT_ACTIVITY_FUNCTIONS
+
+    "AtlasFunction:AzureStorage:MatchingConnectionString"                 = azurerm_storage_account.azure_storage.primary_connection_string
+    "AtlasFunction:AzureStorage:MatchingResultsBlobContainer"             = module.matching_algorithm.azure_storage.search_results_container
+    "AtlasFunction:AzureStorage:RepeatSearchMatchingResultsBlobContainer" = module.repeat_search.storage.repeat_search_matching_results_container_name
+    "AtlasFunction:AzureStorage:RepeatSearchResultsBlobContainer"         = module.repeat_search.storage.repeat_search_results_container_name
+    "AtlasFunction:AzureStorage:SearchResultsBlobContainer"               = azurerm_storage_container.search_results_blob_container.name
+    "AtlasFunction:AzureStorage:MatchPredictionConnectionString"          = azurerm_storage_account.azure_storage.primary_connection_string
+    "AtlasFunction:AzureStorage:MatchPredictionRequestsBlobContainer"     = module.match_prediction.storage.match_prediction_requests_container_name
+    "AtlasFunction:AzureStorage:MatchPredictionResultsBlobContainer"      = module.match_prediction.storage.match_prediction_results_container_name
+    "AtlasFunction:AzureStorage:MatchPredictionDownloadBatchSize"         = var.MATCH_PREDICTION_DOWNLOAD_BATCH_SIZE
+    "AtlasFunction:AzureStorage:MatchPredictionProcessingBatchSize"       = var.MATCHING_PREDICTION_PROCESSING_BATCH_SIZE
+    "AtlasFunction:AzureStorage:ShouldBatchResults"                       = var.SHOULD_BATCH_RESULTS
+
+    "AtlasFunction:MessagingServiceBus:ConnectionString"                        = azurerm_servicebus_namespace_authorization_rule.read-write.primary_connection_string
+    "AtlasFunction:MessagingServiceBus:MatchingResultsSubscription"             = azurerm_servicebus_subscription.match-prediction-orchestration-search-results-ready.name
+    "AtlasFunction:MessagingServiceBus:MatchingResultsTopic"                    = module.matching_algorithm.service_bus.matching_results_topic.name
+    "AtlasFunction:MessagingServiceBus:RepeatSearchMatchingResultsSubscription" = module.repeat_search.service_bus.repeat_search_matching_results_subscription
+    "AtlasFunction:MessagingServiceBus:RepeatSearchMatchingResultsTopic"        = module.repeat_search.service_bus.repeat_search_matching_results_topic
+    "AtlasFunction:MessagingServiceBus:RepeatSearchResultsTopic"                = module.repeat_search.service_bus.repeat_search_results_topic
+    "AtlasFunction:MessagingServiceBus:SearchResultsTopic"                      = azurerm_servicebus_topic.search-results-ready.name
+    "AtlasFunction:Orchestration:MatchPredictionBatchSize"                      = var.ORCHESTRATION_MATCH_PREDICTION_BATCH_SIZE
+
+    "HlaMetadataDictionary:AzureStorageConnectionString" = azurerm_storage_account.azure_storage.primary_connection_string
+    "HlaMetadataDictionary:HlaNomenclatureSourceUrl"     = var.WMDA_FILE_URL
+
+    "MacDictionary:AzureStorageConnectionString" = azurerm_storage_account.azure_storage.primary_connection_string
+    "MacDictionary:Import:CronSchedule"          = var.MAC_IMPORT_CRON_SCHEDULE
+    "MacDictionary:Download:MacSourceUrl"        = var.MAC_SOURCE
+    "MacDictionary:TableName"                    = module.multiple_allele_code_lookup.storage_table.name
+
+    "Matching:AzureStorage:ConnectionString"           = azurerm_storage_account.azure_storage.primary_connection_string
+    "Matching:AzureStorage:SearchResultsBlobContainer" = module.matching_algorithm.azure_storage.search_results_container
+    "Matching:MessagingServiceBus:ConnectionString"    = azurerm_servicebus_namespace_authorization_rule.read-write.primary_connection_string
+    "Matching:MessagingServiceBus:SearchRequestsTopic" = module.matching_algorithm.service_bus.matching_requests_topic.name
+    "Matching:MessagingServiceBus:SearchResultsTopic"  = module.matching_algorithm.service_bus.matching_results_topic.name
+
+    "MatchPrediction:AzureStorage:ConnectionString"                    = azurerm_storage_account.azure_storage.primary_connection_string
+    "MatchPrediction:AzureStorage:MatchPredictionResultsBlobContainer" = module.match_prediction.storage.match_prediction_results_container_name
+
+    "NotificationsServiceBus:AlertsTopic"                     = module.support.general.alerts_servicebus_topic.name
+    "NotificationsServiceBus:ConnectionString"                = azurerm_servicebus_namespace_authorization_rule.read-write.primary_connection_string
+    "NotificationsServiceBus:Debug:AlertsSubscription"        = module.support.general.alerts_servicebus_debug_subscription
+    "NotificationsServiceBus:Debug:NotificationsSubscription" = module.support.general.notifications_servicebus_debug_subscription
+    "NotificationsServiceBus:NotificationsTopic"              = module.support.general.notifications_servicebus_topic.name
+    "WEBSITE_RUN_FROM_PACKAGE"                                = var.WEBSITE_RUN_FROM_PACKAGE
+
+    "APPINSIGHTS_PROFILERFEATURE_VERSION"  = "1.0.0"
+    "DiagnosticServices_EXTENSION_VERSION" = "~3"
+  }
+
+  connection_string {
+    name  = "Matching:Sql:Persistent"
+    type  = "SQLAzure"
+    value = module.matching_algorithm.sql_database.persistent_database_connection_string
+  }
+
+  connection_string {
+    name  = "Matching:Sql:A"
+    type  = "SQLAzure"
+    value = module.matching_algorithm.sql_database.transient_a_database_connection_string
+  }
+
+  connection_string {
+    name  = "Matching:Sql:B"
+    type  = "SQLAzure"
+    value = module.matching_algorithm.sql_database.transient_b_database_connection_string
+  }
+
+  connection_string {
+    name  = "MatchPrediction:Sql"
+    type  = "SQLAzure"
+    value = module.match_prediction.sql_database.connection_string
+  }
+}
+
+
+
 
 resource "azurerm_windows_function_app" "atlas_public_api_function" {
   name                        = local.atlas_public_api_function_app_name
