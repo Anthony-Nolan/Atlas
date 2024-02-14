e8a220a246806a9b177c2d5b7675151c2926de25
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2024-02-14 19:10:50 +0000

    chore: #1220: DebugDonorInfo now IEquatable.

--- Atlas.Debug.Client.Models/DonorImport/DonorDebugInfo.cs
+++ Atlas.Debug.Client.Models/DonorImport/DonorDebugInfo.cs
@@ -1,20 +1,51 @@
 ﻿using Atlas.Common.Public.Models.GeneticData.PhenotypeInfo.TransferModels;
+using System;
 
-namespace Atlas.Debug.Client.Models.DonorImport
+namespace Atlas.Debug.Client.Models.DonorImport;
+
+/// <summary>
+/// Info of a donor that was found in the target donor database during a debug request.
+/// </summary>
+public class DonorDebugInfo : IEquatable<DonorDebugInfo>
 {
     /// <summary>
-    /// Info of a donor that was found in the target donor database during a debug request.
+    /// Equivalent to recordId in the donor import file
     /// </summary>
-    public class DonorDebugInfo
-    {
-        /// <summary>
-        /// Equivalent to recordId in the donor import file
-        /// </summary>
-        public string ExternalDonorCode { get; set; }
+    public string ExternalDonorCode { get; set; }
 
-        public string DonorType { get; set; }
-        public string RegistryCode { get; set; }
-        public string EthnicityCode { get; set; }
-        public PhenotypeInfoTransfer<string> Hla { get; set; }
+    public string DonorType { get; set; }
+    public string RegistryCode { get; set; }
+    public string EthnicityCode { get; set; }
+    public PhenotypeInfoTransfer<string> Hla { get; set; }
+
+    #region IEquatable implementation
+    /// <inheritdoc />
+    public bool Equals(DonorDebugInfo other)
+    {
+        if (ReferenceEquals(null, other)) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return
+            ExternalDonorCode == other.ExternalDonorCode &&
+            DonorType == other.DonorType &&
+            RegistryCode == other.RegistryCode &&
+            EthnicityCode == other.EthnicityCode &&
+            // `PhenotypeInfoTransfer` does not implement IEquatable, but `PhenotypeInfo` does.
+            Hla?.ToPhenotypeInfo() == other.Hla?.ToPhenotypeInfo();
     }
+
+    /// <inheritdoc />
+    public override bool Equals(object obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != GetType()) return false;
+        return Equals((DonorDebugInfo)obj);
+    }
+
+    /// <inheritdoc />
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(ExternalDonorCode, DonorType, RegistryCode, EthnicityCode, Hla?.ToPhenotypeInfo());
+    }
+    #endregion
 }
