19ba16a35d32d98c2a037fc6c921bace9825c1b3
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2024-02-20 21:17:00 +0000

    chore: #1235: Debug endpoints to check if results are batched.

---
+++ Atlas.Functions/Functions/Debug/SearchResultFunctions.cs
@@ -0,0 +1,30 @@
+﻿using Atlas.Common.Utils.Http;
+using Atlas.Functions.Settings;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Options;
+using System.Net;
+
+namespace Atlas.Functions.Functions.Debug
+{
+    public class SearchResultFunctions
+    {
+        private readonly AzureStorageSettings azureStorageSettings;
+
+        public SearchResultFunctions(IOptions<AzureStorageSettings> azureStorageSettings)
+        {
+            this.azureStorageSettings = azureStorageSettings.Value;
+        }
+
+        [FunctionName(nameof(AreResultsBatched))]
+        [ProducesResponseType(typeof(bool), (int)HttpStatusCode.OK)]
+        public IActionResult AreResultsBatched(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = $"{RouteConstants.DebugRoutePrefix}/searchResults/areBatched")]
+            HttpRequest request)
+        {
+            return new JsonResult(azureStorageSettings.ShouldBatchResults);
+        }
+    }
+}
