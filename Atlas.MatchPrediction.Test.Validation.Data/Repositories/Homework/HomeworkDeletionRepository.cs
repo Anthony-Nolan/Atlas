0d42d9a19181ff6bd4ef1c088048fa25f7d13e1f
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2024-04-27 15:39:57 +0100

    chore: #1294: Process all sets imported found in a directory.

---
+++ Atlas.MatchPrediction.Test.Validation.Data/Repositories/Homework/HomeworkDeletionRepository.cs
@@ -0,0 +1,38 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace Atlas.MatchPrediction.Test.Validation.Data.Repositories.Homework
+{
+    public interface IHomeworkDeletionRepository
+    {
+        Task DeleteAll();
+    }
+
+    public class HomeworkDeletionRepository : IHomeworkDeletionRepository
+    {
+        private readonly string connectionString;
+
+        public HomeworkDeletionRepository(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        /// <inheritdoc />
+        public async Task DeleteAll()
+        {
+            const string deleteAllPairs = "DELETE FROM PatientDonorPairs";
+
+            await using (var connection = new SqlConnection(connectionString))
+            {
+                await connection.ExecuteAsync(deleteAllPairs);
+            }
+
+            const string deleteAllSets = "DELETE FROM HomeworkSets";
+
+            await using (var connection = new SqlConnection(connectionString))
+            {
+                await connection.ExecuteAsync(deleteAllSets);
+            }
+        }
+    }
+}
