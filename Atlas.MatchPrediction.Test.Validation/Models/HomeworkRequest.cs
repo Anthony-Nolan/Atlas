5b6e5cadba47bfd6aee6373bea516e8bf1fd5230
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2024-04-29 15:48:13 +0100

    chore: #1294: Add HLA version to homework set.

--- Atlas.MatchPrediction.Test.Validation/Models/HomeworkRequest.cs
+++ Atlas.MatchPrediction.Test.Validation/Models/HomeworkRequest.cs
@@ -22,6 +22,11 @@ namespace Atlas.MatchPrediction.Test.Validation.Models
         public LociInfoTransfer<bool> MatchLoci { get; set; }
 
         /// <summary>
+        /// E.g., "3520".
+        /// </summary>
+        public string MatchingAlgorithmHlaNomenclatureVersion { get; set; }
+
+        /// <summary>
         /// If `true`, all previously stored homework sets will be deleted from the validation database.
         /// </summary>
         public bool DeletePreviousHomeworkSets { get; set; }
