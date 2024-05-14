7be1ff9fbc6b1f6f8f7dcd2bb5cb0fe9419e3d55
Peter Chenery <peter.chenery@anthonynolan.org>
2024-05-14 22:06:50 +0100

    feature: #1274: CBT-1473: Review Markups

--- Search.Tracking/Models/MatchPredictionCompletedEvent.cs
+++ Search.Tracking/Models/MatchPredictionCompletedEvent.cs
@@ -4,10 +4,7 @@
     {
         public int SearchRequestId { get; set; }
         public DateTime CompletionTimeUtc { get; set; }
-        public bool MatchPrediction_IsSuccessful { get; set; }
-        public MatchPredictionCompletionDetails MatchPrediction_FailureInfo_Json { get; set; }
-        public int MatchPrediction_DonorsPerBatch { get; set; }
-        public int MatchPrediction_TotalNumberOfBatches { get; set; }
+        public MatchPredictionCompletionDetails CompletionDetails { get; set; }
     }
 
     public class MatchPredictionCompletionDetails
@@ -15,5 +12,6 @@
         public bool IsSuccessful { get; set; }
         public string? FailureInfoJson { get; set; }
         public int? DonorsPerBatch { get; set; }
+        public int TotalNumberOfBatches { get; set; }
     }
 }
--- Search.Tracking/Models/MatchingAlgorithmCompletedEvent.cs
+++ Search.Tracking/Models/MatchingAlgorithmCompletedEvent.cs
@@ -5,21 +5,19 @@
         public int SearchRequestId { get; set; }
         public byte AttemptNumber { get; set; }
         public DateTime CompletionTimeUtc { get; set; }
-        public bool MatchingAlgorithm_IsSuccessful { get; set; }
-        public MatchingAlgorithmCompletionDetails MatchingAlgorithm_FailureInfo_Json { get; set; }
-        public byte MatchingAlgorithm_TotalAttemptsNumber { get; set; }
-        public int MatchingAlgorithm_NumberOfMatching { get; set; }
-        public int MatchingAlgorithm_NumberOfNoLongerMatching { get; set; }
-        public int MatchingAlgorithm_NumberOfResults { get; set; }
-        public string MatchingAlgorithm_HlaNomenclatureVersion { get; set; }
-        public bool MatchingAlgorithm_ResultsSent { get; set; }
-        public DateTime? MatchingAlgorithm_ResultsSentTimeUtc { get; set; }
+        public MatchingAlgorithmCompletionDetails CompletionDetails { get; set; }
+        public int NumberOfNoLongerMatching { get; set; }
+        public string HlaNomenclatureVersion { get; set; }
+        public bool ResultsSent { get; set; }
+        public DateTime? ResultsSentTimeUtc { get; set; }
     }
 
     public class MatchingAlgorithmCompletionDetails
     {
-        private bool IsSuccessful { get; set; }
-        private string? FailureInfoJson { get; set; }
-        private int? NumberOfMatching { get; set; }
+        public bool IsSuccessful { get; set; }
+        public string? FailureInfoJson { get; set; }
+        public byte TotalAttemptsNumber { get; set; }
+        public int NumberOfResults { get; set; }
+        public int? NumberOfMatching { get; set; }
     }
 }
