09b339718a46d5d0598fbabb47d80ada5643d65c
pchenery <peter.chenery@anthonynolan.org>
2024-05-15 14:07:55 +0100

    feature: #1274: CBT-1473: Create project and event models for search tracking (#1304)

---
+++ Search.Tracking/Models/SearchRequestedEvent.cs
@@ -0,0 +1,14 @@
+﻿namespace Atlas.SearchTracking.Models
+{
+    public class SearchRequestedEvent
+    {
+        public Guid SearchRequestId { get; set; }
+        public bool IsRepeatSearch { get; set; }
+        public Guid? OriginalSearchRequestId { get; set; }
+        public DateTime? RepeatSearchCutOffDate { get; set; }
+        public string RequestJson { get; set; }
+        public string SearchCriteria { get; set; }
+        public string DonorType { get; set; }
+        public DateTime RequestTimeUtc { get; set; }
+    }
+}
