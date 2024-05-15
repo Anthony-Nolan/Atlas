065b0733dd157d970c70af1d03543850f11dba96
pchenery <peter.chenery@anthonynolan.org>
2024-05-15 14:07:55 +0100

    feature: #1274: CBT-1473: Create project and event models for search tracking (#1304)

---
+++ Search.Tracking/Models/MatchPredictionStartedEvent.cs
@@ -0,0 +1,9 @@
+﻿namespace Atlas.SearchTracking.Models
+{
+    public class MatchPredictionStartedEvent
+    {
+        public int SearchRequestId { get; set; }
+        public DateTime InitiationTimeUtc { get; set; }
+        public DateTime StartTimeUtc { get; set; }
+    }
+}
---
+++ Search.Tracking/Models/MatchingAlgorithmAttemptStartedEvent.cs
@@ -0,0 +1,10 @@
+﻿namespace Atlas.SearchTracking.Models
+{
+    public class MatchingAlgorithmAttemptStartedEvent
+    {
+        public int SearchRequestId { get; set; }
+        public byte AttemptNumber { get; set; }
+        public DateTime InitiationTimeUtc { get; set; }
+        public DateTime StartTimeUtc { get; set; }
+    }
+}
