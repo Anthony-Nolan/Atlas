e1697bcd92844c99aafa10dee9f7d0fe78949097
pchenery <peter.chenery@anthonynolan.org>
2024-05-16 12:11:16 +0100

    feature: #1274: CBT-1473: Create client to send events to the topic (#1307)

---
+++ Search.Tracking/Settings/ServiceBus/MessagingServiceBusSettings.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Atlas.SearchTracking.Settings.ServiceBus
+{
+    public class MessagingServiceBusSettings
+    {
+        public string ConnectionString { get; set; }
+        public string SearchTrackingEventsTopic { get; set; }
+    }
+}
