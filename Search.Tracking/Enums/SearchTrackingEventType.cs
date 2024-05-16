2d4c441595658db990f2cefc08825ed704e68986
pchenery <peter.chenery@anthonynolan.org>
2024-05-16 12:11:16 +0100

    feature: #1274: CBT-1473: Create client to send events to the topic (#1307)

---
+++ Search.Tracking/Enums/SearchTrackingEventType.cs
@@ -0,0 +1,24 @@
+﻿namespace Atlas.SearchTracking.Enums
+{
+    public enum SearchTrackingEventType
+    {
+        SearchRequested,
+        MatchingAlgorithmAttemptStarted,
+        MatchingAlgorithmCoreMatchingStarted,
+        MatchingAlgorithmCoreMatchingEnded,
+        MatchingAlgorithmCoreScoringStarted,
+        MatchingAlgorithmCoreScoringEnded,
+        MatchingAlgorithmPersistingResultsStarted,
+        MatchingAlgorithmPersistingResultsEnded,
+        MatchingAlgorithmCompleted,
+        MatchPredictionStarted,
+        MatchPredictionBatchPreparationStarted,
+        MatchPredictionBatchPreparationEnded,
+        MatchPredictionRunningBatchesStarted,
+        MatchPredictionRunningBatchesEnded,
+        MatchPredictionPersistingResultsStarted,
+        MatchPredictionPersistingResultsEnded,
+        MatchPredictionCompleted,
+        SearchRequestCompleted
+    }
+}
