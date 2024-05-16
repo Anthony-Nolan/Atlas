00d4628b61efcfc788b78aa6adf5949453ffa06b
pchenery <peter.chenery@anthonynolan.org>
2024-05-16 12:11:16 +0100

    feature: #1274: CBT-1473: Create client to send events to the topic (#1307)

---
+++ Search.Tracking/Clients/SearchTrackingServiceBusClient.cs
@@ -0,0 +1,37 @@
+﻿using Atlas.SearchTracking.Settings.ServiceBus;
+using System.Text;
+using Atlas.SearchTracking.Enums;
+using Microsoft.Azure.ServiceBus;
+using Newtonsoft.Json;
+
+namespace Atlas.SearchTracking.Clients
+{
+    public interface ISearchTrackingServiceBusClient
+    {
+          Task PublishSearchTrackingEvent<TEvent>(TEvent searchTrackingEvent, SearchTrackingEventType eventType);
+    }
+
+    public class SearchTrackingServiceBusClient : ISearchTrackingServiceBusClient
+    {
+        private readonly string connectionString;
+        private readonly string searchTrackingEventsTopicName;
+        private readonly string EventTypePropertyName = "EventType";
+
+        public SearchTrackingServiceBusClient(MessagingServiceBusSettings messagingServiceBusSettings)
+        {
+            connectionString = messagingServiceBusSettings.ConnectionString;
+            searchTrackingEventsTopicName = messagingServiceBusSettings.SearchTrackingEventsTopic;
+        }
+
+        public async Task PublishSearchTrackingEvent<TEvent>(TEvent searchTrackingEvent, SearchTrackingEventType eventType)
+        {
+            var json = JsonConvert.SerializeObject(searchTrackingEvent);
+            var message = new Message(Encoding.UTF8.GetBytes(json));
+
+            message.UserProperties[EventTypePropertyName] = eventType.ToString();
+
+            var client = new TopicClient(connectionString, searchTrackingEventsTopicName);
+            await client.SendAsync(message);
+        }
+    }
+}
