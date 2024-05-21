2e7bef6c3eee7ec69fa48b1aa1870e4ffd01db5a
Peter Chenery <peter.chenery@anthonynolan.org>
2024-05-21 09:56:02 +0100

    feature: #1274: CBT-1472: Add migration for search tracking tables

---
+++ Atlas.SearchTracking.Data/Models/SearchRequest.cs
@@ -0,0 +1,67 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Atlas.SearchTracking.Data.Models
+{
+    public class SearchRequest
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public Guid SearchRequestId { get; set; }
+
+        public bool? IsRepeatSearch { get; set; }
+
+        public Guid? OriginalSearchRequestId { get; set; }
+
+        public DateTime? RepeatSearchCutOffDate { get; set; }
+
+        [Required]
+        public string RequestJson { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        public string SearchCriteria { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        public string DonorType { get; set; }
+
+        [Required]
+        public DateTime RequestTimeUTC { get; set; }
+
+        public bool? MatchingAlgorithm_IsSuccessful { get; set; }
+
+        public string? MatchingAlgorithm_FailureInfo_Json { get; set; }
+
+        public byte? MatchingAlgorithm_TotalAttemptsNumber { get; set; }
+
+        public int? MatchingAlgorithm_NumberOfMatching { get; set; }
+
+        public int? MatchingAlgorithm_NumberOfNoLongerMatching { get; set; }
+
+        public int? MatchingAlgorithm_NumberOfResults { get; set; }
+
+        [MaxLength(10)]
+        public string? MatchingAlgorithm_HlaNomenclatureVersion { get; set; }
+
+        public bool? MatchingAlgorithm_ResultsSent { get; set; }
+
+        public DateTime? MatchingAlgorithm_ResultsSentTimeUTC { get; set; }
+
+        public bool? MatchPrediction_IsSuccessful { get; set; }
+
+        public string? MatchPrediction_FailureInfo_Json { get; set; }
+
+        public int? MatchPrediction_DonorsPerBatch { get; set; }
+
+        public int? MatchPrediction_TotalNumberOfBatches { get; set; }
+
+        public bool? ResultsSent { get; set; }
+
+        public DateTime? ResultsSentTimeUTC { get; set; }
+
+        public SearchRequestMatchPredictionTiming? SearchRequestMatchPredictionTiming { get; set; }
+
+        public ICollection<SearchRequestMatchingAlgorithmAttemptTiming>? SearchRequestMatchingAlgorithmAttemptTimings { get; set; }
+    }
+}
---
+++ Atlas.SearchTracking.Data/Models/SearchRequestMatchingAlgorithmAttemptTiming.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Atlas.SearchTracking.Data.Models
+{
+    public class SearchRequestMatchingAlgorithmAttemptTiming
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [ForeignKey("SearchRequest")]
+        public int SearchRequestId { get; set; }
+
+        public SearchRequest SearchRequest { get; set; }
+
+        [Required]
+        public byte AttemptNumber { get; set; }
+
+        [Required]
+        public DateTime InitiationTimeUtc { get; set; }
+
+        [Required]
+        public DateTime StartTimeUtc { get; set; }
+
+        public DateTime? AlgorithmCore_Matching_StartTimeUtc { get; set; }
+
+        public DateTime? AlgorithmCore_Matching_EndTimeUtc { get; set; }
+
+        public DateTime? AlgorithmCore_Scoring_StartTimeUtc { get; set; }
+
+        public DateTime? AlgorithmCore_Scoring_EndTimeUtc { get; set; }
+
+        public DateTime? PersistingResults_StartTimeUtc { get; set; }
+
+        public DateTime? PersistingResults_EndTimeUtc { get; set; }
+
+        public DateTime? CompletionTimeUtc { get; set; }
+    }
+
+    internal static class SearchRequestMatchingAlgorithmAttemptTimingModelBuilder
+    {
+        public static void SetUpModel(this EntityTypeBuilder<SearchRequestMatchingAlgorithmAttemptTiming> modelBuilder)
+        {
+            modelBuilder.HasIndex(d => new { d.SearchRequestId, d.AttemptNumber })
+                .HasDatabaseName("IX_SearchRequestId_And_AttemptNumber")
+                .IsUnique();
+        }
+    }
+}
