5617769bb56a07ba3fdca7e6fb38be95227a8390
pchenery <peter.chenery@anthonynolan.org>
2024-06-10 10:00:08 +0100

    feature: #1274: CBT-1472: Add unit tests for DAL (#1316)

---
+++ Atlas.SearchTracking.Data.Test/Repositories/SearchRequestRepositoryTests.cs
@@ -0,0 +1,141 @@
+﻿using Atlas.SearchTracking.Data.Repositories;
+using Atlas.SearchTracking.Data.Test.Builders;
+using Atlas.SearchTracking.Data.Test.TestHelpers;
+using Atlas.SearchTracking.Models;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Atlas.SearchTracking.Data.Test.Repositories
+{
+    [TestFixture]
+    public class SearchRequestRepositoryTests : SearchTrackingContextTestBase
+    {
+        private ISearchRequestRepository searchRequestRepository;
+
+        [SetUp]
+        public async Task SetUp()
+        {
+            await SetUpBase();
+            searchRequestRepository = new SearchRequestRepository(SearchTrackingContext);
+            await InitiateData();
+        }
+
+        [TearDown]
+        public async Task TearDown()
+        {
+            await TearDownBase();
+        }
+
+        [Test]
+        public async Task GetSearchRequest_WhenRecordNotInDb_ThrowsException()
+        {
+            const int expectedSearchRequestId = 100;
+
+            var act = async () => await searchRequestRepository.GetSearchRequestById(expectedSearchRequestId);
+
+            act.Should().Throw<Exception>().WithMessage($"Search request with id {expectedSearchRequestId} not found");
+        }
+
+        [Test]
+        public async Task TrackSearchRequestedEvent_WithValidData_IsAddedToDb()
+        {
+            const int expectedSearchRequestId = 2;
+            var expectedSearchRequestEntity = SearchRequestEntityBuilder.NewRecord.Build();
+
+            var searchRequestedEvent = new SearchRequestedEvent
+            {
+                SearchRequestId = expectedSearchRequestEntity.SearchRequestId,
+                IsRepeatSearch = expectedSearchRequestEntity.IsRepeatSearch,
+                OriginalSearchRequestId = expectedSearchRequestEntity.OriginalSearchRequestId,
+                RepeatSearchCutOffDate = expectedSearchRequestEntity.RepeatSearchCutOffDate,
+                RequestJson = expectedSearchRequestEntity.RequestJson,
+                SearchCriteria = expectedSearchRequestEntity.SearchCriteria,
+                DonorType = expectedSearchRequestEntity.DonorType,
+                RequestTimeUtc = expectedSearchRequestEntity.RequestTimeUTC
+            };
+
+            await searchRequestRepository.TrackSearchRequestedEvent(searchRequestedEvent);
+
+            var actualSearchRequestEntity = await searchRequestRepository.GetSearchRequestById(expectedSearchRequestId);
+
+            expectedSearchRequestEntity.Should().BeEquivalentTo(actualSearchRequestEntity);
+        }
+
+        [Test]
+        public async Task TrackMatchPredictionCompletedEvent_WhenCompleted_UpdatesSearchRequestInDb()
+        {
+            var expectedSearchRequestEntity = SearchRequestEntityBuilder.WithMatchingPredictionCompleted.Build();
+
+            var matchPredictionCompletedEvent = new MatchPredictionCompletedEvent
+            {
+                SearchRequestId = expectedSearchRequestEntity.Id,
+                CompletionDetails = new MatchPredictionCompletionDetails
+                {
+                    IsSuccessful = expectedSearchRequestEntity.MatchPrediction_IsSuccessful.Value,
+                    FailureInfoJson = expectedSearchRequestEntity.MatchPrediction_FailureInfo_Json,
+                    DonorsPerBatch = expectedSearchRequestEntity.MatchPrediction_DonorsPerBatch.Value,
+                    TotalNumberOfBatches = expectedSearchRequestEntity.MatchPrediction_TotalNumberOfBatches.Value
+                }
+            };
+
+            await searchRequestRepository.TrackMatchPredictionCompletedEvent(matchPredictionCompletedEvent);
+
+            var actualSearchRequestEntity = await searchRequestRepository.GetSearchRequestById(expectedSearchRequestEntity.Id);
+
+            expectedSearchRequestEntity.Should().BeEquivalentTo(actualSearchRequestEntity);
+        }
+
+        [Test]
+        public async Task TrackMatchingAlgorithmCompletedEvent_WhenCompleted_UpdatesSearchRequestInDb()
+        {
+            var expectedSearchRequestEntity = SearchRequestEntityBuilder.WithMatchingAlgorithmCompleted.Build();
+
+            var matchingAlgorithmCompletedEvent = new MatchingAlgorithmCompletedEvent
+            {
+                SearchRequestId = expectedSearchRequestEntity.Id,
+                HlaNomenclatureVersion = expectedSearchRequestEntity.MatchingAlgorithm_HlaNomenclatureVersion,
+                ResultsSent = expectedSearchRequestEntity.MatchingAlgorithm_ResultsSent.Value,
+                ResultsSentTimeUtc = expectedSearchRequestEntity.MatchingAlgorithm_ResultsSentTimeUTC,
+                CompletionDetails = new MatchingAlgorithmCompletionDetails
+                {
+                    IsSuccessful = expectedSearchRequestEntity.MatchingAlgorithm_IsSuccessful.Value,
+                    FailureInfoJson = expectedSearchRequestEntity.MatchingAlgorithm_FailureInfo_Json,
+                    TotalAttemptsNumber = expectedSearchRequestEntity.MatchingAlgorithm_TotalAttemptsNumber.Value,
+                    NumberOfMatching = expectedSearchRequestEntity.MatchingAlgorithm_NumberOfMatching.Value,
+                    NumberOfResults = expectedSearchRequestEntity.MatchingAlgorithm_NumberOfResults.Value,
+                    RepeatSearchResultsDetails = new MatchingAlgorithmRepeatSearchResultsDetails
+                    {
+                        AddedResultCount = expectedSearchRequestEntity.MatchingAlgorithm_RepeatSearch_AddedResultCount.Value,
+                        RemovedResultCount = expectedSearchRequestEntity.MatchingAlgorithm_RepeatSearch_RemovedResultCount.Value,
+                        UpdatedResultCount = expectedSearchRequestEntity.MatchingAlgorithm_RepeatSearch_UpdatedResultCount.Value
+                    }
+                }
+            };
+
+            await searchRequestRepository.TrackMatchingAlgorithmCompletedEvent(matchingAlgorithmCompletedEvent);
+
+            var actualSearchRequestEntity = await searchRequestRepository.GetSearchRequestById(expectedSearchRequestEntity.Id);
+
+            expectedSearchRequestEntity.Should().BeEquivalentTo(actualSearchRequestEntity);
+        }
+
+        [Test]
+        public async Task TrackSearchRequestCompletedEvent_WhenCompleted_UpdatesSearchRequestInDb()
+        {
+            var expectedSearchRequestEntity = SearchRequestEntityBuilder.WithSearchRequestCompleted.Build();
+
+            var searchRequestCompletedEvent = new SearchRequestCompletedEvent
+            {
+                SearchRequestId = expectedSearchRequestEntity.Id,
+                ResultsSent = expectedSearchRequestEntity.ResultsSent.Value,
+                ResultsSentTimeUtc = expectedSearchRequestEntity.ResultsSentTimeUTC.Value
+            };
+
+            await searchRequestRepository.TrackSearchRequestCompletedEvent(searchRequestCompletedEvent);
+
+            var actualSearchRequestEntity = await searchRequestRepository.GetSearchRequestById(expectedSearchRequestEntity.Id);
+
+            expectedSearchRequestEntity.Should().BeEquivalentTo(actualSearchRequestEntity);
+        }
+    }
+}
---
+++ Atlas.SearchTracking.Data.Test/TestHelpers/SqliteMemoryContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data;
+using Atlas.SearchTracking.Data.Context;
+using Microsoft.Data.Sqlite;
+
+namespace Atlas.SearchTracking.Data.Test.TestHelpers
+{
+    public static class SqliteMemoryContext
+    {
+        /// <summary>
+        /// Connection must be closed in TearDown
+        /// </summary>
+        public static (IDbConnection, SearchTrackingContext) Create()
+        {
+            var connection = new SqliteConnection("DataSource=:memory:");
+            connection.Open();
+
+            var options = new DbContextOptionsBuilder<SearchTrackingContext>()
+                .UseSqlite(connection).Options;
+
+            return (connection, new SearchTrackingContext(options));
+        }
+    }
+}
