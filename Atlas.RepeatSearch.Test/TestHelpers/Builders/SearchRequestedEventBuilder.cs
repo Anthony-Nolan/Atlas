c88b7e46d584db2198b3d7837a33eb4eab5e3293
Peter Chenery <peter.chenery@anthonynolan.org>
2024-07-08 14:41:03 +0100

    fix: #1274: CBT-1475: fix unit test

--- Atlas.RepeatSearch.Test/TestHelpers/Builders/SearchRequestedEventBuilder.cs
+++ Atlas.RepeatSearch.Test/TestHelpers/Builders/SearchRequestedEventBuilder.cs
@@ -12,7 +12,8 @@ namespace Atlas.RepeatSearch.Test.TestHelpers.Builders
                 .With(x => x.SearchRequestId, new Guid("aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee"))
                 .With(x => x.IsRepeatSearch, true)
                 .With(x => x.OriginalSearchRequestId, new Guid("11111111-2222-3333-4444-555555555555"))
-                .With(x => x.RepeatSearchCutOffDate, DateTime.UtcNow)
+                .With(x => x.RepeatSearchCutOffDate,
+                    new DateTime(2024, 5, 11, 14, 30, 45, 0, 0, DateTimeKind.Utc))
                 .With(x => x.SearchCriteria, "6/6")
                 .With(x => x.DonorType, "Adult")
                 .With(x => x.RequestTimeUtc, DateTime.UtcNow);
