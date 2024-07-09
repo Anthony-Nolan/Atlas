137d79a5c9870abc38f4cc0a2ae7dfad58c89668
Sergey Ezhov <sezhov@nmdp.org>
2024-07-09 17:18:05 +0200

    update migration to use pure sql

--- Atlas.MatchingAlgorithm.Data/Migrations/20240709124454_AddDonorTypeAndRegistryCodeIndex.cs
+++ Atlas.MatchingAlgorithm.Data/Migrations/20240709124454_AddDonorTypeAndRegistryCodeIndex.cs
@@ -7,37 +7,46 @@ namespace Atlas.MatchingAlgorithm.Data.Migrations
     public partial class AddDonorTypeAndRegistryCodeIndex : Migration
     {
         private const string createIndexSql = @"
-if not exists (SELECT 1 FROM sys.indexes WHERE name = 'IX_Donors_DonorType_RegistryCode' AND object_id = OBJECT_ID('dbo.Donors')) 
-	CREATE INDEX [IX_Donors_DonorType_RegistryCode] ON [Donors] ([DonorType], [RegistryCode]);
-";
+            if not exists (SELECT 1 FROM sys.indexes WHERE name = 'IX_Donors_DonorType_RegistryCode' AND object_id = OBJECT_ID('dbo.Donors')) 
+	            CREATE INDEX [IX_Donors_DonorType_RegistryCode] ON [Donors] ([DonorType], [RegistryCode]);
+            ";
+
+        private const string changeColumnsTypeSql = @"
+            ALTER TABLE [Donors] ALTER COLUMN [EthnicityCode] nvarchar(256) NULL;
+            ALTER TABLE [Donors] ALTER COLUMN [RegistryCode] nvarchar(256) NULL;
+            ";
 
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.AlterColumn<string>(
-                name: "RegistryCode",
-                table: "Donors",
-                type: "nvarchar(256)",
-                maxLength: 256,
-                nullable: true,
-                oldClrType: typeof(string),
-                oldType: "nvarchar(max)",
-                oldNullable: true);
+            //migrationBuilder.AlterColumn<string>(
+            //    name: "RegistryCode",
+            //    table: "Donors",
+            //    type: "nvarchar(256)",
+            //    maxLength: 256,
+            //    nullable: true,
+            //    oldClrType: typeof(string),
+            //    oldType: "nvarchar(max)",
+            //    oldNullable: true);
 
-            migrationBuilder.AlterColumn<string>(
-                name: "EthnicityCode",
-                table: "Donors",
-                type: "nvarchar(256)",
-                maxLength: 256,
-                nullable: true,
-                oldClrType: typeof(string),
-                oldType: "nvarchar(max)",
-                oldNullable: true);
+            //migrationBuilder.AlterColumn<string>(
+            //    name: "EthnicityCode",
+            //    table: "Donors",
+            //    type: "nvarchar(256)",
+            //    maxLength: 256,
+            //    nullable: true,
+            //    oldClrType: typeof(string),
+            //    oldType: "nvarchar(max)",
+            //    oldNullable: true);
 
             //migrationBuilder.CreateIndex(
             //    name: "IX_Donors_DonorType_RegistryCode",
             //    table: "Donors",
             //    columns: new[] { "DonorType", "RegistryCode" });
 
+            // Leaving original migration code above
+
+
+            migrationBuilder.Sql(changeColumnsTypeSql);
             migrationBuilder.Sql(createIndexSql);
         }
 
