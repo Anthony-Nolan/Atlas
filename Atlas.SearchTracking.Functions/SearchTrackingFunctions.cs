57b89cc610fcfa2fdf501e5e8e5493a6559a8701
Peter Chenery <peter.chenery@anthonynolan.org>
2024-07-29 12:00:16 +0100

    feature: CBT-1475: Create constant file

--- Atlas.SearchTracking.Functions/SearchTrackingFunctions.cs
+++ Atlas.SearchTracking.Functions/SearchTrackingFunctions.cs
@@ -3,12 +3,14 @@ using Atlas.SearchTracking.Services;
 using Azure.Messaging.ServiceBus;
 using Microsoft.Azure.Functions.Worker;
 using System.Text;
+using Atlas.SearchTracking.Common.Config;
 
 namespace Atlas.SearchTracking.Functions
 {
     public class SearchTrackingFunctions
     {
         private readonly ISearchTrackingEventProcessor searchTrackingEventProcessor;
+        private const string EventType = SearchTrackingConstants.SearchTrackingEventType;
 
         public SearchTrackingFunctions(ISearchTrackingEventProcessor searchTrackingEventProcessor)
         {
@@ -23,7 +25,7 @@ namespace Atlas.SearchTracking.Functions
                 ServiceBusReceivedMessage message)
         {
             var body = Encoding.UTF8.GetString(message.Body);
-            var eventType = Enum.Parse<SearchTrackingEventType>(message.ApplicationProperties.GetValueOrDefault("EventType").ToString());
+            var eventType = Enum.Parse<SearchTrackingEventType>(message.ApplicationProperties.GetValueOrDefault(EventType).ToString());
 
             await searchTrackingEventProcessor.HandleEvent(body, eventType);
         }
