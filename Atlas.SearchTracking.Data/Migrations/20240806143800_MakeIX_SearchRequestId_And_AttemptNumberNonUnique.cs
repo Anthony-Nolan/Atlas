616a7503ed39d941f1de9cdbd8949599d73d6025
pchenery <peter.chenery@anthonynolan.org>
2024-08-06 15:54:05 +0100

    feature: CBT-1476: Make IX_SearchRequestId_And_AttemptNumber index non unique (#1349)

---
+++ Atlas.SearchTracking.Data/Migrations/20240806143800_MakeIX_SearchRequestId_And_AttemptNumberNonUnique.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Atlas.SearchTracking.Data.Migrations
+{
+    public partial class MakeIX_SearchRequestId_And_AttemptNumberNonUnique : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_SearchRequestId_And_AttemptNumber",
+                schema: "SearchTracking",
+                table: "SearchRequestMatchingAlgorithmAttemptTimings");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SearchRequestId_And_AttemptNumber",
+                schema: "SearchTracking",
+                table: "SearchRequestMatchingAlgorithmAttemptTimings",
+                columns: new[] { "SearchRequestId", "AttemptNumber" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_SearchRequestId_And_AttemptNumber",
+                schema: "SearchTracking",
+                table: "SearchRequestMatchingAlgorithmAttemptTimings");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SearchRequestId_And_AttemptNumber",
+                schema: "SearchTracking",
+                table: "SearchRequestMatchingAlgorithmAttemptTimings",
+                columns: new[] { "SearchRequestId", "AttemptNumber" },
+                unique: true);
+        }
+    }
+}
