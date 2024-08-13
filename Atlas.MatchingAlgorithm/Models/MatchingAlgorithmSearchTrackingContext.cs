e3a7f27c2ecc444b31bb7cfa562b7e05aac209c4
pchenery <peter.chenery@anthonynolan.org>
2024-08-13 16:47:54 +0100

    feature: CBT-1476: Send matching algorithm attempt started event

---
+++ Atlas.MatchingAlgorithm/Models/MatchingAlgorithmSearchTrackingContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Atlas.MatchingAlgorithm.Models
+{
+    public class MatchingAlgorithmSearchTrackingContext
+    {
+        public Guid SearchRequestId { get; set; }
+        public byte AttemptNumber { get; set; }
+    }
+}
---
+++ Atlas.MatchingAlgorithm/Services/Search/MatchingAlgorithmSearchTrackingContextManager.cs
@@ -0,0 +1,26 @@
+﻿using Atlas.MatchingAlgorithm.Models;
+
+namespace Atlas.MatchingAlgorithm.Services.Search
+{
+    public interface IMatchingAlgorithmSearchTrackingContextManager
+    {
+        void Set(MatchingAlgorithmSearchTrackingContext context);
+
+        MatchingAlgorithmSearchTrackingContext Retrieve();
+    }
+
+    public class MatchingAlgorithmSearchTrackingContextManager(MatchingAlgorithmSearchTrackingContext currentContext)
+        : IMatchingAlgorithmSearchTrackingContextManager
+    {
+        public void Set(MatchingAlgorithmSearchTrackingContext context)
+        {
+            currentContext.SearchRequestId = context.SearchRequestId;
+            currentContext.AttemptNumber = context.AttemptNumber;
+        }
+
+        public MatchingAlgorithmSearchTrackingContext Retrieve()
+        {
+            return currentContext;
+        }
+    }
+}
