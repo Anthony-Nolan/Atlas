539c13d1e0d49d5f6e2ca00299362564428c7ca0
pchenery <peter.chenery@anthonynolan.org>
2024-10-01 11:47:49 +0100

    feature: CBT-1477: Change datatype of search tracking matching algorithm failure info type to string (#1372)

---
+++ Atlas.SearchTracking.Data/Migrations/20241001100714_ChangeSearchTrackingMatchingAlgorithmFailureInfoTypeToString.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Atlas.SearchTracking.Data.Migrations
+{
+    public partial class ChangeSearchTrackingMatchingAlgorithmFailureInfoTypeToString : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "MatchingAlgorithm_FailureInfo_Type",
+                schema: "SearchTracking",
+                table: "SearchRequests",
+                type: "nvarchar(50)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "MatchingAlgorithm_FailureInfo_Type",
+                schema: "SearchTracking",
+                table: "SearchRequests",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldNullable: true);
+        }
+    }
+}
