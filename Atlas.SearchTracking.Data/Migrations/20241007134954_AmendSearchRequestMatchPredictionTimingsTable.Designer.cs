02ec509fa2ef9eec265cbe4e2606970318548a92
pchenery <peter.chenery@anthonynolan.org>
2024-10-07 16:11:43 +0100

    feature: CBT-1477: Add columns to SearchRequestMatchPredictionTimings table (#1373)

--- Atlas.SearchTracking.Data/Migrations/20241007134954_AmendSearchRequestMatchPredictionTimingsTable.Designer.cs
+++ Atlas.SearchTracking.Data/Migrations/20241007134954_AmendSearchRequestMatchPredictionTimingsTable.Designer.cs
@@ -4,6 +4,7 @@ using Atlas.SearchTracking.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Atlas.SearchTracking.Data.Migrations
 {
     [DbContext(typeof(SearchTrackingContext))]
-    partial class SearchTrackingContextModelSnapshot : ModelSnapshot
+    [Migration("20241007134954_AmendSearchRequestMatchPredictionTimingsTable")]
+    partial class AmendSearchRequestMatchPredictionTimingsTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Atlas.SearchTracking.Data/Migrations/20241007134954_AmendSearchRequestMatchPredictionTimingsTable.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Atlas.SearchTracking.Data.Migrations
+{
+    public partial class AmendSearchRequestMatchPredictionTimingsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FailureInfo_ExceptionStacktrace",
+                schema: "SearchTracking",
+                table: "SearchRequestMatchPredictionTimings",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FailureInfo_Message",
+                schema: "SearchTracking",
+                table: "SearchRequestMatchPredictionTimings",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FailureInfo_Type",
+                schema: "SearchTracking",
+                table: "SearchRequestMatchPredictionTimings",
+                type: "nvarchar(50)",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FailureInfo_Type",
+                schema: "SearchTracking",
+                table: "SearchRequestMatchingAlgorithmAttempts",
+                type: "nvarchar(50)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FailureInfo_ExceptionStacktrace",
+                schema: "SearchTracking",
+                table: "SearchRequestMatchPredictionTimings");
+
+            migrationBuilder.DropColumn(
+                name: "FailureInfo_Message",
+                schema: "SearchTracking",
+                table: "SearchRequestMatchPredictionTimings");
+
+            migrationBuilder.DropColumn(
+                name: "FailureInfo_Type",
+                schema: "SearchTracking",
+                table: "SearchRequestMatchPredictionTimings");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "FailureInfo_Type",
+                schema: "SearchTracking",
+                table: "SearchRequestMatchingAlgorithmAttempts",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldNullable: true);
+        }
+    }
+}
