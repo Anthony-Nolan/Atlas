3766e7d02e29f2120ecfa64668607ef341c36554
Peter Chenery <peter.chenery@anthonynolan.org>
2024-11-22 15:30:26 +0000

    feature: CBT-3345: Added Donor Management function health check

---
+++ Atlas.MatchingAlgorithm.Functions.DonorManagement/Functions/HealthCheckFunctions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs;
+using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace Atlas.MatchingAlgorithm.Functions.DonorManagement.Functions
+{
+    public class HealthCheckFunctions
+    {
+        private readonly HealthCheckService healthCheckService;
+
+        public HealthCheckFunctions(HealthCheckService healthCheckService)
+        {
+            this.healthCheckService = healthCheckService;
+        }
+
+        [FunctionName(nameof(HealthCheck))]
+        public async Task<IActionResult> HealthCheck([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequest request)
+        {
+            var healthStatus = await healthCheckService.CheckHealthAsync();
+            return new OkObjectResult(Enum.GetName(typeof(HealthStatus), healthStatus.Status));
+        }
+    }
+}
