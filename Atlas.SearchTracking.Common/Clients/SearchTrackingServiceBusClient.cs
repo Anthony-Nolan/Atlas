62a8a1e7011aa5948aaf1019522160e0c65fa9f9
Peter Chenery <peter.chenery@anthonynolan.org>
2025-06-16 09:32:03 +0100

    feature: CBT-4385: Add custom properties to Search Tracking message

--- Atlas.SearchTracking.Common/Clients/SearchTrackingServiceBusClient.cs
+++ Atlas.SearchTracking.Common/Clients/SearchTrackingServiceBusClient.cs
@@ -39,6 +39,10 @@ namespace Atlas.SearchTracking.Common.Clients
             var message = new Message(Encoding.UTF8.GetBytes(json));
 
             message.UserProperties[SearchTrackingConstants.EventType] = eventType.ToString();
+            message.UserProperties.Add("SearchIdentifier", typeof(TEvent).GetProperty("SearchIdentifier")?.ToString());
+            message.UserProperties.Add("OriginalSearchIdentifier", typeof(TEvent).GetProperty("OriginalSearchIdentifier")?.ToString());
+            message.UserProperties.Add("AttemptNumber", typeof(TEvent).GetProperty("AttemptNumber")?.ToString());
+
             var client = new TopicClient(connectionString, searchTrackingTopicName);
 
             using (new AsyncTransactionScope(TransactionScopeOption.Suppress))
