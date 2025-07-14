f48926b709f03f5102fd4eb5d24b4bfb6ce27e80
Daria Sorokina <84312905+daria-sorokina-da@users.noreply.github.com>
2025-07-14 20:54:37 +0200

    chore: Changed Search Tracking audit subscription expiry to be shorter (1 day)

--- terraform/core/modules/search_tracking/servicebus.tf
+++ terraform/core/modules/search_tracking/servicebus.tf
@@ -11,7 +11,7 @@ resource "azurerm_servicebus_subscription" "audit" {
   name                                 = "audit"
   topic_id                             = azurerm_servicebus_topic.search-tracking-events.id
   auto_delete_on_idle                  = var.default_servicebus_settings.long-expiry
-  default_message_ttl                  = var.default_servicebus_settings.audit-subscription-ttl-expiry
+  default_message_ttl                  = var.default_servicebus_settings.audit-subscription-short-ttl-expiry
   lock_duration                        = var.default_servicebus_settings.default-read-lock
   max_delivery_count                   = var.default_servicebus_settings.default-message-retries
   dead_lettering_on_message_expiration = false
