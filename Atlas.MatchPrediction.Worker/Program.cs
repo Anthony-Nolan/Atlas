e877a66efe06bdf9cb4c60f3e01dcd204848aa7b
Sergey Salin <91747559+SergeySalinAtDataart@users.noreply.github.com>
2026-04-24 13:33:22 +0200

    feat: ATL-14: dockerise + add healthchecks

--- Atlas.MatchPrediction.Worker/Program.cs
+++ Atlas.MatchPrediction.Worker/Program.cs
@@ -1,8 +1,22 @@
 using Atlas.MatchPrediction.Worker;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
-var builder = Host.CreateApplicationBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
 Startup.Configure(builder.Services);
 
-var host = builder.Build();
-host.Run();
+var app = builder.Build();
+
+app.MapHealthChecks("/health/live", new HealthCheckOptions
+    {
+        Predicate = check => check.Tags.Contains("live")
+    }
+);
+
+app.MapHealthChecks("/health/ready", new HealthCheckOptions
+    {
+        Predicate = check => check.Tags.Contains("ready")
+    }
+);
+
+app.Run();
