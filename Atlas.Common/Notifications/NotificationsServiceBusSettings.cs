14cfdff5eeac34cfb62b26145c138564656343c5
Sergey Salin <91747559+SergeySalinAtDataart@users.noreply.github.com>
2026-04-30 15:27:25 +0200

    chore: ATL-51: implement startup settings validation

--- Atlas.Common/Notifications/NotificationsServiceBusSettings.cs
+++ Atlas.Common/Notifications/NotificationsServiceBusSettings.cs
@@ -1,11 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Atlas.Common.Notifications
 {
     public class NotificationsServiceBusSettings
     {
+        [Required(AllowEmptyStrings = false)]
         public string ConnectionString { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
         public string AlertsTopic { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
         public string NotificationsTopic { get; set; }
+
+        [Range(0, int.MaxValue)]
         public int SendRetryCount { get; set; }
+
+        [Range(0, int.MaxValue)]
         public int SendRetryCooldownSeconds { get; set; }
     }
 }
