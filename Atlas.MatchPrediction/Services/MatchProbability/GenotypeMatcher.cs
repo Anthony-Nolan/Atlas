aa3684311c3bbf5b1def9235326e45057a19925b
Sergey Salin <91747559+SergeySalinAtDataart@users.noreply.github.com>
2026-05-05 13:00:37 +0200

    chore: ATL-57: small refactoring

--- Atlas.MatchPrediction/Services/MatchProbability/GenotypeMatcher.cs
+++ Atlas.MatchPrediction/Services/MatchProbability/GenotypeMatcher.cs
@@ -12,161 +12,160 @@ using System.Threading.Tasks;
 using Atlas.Common.Public.Models.MatchPrediction;
 using static Atlas.Common.Maths.Combinations;
 
-namespace Atlas.MatchPrediction.Services.MatchProbability
+namespace Atlas.MatchPrediction.Services.MatchProbability;
+
+public class GenotypeMatcherInput
 {
-    public class GenotypeMatcherInput
-    {
-        public SubjectData PatientData { get; set; }
-        public SubjectData DonorData { get; set; }
-        public SubjectGenotypeSet PatientGenotypeSet { get; set; }
-        public MatchPredictionParameters MatchPredictionParameters { get; set; }
-    }
+    public SubjectData PatientData { get; set; }
+    public SubjectData DonorData { get; set; }
+    public SubjectGenotypeSet PatientGenotypeSet { get; set; }
+    public MatchPredictionParameters MatchPredictionParameters { get; set; }
+}
 
-    public class GenotypeMatcherResult
-    {
-        public SubjectResult PatientResult { get; set; }
-        public SubjectResult DonorResult { get; set; }
-        public IEnumerable<GenotypeMatchDetails> GenotypeMatchDetails { get; set; }
+public class GenotypeMatcherResult
+{
+    public SubjectResult PatientResult { get; set; }
+    public SubjectResult DonorResult { get; set; }
+    public IEnumerable<GenotypeMatchDetails> GenotypeMatchDetails { get; set; }
 
-        public class SubjectResult
+    public class SubjectResult
+    {
+        public bool IsUnrepresented { get; set; }
+
+        /// <summary>
+        /// Note: This was added for debugging purposes, it is isn't (currently) needed for calculating match probabilities.
+        /// </summary>
+        public int GenotypeCount { get; set; }
+
+        public decimal SumOfLikelihoods { get; set; }
+
+        public SubjectResult(bool isUnrepresented, int genotypeCount, decimal sumOfLikelihoods)
         {
-            public bool IsUnrepresented { get; set; }
-
-            /// <summary>
-            /// Note: This was added for debugging purposes, it is isn't (currently) needed for calculating match probabilities.
-            /// </summary>
-            public int GenotypeCount { get; set; }
-
-            public decimal SumOfLikelihoods { get; set; }
-
-            public SubjectResult(bool isUnrepresented, int genotypeCount, decimal sumOfLikelihoods)
-            {
-                IsUnrepresented = isUnrepresented;
-                GenotypeCount = genotypeCount;
-                SumOfLikelihoods = sumOfLikelihoods;
-            }
+            IsUnrepresented = isUnrepresented;
+            GenotypeCount = genotypeCount;
+            SumOfLikelihoods = sumOfLikelihoods;
         }
     }
+}
 
-    public interface IGenotypeMatcher
+public interface IGenotypeMatcher
+{
+    Task<GenotypeMatcherResult> MatchPatientDonorGenotypes(GenotypeMatcherInput input);
+}
+
+internal class GenotypeMatcher : IGenotypeMatcher
+{
+    private readonly IGenotypeSetService genotypeSetService;
+    private readonly IMatchCalculationService matchCalculationService;
+    private readonly ILogger logger;
+
+    public GenotypeMatcher(
+        IGenotypeSetService genotypeSetService,
+        IMatchCalculationService matchCalculationService,
+        // ReSharper disable once SuggestBaseTypeForParameterInConstructor
+        IMatchPredictionLogger<MatchProbabilityLoggingContext> logger)
     {
-        Task<GenotypeMatcherResult> MatchPatientDonorGenotypes(GenotypeMatcherInput input);
+        this.genotypeSetService = genotypeSetService;
+        this.matchCalculationService = matchCalculationService;
+        this.logger = logger;
     }
 
-    internal class GenotypeMatcher : IGenotypeMatcher
+    /// <inheritdoc />
+    public async Task<GenotypeMatcherResult> MatchPatientDonorGenotypes(GenotypeMatcherInput input)
     {
-        private readonly IGenotypeSetService genotypeSetService;
-        private readonly IMatchCalculationService matchCalculationService;
-        private readonly ILogger logger;
+        var patientGenotypeSet = input.PatientGenotypeSet;
+        var donorGenotypeSet = await genotypeSetService.GetGenotypeSet(input.DonorData, input.MatchPredictionParameters);
 
-        public GenotypeMatcher(
-            IGenotypeSetService genotypeSetService,
-            IMatchCalculationService matchCalculationService,
-            // ReSharper disable once SuggestBaseTypeForParameterInConstructor
-            IMatchPredictionLogger<MatchProbabilityLoggingContext> logger)
+        if (patientGenotypeSet.IsUnrepresented || donorGenotypeSet.IsUnrepresented)
         {
-            this.genotypeSetService = genotypeSetService;
-            this.matchCalculationService = matchCalculationService;
-            this.logger = logger;
-        }
-
-        /// <inheritdoc />
-        public async Task<GenotypeMatcherResult> MatchPatientDonorGenotypes(GenotypeMatcherInput input)
-        {
-            var patientGenotypeSet = input.PatientGenotypeSet;
-            var donorGenotypeSet = await genotypeSetService.GetGenotypeSet(input.DonorData, input.MatchPredictionParameters);
-
-            if (patientGenotypeSet.IsUnrepresented || donorGenotypeSet.IsUnrepresented)
-            {
-                return new GenotypeMatcherResult
-                {
-                    PatientResult = new GenotypeMatcherResult.SubjectResult(
-                        patientGenotypeSet.IsUnrepresented,
-                        patientGenotypeSet.Genotypes.Count,
-                        patientGenotypeSet.SumOfLikelihoods),
-                    DonorResult = new GenotypeMatcherResult.SubjectResult(
-                        donorGenotypeSet.IsUnrepresented,
-                        donorGenotypeSet.Genotypes.Count,
-                        donorGenotypeSet.SumOfLikelihoods)
-                };
-            }
-
-            var pdpCount = NumberOfPairsOfCartesianProduct(donorGenotypeSet.Genotypes, patientGenotypeSet.Genotypes);
-            logger.SendTrace($"Patient/donor pairs: {pdpCount:n0}");
-
-            var allPatientDonorCombinations = CombineGenotypes(patientGenotypeSet.Genotypes, donorGenotypeSet.Genotypes);
-
-            using var matchCountLogger = MatchCountLogger(pdpCount);
-
             return new GenotypeMatcherResult
             {
                 PatientResult = new GenotypeMatcherResult.SubjectResult(
-                    false,
+                    patientGenotypeSet.IsUnrepresented,
                     patientGenotypeSet.Genotypes.Count,
                     patientGenotypeSet.SumOfLikelihoods),
-
                 DonorResult = new GenotypeMatcherResult.SubjectResult(
-                    false,
+                    donorGenotypeSet.IsUnrepresented,
                     donorGenotypeSet.Genotypes.Count,
-                    donorGenotypeSet.SumOfLikelihoods),
-
-                GenotypeMatchDetails = CalculatePairsMatchCounts(allPatientDonorCombinations, input.MatchPredictionParameters.AllowedLoci, matchCountLogger)
+                    donorGenotypeSet.SumOfLikelihoods)
             };
         }
 
-        private IEnumerable<Tuple<GenotypeAtDesiredResolutions, GenotypeAtDesiredResolutions>> CombineGenotypes(
-            IEnumerable<GenotypeAtDesiredResolutions> patientGenotypes,
-            ICollection<GenotypeAtDesiredResolutions> donorGenotypes)
-        {
-            using (logger.RunTimed("Combining patient/donor genotypes", LogLevel.Verbose))
-            {
-                var combinations = patientGenotypes.SelectMany(patientHla =>
-                    donorGenotypes.Select(donorHla =>
-                        new Tuple<GenotypeAtDesiredResolutions, GenotypeAtDesiredResolutions>(patientHla, donorHla)));
+        var pdpCount = NumberOfPairsOfCartesianProduct(donorGenotypeSet.Genotypes, patientGenotypeSet.Genotypes);
+        logger.SendTrace($"Patient/donor pairs: {pdpCount:n0}");
 
-                return combinations;
-            }
+        var allPatientDonorCombinations = CombineGenotypes(patientGenotypeSet.Genotypes, donorGenotypeSet.Genotypes);
+
+        using var matchCountLogger = MatchCountLogger(pdpCount);
+
+        return new GenotypeMatcherResult
+        {
+            PatientResult = new GenotypeMatcherResult.SubjectResult(
+                false,
+                patientGenotypeSet.Genotypes.Count,
+                patientGenotypeSet.SumOfLikelihoods),
+
+            DonorResult = new GenotypeMatcherResult.SubjectResult(
+                false,
+                donorGenotypeSet.Genotypes.Count,
+                donorGenotypeSet.SumOfLikelihoods),
+
+            GenotypeMatchDetails = CalculatePairsMatchCounts(allPatientDonorCombinations, input.MatchPredictionParameters.AllowedLoci, matchCountLogger)
+        };
+    }
+
+    private IEnumerable<Tuple<GenotypeAtDesiredResolutions, GenotypeAtDesiredResolutions>> CombineGenotypes(
+        IEnumerable<GenotypeAtDesiredResolutions> patientGenotypes,
+        ICollection<GenotypeAtDesiredResolutions> donorGenotypes)
+    {
+        using (logger.RunTimed("Combining patient/donor genotypes", LogLevel.Verbose))
+        {
+            var combinations = patientGenotypes.SelectMany(patientHla =>
+                donorGenotypes.Select(donorHla =>
+                    new Tuple<GenotypeAtDesiredResolutions, GenotypeAtDesiredResolutions>(patientHla, donorHla)));
+
+            return combinations;
         }
+    }
 
-        private ILongOperationLoggingStopwatch MatchCountLogger(long patientDonorPairCount) => logger.RunLongOperationWithTimer(
-            "Calculate match counts.",
-            new LongLoggingSettings
-            {
-                ExpectedNumberOfIterations = patientDonorPairCount,
-                // The higher this number, the slower the algorithm will be. Every 500,000 seems to be a good balance opf performance vs. Information.
-                InnerOperationLoggingPeriod = 500_000
-            },
-            LogLevel.Verbose
-        );
-
-        // This needs to return an IEnumerable to enable lazy evaluation - the list of combinations will be very long, so we do not want to enumerate 
-        // it more than once, so we leave it as an IEnumerable until match probability aggregation.
-        private IEnumerable<GenotypeMatchDetails> CalculatePairsMatchCounts(
-            IEnumerable<Tuple<GenotypeAtDesiredResolutions, GenotypeAtDesiredResolutions>> allPatientDonorCombinations,
-            ISet<Locus> allowedLoci,
-            ILongOperationLoggingStopwatch stopwatch)
+    private ILongOperationLoggingStopwatch MatchCountLogger(long patientDonorPairCount) => logger.RunLongOperationWithTimer(
+        "Calculate match counts.",
+        new LongLoggingSettings
         {
-            return allPatientDonorCombinations
-                .Select(pd =>
+            ExpectedNumberOfIterations = patientDonorPairCount,
+            // The higher this number, the slower the algorithm will be. Every 500,000 seems to be a good balance opf performance vs. Information.
+            InnerOperationLoggingPeriod = 500_000
+        },
+        LogLevel.Verbose
+    );
+
+    // This needs to return an IEnumerable to enable lazy evaluation - the list of combinations will be very long, so we do not want to enumerate 
+    // it more than once, so we leave it as an IEnumerable until match probability aggregation.
+    private IEnumerable<GenotypeMatchDetails> CalculatePairsMatchCounts(
+        IEnumerable<Tuple<GenotypeAtDesiredResolutions, GenotypeAtDesiredResolutions>> allPatientDonorCombinations,
+        ISet<Locus> allowedLoci,
+        ILongOperationLoggingStopwatch stopwatch)
+    {
+        return allPatientDonorCombinations
+            .Select(pd =>
+            {
+                using (stopwatch.TimeInnerOperation())
                 {
-                    using (stopwatch.TimeInnerOperation())
+                    var (patient, donor) = pd;
+                    return new GenotypeMatchDetails
                     {
-                        var (patient, donor) = pd;
-                        return new GenotypeMatchDetails
-                        {
-                            AvailableLoci = allowedLoci,
-                            DonorGenotype = donor.HaplotypeResolution,
-                            DonorGenotypeLikelihood = donor.GenotypeLikelihood,
-                            PatientGenotype = patient.HaplotypeResolution,
-                            PatientGenotypeLikelihood = patient.GenotypeLikelihood,
-                            MatchCounts = matchCalculationService.CalculateMatchCounts_Fast(
-                                patient.StringMatchableResolution,
-                                donor.StringMatchableResolution,
-                                allowedLoci
-                            )
-                        };
-                    }
-                });
-        }
+                        AvailableLoci = allowedLoci,
+                        DonorGenotype = donor.HaplotypeResolution,
+                        DonorGenotypeLikelihood = donor.GenotypeLikelihood,
+                        PatientGenotype = patient.HaplotypeResolution,
+                        PatientGenotypeLikelihood = patient.GenotypeLikelihood,
+                        MatchCounts = matchCalculationService.CalculateMatchCounts_Fast(
+                            patient.StringMatchableResolution,
+                            donor.StringMatchableResolution,
+                            allowedLoci
+                        )
+                    };
+                }
+            });
     }
 }
--- Atlas.MatchPrediction/Services/MatchProbability/MatchProbabilityService.cs
+++ Atlas.MatchPrediction/Services/MatchProbability/MatchProbabilityService.cs
@@ -18,105 +18,104 @@ using Atlas.Common.Public.Models.MatchPrediction;
 // ReSharper disable ParameterTypeCanBeEnumerable.Local
 // ReSharper disable SuggestBaseTypeForParameter
 
-namespace Atlas.MatchPrediction.Services.MatchProbability
+namespace Atlas.MatchPrediction.Services.MatchProbability;
+
+public interface IMatchProbabilityService
 {
-    public interface IMatchProbabilityService
+    public Task<MatchProbabilityResponse> CalculateMatchProbability(
+        SingleDonorMatchProbabilityInput singleDonorMatchProbabilityInput,
+        SubjectGenotypeSet patientGenotypeSet);
+}
+
+internal class MatchProbabilityService : IMatchProbabilityService
+{
+    private readonly IGenotypeMatcher genotypeMatcher;
+    private readonly IMatchProbabilityCalculator matchProbabilityCalculator;
+    private readonly IHaplotypeFrequencyService haplotypeFrequencyService;
+    private readonly ILogger logger;
+    private readonly MatchProbabilityLoggingContext matchProbabilityLoggingContext;
+
+    private record FrequencySets(SubjectFrequencySet Patient, SubjectFrequencySet Donor);
+
+    public MatchProbabilityService(
+        IMatchProbabilityCalculator matchProbabilityCalculator,
+        IHaplotypeFrequencyService haplotypeFrequencyService,
+        IGenotypeMatcher genotypeMatcher,
+        // ReSharper disable once SuggestBaseTypeForParameterInConstructor
+        IMatchPredictionLogger<MatchProbabilityLoggingContext> logger,
+        MatchProbabilityLoggingContext matchProbabilityLoggingContext)
     {
-        public Task<MatchProbabilityResponse> CalculateMatchProbability(
-            SingleDonorMatchProbabilityInput singleDonorMatchProbabilityInput,
-            SubjectGenotypeSet patientGenotypeSet);
+        this.matchProbabilityCalculator = matchProbabilityCalculator;
+        this.haplotypeFrequencyService = haplotypeFrequencyService;
+        this.genotypeMatcher = genotypeMatcher;
+        this.logger = logger;
+        this.matchProbabilityLoggingContext = matchProbabilityLoggingContext;
     }
 
-    internal class MatchProbabilityService : IMatchProbabilityService
+    public async Task<MatchProbabilityResponse> CalculateMatchProbability(
+        SingleDonorMatchProbabilityInput singleDonorMatchProbabilityInput,
+        SubjectGenotypeSet patientGenotypeSet)
     {
-        private readonly IGenotypeMatcher genotypeMatcher;
-        private readonly IMatchProbabilityCalculator matchProbabilityCalculator;
-        private readonly IHaplotypeFrequencyService haplotypeFrequencyService;
-        private readonly ILogger logger;
-        private readonly MatchProbabilityLoggingContext matchProbabilityLoggingContext;
+        await new MatchProbabilityInputValidator().ValidateAndThrowAsync(singleDonorMatchProbabilityInput);
 
-        private record FrequencySets(SubjectFrequencySet Patient, SubjectFrequencySet Donor);
+        matchProbabilityLoggingContext.Initialise(singleDonorMatchProbabilityInput);
 
-        public MatchProbabilityService(
-            IMatchProbabilityCalculator matchProbabilityCalculator,
-            IHaplotypeFrequencyService haplotypeFrequencyService,
-            IGenotypeMatcher genotypeMatcher,
-            // ReSharper disable once SuggestBaseTypeForParameterInConstructor
-            IMatchPredictionLogger<MatchProbabilityLoggingContext> logger,
-            MatchProbabilityLoggingContext matchProbabilityLoggingContext)
+        var frequencySets = await GetFrequencySets(singleDonorMatchProbabilityInput);
+        var allowedLoci = LocusSettings.MatchPredictionLoci.Except(singleDonorMatchProbabilityInput.ExcludedLoci).ToHashSet();
+
+        var matcherResult = await genotypeMatcher.MatchPatientDonorGenotypes(new GenotypeMatcherInput
         {
-            this.matchProbabilityCalculator = matchProbabilityCalculator;
-            this.haplotypeFrequencyService = haplotypeFrequencyService;
-            this.genotypeMatcher = genotypeMatcher;
-            this.logger = logger;
-            this.matchProbabilityLoggingContext = matchProbabilityLoggingContext;
+            PatientData = new SubjectData(singleDonorMatchProbabilityInput.PatientHla.ToPhenotypeInfo(), frequencySets.Patient),
+            DonorData = new SubjectData(singleDonorMatchProbabilityInput.Donor.DonorHla.ToPhenotypeInfo(), frequencySets.Donor),
+            PatientGenotypeSet = patientGenotypeSet,
+            MatchPredictionParameters = new MatchPredictionParameters(allowedLoci, singleDonorMatchProbabilityInput.MatchingAlgorithmHlaNomenclatureVersion)
+        });
+
+        if (matcherResult.PatientResult.IsUnrepresented || matcherResult.DonorResult.IsUnrepresented)
+        {
+            return new MatchProbabilityResponse(null, allowedLoci)
+            {
+                IsPatientPhenotypeUnrepresented = matcherResult.PatientResult.IsUnrepresented,
+                IsDonorPhenotypeUnrepresented = matcherResult.DonorResult.IsUnrepresented,
+                DonorHaplotypeFrequencySet = frequencySets.Donor.FrequencySet.ToClientHaplotypeFrequencySet(),
+                PatientHaplotypeFrequencySet = frequencySets.Patient.FrequencySet.ToClientHaplotypeFrequencySet()
+            };
         }
 
-        public async Task<MatchProbabilityResponse> CalculateMatchProbability(
-            SingleDonorMatchProbabilityInput singleDonorMatchProbabilityInput,
-            SubjectGenotypeSet patientGenotypeSet)
+        return CalculateMatchProbabilityFromMatcherResult(matcherResult, allowedLoci, frequencySets);
+    }
+
+    private async Task<FrequencySets> GetFrequencySets(SingleDonorMatchProbabilityInput singleDonorMatchProbabilityInput)
+    {
+        var frequencySets = await haplotypeFrequencyService.GetHaplotypeFrequencySets(
+            singleDonorMatchProbabilityInput.Donor.DonorFrequencySetMetadata,
+            singleDonorMatchProbabilityInput.PatientFrequencySetMetadata
+        );
+
+        return new FrequencySets(
+            new SubjectFrequencySet(frequencySets.PatientSet, "patient"),
+            new SubjectFrequencySet(frequencySets.DonorSet, "donor")
+        );
+    }
+
+    private MatchProbabilityResponse CalculateMatchProbabilityFromMatcherResult(
+        GenotypeMatcherResult matcherResult, 
+        HashSet<Locus> allowedLoci,
+        FrequencySets frequencySets)
+    {
+        using (logger.RunTimed("Calculate match probability", LogLevel.Verbose))
         {
-            await new MatchProbabilityInputValidator().ValidateAndThrowAsync(singleDonorMatchProbabilityInput);
-
-            matchProbabilityLoggingContext.Initialise(singleDonorMatchProbabilityInput);
-
-            var frequencySets = await GetFrequencySets(singleDonorMatchProbabilityInput);
-            var allowedLoci = LocusSettings.MatchPredictionLoci.Except(singleDonorMatchProbabilityInput.ExcludedLoci).ToHashSet();
-
-            var matcherResult = await genotypeMatcher.MatchPatientDonorGenotypes(new GenotypeMatcherInput
-            {
-                PatientData = new SubjectData(singleDonorMatchProbabilityInput.PatientHla.ToPhenotypeInfo(), frequencySets.Patient),
-                DonorData = new SubjectData(singleDonorMatchProbabilityInput.Donor.DonorHla.ToPhenotypeInfo(), frequencySets.Donor),
-                PatientGenotypeSet = patientGenotypeSet,
-                MatchPredictionParameters = new MatchPredictionParameters(allowedLoci, singleDonorMatchProbabilityInput.MatchingAlgorithmHlaNomenclatureVersion)
-            });
-
-            if (matcherResult.PatientResult.IsUnrepresented || matcherResult.DonorResult.IsUnrepresented)
-            {
-                return new MatchProbabilityResponse(null, allowedLoci)
-                {
-                    IsPatientPhenotypeUnrepresented = matcherResult.PatientResult.IsUnrepresented,
-                    IsDonorPhenotypeUnrepresented = matcherResult.DonorResult.IsUnrepresented,
-                    DonorHaplotypeFrequencySet = frequencySets.Donor.FrequencySet.ToClientHaplotypeFrequencySet(),
-                    PatientHaplotypeFrequencySet = frequencySets.Patient.FrequencySet.ToClientHaplotypeFrequencySet()
-                };
-            }
-
-            return CalculateMatchProbabilityFromMatcherResult(matcherResult, allowedLoci, frequencySets);
-        }
-
-        private async Task<FrequencySets> GetFrequencySets(SingleDonorMatchProbabilityInput singleDonorMatchProbabilityInput)
-        {
-            var frequencySets = await haplotypeFrequencyService.GetHaplotypeFrequencySets(
-                singleDonorMatchProbabilityInput.Donor.DonorFrequencySetMetadata,
-                singleDonorMatchProbabilityInput.PatientFrequencySetMetadata
+            var matchProbability = matchProbabilityCalculator.CalculateMatchProbability(
+                matcherResult.PatientResult.SumOfLikelihoods,
+                matcherResult.DonorResult.SumOfLikelihoods,
+                matcherResult.GenotypeMatchDetails,
+                allowedLoci
             );
 
-            return new FrequencySets(
-                new SubjectFrequencySet(frequencySets.PatientSet, "patient"),
-                new SubjectFrequencySet(frequencySets.DonorSet, "donor")
-                );
-        }
+            matchProbability.PatientHaplotypeFrequencySet = frequencySets.Patient.FrequencySet.ToClientHaplotypeFrequencySet();
+            matchProbability.DonorHaplotypeFrequencySet = frequencySets.Donor.FrequencySet.ToClientHaplotypeFrequencySet();
 
-        private MatchProbabilityResponse CalculateMatchProbabilityFromMatcherResult(
-            GenotypeMatcherResult matcherResult, 
-            HashSet<Locus> allowedLoci,
-            FrequencySets frequencySets)
-        {
-            using (logger.RunTimed("Calculate match probability", LogLevel.Verbose))
-            {
-                var matchProbability = matchProbabilityCalculator.CalculateMatchProbability(
-                    matcherResult.PatientResult.SumOfLikelihoods,
-                    matcherResult.DonorResult.SumOfLikelihoods,
-                    matcherResult.GenotypeMatchDetails,
-                    allowedLoci
-                );
-
-                matchProbability.PatientHaplotypeFrequencySet = frequencySets.Patient.FrequencySet.ToClientHaplotypeFrequencySet();
-                matchProbability.DonorHaplotypeFrequencySet = frequencySets.Donor.FrequencySet.ToClientHaplotypeFrequencySet();
-
-                return matchProbability;
-            }
+            return matchProbability;
         }
     }
 }
