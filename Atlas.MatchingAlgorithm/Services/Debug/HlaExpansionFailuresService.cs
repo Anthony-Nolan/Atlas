1331289379398759df021293e8490cf87fb9a4fe
Sergey Salin <91747559+SergeySalinAtDataart@users.noreply.github.com>
2026-06-22 14:14:36 +0200

    feat: ATL-133: upgrade azure monitor query logs

--- Atlas.MatchingAlgorithm/Services/Debug/HlaExpansionFailuresService.cs
+++ Atlas.MatchingAlgorithm/Services/Debug/HlaExpansionFailuresService.cs
@@ -1,34 +1,32 @@
 ﻿using Atlas.Debug.Client.Models.ApplicationInsights;
 using Atlas.MatchingAlgorithm.Settings.Azure;
-using Azure.Monitor.Query;
-using Azure.Monitor.Query.Models;
+using Azure.Monitor.Query.Logs;
+using Azure.Monitor.Query.Logs.Models;
 using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
-namespace Atlas.MatchingAlgorithm.Services.Debug
+namespace Atlas.MatchingAlgorithm.Services.Debug;
+
+public interface IHlaExpansionFailuresService
 {
-    public interface IHlaExpansionFailuresService
-    {
-        /// <summary>
-        /// Returns Hla Expansion Failures from logs for last <paramref name="daysToQuery"/> days
-        /// </summary>
-        /// <param name="daysToQuery"></param>
-        /// <returns></returns>
-        public Task<IEnumerable<HlaExpansionFailure>> Query(int daysToQuery);
-    }
+    /// <summary>
+    /// Returns Hla Expansion Failures from logs for last <paramref name="daysToQuery"/> days
+    /// </summary>
+    /// <param name="daysToQuery"></param>
+    /// <returns></returns>
+    public Task<IEnumerable<HlaExpansionFailure>> Query(int daysToQuery);
+}
 
-    public class HlaExpansionFailuresService : IHlaExpansionFailuresService
-    {
-        private readonly LogsQueryClient logsQueryClient;
-        private readonly AzureMonitoringSettings azureMonitoringSettings;
+public class HlaExpansionFailuresService : IHlaExpansionFailuresService
+{
+    private readonly LogsQueryClient logsQueryClient;
+    private readonly AzureMonitoringSettings azureMonitoringSettings;
 
-        private const string HlaExpansionFailuresQuery = @"
+    private const string HlaExpansionFailuresQuery = @"
             AppEvents
             | where Name startswith ""HLA Expansion""
             | extend
@@ -45,30 +43,29 @@ namespace Atlas.MatchingAlgorithm.Services.Debug
                 DonorCount = count() by InvalidHLA
             | order by DonorCount desc";
 
-        public HlaExpansionFailuresService(LogsQueryClient logsQueryClient, IOptions<AzureMonitoringSettings> azureMonitoringSettings)
-        {
-            this.logsQueryClient = logsQueryClient;
-            this.azureMonitoringSettings = azureMonitoringSettings.Value;
-        }
-
-        public async Task<IEnumerable<HlaExpansionFailure>> Query(int daysToQuery)
-        {
-            var response = await logsQueryClient.QueryWorkspaceAsync(
-                azureMonitoringSettings.WorkspaceId, 
-                HlaExpansionFailuresQuery, 
-                new QueryTimeRange(TimeSpan.FromDays(daysToQuery)));
-            var result = response.Value;
-
-            return result.Table.Rows.Select(MapHlaExpansionFailureFailure);
-        }
-
-
-        private static HlaExpansionFailure MapHlaExpansionFailureFailure(LogsTableRow input) => new HlaExpansionFailure
-        {
-            DonorCount = (long)input[nameof(HlaExpansionFailure.DonorCount)],
-            ExceptionType = input[nameof(HlaExpansionFailure.ExceptionType)].ToString(),
-            ExternalDonorCodes = JsonConvert.DeserializeObject<string[]>(input[nameof(HlaExpansionFailure.ExternalDonorCodes)].ToString()),
-            InvalidHLA = (string)input[nameof(HlaExpansionFailure.InvalidHLA)]
-        };
+    public HlaExpansionFailuresService(LogsQueryClient logsQueryClient, IOptions<AzureMonitoringSettings> azureMonitoringSettings)
+    {
+        this.logsQueryClient = logsQueryClient;
+        this.azureMonitoringSettings = azureMonitoringSettings.Value;
     }
+
+    public async Task<IEnumerable<HlaExpansionFailure>> Query(int daysToQuery)
+    {
+        var response = await logsQueryClient.QueryWorkspaceAsync(
+            azureMonitoringSettings.WorkspaceId, 
+            HlaExpansionFailuresQuery, 
+            new LogsQueryTimeRange(TimeSpan.FromDays(daysToQuery)));
+        var result = response.Value;
+
+        return result.Table.Rows.Select(MapHlaExpansionFailureFailure);
+    }
+
+
+    private static HlaExpansionFailure MapHlaExpansionFailureFailure(LogsTableRow input) => new HlaExpansionFailure
+    {
+        DonorCount = (long)input[nameof(HlaExpansionFailure.DonorCount)],
+        ExceptionType = input[nameof(HlaExpansionFailure.ExceptionType)].ToString(),
+        ExternalDonorCodes = JsonConvert.DeserializeObject<string[]>(input[nameof(HlaExpansionFailure.ExternalDonorCodes)].ToString()),
+        InvalidHLA = (string)input[nameof(HlaExpansionFailure.InvalidHLA)]
+    };
 }
